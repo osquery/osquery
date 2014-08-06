7d9dc341ced19c95a8fef9cf20af68c5ce31a067
mike@arpaia.co <mike@arpaia.co>
2014-08-06 16:24:44 -0700

    getting rid of bind1st and relevant headers

--- osquery/core/text.cpp
+++ osquery/core/text.cpp
@@ -2,20 +2,8 @@
 
 #include "osquery/core.h"
 
-#include <algorithm>
-#include <cctype>
-#include <fstream>
-#include <functional>
-#include <string>
-#include <sstream>
-#include <vector>
-
-#include <boost/algorithm/string/classification.hpp>
 #include <boost/algorithm/string/split.hpp>
 #include <boost/algorithm/string/trim.hpp>
-#include <boost/algorithm/string/predicate.hpp>
-
-#include <glog/logging.h>
 
 namespace osquery { namespace core {
 
@@ -28,7 +16,7 @@ std::vector<std::string> split(const std::string& s,
   std::vector<std::string> elems;
   boost::split(elems, s, boost::is_any_of(delim));
   auto start = std::remove_if(elems.begin(), elems.end(),
-    std::bind1st(std::equal_to<std::string>(), std::string(""))
+    [](const std::string &s) { return s == ""; }
   );
   elems.erase(start, elems.end());
   for (auto& each : elems) {
