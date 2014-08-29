da7ec74840b7f51b7e6f6a1342413ba5317bac09
mike@arpaia.co <mike@arpaia.co>
2014-08-28 23:15:45 -0700

    new time virtual table. it's pretty useful to have a light weight table that doesn't leak at all which returns data that's always changing.

---
+++ osquery/tables/utility/time.cpp
@@ -0,0 +1,30 @@
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+#include <ctime>
+
+#include <boost/lexical_cast.hpp>
+
+#include "osquery/database.h"
+
+using namespace osquery::db;
+
+namespace osquery {
+namespace tables {
+
+const int kNumCols = 1;
+
+QueryData genTime() {
+  Row r;
+  time_t _time = time(0);
+  struct tm* now = localtime(&_time);
+  r["hour"] = boost::lexical_cast<std::string>(now->tm_hour);
+  r["minutes"] = boost::lexical_cast<std::string>(now->tm_min);
+  r["seconds"] = boost::lexical_cast<std::string>(now->tm_sec);
+  QueryData results;
+  for (int i = 0; i < kNumCols; ++i) {
+    results.push_back(r);
+  }
+  return results;
+}
+}
+}
