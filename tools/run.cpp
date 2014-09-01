376d1779db85a85aa333d9bba88ec3efb57f919f
mike@arpaia.co <mike@arpaia.co>
2014-09-01 14:07:53 -0700

    adding an 'iterations' flag to the run tool to look at memory usage trends

--- tools/run.cpp
+++ tools/run.cpp
@@ -6,19 +6,22 @@
 #include "osquery/core.h"
 
 DEFINE_string(query, "", "query to execute");
+DEFINE_int32(iterations, 1, "times to run the query in question");
 
 int main(int argc, char* argv[]) {
   osquery::core::initOsquery(argc, argv);
 
   if (FLAGS_query != "") {
-    int err;
-    LOG(INFO) << "Executing: " << FLAGS_query;
-    osquery::core::aggregateQuery(FLAGS_query, err);
-    if (err != 0) {
-      LOG(ERROR) << "Query failed: " << err;
-      return 1;
+    for (int i = 0; i < FLAGS_iterations; ++i) {
+      int err;
+      LOG(INFO) << "Executing: " << FLAGS_query;
+      osquery::core::aggregateQuery(FLAGS_query, err);
+      if (err != 0) {
+        LOG(ERROR) << "Query failed: " << err;
+        return 1;
+      }
+      LOG(INFO) << "Query succedded";
     }
-    LOG(INFO) << "Query succedded";
   } else {
     LOG(ERROR) << "Usage: run --query=\"<query>\"";
     return 1;
