130fe2ad41809cd7f562f5948c1269c51676c450
mike@arpaia.co <mike@arpaia.co>
2014-09-09 11:02:17 -0700

    updates to status.h
    making it such that all the return values are const. added a few method
    comments.

--- osquery/status.h
+++ osquery/status.h
@@ -7,16 +7,29 @@
 
 namespace osquery {
 
+// osquery::Status is a utility class which is used all over osquery to
+// express the state of operations.
 class Status {
  public:
+  // default constructor
   Status() : code_(0), message_("OK") {}
+
+  // constructor which allows your to do something like:
+  //   auto s = Status(0, "OK);
   Status(int c, std::string m) : code_(c), message_(m) {}
 
  public:
-  int getCode() { return code_; }
-  std::string getMessage() { return message_; }
-  bool ok() { return getCode() == 0; }
-  std::string toString() { return getMessage(); }
+  // a getter for the code property
+  int getCode() const { return code_; }
+
+  // a getter for the message property
+  std::string getMessage() const { return message_; }
+
+  // a convenience method to check if the return code of the status is 0
+  bool ok() const { return getCode() == 0; }
+
+  // a synonym for getMessage().
+  std::string toString() const { return getMessage(); }
 
  private:
   int code_;
