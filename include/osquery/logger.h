798a8aa02aa140e3b108bb5f1f146474f3c3d9dd
mike@arpaia.co <mike@arpaia.co>
2014-09-15 13:02:23 -0700

    logger documentation

--- include/osquery/logger.h
+++ include/osquery/logger.h
@@ -11,21 +11,58 @@
 
 namespace osquery {
 
-// kDefaultLogReceiverName is a const std::string which represents the "name"
-// of the default log receiver
+/** @brief A string which represents the default logger receiver
+ *
+ *  The logger plugin that you use to define your config receiver can be
+ *  defined via a command-line flag, however, if you don't define a logger
+ *  plugin to use via the command-line, then the logger receiver which is
+ *  represented by the string stored kDefaultLogReceiverName will be used.
+ */
 extern const std::string kDefaultLogReceiverName;
 
-// logString accepts a const reference to a string and logs it to a specified
-// upstream receiver. If no receiver is specified, it will fail back to what
-// was defined via the command-line flags. If none was defined, it will fail
-// back to using the default log receiver.
+/** @brief Log a string using the default logger receiver.
+ *
+ *  Note that this method should only be used to log results. If you'd like to
+ *  log normal osquery operations, use Google Logging.
+ *
+ *  @param s the string to log
+ *
+ *  @return an instance of osquery::Status, indicating the success or failure
+ *  of the operation.
+ */
 osquery::Status logString(const std::string& s);
+
+/** @brief Log a string using a specific logger receiver.
+ *
+ *  Note that this method should only be used to log results. If you'd like to
+ *  log normal osquery operations, use Google Logging.
+ *
+ *  @param s the string to log
+ *  @param receiver a string representing the log receiver to use
+ *
+ *  @return an instance of osquery::Status, indicating the success or failure
+ *  of the operation.
+ */
 osquery::Status logString(const std::string& s, const std::string& receiver);
 
-// logScheduledQueryLogItem accepts a const reference to a
-// ScheduledQueryLogItem struct and logs it to a specified upstream receiver.
+/** @brief Directly log results of scheduled queries to the default receiver
+ *
+ *  @param item a struct representing the results of a scheduled query
+ *
+ *  @return an instance of osquery::Status, indicating the success or failure
+ *  of the operation.
+ */
 osquery::Status logScheduledQueryLogItem(
     const osquery::db::ScheduledQueryLogItem& item);
+
+/** @brief Directly log results of scheduled queries to a specified receiver
+ *
+ *  @param item a struct representing the results of a scheduled query
+ *  @param receiver a string representing the log receiver to use
+ *
+ *  @return an instance of osquery::Status, indicating the success or failure
+ *  of the operation.
+ */
 osquery::Status logScheduledQueryLogItem(
     const osquery::db::ScheduledQueryLogItem& item,
     const std::string& receiver);
