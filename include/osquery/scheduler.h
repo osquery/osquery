6985d4bfa5c32b94fc115730243382e9b251daf9
mike@arpaia.co <mike@arpaia.co>
2014-09-15 13:11:39 -0700

    scheduler documentation

--- include/osquery/scheduler.h
+++ include/osquery/scheduler.h
@@ -4,8 +4,17 @@
 
 namespace osquery {
 
-// initialize is the entry point for osquery's scheduler. One needs to simply
-// launch a new thread with `initialize` as the target and the scheduler will
-// begin executing and aggregating scheduled queries
+/** @brief Launch the scheduler.
+ *
+ *  osquery comes with a scheduler, which schedules a variety of things. This
+ *  is one of the core parts of the osqueryd daemon. To use this, simply use
+ *  this function as your entry point when creating a new thread.
+ *
+ *  @code{.cpp}
+ *    boost::thread scheduler_thread(osquery::initializeScheduler);
+ *    // do some other stuff
+ *    scheduler_thread.join();
+ *  @endcode
+ */
 void initializeScheduler();
 }
