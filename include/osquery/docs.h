441ca7bb369bda8f109cf3a09d581d66944577c6
mike@arpaia.co <mike@arpaia.co>
2014-09-15 18:11:49 -0700

    better namespace documentation

--- include/osquery/docs.h
+++ include/osquery/docs.h
@@ -4,12 +4,15 @@
 
 /** @brief The main osquery namespace
  *
- *  If you're uncertain about where code should go, it should probably be the
- *  global osquery namespace.
+ *  @namespace osquery If you're uncertain about where code should go, it
+ *  should probably be in the global osquery namespace.
  */
 namespace osquery {
 
 /** @brief The namespace which contains all database related functionality
+ *
+ *  @namespace osquery::db All code in the `osquery/database` subdirectory is
+ *  apart of the osquery::db namespace.
  */
 namespace db {}
 }
