e314cdf51396468e46a83e22d766921238476256
mike@arpaia.co <mike@arpaia.co>
2014-09-21 14:30:28 -0700

    updating docs header

--- doxygen/documentation.h
+++ doxygen/documentation.h
@@ -8,13 +8,4 @@
  * @namespace osquery If you're uncertain about where code should go, it
  * should probably be in the global osquery namespace.
  */
-namespace osquery {
-
-/**
- * @brief The namespace which contains all database related functionality
- *
- * @namespace osquery::db All code in the `osquery/database` subdirectory is
- * apart of the osquery::db namespace.
- */
-namespace db {}
-}
+namespace osquery {}
