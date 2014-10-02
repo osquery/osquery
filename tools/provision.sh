9355009a5f65d64f5dc756d8713a2e54a836f0e9
mike@arpaia.co <mike@arpaia.co>
2014-10-02 16:50:48 -0700

    rocksdb from brew

--- tools/provision.sh
+++ tools/provision.sh
@@ -245,21 +245,17 @@ function main() {
       fatal "could not find homebrew. please install it from http://brew.sh/"
     fi
 
-    if brew list | grep --quiet wget; then
-      log "wget is already installed. skipping"
-    else
-      brew install wget
-    fi
-
+    brew update
+    package rocksdb
+    package wget
     package cmake
-    package boost --c++11
+    package boost
     package gflags
     package glog
     package snappy
     package readline
     package thrift
     package lz4
-    install_rocksdb
   fi
 
   cd $SCRIPT_DIR/../
