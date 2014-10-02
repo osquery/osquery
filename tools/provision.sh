126f66bfa34053ee56a4dde8b691c6e90f9d275b
Javier Marcos <javier.marcos@gmail.com>
2014-10-02 22:54:55 +0000

    Using clang to build all and refactoring using functions

--- tools/provision.sh
+++ tools/provision.sh
@@ -27,7 +27,7 @@ function install_cmake() {
     else
       log "building cmake"
       pushd cmake-2.8.12.2 > /dev/null
-      CC=gcc CXX=g++ ./configure
+      CC=clang CXX=clang++ ./configure
       make
       make install
       popd
@@ -36,17 +36,13 @@ function install_cmake() {
 }
 
 function install_thrift() {
-  if [[ -f /usr/local/lib/libthrift.a ]]; then
-    log "thrift is installed. skipping."
-    return
-  fi
-  if [[ ! -f 0.9.1.tar.gz ]]; then
-    wget https://github.com/apache/thrift/archive/0.9.1.tar.gz
-  fi
-  if [[ ! -d thrift-0.9.1 ]]; then
-    tar -xf 0.9.1.tar.gz
-  fi
   if [[ ! -f /usr/local/lib/libthrift.a ]]; then
+    if [[ ! -f 0.9.1.tar.gz ]]; then
+      wget https://github.com/apache/thrift/archive/0.9.1.tar.gz
+    fi
+    if [[ ! -d thrift-0.9.1 ]]; then
+      tar -xf 0.9.1.tar.gz
+    fi
     pushd thrift-0.9.1
     ./bootstrap.sh
     ./configure
@@ -59,45 +55,94 @@ function install_thrift() {
 }
 
 function install_rocksdb() {
-  if [[ -f /usr/local/lib/librocksdb.a ]]; then
-    log "rocksdb is installed. skipping."
-    return
-  fi
-  if [[ ! -f rocksdb-3.5.tar.gz ]]; then
-    wget https://github.com/facebook/rocksdb/archive/rocksdb-3.5.tar.gz
-  fi
-  if [[ ! -d rocksdb-rocksdb-3.5 ]]; then
-    tar -xf rocksdb-3.5.tar.gz
-  fi
-  if [ $OS = "ubuntu" ] || [ $OS = "centos" ]; then
-    if [[ ! -f rocksdb-rocksdb-3.5/librocksdb.a ]]; then
-      pushd rocksdb-rocksdb-3.5
-      make static_lib CFLAGS="-I /usr/include/clang/3.4/include"
-      popd
+  if [[ ! -f /usr/local/lib/librocksdb.a ]]; then
+    if [[ ! -f rocksdb-3.5.tar.gz ]]; then
+      wget https://github.com/facebook/rocksdb/archive/rocksdb-3.5.tar.gz
     fi
-    if [[ ! -f /usr/local/lib/librocksdb.a ]]; then
-      sudo cp rocksdb-rocksdb-3.5/librocksdb.a /usr/local/lib
-    else
-      log "librocksdb already installed. skipping."
+    if [[ ! -d rocksdb-rocksdb-3.5 ]]; then
+      tar -xf rocksdb-3.5.tar.gz
     fi
-    if [[ ! -d /usr/local/include/rocksdb ]]; then
-      mkdir -p /usr/local/include
-      sudo cp -R rocksdb-rocksdb-3.5/include/rocksdb /usr/local/include
-    else
-      log "rocksdb header already installed. skipping."
-    fi
-  elif [[ $OS = "darwin" ]]; then
-    if [[ ! -f rocksdb-rocksdb-3.5/librocksdb.a ]]; then
-      pushd rocksdb-rocksdb-3.5
-      make static_lib
-      popd
-    fi
-    if [[ ! -f /usr/local/lib/librocksdb.a ]]; then
+    if [ $OS = "ubuntu" ] || [ $OS = "centos" ]; then
+      if [[ ! -f rocksdb-rocksdb-3.5/librocksdb.a ]]; then
+        pushd rocksdb-rocksdb-3.5
+        make static_lib CFLAGS="-I /usr/include/clang/3.4/include"
+        popd
+      fi
       sudo cp rocksdb-rocksdb-3.5/librocksdb.a /usr/local/lib
       sudo cp -R rocksdb-rocksdb-3.5/include/rocksdb /usr/local/include
-    else
-      log "rocksdb already installed. skipping."
+    elif [[ $OS = "darwin" ]]; then
+      if [[ ! -f rocksdb-rocksdb-3.5/librocksdb.a ]]; then
+        pushd rocksdb-rocksdb-3.5
+        make static_lib
+        popd
+      else
+        sudo cp rocksdb-rocksdb-3.5/librocksdb.a /usr/local/lib
+        sudo cp -R rocksdb-rocksdb-3.5/include/rocksdb /usr/local/include
+      fi
     fi
+  else
+    log "rocksdb already installed. skipping."
+  fi
+}
+
+function install_boost() {
+  if [[ ! -f /usr/local/lib/libboost_thread.a ]]; then
+    if [[ ! -f boost_1_55_0.tar.gz ]]; then
+      wget -O boost_1_55_0.tar.gz http://sourceforge.net/projects/boost/files/boost/1.55.0/boost_1_55_0.tar.gz/download
+    else
+      log "boost source is already downloaded. skipping."
+    fi
+    if [[ ! -d boost_1_55_0 ]]; then
+      tar -xf boost_1_55_0.tar.gz
+    fi
+    pushd boost_1_55_0
+    ./bootstrap.sh
+    n=`cat /proc/cpuinfo | grep "cpu cores" | uniq | awk '{print $NF}'`
+    sudo ./b2 --with=all -j $n toolset=clang install
+    sudo ldconfig
+    popd
+  else
+    log "boost library is already installed. skipping."
+  fi
+}
+
+function install_gflags() {
+  if [[ ! -f /usr/local/lib/libgflags.a ]]; then
+    if [[ ! -f v2.1.1.tar.gz ]]; then
+      wget https://github.com/schuhschuh/gflags/archive/v2.1.1.tar.gz
+    else
+      log "gflags source is already downloaded. skipping."
+    fi
+    if [[ ! -d gflags-2.1.1 ]]; then
+      tar -xf v2.1.1.tar.gz
+    fi
+    pushd gflags-2.1.1
+    cmake -DCMAKE_CXX_FLAGS=-fPIC -DGFLAGS_NAMESPACE:STRING=google .
+    make
+    sudo make install
+    popd
+  else
+    log "gflags library is already installed. skipping."
+  fi
+}
+
+function install_glog() {
+  if [[ ! -d /usr/local/include/glog ]]; then
+    if [[ ! -f glog-0.3.3.tar.gz ]]; then
+      wget https://google-glog.googlecode.com/files/glog-0.3.3.tar.gz
+    else
+      log "glog source is already downloaded. skipping."
+    fi
+    if [[ ! -d glog-0.3.3 ]]; then
+      tar -xf glog-0.3.3.tar.gz
+    fi
+    pushd glog-0.3.3
+    ./configure CXXFLAGS="-DGFLAGS_NAMESPACE=google"
+    make
+    sudo make install
+    popd
+  else
+    log "glog is already installed. skipping."
   fi
 }
 
@@ -169,105 +214,52 @@ function main() {
     package python-pip
     package python-dev
     package libbz2-dev
-
+    package devscripts
+    package debhelper
+    package clang-3.4
+    package clang-format-3.4
+    set_cc clang
+    set_cxx clang++
     if [[ $DISTRO = "precise" ]]; then
       package gcc-4.7
       package g++-4.7
       update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.7 100 --slave /usr/bin/g++ g++ /usr/bin/g++-4.7
+      install_boost
       install_cmake
-      if [[ ! -f boost_1_55_0.tar.gz ]]; then
-        wget -O boost_1_55_0.tar.gz http://sourceforge.net/projects/boost/files/boost/1.55.0/boost_1_55_0.tar.gz/download
-      else
-        log "boost source is already downloaded. skipping."
-      fi
-      if [[ ! -d boost_1_55_0 ]]; then
-        tar -xf boost_1_55_0.tar.gz
-      fi
-      if [[ ! -f /usr/local/lib/libboost_thread.a ]]; then
-        pushd boost_1_55_0
-        ./bootstrap.sh
-        n=`cat /proc/cpuinfo | grep "cpu cores" | uniq | awk '{print $NF}'`
-        sudo ./b2 --with=all -j $n install
-        sudo ldconfig
-        popd
-      else
-	log "boost library is already installed. skipping."
-      fi
     else
       package cmake
       package libboost1.55-all-dev
     fi
-    package devscripts
-    package debhelper
     if [[ $DISTRO = "precise" ]]; then
       package libunwind7-dev
     fi
     if [[ $DISTRO = "trusty" ]]; then
       package libunwind8-dev
     fi
-
-    package clang-3.4
-    package clang-format-3.4
-
-    set_cc clang
-    set_cxx clang++
-
     if [[ $DISTRO = "precise" ]]; then
-      if [[ ! -f v2.1.1.tar.gz ]]; then
-        wget https://github.com/schuhschuh/gflags/archive/v2.1.1.tar.gz
-      else
-        log "gflags source is already downloaded. skipping."
-      fi
-      if [[ ! -f /usr/lib/libgflags.a ]]; then
-        if [[ ! -d gflags-2.1.1 ]]; then
-          tar -xf v2.1.1.tar.gz
-	fi
-        pushd gflags-2.1.1
-        cmake -DCMAKE_CXX_FLAGS=-fPIC -DGFLAGS_NAMESPACE:STRING=google .
-        make
-        sudo make install
-        popd
-      fi
+      install_gflags
     else
       package libgoogle-glog-dev
     fi
 
     if [[ $DISTRO = "precise" ]]; then
-      if [[ ! -f glog-0.3.3.tar.gz ]]; then
-        wget https://google-glog.googlecode.com/files/glog-0.3.3.tar.gz
-      fi
-      if [[ ! -d glog-0.3.3 ]]; then
-        tar -xf glog-0.3.3.tar.gz
-      fi
-      if [[ ! -d /usr/local/include/glog ]]; then
-        pushd glog-0.3.3
-        ./configure CXXFLAGS="-DGFLAGS_NAMESPACE=google"
-        make
-        sudo make install
-        popd
-      else
-      	log "glog is already installed. skipping."
-      fi
+      install_glog
     else
       package libgoogle-glog-dev
     fi
     package libsnappy-dev
     package libbz2-dev
     package libreadline-dev
-
     if [[ $DISTRO = "precise" ]]; then
       package libproc-dev
     else
       package libprocps3-dev
     fi
-
     install_thrift
-
     install_rocksdb
 
   elif [[ $OS = "centos" ]]; then
     sudo yum update -y
-
     package git-all
     package unzip
     package xz
@@ -276,13 +268,11 @@ function main() {
     if [[ ! -f "/usr/local/bin/brew" ]]; then
       fatal "could not find homebrew. please install it from http://brew.sh/"
     fi
-
     if brew list | grep --quiet wget; then
       log "wget is already installed. skipping"
     else
       brew install wget
     fi
-
     package cmake
     package boost --c++11
     package gflags
