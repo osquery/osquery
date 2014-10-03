61fe059bf20b9d3c61bc5998d686e68e93a9d75d
mike@arpaia.co <mike@arpaia.co>
2014-10-02 19:07:08 -0700

    iostream

--- tools/provision.sh
+++ tools/provision.sh
@@ -244,7 +244,6 @@ function main() {
     if [[ ! -f "/usr/local/bin/brew" ]]; then
       fatal "could not find homebrew. please install it from http://brew.sh/"
     fi
-    brew update
 
     package wget
     package cmake
