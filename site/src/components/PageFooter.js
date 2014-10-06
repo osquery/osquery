1e8b3c1ee99219d94098dc2a1d302c57ff339cc6
mike@arpaia.co <mike@arpaia.co>
2014-10-06 11:34:01 -0700

    buttons

--- site/src/components/PageFooter.js
+++ site/src/components/PageFooter.js
@@ -6,27 +6,47 @@ var React = require('react');
 
 var PageHeader = React.createClass({
   render: function () {
+    var user = 'marpaia';
+    var repo = 'chef-golang';
+    var github = 'https://github.com/' + user + '/' + repo;
+    var license = github + '/blob/master/LICENSE';
+    var issues = github + '/issues?state=open';
+    var releases = github + '/releases';
+
+    var ghbtns = 'http://ghbtns.com/github-btn.html?user=' + user + '&repo=' + repo;
+    var starButton = ghbtns + '&type=watch&count=true';
+    var forkButton = ghbtns + '&type=fork&count=true';
     return (
         <footer className="bs-docs-footer" role="contentinfo">
           <div className="container">
             <div className="bs-docs-social">
               <ul className="bs-docs-social-buttons">
                 <li>
-                  <iframe className="github-btn" src={'http://ghbtns.com/github-btn.html?user=react-bootstrap&repo=react-bootstrap&type=watch&count=true'} width={90} height={20} title="Star on GitHub" />
+                  <iframe
+                    className="github-btn"
+                    src={starButton}
+                    width={90}
+                    height={20}
+                    title="Star on GitHub" />
                 </li>
                 <li>
-                  <iframe className="github-btn" src={'http://ghbtns.com/github-btn.html?user=react-bootstrap&repo=react-bootstrap&type=fork&count=true'} width={92} height={20} title="Fork on GitHub" />
+                  <iframe
+                    className="github-btn"
+                    src={forkButton}
+                    width={92}
+                    height={20}
+                    title="Fork on GitHub" />
                 </li>
               </ul>
             </div>
-            <p>Code licensed under <a href="https://github.com/react-bootstrap/react-bootstrap/blob/master/LICENSE" target="_blank">MIT</a>.</p>
+            <p>Code licensed under <a href={license} target="_blank">MIT</a>.</p>
             <ul className="bs-docs-footer-links muted">
               <li>·</li>
-              <li><a href="https://github.com/react-bootstrap/react-bootstrap/">GitHub</a></li>
+              <li><a href={github} target="_blank">GitHub</a></li>
               <li>·</li>
-              <li><a href="https://github.com/react-bootstrap/react-bootstrap/issues?state=open">Issues</a></li>
+              <li><a href={issues} target="_blank">Issues</a></li>
               <li>·</li>
-              <li><a href="https://github.com/react-bootstrap/react-bootstrap/releases">Releases</a></li>
+              <li><a href={releases} target="_blank">Releases</a></li>
             </ul>
           </div>
         </footer>
