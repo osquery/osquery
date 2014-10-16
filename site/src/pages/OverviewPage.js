05dd68291d8a4706a9727d9bf72e7e212f6a26da
mike@arpaia.co <mike@arpaia.co>
2014-10-15 23:54:34 -0700

    updating the overview page [skip ci]

--- site/src/pages/OverviewPage.js
+++ site/src/pages/OverviewPage.js
@@ -19,9 +19,13 @@ var Page = React.createClass({
 
         <PageContainer>
 
-          <p className="lead">osquery exposes an operating system as a
-          high-performance relational database. This allows you to write
-          SQL-based queries to explore operating system data.</p>
+          <p className="lead">With osquery, you can use SQL to query low-level
+          operating system information. Under the hood, instead of tables, these
+          queries dynamically execute high-performance native code. The results
+          of the SQL query are transparently returned to you quickly and easily.</p>
+
+          <p className="lead">Consider the following example, which uses osqueryi,
+          the interactive query console, to execute a few SQL queries.</p>
 
           <div className="showterm">
             <iframe width="100%" height="480" src="http://showterm.io/7b5f8d42ba021511e627e"></iframe>
