bf3cd15c9150ffdc298ca568713b158db894f2e2
Javier Marcos <javier.marcos@gmail.com>
2014-10-23 17:17:50 -0700

    Final fix for the allocation problem

--- osquery/tables/system/darwin/groups.mm
+++ osquery/tables/system/darwin/groups.mm
@@ -19,60 +19,50 @@ namespace osquery {
 namespace tables {
 
 QueryData genGroups() {
-  QueryData results;
+  @autoreleasepool {
+    QueryData results;
 
-  /*
-  ODSession *s = [ODSession defaultSession];
-  NSError *err;
-  ODNode *root = [ODNode nodeWithSession:s name:@"/Local/Default" error:&err];
-  if (err) {
-    LOG(ERROR) << "Error with OD node: " << std::string([[err localizedDescription] UTF8String]);
-    return results;
-  }
-  ODQuery *q =
-    [ODQuery queryWithNode:root forRecordTypes:kODRecordTypeGroups attribute:nil matchType:0 queryValues:nil returnAttributes:nil maximumResults:0 error:&err];
-  if (err) {
-    LOG(ERROR) << "Error with OD query: " << std::string([[err localizedDescription] UTF8String]);
-    return results;
-  }
-  NSArray *od_results = [q resultsAllowingPartial:NO error:&err];
-  if (err) {
-    LOG(ERROR) << "Error with OD results: " << std::string([[err localizedDescription] UTF8String]);
-    return results;
-  }
-  for (ODRecord *re in od_results) {
-    Row r;
-    r["name"] = std::string([[re recordName] UTF8String]);
-    struct group *grp = nullptr;
-    grp = getgrnam(r["name"].c_str());
-    if (grp != nullptr) {
-      r["gid"] = boost::lexical_cast<std::string>(grp->gr_gid);
-      results.push_back(r);
+    ODSession *s = [ODSession defaultSession];
+    NSError *err;
+    ODNode *root = [ODNode nodeWithSession:s
+                           name:@"/Local/Default"
+                           error:&err];
+    if (err) {
+      LOG(ERROR) << "Error with OD node: " << std::string([[err localizedDescription] UTF8String]);
+      return results;
     }
-  }
-  */
-  std::string content;
-  Status s = readFile("/etc/group", content);
-
-  if (!s.ok()) {
-    LOG(ERROR) << "Error reading /etc/group: " << s.toString();
-  }
-
-  for (const auto& line : split(content, "\n")) {
-    auto user_bits = split(line, ":");
-    if (user_bits.size() != 3) {
-      continue;
+    ODQuery *q = [ODQuery queryWithNode:root
+                          forRecordTypes:kODRecordTypeGroups
+                          attribute:nil
+                          matchType:0
+                          queryValues:nil
+                          returnAttributes:nil
+                          maximumResults:0
+                          error:&err];
+    if (err) {
+      LOG(ERROR) << "Error with OD query: " << std::string([[err localizedDescription] UTF8String]);
+      return results;
     }
 
-    Row r;
+    NSArray *od_results = [q resultsAllowingPartial:NO
+                             error:&err];
+    if (err) {
+      LOG(ERROR) << "Error with OD results: " << std::string([[err localizedDescription] UTF8String]);
+      return results;
+    }
+    for (ODRecord *re in od_results) {
+      Row r;
+      r["name"] = std::string([[re recordName] UTF8String]);
+      struct group *grp = nullptr;
+      grp = getgrnam(r["name"].c_str());
+      if (grp != nullptr) {
+        r["gid"] = boost::lexical_cast<std::string>(grp->gr_gid);
+        results.push_back(r);
+      }
+    }
 
-    r["name"] = user_bits[0];
-    r["gid"] = user_bits[2];
-
-    results.push_back(r);
+    return results;
   }
-
-  return results;
 }
 }
 }
--- osquery/tables/system/darwin/users.mm
+++ osquery/tables/system/darwin/users.mm
@@ -19,76 +19,55 @@ namespace osquery {
 namespace tables {
 
 QueryData genUsers() {
-  QueryData results;
+  @autoreleasepool {
+    QueryData results;
 
-
-  /*
-  // OpenDirectory should be used but this leaks a huge amount of memory
-
-  ODSession *session = [ODSession defaultSession];
-  NSError *err;
-  ODNode *root = [ODNode nodeWithSession:session name:@"/Local/Default" error:&err];
-  if (err) {
-    LOG(ERROR) << "Error with OD node: " << std::string([[err localizedDescription] UTF8String]);
-    return results;
-  }
-  ODQuery *q = [ODQuery queryWithNode:root forRecordTypes:kODRecordTypeUsers attribute:nil matchType:0 queryValues:nil returnAttributes:nil maximumResults:0 error:&err];
-  if (err) {
-    LOG(ERROR) << "Error with OD query: " << std::string([[err localizedDescription] UTF8String]);
-    return results;
-  }
-
-  NSArray *od_results = [q resultsAllowingPartial:NO error:&err];
-  if (err) {
-    LOG(ERROR) << "Error with OD results: " << std::string([[err localizedDescription] UTF8String]);
-    return results;
-  }
-
-  for (ODRecord *re in od_results) {
-    Row r;
-    r["username"] = std::string([[re recordName] UTF8String]);
-    struct passwd *pwd = nullptr;
-    pwd = getpwnam(r["username"].c_str());
-    if (pwd != nullptr) {
-      r["uid"] = boost::lexical_cast<std::string>(pwd->pw_uid);
-      r["gid"] = boost::lexical_cast<std::string>(pwd->pw_gid);
-      r["description"] = std::string(pwd->pw_gecos);
-      r["directory"] = std::string(pwd->pw_dir);
-      r["shell"] = std::string(pwd->pw_shell);
-      results.push_back(r);
+    ODSession *session = [ODSession defaultSession];
+    NSError *err;
+    ODNode *root = [ODNode nodeWithSession:session
+                           name:@"/Local/Default"
+                           error:&err];
+    if (err) {
+      LOG(ERROR) << "Error with OD node: " << std::string([[err localizedDescription] UTF8String]);
+      return results;
     }
-  }
-  */
-
-  // For single-user mode the following is used:
-
-  std::string content;
-  Status ss = readFile("/etc/master.passwd", content);
-
-  if (!ss.ok()) {
-    LOG(ERROR) << "Error reading /etc/passwd: " << ss.toString();
-  }
-
-  std::string line;
-  for (const auto& line : split(content, "\n")) {
-    auto user_bits = split(line, ":");
-    if (user_bits.size() != 7) {
-      continue;
+    ODQuery *q = [ODQuery queryWithNode:root
+                          forRecordTypes:kODRecordTypeUsers
+                          attribute:nil
+                          matchType:0
+                          queryValues:nil
+                          returnAttributes:nil
+                          maximumResults:0
+                          error:&err];
+    if (err) {
+      LOG(ERROR) << "Error with OD query: " << std::string([[err localizedDescription] UTF8String]);
+      return results;
     }
 
-    Row r;
+    NSArray *od_results = [q resultsAllowingPartial:NO
+                           error:&err];
+    if (err) {
+      LOG(ERROR) << "Error with OD results: " << std::string([[err localizedDescription] UTF8String]);
+      return results;
+    }
 
-    r["username"] = user_bits[0];
-    r["uid"] = user_bits[2];
-    r["gid"] = user_bits[3];
-    r["description"] = user_bits[4];
-    r["directory"] = user_bits[5];
-    r["shell"] = user_bits[6];
+    for (ODRecord *re in od_results) {
+      Row r;
+      r["username"] = std::string([[re recordName] UTF8String]);
+      struct passwd *pwd = nullptr;
+      pwd = getpwnam(r["username"].c_str());
+      if (pwd != nullptr) {
+        r["uid"] = boost::lexical_cast<std::string>(pwd->pw_uid);
+        r["gid"] = boost::lexical_cast<std::string>(pwd->pw_gid);
+        r["description"] = std::string(pwd->pw_gecos);
+        r["directory"] = std::string(pwd->pw_dir);
+        r["shell"] = std::string(pwd->pw_shell);
+        results.push_back(r);
+      }
+    }
 
-    results.push_back(r);
+    return results;
   }
-
-  return results;
 }
 }
 }
