73c098620b2d20d7bf0e18bd1f19ab851156f1d3
Martin Majlis <martinmajlis@fb.com>
2014-10-28 23:27:53 -0700

    Functions for reading content of the database have been added. Mapping between internal and external column representation has been added.

--- osquery/core/sqlite_util.h
+++ osquery/core/sqlite_util.h
@@ -3,10 +3,20 @@
 #pragma once
 
 namespace osquery {
+
+// returns data corresponding to executing query q on database db
+QueryData query(const std::string& q, int& error_return, sqlite3* db);
+
+// opens SQLite database
+sqlite3* openDB(const char* file_name);
+
+// closes SQLite database
+int closeDB(sqlite3* db);
+
 namespace core {
 
 // the callback for populating a std::vector<row> set of results. "argument"
 // should be a non-const reference to a std::vector<row>
-int query_data_callback(void *argument, int argc, char *argv[], char *column[]);
+int query_data_callback(void* argument, int argc, char* argv[], char* column[]);
 }
 }
