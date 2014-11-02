37b8336a1f5862c7aa8163fe41ba89ecc59a1507
Teddy Reed <teddy@prosauce.org>
2014-11-02 01:42:04 -0800

    Silence parentheses warnings in linux/mounts

--- osquery/tables/system/linux/mounts.cpp
+++ osquery/tables/system/linux/mounts.cpp
@@ -1,15 +1,17 @@
+
+#include <stdio.h>
+#include <mntent.h>
+#include <sys/vfs.h>
+
 #include <boost/lexical_cast.hpp>
 
 #include "osquery/core.h"
 #include "osquery/database.h"
 #include "osquery/filesystem.h"
 
-#include <stdio.h>
-#include <mntent.h>
-#include <sys/vfs.h>
-
 namespace osquery {
 namespace tables {
+
 QueryData genMounts() {
   QueryData results;
   FILE *mounts;
@@ -17,8 +19,8 @@ QueryData genMounts() {
   char real_path[PATH_MAX];
   struct statfs st;
 
-  if (mounts = setmntent("/proc/mounts", "r")) {
-    while (ent = getmntent(mounts)) {
+  if ((mounts = setmntent("/proc/mounts", "r"))) {
+    while ((ent = getmntent(mounts))) {
       Row r;
 
       r["fsname"] = std::string(ent->mnt_fsname);
