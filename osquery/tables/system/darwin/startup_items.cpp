05cfff81c837ba482a2bfd1b0a8afb3df7fb70c9
mike@arpaia.co <mike@arpaia.co>
2014-11-04 11:42:30 -0800

    clang-format

--- osquery/tables/system/darwin/startup_items.cpp
+++ osquery/tables/system/darwin/startup_items.cpp
@@ -18,8 +18,7 @@ namespace osquery {
 namespace tables {
 
 const std::vector<std::string> kLibraryStartupItemPaths = {
-  "/System/Library/StartupItems/",
-  "/Library/StartupItems/",
+    "/System/Library/StartupItems/", "/Library/StartupItems/",
 };
 
 /**
@@ -28,14 +27,14 @@ const std::vector<std::string> kLibraryStartupItemPaths = {
  * Based on
  * https://github.com/synack/knockknock/blob/master/plugins/startupItem.py
  */
-void getLibraryStartupItems(QueryData &results) {
+void getLibraryStartupItems(QueryData& results) {
   for (const auto& dir : kLibraryStartupItemPaths) {
     try {
       fs::directory_iterator end_iter;
-      for (auto it = fs::directory_iterator(fs::path(dir)); it != end_iter; it++) {
+      for (auto it = fs::directory_iterator(fs::path(dir)); it != end_iter;
+           it++) {
         if (fs::is_directory(it->status())) {
-          if (fs::exists(it->status())
-              && fs::is_regular_file(it->status())) {
+          if (fs::exists(it->status()) && fs::is_regular_file(it->status())) {
             Row r;
             r["name"] = it->path().string();
             r["path"] = it->path().string();
@@ -54,7 +53,7 @@ void getLibraryStartupItems(QueryData &results) {
  */
 osquery::Status parseAliasData(const std::string& data, std::string& filepath) {
   for (int i = 0; i < data.size(); i++) {
-    int size = (int) data[i];
+    int size = (int)data[i];
     if (size < 2 || size > data.length() - i) {
       continue;
     }
@@ -75,7 +74,7 @@ osquery::Status parseAliasData(const std::string& data, std::string& filepath) {
 
 // Path (after /Users/foo) where the login items plist will be found
 const std::string kLoginItemsPlistPath =
-  "Library/Preferences/com.apple.loginitems.plist";
+    "Library/Preferences/com.apple.loginitems.plist";
 // Key to the array within the Login Items plist containing the items
 const std::string kLoginItemsKeyPath = "SessionItems.CustomListItems";
 
@@ -85,7 +84,7 @@ const std::string kLoginItemsKeyPath = "SessionItems.CustomListItems";
  * Based on
  * https://github.com/synack/knockknock/blob/master/plugins/loginItem.py
  */
-void getLoginItems(QueryData &results) {
+void getLoginItems(QueryData& results) {
   try {
     fs::directory_iterator end_iter;
     for (const auto& dir : getHomeDirectories()) {
@@ -98,7 +97,7 @@ void getLoginItems(QueryData &results) {
         if (!(fs::exists(plist_path) && fs::is_regular_file(plist_path))) {
           continue;
         }
-      } catch (const fs::filesystem_error &e) {
+      } catch (const fs::filesystem_error& e) {
         // Likely permission denied
         VLOG(1) << "Error checking path " << plist_path << ": " << e.what();
         continue;
@@ -135,13 +134,11 @@ void getLoginItems(QueryData &results) {
   }
 }
 
-
 QueryData genStartupItems() {
   QueryData results;
   getLoginItems(results);
   getLibraryStartupItems(results);
   return results;
 }
-
 }
 }
