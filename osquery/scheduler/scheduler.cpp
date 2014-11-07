cd6a191d6c0574e7982fcd3de5c0c963f6a1776c
mike@arpaia.co <mike@arpaia.co>
2014-11-07 12:29:01 -0800

    adding the scheduled query runnable

--- osquery/scheduler/scheduler.cpp
+++ osquery/scheduler/scheduler.cpp
@@ -10,48 +10,64 @@
 #include "osquery/config.h"
 #include "osquery/core.h"
 #include "osquery/database.h"
+#include "osquery/dispatcher.h"
 #include "osquery/logger.h"
 #include "osquery/sql.h"
 
 namespace osquery {
+
+class ScheduledQueryRunnable : public apache::thrift::concurrency::Runnable {
+ public:
+  ScheduledQueryRunnable(const OsqueryScheduledQuery& q) : _query(q) {}
+
+  virtual void run() {
+    LOG(INFO) << "executing query: " << _query.query;
+    int unix_time = std::time(0);
+    auto sql = SQL(_query.query);
+    if (!sql.ok()) {
+      LOG(ERROR) << "error executing query (" << _query.query
+                 << "): " << sql.getMessageString();
+      return;
+    }
+    auto dbQuery = Query(_query);
+    DiffResults diff_results;
+    auto status = dbQuery.addNewResults(sql.rows(), diff_results, unix_time);
+    if (!status.ok()) {
+      LOG(ERROR)
+          << "error adding new results to database: " << status.toString();
+      return;
+    }
+
+    if (diff_results.added.size() > 0 || diff_results.removed.size() > 0) {
+      VLOG(1) << "Results found for query: \"" << _query.query << "\"";
+      ScheduledQueryLogItem item;
+      item.diffResults = diff_results;
+      item.name = _query.name;
+      item.hostname = osquery::getHostname();
+      item.unixTime = osquery::getUnixTime();
+      item.calendarTime = osquery::getAsciiTime();
+      auto s = logScheduledQueryLogItem(item);
+      if (!s.ok()) {
+        LOG(ERROR) << "Error logging the results of query \"" << _query.query
+                   << "\""
+                   << ": " << s.toString();
+      }
+    }
+  }
+
+ private:
+  OsqueryScheduledQuery _query;
+};
 
 void launchQueries(const std::vector<OsqueryScheduledQuery>& queries,
                    const int64_t& second) {
   LOG(INFO) << "launchQueries: " << second;
   for (const auto& q : queries) {
     if (second % q.interval == 0) {
-      LOG(INFO) << "executing query: " << q.query;
-      int unix_time = std::time(0);
-      auto sql = SQL(q.query);
-      if (!sql.ok()) {
-        LOG(ERROR) << "error executing query (" << q.query
-                   << "): " << sql.getMessageString();
-        continue;
-      }
-      auto dbQuery = Query(q);
-      DiffResults diff_results;
-      auto status = dbQuery.addNewResults(sql.rows(), diff_results, unix_time);
-      if (!status.ok()) {
-        LOG(ERROR)
-            << "error adding new results to database: " << status.toString();
-        continue;
-      }
-
-      if (diff_results.added.size() > 0 || diff_results.removed.size() > 0) {
-        VLOG(1) << "Results found for query: \"" << q.query << "\"";
-        ScheduledQueryLogItem item;
-        item.diffResults = diff_results;
-        item.name = q.name;
-        item.hostname = osquery::getHostname();
-        item.unixTime = osquery::getUnixTime();
-        item.calendarTime = osquery::getAsciiTime();
-        auto s = logScheduledQueryLogItem(item);
-        if (!s.ok()) {
-          LOG(ERROR) << "Error logging the results of query \"" << q.query
-                     << "\""
-                     << ": " << s.toString();
-        }
-      }
+      auto& d = Dispatcher::getInstance();
+      LOG(INFO) << "total count of tasks on the thread pool: "
+                << d.totalTaskCount();
+      d.add(std::make_shared<ScheduledQueryRunnable>(q));
     }
   }
 }
