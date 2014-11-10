1ce1e1790280bfd9900b44e0943de58b938d565c
mike@arpaia.co <mike@arpaia.co>
2014-11-10 17:38:35 -0500

    new headers

---
+++ include/osquery/kernel/linux/sysfs.h
@@ -0,0 +1,55 @@
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+#pragma once
+
+#include <linux/kobject.h>
+
+/**
+ * @brief XXX
+ *
+ * @param foo this is what foo does
+ *
+ * @return XXX
+ */
+int expose_sysfs(void);
+
+/**
+ * @brief XXX
+ *
+ * @param foo this is what foo does
+ * @param bar this is what bar does
+ * @param baz this is what baz does
+ *
+ * @return XXX
+ */
+ssize_t text_segment_hash_show(struct kobject *, struct attribute *, char *);
+
+
+/**
+ * @brief XXX
+ */
+struct camb_attr {
+  /// This is what this is
+  struct attribute attr;
+
+  /// This is what that is
+  int value;
+};
+
+
+
+
+
+
+
+unsigned char *kernel_text_hash(void);
+
+/**
+ * @brief XXX
+ *
+ * @param foo this is what foo does
+ * @param bar this is what bar does
+ *
+ * @return XXX
+ */
+unsigned char *hash_data(const void *, size_t);
