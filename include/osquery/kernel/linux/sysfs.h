376b292c57aa8b222a087ee2f237f30b6f95f731
mike@arpaia.co <mike@arpaia.co>
2014-11-10 17:44:06 -0500

    removing trailing content

--- include/osquery/kernel/linux/sysfs.h
+++ include/osquery/kernel/linux/sysfs.h
@@ -5,3 +5,26 @@
 #include <linux/kobject.h>
 
 int expose_sysfs(void);
+
+/**
+ * @brief XXX
+ *
+ * @param foo this is what foo does
+ * @param bar this is what bar does
+ * @param baz this is what baz does
+ *
+ * @return XXX
+ */
+ssize_t text_segment_hash_show(struct kobject *, struct attribute *, char *);
+
+
+/**
+ * @brief XXX
+ */
+struct camb_attr {
+  /// This is what this is
+  struct attribute attr;
+
+  /// This is what that is
+  int value;
+};
