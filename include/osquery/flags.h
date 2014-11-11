8e408f987ee0bce2b70d4ddc45d02d9d1affdd68
Teddy Reed <teddy@prosauce.org>
2014-11-11 08:35:25 -0800

    Table spec documentation examples

--- include/osquery/flags.h
+++ include/osquery/flags.h
@@ -2,8 +2,6 @@
 
 #pragma once
 
-#include <boost/make_shared.hpp>
-
 #define STRIP_FLAG_HELP 1
 #include <gflags/gflags.h>
 
@@ -24,9 +22,15 @@ class Flag {
   /*
    * @brief the instance accessor, but also can register flag data.
    *
+   * The accessor is mostly needless. The static instance and registration of
+   * flag data requires the accessor wrapper.
+   *
    * @param name The 'name' or the options switch data.
    * @param value The default value for this flag.
    * @param desc The description printed to the screen during help.
+   * @param shell_only Only print flag help when using `OSQUERY_TOOL_SHELL`.
+   *
+   * @return A mostly needless flag instance.
    */
   static Flag& get(const std::string& name = "",
                    const std::string& value = "",
@@ -45,7 +49,7 @@ class Flag {
    * @param name The 'name' or the options switch data.
    * @param value The default value for this flag.
    * @param desc The description printed to the screen during help.
-   * @param shell_only Restrict this flag to the shell.
+   * @param shell_only Restrict this flag to the shell help output.
    */
   void add(const std::string& name,
            const std::string& value,
@@ -67,7 +71,13 @@ class Flag {
   std::map<std::string, FlagDetail> flags() { return flags_; }
   /// The public flags instance, usable when parsing `--help` for the shell.
   std::map<std::string, FlagDetail> shellFlags() { return shell_flags_; }
-  static void print_flags(const std::map<std::string, FlagDetail> flags) {
+
+  /*
+   * @brief Print help-style output to stdout for a given flag set.
+   *
+   * @param flags A flag set (usually generated from Flag::flags).
+   */
+  static void printFlags(const std::map<std::string, FlagDetail> flags) {
     for (const auto& flag : flags) {
       fprintf(stdout,
               "  --%s, --%s=VALUE\n    %s (default: %s)\n",
@@ -100,7 +110,15 @@ class Flag {
     Flag flag = Flag::get(#name, #value, #desc);     \
   }
 
-/// Wrapper to bypass osquery help output
+/*
+ * @brief A duplicate of DEFINE_osquery_flag except the help output will only
+ * show when using OSQUERY_TOOL_SHELL (osqueryi).
+ *
+ * @param type The `_type` symbol portion of the gflags define.
+ * @param name The name symbol passed to gflags' `DEFINE_type`.
+ * @param value The default value, use a C++ literal.
+ * @param desc A string literal used for help display.
+ */
 #define DEFINE_shell_flag(type, name, value, desc)     \
   DEFINE_##type(name, value, desc);                    \
   namespace flag_##name {                              \
