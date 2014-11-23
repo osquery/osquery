2b1cd4eee3776531fd2745cd361345d3637ea7ec
Teddy Reed <teddy@prosauce.org>
2014-11-23 13:17:38 -0700

    Towards predicate constraint checking

---
+++ osquery/tables/utility/hash.cpp
@@ -0,0 +1,16 @@
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+#include "osquery/database.h"
+#include "osquery/filesystem.h"
+
+namespace osquery {
+namespace tables {
+
+
+QueryData genHash() {
+  QueryData results;
+
+  return results;
+}
+}
+}
