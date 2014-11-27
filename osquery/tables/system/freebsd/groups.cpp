8ab1863790f25c2cb5a04fa743ef4ee96aa4f70e
Theodore M. Reed <teddy@prosauce.org>
2014-11-26 22:46:55 -0800

    Predicate constraints for FreeBSD

--- osquery/tables/system/freebsd/groups.cpp
+++ osquery/tables/system/freebsd/groups.cpp
@@ -1,12 +1,14 @@
+// Copyright 2004-present Facebook. All Rights Reserved.
+
 #include "osquery/core.h"
-#include "osquery/database.h"
+#include "osquery/tables.h"
 
 namespace osquery {
 namespace tables {
 
 std::mutex grpEnumerationMutex;
 
-QueryData genGroups() {
+QueryData genGroups(QueryContext& context) {
   QueryData results;
 
   throw std::domain_error("Table not implemented for FreeBSD");
