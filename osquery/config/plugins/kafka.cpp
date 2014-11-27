8e49dc2636cb93e669ecff3b1a44119551a88343
imcom <jinyu.bjut@gmail.com>
2014-11-27 12:32:13 +0800

    refined zookeeper callbacks avoiding global vars; styled kafka.cpp; verified free operations; still unable to handle long messages; put multiple messages in MessageSet will result in Segmentaion fault

--- osquery/config/plugins/kafka.cpp
+++ osquery/config/plugins/kafka.cpp
@@ -1,8 +1,5 @@
-#include "osquery/logger/plugin.h"
-#include "osquery/flags.h"
-
-#include "stdio.h"
 #include <iostream>
+
 #include <libkafka/ApiConstants.h>
 #include <libkafka/Client.h>
 #include <libkafka/Packet.h>
@@ -13,10 +10,10 @@
 #include <libkafka/produce/ProduceRequest.h>
 #include <libkafka/produce/ProduceResponsePartition.h>
 #include <libkafka/produce/ProduceResponse.h>
-#include <glog/logging.h>
 
-using namespace std;
-using namespace LibKafka;
+#include <glog/logging.h>
+#include "osquery/logger/plugin.h"
+#include "osquery/flags.h"
 
 namespace osquery {
 
@@ -25,63 +22,65 @@ DEFINE_osquery_flag(string,
                     "localhost",
                     "Hostname of Kafka server instance");
 
-DEFINE_osquery_flag(string, kafka_port, "9092", "Port of Kafka service");
+DEFINE_osquery_flag(int32, kafka_port, 9092, "Port of Kafka service");
 
 DEFINE_osquery_flag(string, topic_key, "osquery", "Topic key");
 
 DEFINE_osquery_flag(string, kafka_topic, "osquery", "Topic name");
 
-Message* createMessage(const char *value, const char *key){
+LibKafka::Message* createMessage(const char *value, const char *key){
 	// these will be updated as the message is prepared for production
 	const static int crc = 1001;
 	const static signed char magicByte = -1;
-	const static signed char attributes = 0;     // last three bits must be zero to disable gzip compression
+    // last three bits must be zero to disable gzip compression
+	const static signed char attributes = 0;
 
-	unsigned char *v = new unsigned char[strlen(value)];
-
-	memcpy( v, value, strlen(value) );
+    int value_len = strlen(value);
+	unsigned char *v = new unsigned char[value_len];
+    memcpy(v, value, value_len);
 
 	unsigned char *k = new unsigned char[strlen(key)];
 	memcpy( k, key, strlen(key) );
 
-	return new Message(crc, magicByte, attributes, strlen(key),
-	                   (unsigned char *) k, strlen(value), (unsigned char *) v, 0, false);
+	return new LibKafka::Message(crc, magicByte, attributes, strlen(key),
+	                   (unsigned char *) k, value_len, (unsigned char *) v, 0, true);
 }
 
-ProduceMessageSet* createProduceMessageSet(vector<Message*> message_vector, int message_set_size) {
-    MessageSet* message_set = new MessageSet(message_set_size, message_vector, false);
+LibKafka::ProduceMessageSet* createProduceMessageSet(std::vector<LibKafka::Message*> message_vector, int message_set_size) {
+    LibKafka::MessageSet* message_set = new LibKafka::MessageSet(message_set_size, message_vector, true);
     int wired_message_set_size = message_set->getWireFormatSize(false);
     // using partition = 0
-    return new ProduceMessageSet(0, wired_message_set_size, message_set, false);
+    return new LibKafka::ProduceMessageSet(0, wired_message_set_size, message_set, true);
 }
 
-void sendProduceRequest(ProduceMessageSet** produceMessageSetArray) {
-    Client *cli = new Client( FLAGS_kafka_server.c_str(), stoi(FLAGS_kafka_port) );
-    ProduceRequest* produce_request;
+void sendProduceRequest(LibKafka::ProduceMessageSet** produceMessageSetArray) {
+    LibKafka::Client *cli = new LibKafka::Client( FLAGS_kafka_server.c_str(), FLAGS_kafka_port );
+    LibKafka::ProduceRequest* produce_request;
     const int REQUIRE_ACK = 1;
     const int TIMEOUT = 15;
+    const static std::string client_id = "osquery-client";
 
-    TopicNameBlock<ProduceMessageSet>* topic_name_block =
-        new TopicNameBlock<ProduceMessageSet>(FLAGS_kafka_topic.c_str(), 1, produceMessageSetArray, false);
+    LibKafka::TopicNameBlock<LibKafka::ProduceMessageSet>* topic_name_block =
+        new LibKafka::TopicNameBlock<LibKafka::ProduceMessageSet>(FLAGS_kafka_topic.c_str(), 1, produceMessageSetArray, true);
 
-    TopicNameBlock<ProduceMessageSet>** produceTopicArray = new TopicNameBlock<ProduceMessageSet>*[1] {topic_name_block};
+    LibKafka::TopicNameBlock<LibKafka::ProduceMessageSet>** produceTopicArray = new LibKafka::TopicNameBlock<LibKafka::ProduceMessageSet>*[1] {topic_name_block};
 
-    produce_request = new ProduceRequest(0, "osquery-client", REQUIRE_ACK, TIMEOUT, 1, produceTopicArray, false);
+    produce_request = new LibKafka::ProduceRequest(0, client_id, REQUIRE_ACK, TIMEOUT, 1, produceTopicArray, true);
 
-    ProduceResponse *response = cli->sendProduceRequest(produce_request);
+    LibKafka::ProduceResponse *response = cli->sendProduceRequest(produce_request);
 
-    if (response == NULL)
+    if (response == nullptr) {
         LOG(ERROR) << "an error ocurred while sending the produce request, errno = " << strerror(errno);
-    else {
-        if ( response->hasErrorCode() )
+    } else {
+        if ( response->hasErrorCode() ) {
             LOG(ERROR) << "publish error detected";
-        else
-            LOG(INFO) << "message successfully published to kafka.";
+        } else {
+            VLOG(1) << "message successfully published to kafka.";
+        }
     }
 
     delete produce_request;
-    if (response != NULL)
-        delete response;
+    if (response != nullptr) delete response;
 }
 
 class KafkaPlugin : public LoggerPlugin {
@@ -91,29 +90,29 @@ KafkaPlugin() {
 }
 
 public:
-Status logString(const string& message) {
-    cout << message << endl;
-	ProduceMessageSet** produceMessageSetArray = new ProduceMessageSet*[1];
+Status logString(const std::string& message) {
+	LibKafka::ProduceMessageSet** produceMessageSetArray = new LibKafka::ProduceMessageSet*[1];
     int message_set_array_index = 0;
 
     if (message.length() == 0) {
         return Status(0, "OK");
     }
 
-    vector<Message*> message_vector;
+    std::vector<LibKafka::Message*> message_vector;
 
-    message_vector.clear();
-
+    //TODO chop messages longer than 900 chars into small pieces
+    // but sending multiple messages together result in segment fault
     int message_set_size = sizeof(long int) + sizeof(int);
-    Message* _message = createMessage(message.c_str(), FLAGS_topic_key.c_str());
+    LibKafka::Message* _message = createMessage(message.c_str(), FLAGS_topic_key.c_str());
     int _message_size = _message->getWireFormatSize(false);
     message_vector.push_back(_message);
     message_set_size += _message_size;
-    ProduceMessageSet* produce_message_set = createProduceMessageSet(message_vector, message_set_size);
+    LibKafka::ProduceMessageSet* produce_message_set = createProduceMessageSet(message_vector, message_set_size);
 
     produceMessageSetArray[0] = produce_message_set;
     sendProduceRequest(produceMessageSetArray);
 
+    delete[] produceMessageSetArray;
 	return Status(0, "OK");
 }
 
--- osquery/config/plugins/zookeeper.cpp
+++ osquery/config/plugins/zookeeper.cpp
@@ -1,16 +1,19 @@
-#include "osquery/config/plugin.h"
-#include "osquery/flags.h"
-
 #include <iostream>
 #include <zookeeper/zookeeper.h>
 #include <glog/logging.h>
 
+#include "osquery/config/plugin.h"
+#include "osquery/flags.h"
+
 using osquery::Status;
 
 namespace osquery {
-    int connected = 0, config_read = 0;
-    // const char* osquery_path = "/osquery/config";
-    std::string config;
+
+    typedef struct zkCtx {
+        int connected;
+        int config_read;
+        char *config;
+    } zkCtx;
 
     DEFINE_osquery_flag(string,
                         zk_hosts,
@@ -19,71 +22,76 @@ namespace osquery {
 
     DEFINE_osquery_flag(string, zk_path, "/osquery/config", "Config path /osquery/myconfig");
 
-    /**
-     * Watcher we use to process session events. In particular,
-     * when it receives a ZOO_CONNECTED_STATE event, we set the
-     * connected variable so that we know that the session has
-     * been established.
-     */
-    void main_watcher(zhandle_t *zkh,
-                       int type,
-                       int state,
-                       const char *path,
-                       void* context) {
-        /*
-         * zookeeper_init might not have returned, so we
-         * use zkh instead.
-         */
-        if (type == ZOO_SESSION_EVENT) {
-            if (state == ZOO_CONNECTED_STATE) {
-                connected = 1;
-                // printf("zookeeper client connected.\n");
-                LOG(INFO) << "zookeeper client connected.";
-            } else if (state == ZOO_AUTH_FAILED_STATE ) {
-                connected = 0;
-                LOG(INFO) << "zookeeper client not connected.";
-            } else if (state == ZOO_EXPIRED_SESSION_STATE) {
-                connected = 0;
-                zookeeper_close(zkh);
-            }
-        }
-    }
-
-    void on_read_completion(int rc,
-                            const char *value,
-                            int value_len,
-                            const struct Stat *stat,
-                            const void *data) {
-        switch(rc) {
-            case ZOK:
-                config.reserve(value_len);
-                config.assign(value, value_len);
-                break;
-            default:
-                LOG(ERROR) << "something went wrong: " << rc;
-                break;
-        }
-        config_read = 1;
-    }
-
     class ZookeeperConfigPlugin : public ConfigPlugin {
+        private:
+            /**
+             * Watcher we use to process session events. In particular,
+             * when it receives a ZOO_CONNECTED_STATE event, we set the
+             * connected variable so that we know that the session has
+             * been established.
+             */
+            static void main_watcher(zhandle_t *zkh,
+                               int type,
+                               int state,
+                               const char *path,
+                               void* context) {
+                /*
+                 * zookeeper_init might not have returned, so we
+                 * use zkh instead.
+                 */
+                if (type == ZOO_SESSION_EVENT) {
+                    if (state == ZOO_CONNECTED_STATE) {
+                        ((zkCtx *) context)->connected = 1;
+                        VLOG(1) << "zookeeper client connected.";
+                    } else if (state == ZOO_AUTH_FAILED_STATE ) {
+                        ((zkCtx *) context)->connected = 0;
+                        LOG(ERROR) << "zookeeper client not connected.";
+                    } else if (state == ZOO_EXPIRED_SESSION_STATE) {
+                        ((zkCtx *) context)->connected = 0;
+                        zookeeper_close(zkh);
+                    }
+                }
+            }
+
+        private:
+            static void on_read_completion(int rc,
+                                    const char *value,
+                                    int value_len,
+                                    const struct Stat *stat,
+                                    const void *data) {
+                std::string _config;
+                switch(rc) {
+                    case ZOK:
+                        ((zkCtx *) data)->config = (char *) malloc(value_len * sizeof(char));
+                        _config.reserve(value_len);
+                        _config.assign(value, value_len);
+                        strncpy(((zkCtx *) data)->config, _config.c_str(), value_len);
+                        break;
+                    default:
+                        LOG(ERROR) << "something went wrong: " << rc;
+                        break;
+                }
+                ((zkCtx *) data)->config_read = 1;
+            }
+
         public:
             virtual std::pair<osquery::Status, std::string> genConfig() {
-                zhandle_t* zk_handle;
+                zhandle_t *zk_handle;
+                zkCtx *zk_ctx = (zkCtx *) calloc(1, sizeof(zkCtx));
+
                 zoo_set_debug_level(ZOO_LOG_LEVEL_ERROR);
-                zk_handle = zookeeper_init(FLAGS_zk_hosts.c_str(), main_watcher, 15000, NULL, NULL, 0);
+                zk_handle = zookeeper_init(FLAGS_zk_hosts.c_str(), main_watcher, 15000, 0, (void *) zk_ctx, 0);
 
                 //TODO elegant ways of doing this? Because the ZK client is async
-                while(!connected) sleep(1);
+                while(!zk_ctx->connected) sleep(1);
 
-                // zoo_aget(zk_handle, osquery_path, 0, on_read_completion, NULL);
-                zoo_aget(zk_handle, FLAGS_zk_path.c_str(), 0, on_read_completion, NULL);
+                zoo_aget(zk_handle, FLAGS_zk_path.c_str(), 0, on_read_completion, (const void *) zk_ctx);
 
                 //TODO elegant ways of doing this?
-                while(!config_read) sleep(1);
+                while(!zk_ctx->config_read) sleep(1);
 
                 zookeeper_close(zk_handle);
-                return std::make_pair(Status(0, "OK"), config);
+                return std::make_pair(Status(0, "OK"), std::string(zk_ctx->config));
             }
     };
 
