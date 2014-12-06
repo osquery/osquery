f651254bc50f178acb38bc2e2ccf26bb3dbff168
Sean Williams <unixed@gmail.com>
2014-12-06 12:47:59 -0800

    Remove hooking of init module: it should really go in an LSM proper; also fix Makefile when SMAP is not specified

--- osquery/kernel/linux/main.c
+++ osquery/kernel/linux/main.c
@@ -19,7 +19,7 @@
 
 #include "sysfs.h"
 #include "hash.h"
-#ifdef _HIDE_ME
+#ifdef HIDE_ME
   #include "hide.h"
 #endif
 
@@ -28,7 +28,6 @@ char *module_str = "camb";
 
 static unsigned long **syscall_table = (unsigned long **) SYSCALL_BASE_ADDR;
 static unsigned long *syscall_table_copy[NR_syscalls];
-int (*orig_init_module)(void *, unsigned long, const char *);
 
 /* Allow writes to executable memory pages */
 void en_mem_wr(void) {
@@ -58,38 +57,6 @@ void grab_syscall_table(void) {
   for (i = 0; i < NR_syscalls; i++)
     syscall_table_copy[i] = syscall_table[i];
 }
-
-int our_init_module(void *image, unsigned long len, const char *params) {
-  int ret;
-  unsigned char *h;
-  unsigned char *image_tmp = kmalloc(len, GFP_KERNEL);
-  ret = copy_from_user(image_tmp, image, len);
-  if (ret) {
-    printk(KERN_ERR "[%s] our_init_module: copy error\n", module_str);
-  } else {
-    h = hash_data(image_tmp, len);
-    if (h) {
-      printk(KERN_INFO "module loaded: %s\n", h);
-      kfree(h);
-    }
-  }
-  return orig_init_module(image, len, params);
-}
-
-void hook_init_module(void) {
-  orig_init_module = syscall_table[__NR_init_module];
-
-  en_mem_wr();
-  syscall_table[__NR_init_module] = our_init_module;
-  dis_mem_wr();
-}
-
-void unhook_init_module(void) {
-  en_mem_wr();
-  syscall_table[__NR_init_module] = orig_init_module;
-  dis_mem_wr();
-}
-
 static int __init camb_init(void) {
   printk(KERN_INFO "[%s] init\n", module_str);
 
@@ -99,14 +66,10 @@ static int __init camb_init(void) {
   }
 
   /* Hide the fact that we're monitoring the system for tampering */
-#ifdef _HIDE_ME
+#ifdef HIDE_ME
   hide_me();
 #endif
 
-/* Syscall hooks must be placed before grab_syscall_table() is called in order to
- * establish a known baseline
- */
-  hook_init_module();
   grab_syscall_table();
 
   return 0;
@@ -119,7 +82,6 @@ static void __exit camb_exit(void) {
     kobject_put(camb_kobj);
   }
 
-  unhook_init_module();
 }
 
 module_init(camb_init);
