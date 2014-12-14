fa95ff09d86ecf9221534fc8961ddc841b19fa89
anuka <david.vas1@gmail.com>
2014-12-14 22:14:00 +0100

    Some fix for etc_services.
    Signed-off-by: anuka <david.vas1@gmail.com>

--- osquery/tables/networking/etc_services.cpp
+++ osquery/tables/networking/etc_services.cpp
@@ -3,13 +3,12 @@
 
 #include <boost/algorithm/string/join.hpp>
 #include <boost/algorithm/string/predicate.hpp>
-#include <boost/lexical_cast.hpp>
 
 #include <glog/logging.h>
 
-#include "osquery/core.h"
-#include "osquery/database.h"
-#include "osquery/filesystem.h"
+#include <osquery/core.h>
+#include <osquery/tables.h>
+#include <osquery/filesystem.h>
 
 namespace osquery {
 namespace tables {
@@ -24,11 +23,10 @@ QueryData parseEtcServicesContent(const std::string& content) {
     }
 
     // [0]: name port/protocol [aliases]
-    // [1]: [comment]
+    // [1]: [comment part1]
+    // [2]: [comment part2]
+    // [n]: [comment partn]
     auto service_info_comment = split(line, "#");
-    if (service_info_comment.size() > 2) {
-      continue;
-    }
 
     // [0]: name
     // [1]: port/protocol
@@ -37,38 +35,40 @@ QueryData parseEtcServicesContent(const std::string& content) {
     // [n]: [aliasesn]
     auto service_info = split(service_info_comment[0]);
     if (service_info.size() < 2) {
+      LOG(WARNING) << "Line of /etc/services wasn't properly formatted. "
+                   << "Expected at least 2, got " << service_info.size();
       continue;
     }
 
     // [0]: port [1]: protocol
     auto service_port_protocol = split(service_info[1], "/");
     if (service_port_protocol.size() != 2) {
+      LOG(WARNING) << "Line of /etc/services wasn't properly formatted. "
+                   << "Expected 2, got " << service_port_protocol.size();
       continue;
     }
 
-    auto service_name = service_info[0];
-    auto service_port = boost::lexical_cast<std::string>(service_port_protocol[0]);
-    auto service_protocol = service_port_protocol[1];
+    Row r;
+    r["name"] = TEXT(service_info[0]);
+    r["port"] = INTEGER(service_port_protocol[0]);
+    r["protocol"] = TEXT(service_port_protocol[1]);
 
     // Removes the name and the port/protcol elements.
     service_info.erase(service_info.begin(), service_info.begin() + 2);
-    auto service_aliases = boost::algorithm::join(service_info, " ");
+    r["aliases"] = TEXT(boost::algorithm::join(service_info, " "));
 
-    Row r;
-    r["name"] = service_name;
-    r["port"] = service_port;
-    r["protocol"] = service_protocol;
-    r["aliases"] = service_aliases;
     // If there is a comment for the service.
-    if (service_info_comment.size() == 2) {
-      r["comment"] = service_info_comment[1];
+    if (service_info_comment.size() > 1) {
+      // Removes everything except the comment (parts of the comment).
+      service_info_comment.erase(service_info_comment.begin(), service_info_comment.begin() + 1);
+      r["comment"] = TEXT(boost::algorithm::join(service_info_comment, " # "));
     }
     results.push_back(r);
   }
   return results;
 }
 
-QueryData genEtcServices() {
+QueryData genEtcServices(QueryContext& context) {
   std::string content;
   auto s = osquery::readFile("/etc/services", content);
   if (s.ok()) {
