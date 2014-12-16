d5c5253bbc4fc29a628d1e17c7e82b0156a0af78
Teddy Reed <teddy@prosauce.org>
2014-12-16 01:51:54 -0800

    Add osquery_flags vtable

--- osquery/core/flags.cpp
+++ osquery/core/flags.cpp
@@ -5,32 +5,41 @@
 namespace osquery {
 
 Flag& Flag::get(const std::string& name,
+                const std::string& type,
                 const std::string& value,
                 const std::string& desc,
                 bool shell_only) {
   static Flag f;
   if (name != "") {
-    f.add(name, value, desc, shell_only);
+    f.add(name, type, value, desc, shell_only);
   }
   return f;
 }
 
 void Flag::add(const std::string& name,
+               const std::string& type,
                const std::string& value,
                const std::string& desc,
                bool shell_only) {
+  auto escaped_value = value;
+  if (type == "string") {
+    escaped_value.erase(0, 1);
+    escaped_value.erase(escaped_value.end() - 1, escaped_value.end());
+  }
   if (!shell_only) {
-    flags_.insert(std::make_pair(name, std::make_pair(value, desc)));
+    flags_.insert(
+        std::make_pair(name, std::make_tuple(type, escaped_value, desc)));
   } else {
-    shell_flags_.insert(std::make_pair(name, std::make_pair(value, desc)));
+    shell_flags_.insert(
+        std::make_pair(name, std::make_tuple(type, escaped_value, desc)));
   }
 }
 
 Status Flag::getDefaultValue(const std::string& name, std::string& value) {
   if (Flag::get().flags().count(name)) {
-    value = Flag::get().flags()[name].first;
+    value = std::get<1>(Flag::get().flags()[name]);
   } else if (Flag::get().shellFlags().count(name)) {
-    value = Flag::get().shellFlags()[name].first;
+    value = std::get<1>(Flag::get().shellFlags()[name]);
   } else {
     return Status(1, "Flag name not found.");
   }
@@ -47,8 +56,13 @@ bool Flag::isDefault(const std::string& name) {
   if (!getDefaultValue(name, default_value).ok()) {
     return false;
   }
-
   return (default_value == current_value);
+}
+
+std::string Flag::getValue(const std::string& name) {
+  std::string current_value;
+  __GFLAGS_NAMESPACE::GetCommandLineOption(name.c_str(), &current_value);
+  return current_value;
 }
 
 Status Flag::updateValue(const std::string& name, const std::string& value) {
@@ -59,11 +73,10 @@ Status Flag::updateValue(const std::string& name, const std::string& value) {
 void Flag::printFlags(const std::map<std::string, FlagDetail> flags) {
   for (const auto& flag : flags) {
     fprintf(stdout,
-            "  --%s, --%s=%s\n    %s\n",
+            "  --%s=%s\n    %s\n",
             flag.first.c_str(),
-            flag.first.c_str(),
-            flag.second.first.c_str(),
-            flag.second.second.c_str());
+            std::get<1>(flag.second).c_str(),
+            std::get<2>(flag.second).c_str());
   }
 }
 }
