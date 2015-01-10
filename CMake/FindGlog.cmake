f4a264320d70c77edc83ea844ade1673365431ad
Teddy Reed <teddy@prosauce.org>
2015-01-10 14:52:20 -0700

    Remove installed unwind headers

--- CMake/FindGlog.cmake
+++ CMake/FindGlog.cmake
@@ -3,14 +3,41 @@ include(FindPackageHandleStandardArgs)
 set(GLOG_ROOT_DIR "${CMAKE_BINARY_DIR}/third-party/glog")
 set(GLOG_SOURCE_DIR "${CMAKE_SOURCE_DIR}/third-party/glog")
 
+execute_process(
+  COMMAND ./tools/provision.sh
+  WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+  ERROR_QUIET
+)
+
+execute_process(
+  COMMAND make distclean
+  WORKING_DIRECTORY ${GLOG_SOURCE_DIR}
+  ERROR_QUIET
+)
+
+if(NOT APPLE)
+  include(CheckIncludeFiles)
+  unset(LIBUNWIND_FOUND CACHE)
+  check_include_files("libunwind.h;unwind.h" LIBUNWIND_FOUND)
+  if(LIBUNWIND_FOUND)
+    unset(libglog_FOUND CACHE)
+    execute_process(
+      COMMAND rm -rf "${GLOG_ROOT_DIR}" "${CMAKE_BINARY_DIR}/libglog-prefix"
+      ERROR_QUIET
+    )
+    message(WARNING "${Esc}[31mWarning: libunwind headers found [Bug:596], please: make deps\n${Esc}[m")
+  endif()
+endif()
+
 INCLUDE(ExternalProject)
 ExternalProject_Add(
   libglog
   SOURCE_DIR ${GLOG_SOURCE_DIR}
+  INSTALL_DIR ${GLOG_ROOT_DIR}
+  UPDATE_COMMAND ${CMAKE_SOURCE_DIR}/tools/provision.sh
   CONFIGURE_COMMAND ${GLOG_SOURCE_DIR}/configure --enable-frame-pointers --prefix=${GLOG_ROOT_DIR}
   BUILD_COMMAND make
   INSTALL_COMMAND make install
-  BUILD_IN_SOURCE 1
 )
 
 set(GLOG_INCLUDE_DIR "${GLOG_ROOT_DIR}/include")
--- tools/provision.sh
+++ tools/provision.sh
@@ -250,6 +250,34 @@ function package() {
   fi
 }
 
+function remove_package() {
+  if [[ $OS = "ubuntu" ]]; then
+    if dpkg --get-selections | grep --quiet $1; then
+      sudo apt-get remove $@ -y
+    else
+      log "Removing: $1 is not installed. skipping."
+    fi
+  elif [[ $OS = "centos" ]]; then
+    if rpm -qa | grep --quiet $1; then
+      sudo yum remove $@ -y
+    else
+      log "Removing: $1 is not installed. skipping."
+    fi
+  elif [[ $OS = "darwin" ]]; then
+    if brew list | grep --quiet $1; then
+      brew uninstall $@
+    else
+      log "Removing: $1 is not installed. skipping."
+    fi
+  elif [[ $OS = "freebsd" ]]; then
+    if pkg info -q $1; then
+      sudo pkg delete -y $@
+    else
+      log "Removing: $1 is not installed. skipping."
+    fi
+  fi
+}
+
 function gem_install() {
   if gem list | grep --quiet $1; then
     log "$1 is already installed. skipping."
@@ -375,15 +403,19 @@ function main() {
       package cmake
       package libboost1.55-all-dev
     fi
+
     if [[ $DISTRO = "precise" ]]; then
       install_gflags
-      install_glog
+      remove_package libunwind7-dev
     else
       package libgoogle-glog-dev
+      remove_package libunwind8-dev
     fi
+
     package libsnappy-dev
     package libbz2-dev
     package libreadline-dev
+
     if [[ $DISTRO = "precise" ]]; then
       package libproc-dev
     else
@@ -461,6 +493,8 @@ function main() {
     package bison
     package libudev-devel
 
+    remove_package unwind-devel
+
     # package libtool.x86_64
     # package boost.x86_64
 
@@ -468,6 +502,7 @@ function main() {
     install_automake
     install_libtool
     install_thrift
+
     set_cc gcc
     set_cxx g++
     install_rocksdb
@@ -494,7 +529,6 @@ function main() {
     package thrift
 
   elif [[ $OS = "freebsd" ]]; then
-
     package cmake
     package git
     package python
@@ -502,7 +536,6 @@ function main() {
     package rocksdb
     package thrift-cpp
     package glog
-
   fi
 
   cd "$SCRIPT_DIR/../"
@@ -516,6 +549,9 @@ function main() {
   fi
   git submodule init
   git submodule update
+
+  # Remove any previously-cached variables
+  rm build/$OS/CMakeCache.txt >/dev/null 2>&1
 }
 
 check $1 $2
