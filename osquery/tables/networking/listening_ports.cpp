ac0f2f96e4d6eddbf8a40827cea6f220ee89b886
Teddy Reed <teddy@prosauce.org>
2015-01-13 10:42:44 -0800

    Split OSX process_open_files into files/sockets

--- osquery/tables/networking/listening_ports.cpp
+++ osquery/tables/networking/listening_ports.cpp
@@ -14,21 +14,45 @@
 namespace osquery {
 namespace tables {
 
+typedef std::pair<std::string, std::string> ProtoFamilyPair;
+typedef std::map<std::string, std::vector<ProtoFamilyPair> > PortMap;
+
 QueryData genListeningPorts(QueryContext& context) {
   QueryData results;
 
   auto sockets = SQL::selectAllFrom("process_open_sockets");
 
+  PortMap ports;
   for (const auto& socket : sockets) {
     if (socket.at("remote_port") != "0") {
       // Listening UDP/TCP ports have a remote_port == "0"
       continue;
     }
 
+    if (ports.count(socket.at("local_port")) > 0) {
+      bool duplicate = false;
+      for (const auto& entry : ports[socket.at("local_port")]) {
+        if (entry.first == socket.at("protocol") &&
+            entry.second == socket.at("family")) {
+          duplicate = true;
+          break;
+        }
+      }
+
+      if (duplicate) {
+        // There is a duplicate socket descriptor for this bind.
+        continue;
+      }
+    }
+
+    // Add this family/protocol/port bind to the tracked map.
+    ports[socket.at("local_port")].push_back(
+        std::make_pair(socket.at("protocol"), socket.at("family")));
+
     Row r;
     r["pid"] = socket.at("pid");
     r["port"] = socket.at("local_port");
-    r["protocol"] = socket.at("local_port");
+    r["protocol"] = socket.at("protocol");
     r["family"] = socket.at("family");
     r["address"] = socket.at("local_address");
 
