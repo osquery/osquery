2ed0f1716b7112a7f4485904a03c452cd09ed712
Mitchell Grenier <superjedi22@gmail.com>
2015-01-15 15:06:30 -0800

    Most hashing stuff working though rerun bug is still plaguing the queries

--- osquery/tables/utility/hash.cpp
+++ osquery/tables/utility/hash.cpp
@@ -8,9 +8,16 @@
  *
  */
 
-#include "osquery/core/md5.h"
+#ifdef __APPLE__
+  #import <CommonCrypto/CommonDigest.h>
+#else
+  #include <openssl/sha.h>
+  #include <openssl/md5.h>
+#endif
+#include <iomanip>
 
 #include <boost/filesystem.hpp>
+#include <boost/uuid/sha1.hpp>
 
 #include <osquery/tables.h>
 #include <osquery/filesystem.h>
@@ -18,9 +25,79 @@
 namespace osquery {
 namespace tables {
 
+std::string processHash(unsigned char* hash, unsigned int length){
+    std::stringstream ss;
+    for(int i = 0; i < length; i++){
+        ss << std::hex << std::setw(2) << std::setfill('0') << (int)hash[i];
+    }
+    return ss.str();
+}
+
+std::string computeMD5(unsigned char* buffer, long fileLen){
+    #ifdef __APPLE__
+      const unsigned int LENGTH = CC_MD5_DIGEST_LENGTH;
+      unsigned char hash[LENGTH];
+      CC_MD5_CTX md5;
+      CC_MD5_Init(&md5);
+      CC_MD5_Update(&md5, buffer, fileLen);
+      CC_MD5_Final(hash, &md5);
+    #else
+      const unsigned int LENGTH = MD5_DIGEST_LENGTH;
+      unsigned char hash[LENGTH];
+      MD5_CTX md5;
+      MD5_Init(&md5);
+      MD5_Update(&md5, buffer, fileLen);
+      MD5_Final(hash, &md5);
+    #endif
+    
+    return processHash(hash, LENGTH);
+}
+
+std::string computeSHA1(unsigned char* buffer, long fileLen){
+    #ifdef __APPLE__
+      const unsigned int LENGTH = CC_SHA1_DIGEST_LENGTH;
+      unsigned char hash[LENGTH];
+      CC_SHA1_CTX sha1;
+      CC_SHA1_Init(&sha1);
+      CC_SHA1_Update(&sha1, buffer, fileLen);
+      CC_SHA1_Final(hash, &sha1);
+    #else
+      const unsigned int LENGTH = SHA_DIGEST_LENGTH;
+      unsigned char hash[LENGTH];
+      SHA_CTX sha1;
+      SHA1_Init(&sha1);
+      SHA1_Update(&sha1, buffer, fileLen);
+      SHA1_Final(hash, &sha1);
+    #endif
+    return processHash(hash, LENGTH);
+}
+std::string computeSHA256(unsigned char* buffer, long fileLen){
+    #ifdef __APPLE__
+      const unsigned int LENGTH = CC_SHA256_DIGEST_LENGTH;
+      unsigned char hash[LENGTH];
+      CC_SHA256_CTX sha256;
+      CC_SHA256_Init(&sha256);
+      CC_SHA256_Update(&sha256, buffer, fileLen);
+      CC_SHA256_Final(hash, &sha256);
+    #else
+      const unsigned int LENGTH = SHA256_DIGEST_LENGTH;
+      unsigned char hash[LENGTH];
+      SHA256_CTX sha256;
+      SHA256_Init(&sha256);
+      SHA256_Update(&sha256, buffer, fileLen);
+      SHA256_Final(hash, &sha256);
+    #endif
+    return processHash(hash, LENGTH);
+}
+
+void computeAllHashes(Row& r, const std::string& content, long filelen){
+    r["md5"]        = computeMD5(   (unsigned char *)content.c_str(), filelen);
+    r["sha1"]       = computeSHA1(  (unsigned char *)content.c_str(), filelen);
+    r["sha256"]     = computeSHA256((unsigned char *)content.c_str(), filelen);
+}
+
 QueryData genHash(QueryContext& context) {
   QueryData results;
-  osquery::md5::MD5 digest;
 
   auto paths = context.constraints["path"].getAll(EQUALS);
   for (const auto& path_string : paths) {
@@ -28,10 +105,13 @@ QueryData genHash(QueryContext& context) {
     if (!boost::filesystem::is_regular_file(path)) {
       continue;
     }
+    std::string content;
+    auto s = osquery::readFile(path.string(), content);
+    long filelen = (long)content.length();
     Row r;
-    r["path"] = path.string();
-    r["md5"] = std::string(digest.digestFile(path.c_str()));
-    r["directory"] = path.parent_path().string();
+    r["path"]       = path.string();
+    r["directory"]  = path.parent_path().string();
+    computeAllHashes(r, content, filelen);
     results.push_back(r);
   }
 
@@ -49,7 +129,9 @@ QueryData genHash(QueryContext& context) {
       r["path"] = begin->path().string();
       r["directory"] = directory_string;
       if (boost::filesystem::is_regular_file(begin->status())) {
-        r["md5"] = digest.digestFile(begin->path().string().c_str());
+        std::string content;
+        auto s = osquery::readFile(begin->path().string(), content);
+        computeAllHashes(r, content, content.length());
       }
       results.push_back(r);
     }
