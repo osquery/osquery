d2fe1826ae85aea56cac7dce01f96213ff529d1a
Mitchell Grenier <superjedi22@gmail.com>
2015-01-16 12:19:43 -0800

    Minor code change and clang-format

--- osquery/tables/system/darwin/nfs_shares.cpp
+++ osquery/tables/system/darwin/nfs_shares.cpp
@@ -13,65 +13,69 @@
 #include <osquery/filesystem.h>
 
 namespace osquery {
-	namespace tables {
+namespace tables {
 
-		QueryData parseNfsSharesContent(const std::string& content) {
-			QueryData results;
+QueryData parseNfsSharesContent(const std::string& content) {
+  QueryData results;
 
-			for (const auto& i : split(content, "\n")) {
-				auto line = split(i);
-				if (line.size() == 0 || boost::starts_with(line[0], "#")) {
-					continue;
-				}
-				std::vector<std::string> lineExports;
-				unsigned int readonly = 0;
-				int indexOfOptions = -1;
+  for (const auto& i : split(content, "\n")) {
+    auto line = split(i);
+    if (line.size() == 0 || boost::starts_with(line[0], "#")) {
+      continue;
+    }
+    std::vector<std::string> lineExports;
+    unsigned int readonly = 0;
+    int index_of_options = -1;
 
-				for (std::vector<std::string>::iterator iter = line.begin();
-					 iter != line.end();
-					 ++iter){
-					indexOfOptions++;
-					if((*iter)[0] == '/'){
-						lineExports.push_back(*iter);
-						
-					}else{
-						break;
-					}
-				}
-				for(std::vector<std::string>::iterator iter = line.begin()+indexOfOptions;
-					 iter != line.end();
-					 ++iter){
-					if(iter->compare("-ro") == 0|| iter->compare("-o") == 0){
-						readonly = 1;
-					}
-				}
-				for (std::vector<std::string>::iterator iter = lineExports.begin(); iter != lineExports.end(); ++iter)
-				{
-					Row r;
-					r["share"] = *iter;
-					if(readonly){
-						r["readonly"]  = "true";
-					}else{
-						r["readonly"] = "false";
-					}
-					std::ostringstream oss;
-					std::copy(line.begin()+indexOfOptions, line.end(), std::ostream_iterator<std::string>(oss, " "));
-					r["options"] = oss.str();
-					results.push_back(r);
-				}
-			}
-			return results;
-		}
+    for (std::vector<std::string>::iterator iter = line.begin();
+         iter != line.end();
+         ++iter) {
+      index_of_options++;
+      if ((*iter)[0] == '/') {
+        lineExports.push_back(*iter);
 
-		QueryData genNfsShares(QueryContext& context) {
-			std::string content;
-				auto s = osquery::readFile("/etc/exports", content);
-			if (s.ok()) {
-				return parseNfsSharesContent(content);
-			} else {
-				VLOG(1) << "Error reading /etc/exports: " << s.toString();
-				return {};
-			}
-		}
-	}
+      } else {
+        break;
+      }
+    }
+    for (std::vector<std::string>::iterator iter =
+             line.begin() + index_of_options;
+         iter != line.end();
+         ++iter) {
+      if (iter->compare("-ro") == 0 || iter->compare("-o") == 0) {
+        readonly = 1;
+      }
+    }
+    for (std::vector<std::string>::iterator iter = lineExports.begin();
+         iter != lineExports.end();
+         ++iter) {
+      Row r;
+      r["share"] = *iter;
+      if (readonly) {
+        r["readonly"] = "true";
+      } else {
+        r["readonly"] = "false";
+      }
+      std::ostringstream oss;
+      std::copy(line.begin() + index_of_options,
+                line.end(),
+                std::ostream_iterator<std::string>(oss, " "));
+      r["options"] = oss.str();
+      results.push_back(r);
+    }
+  }
+  return results;
+}
+
+QueryData genNfsShares(QueryContext& context) {
+  std::string content;
+  auto s = osquery::readFile("/etc/exports", content);
+  if (s.ok()) {
+    return parseNfsSharesContent(content);
+  } else {
+    VLOG(1) << "Error reading /etc/exports: " << s.toString();
+    return {};
+  }
+}
+}
 }
