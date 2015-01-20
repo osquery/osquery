ecfe29282bd792ee8fcd5f842647b78d50da5ded
mike@arpaia.co <mike@arpaia.co>
2015-01-20 15:36:53 -0800

    hash.h documentation
    I added some doxygen docs for hash.h

--- include/osquery/hash.h
+++ include/osquery/hash.h
@@ -3,7 +3,7 @@
  *  All rights reserved.
  *
  *  This source code is licensed under the BSD-style license found in the
- *  LICENSE file in the root directory of this source tree. An additional grant 
+ *  LICENSE file in the root directory of this source tree. An additional grant
  *  of patent rights can be found in the PATENTS file in the same directory.
  *
  */
@@ -12,29 +12,80 @@
 
 namespace osquery {
 
-// osquery-supported hash function types.
+/**
+ * @brief The supported hashing algorithms in osquery
+ *
+ * These are usually used as a constructor argument to osquery::Hash
+ */
 enum HashType {
   HASH_TYPE_MD5 = 2,
   HASH_TYPE_SHA1 = 4,
   HASH_TYPE_SHA256 = 8,
 };
 
+/**
+ * @brief Hash is a general utility class for hashing content
+ *
+ * @code{.cpp}
+ *   Hash my_hash(HASH_TYPE_SHA256);
+ *   my_hash.update(my_buffer, my_buffer_size);
+ *   std::cout << my_hash.digest();
+ * @endcode
+ *
+ */
 class Hash {
  public:
-  Hash(HashType algorithm) {
-    algorithm_ = algorithm;
-    init();
-  }
+  /**
+   * @brief Hash constructor
+   *
+   * The hash class should be initialized with one of osquery::HashType as a
+   * constructor argument.
+   *
+   * @param algorithm The hashing algorithm which will be used to compute the
+   * hash
+   */
+  explicit Hash(HashType algorithm);
 
+  /**
+   * @brief Hash destructor
+   */
   ~Hash();
 
+  /**
+   * @brief Update the internal context buffer with additional content
+   *
+   * This method allows you to chunk up large content so that it doesn't all
+   * have to be loaded into memory at the same time
+   *
+   * @param buffer The buffer to be hashed
+   * @param size The size of the buffer to be hashed
+   */
   void update(const void* buffer, size_t size);
+
+  /**
+   * @brief Compute the final hash and return it's result
+   *
+   * @return The final hash value
+   */
   std::string digest();
 
  private:
-  void init();
+  /**
+   * @brief Private default constructor
+   *
+   * The osquery::Hash class should only ever be instantiated with a HashType
+   */
+  Hash(){};
+
+ private:
+  /// The hashing algorithm which is used to compute the hash
   HashType algorithm_;
+
+  /// The buffer used to maintain the context and state of the hashing
+  /// operations
   void* ctx_;
+
+  /// The length of the hash to be returned
   size_t length_;
 };
 
