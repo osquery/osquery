4c4cba73c8b493e0070f54eb2448226eef61849f
Teddy Reed <teddy@prosauce.org>
2015-03-08 15:20:36 -0700

    [Implement #593] Add a RLOG, TLOG helper macro

--- include/osquery/logger.h
+++ include/osquery/logger.h
@@ -48,6 +48,25 @@ struct StatusLogLine {
 };
 
 /**
+ * @brief Helper logging macro for table-generated verbose log lines.
+ *
+ * Since logging in tables does not always mean a critical warning or error
+ * but more likely a parsing or expected edge-case, we provide a TLOG.
+ *
+ * The tool user can set within config or via the CLI what level of logging
+ * to tolerate. It's the table developer's job to assume consistency in logging.
+ */
+#define TLOG VLOG(1)
+
+/**
+ * @brief Prepend a reference number to the log line.
+ *
+ * A reference number is an external-search helper for somewhat confusing or
+ * seeminly-critical log lines.
+ */
+#define RLOG(n) "[Ref #" #n "] "
+
+/**
  * @brief Superclass for the pluggable logging facilities.
  *
  * In order to make the logging of osquery results and inline debug, warning,
