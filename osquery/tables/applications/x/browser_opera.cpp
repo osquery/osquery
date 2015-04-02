b67ca8598b73a1d8aa5ae7dc0eeac23f72670f6a
Mitchell Grenier <superjedi22@gmail.com>
2015-04-02 14:03:36 -0700

    Adding Opera support by cookie cutting Chrome
    Apparently if it ain't broke, don't fix it. Opera support seems to be exactly
    the same as Chrome except changing the path. The file is basically just copied
    with Chrome replaced with Opera + path change.
    
    Added cross platform functionality to chrome and opera. Plus abstracted it in
    more general functions that can be used for other chrome based browsers.

---
+++ osquery/tables/applications/x/browser_opera.cpp
@@ -0,0 +1,31 @@
+/*
+ *  Copyright (c) 2014, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#include <osquery/tables/applications/browser_utils.h>
+
+namespace fs = boost::filesystem;
+namespace pt = boost::property_tree;
+
+namespace osquery {
+namespace tables {
+
+/// Each home directory will include custom extensions.
+#ifdef __APPLE__
+  #define kOperaPath "/Library/Application Support/com.operasoftware.Opera/"
+#else
+  #define kOperaPath "/.config/opera/"
+#endif
+#define kOperaExtensionsPath "Extensions/"
+
+QueryData genOperaExtensions(QueryContext& context) {
+  return genChromeBasedExtensions(context, (kOperaPath kOperaExtensionsPath));
+}
+}
+}
