3103843e68c2b415931343b119643df753fb25b8
Mike Arpaia <mike@arpaia.co>
2015-05-05 11:39:41 -0700

    removing old unused kernel code

--- kernel/linux/hash.h
+++
@@ -1,4 +0,0 @@
-// Copyright 2004-present Facebook. All Rights Reserved.
-
-unsigned char *kernel_text_hash(void);
-unsigned char *hash_data(const void *, size_t);
--- kernel/linux/hide.h
+++
@@ -1,6 +0,0 @@
-// Copyright 2004-present Facebook. All Rights Reserved.
-
-void rm_mod_from_list(void);
-void rm_mod_from_sysfs(void);
-void rm_mod_from_ddebug_tables(void);
-void hide_me(void);
