79f0f36d4a363087ee97c2f21e9bcdb44f186cac
Javier Marcos <javier.marcos@gmail.com>
2015-05-07 23:17:21 -0700

    Adding test for parsing ipt_ip entries

--- osquery/tables/networking/linux/iptables.cpp
+++ osquery/tables/networking/linux/iptables.cpp
@@ -28,6 +28,55 @@ const char MAP[] = {'0','1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', '
 const int HIGH_BITS = 4;
 const int LOW_BITS = 15;
 
+void parseIpEntry(ipt_ip *ip, Row &r) {
+  r["protocol"] = INTEGER(ip->proto);
+  if (strlen(ip->iniface)) {
+    r["iniface"] = TEXT(ip->iniface);
+  } else {
+    r["iniface"] = "all";
+  }
+  if (strlen(ip->outiface)) {
+    r["outiface"] = TEXT(ip->outiface);
+  } else {
+   r["outiface"] = "all";
+  }
+  char src_ip_string[INET6_ADDRSTRLEN] = {0};
+  if (inet_ntop(AF_INET, (struct in_addr *)&ip->src, src_ip_string, INET6_ADDRSTRLEN) != NULL) {
+   r["src_ip"] = TEXT(src_ip_string);
+  }
+  char dst_ip_string[INET6_ADDRSTRLEN] = {0};
+  if (inet_ntop(AF_INET, (struct in_addr *)&ip->dst, dst_ip_string, INET6_ADDRSTRLEN) != NULL) {
+   r["dst_ip"] = TEXT(dst_ip_string);
+  }
+  char src_ip_mask[INET6_ADDRSTRLEN] = {0};
+  if (inet_ntop(AF_INET, (struct in_addr *)&ip->smsk, src_ip_mask, INET6_ADDRSTRLEN) != NULL) {
+   r["src_mask"] = TEXT(src_ip_mask);
+  }
+  char dst_ip_mask[INET6_ADDRSTRLEN] = {0};
+  if (inet_ntop(AF_INET, (struct in_addr *)&ip->dmsk, dst_ip_mask, INET6_ADDRSTRLEN) != NULL) {
+   r["dst_mask"] = TEXT(dst_ip_mask);
+  }
+
+  char aux_char[2];
+  std::string iniface_mask = "";
+  for (int i = 0; ip->iniface_mask[i] != 0x00 && i<IFNAMSIZ; i++) {
+    aux_char[0] = MAP[(int) ip->iniface_mask[i] >> HIGH_BITS];
+    aux_char[1] = MAP[(int) ip->iniface_mask[i] & LOW_BITS];
+    iniface_mask += aux_char[0];
+    iniface_mask += aux_char[1];
+  }
+
+  r["iniface_mask"] = TEXT(iniface_mask);
+  std::string outiface_mask = "";
+  for (int i = 0; ip->outiface_mask[i] != 0x00 && i<IFNAMSIZ; i++) {
+    aux_char[0] = MAP[(int) ip->outiface_mask[i] >> HIGH_BITS];
+    aux_char[1] = MAP[(int) ip->outiface_mask[i] & LOW_BITS];
+    outiface_mask += aux_char[0];
+    outiface_mask += aux_char[1];
+  }
+  r["outiface_mask"] = TEXT(outiface_mask);
+}
+
 QueryData getIptablesRules(const std::string& content) {
   QueryData results;
 
@@ -65,63 +114,21 @@ QueryData getIptablesRules(const std::string& content) {
         // Iterating through all the rules per chain
         for (auto chain_rule = iptc_first_rule(chain, (struct iptc_handle*)handle); chain_rule; chain_rule = iptc_next_rule(prev_rule, (struct iptc_handle*)handle))  {
           prev_rule = (struct ipt_entry*)chain_rule;
-          struct ipt_ip *ip = (struct ipt_ip*)&chain_rule->ip;
+
           auto target = iptc_get_target(chain_rule, (struct iptc_handle*)handle);
           if (target) {
             r["target"] = TEXT(target);
           }
-          r["protocol"] = INTEGER(ip->proto);
-          if (strlen(ip->iniface)) {
-            r["iniface"] = TEXT(ip->iniface);
-          } else {
-            r["iniface"] = "all";
-          }
-          if (strlen(ip->outiface)) {
-            r["outiface"] = TEXT(ip->outiface);
-          } else {
-            r["outiface"] = "all";
-          }
-          char src_ip_string[INET6_ADDRSTRLEN] = {0};
-          if (inet_ntop(AF_INET, (struct in_addr *)&ip->src, src_ip_string, INET6_ADDRSTRLEN) != NULL) {
-            r["src_ip"] = TEXT(src_ip_string);
-          }
-          char dst_ip_string[INET6_ADDRSTRLEN] = {0};
-          if (inet_ntop(AF_INET, (struct in_addr *)&ip->dst, dst_ip_string, INET6_ADDRSTRLEN) != NULL) {
-            r["dst_ip"] = TEXT(dst_ip_string);
-          }
-          char src_ip_mask[INET6_ADDRSTRLEN] = {0};
-          if (inet_ntop(AF_INET, (struct in_addr *)&ip->smsk, src_ip_mask, INET6_ADDRSTRLEN) != NULL) {
-            r["src_mask"] = TEXT(src_ip_mask);
-          }
-          char dst_ip_mask[INET6_ADDRSTRLEN] = {0};
-          if (inet_ntop(AF_INET, (struct in_addr *)&ip->dmsk, dst_ip_mask, INET6_ADDRSTRLEN) != NULL) {
-            r["dst_mask"] = TEXT(dst_ip_mask);
-          }
-
-          char aux_char[2];
-          std::string iniface_mask = "";
-          for (int i = 0; ip->iniface_mask[i] != 0x00 && i<IFNAMSIZ; i++) {
-            aux_char[0] = MAP[(int) ip->iniface_mask[i] >> HIGH_BITS];
-            aux_char[1] = MAP[(int) ip->iniface_mask[i] & LOW_BITS];
-            iniface_mask += aux_char[0];
-            iniface_mask += aux_char[1];
-          }
-
-          r["iniface_mask"] = TEXT(iniface_mask);
-          std::string outiface_mask = "";
-          for (int i = 0; ip->outiface_mask[i] != 0x00 && i<IFNAMSIZ; i++) {
-            aux_char[0] = MAP[(int) ip->outiface_mask[i] >> HIGH_BITS];
-            aux_char[1] = MAP[(int) ip->outiface_mask[i] & LOW_BITS];
-            outiface_mask += aux_char[0];
-            outiface_mask += aux_char[1];
-          }
-          r["outiface_mask"] = TEXT(outiface_mask);
 
           if (chain_rule->target_offset) {
             r["match"] = "yes";
           } else {
             r["match"] = "no";
           }
+
+          struct ipt_ip *ip = (struct ipt_ip*)&chain_rule->ip;
+          parseIpEntry(ip, r);
+
           results.push_back(r);
         } // Rule iteration
         results.push_back(r);
