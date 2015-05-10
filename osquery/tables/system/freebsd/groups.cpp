336ee2b99dbb1df37e696b0b0594692f7f6ca845
Ryan Steinmetz <zi@FreeBSD.org>
2015-05-10 16:47:12 -0400

    - Add users/groups support for FreeBSD

--- osquery/tables/system/freebsd/groups.cpp
+++ osquery/tables/system/freebsd/groups.cpp
@@ -8,6 +8,11 @@
  *
  */
 
+#include <set>
+#include <mutex>
+
+#include <grp.h>
+
 #include <osquery/core.h>
 #include <osquery/tables.h>
 
@@ -16,10 +21,26 @@ namespace tables {
 
 std::mutex grpEnumerationMutex;
 
-QueryData genGroups(QueryContext& context) {
+QueryData genGroups(QueryContext &context) {
+  std::lock_guard<std::mutex> lock(grpEnumerationMutex);
   QueryData results;
+  struct group *grp = nullptr;
+  std::set<long> groups_in;
 
-  throw std::domain_error("Table not implemented for FreeBSD");
+  setgrent();
+  while ((grp = getgrent()) != nullptr) {
+    if (std::find(groups_in.begin(), groups_in.end(), grp->gr_gid) ==
+        groups_in.end()) {
+      Row r;
+      r["gid"] = INTEGER(grp->gr_gid);
+      r["gid_signed"] = INTEGER((int32_t) grp->gr_gid);
+      r["groupname"] = TEXT(grp->gr_name);
+      results.push_back(r);
+      groups_in.insert(grp->gr_gid);
+    }
+  }
+  endgrent();
+  groups_in.clear();
 
   return results;
 }
--- osquery/tables/system/freebsd/users.cpp
+++ osquery/tables/system/freebsd/users.cpp
@@ -8,16 +8,45 @@
  *
  */
 
+#include <set>
+#include <mutex>
+#include <vector>
+#include <string>
+
+#include <pwd.h>
+
 #include <osquery/core.h>
 #include <osquery/tables.h>
 
 namespace osquery {
 namespace tables {
 
-QueryData genUsers(QueryContext& context) {
-  QueryData results;
+std::mutex pwdEnumerationMutex;
 
-  throw std::domain_error("Table not implemented for FreeBSD");
+QueryData genUsers(QueryContext& context) {
+  std::lock_guard<std::mutex> lock(pwdEnumerationMutex);
+  QueryData results;
+  struct passwd *pwd = nullptr;
+  std::set<long> users_in;
+
+  while ((pwd = getpwent()) != nullptr) {
+    if (std::find(users_in.begin(), users_in.end(), pwd->pw_uid) ==
+        users_in.end()) {
+      Row r;
+      r["uid"] = BIGINT(pwd->pw_uid);
+      r["gid"] = BIGINT(pwd->pw_gid);
+      r["uid_signed"] = BIGINT((int32_t) pwd->pw_uid);
+      r["gid_signed"] = BIGINT((int32_t) pwd->pw_gid);
+      r["username"] = TEXT(pwd->pw_name);
+      r["description"] = TEXT(pwd->pw_gecos);
+      r["directory"] = TEXT(pwd->pw_dir);
+      r["shell"] = TEXT(pwd->pw_shell);
+      results.push_back(r);
+      users_in.insert(pwd->pw_uid);
+    }
+  }
+  endpwent();
+  users_in.clear();
 
   return results;
 }
