0c3b123cb1975b6a8df060081083ea3898a9174e
Tim Zimmermann <tim.zimmermann@student.hpi.uni-potsdam.de>
2015-07-07 00:00:50 -0700

    Add date information to time table
    The fix also includes the time in ISO 8601 format
    as well as the format returned by C++'s asctime().
    See #1297.

--- osquery/tables/utility/time.cpp
+++ osquery/tables/utility/time.cpp
@@ -9,6 +9,7 @@
  */
 
 #include <ctime>
+#include <boost/algorithm/string/trim.hpp>
 
 #include <osquery/tables.h>
 
@@ -19,9 +20,30 @@ QueryData genTime(QueryContext& context) {
   Row r;
   time_t _time = time(0);
   struct tm* now = localtime(&_time);
+  struct tm* gmt = gmtime(&_time);
+
+  char weekday[10] = {0};
+  strftime(weekday, sizeof(weekday), "%A", now);
+
+  std::string timestamp;
+  timestamp = asctime(gmt);
+  boost::algorithm::trim(timestamp);
+  timestamp += " UTC";
+
+  char iso_8601[21] = {0};
+  strftime(iso_8601, sizeof(iso_8601), "%FT%TZ", gmt);
+
+  r["weekday"] = TEXT(weekday);
+  r["year"] = INTEGER(now->tm_year + 1900);
+  r["month"] = INTEGER(now->tm_mon + 1);
+  r["day"] = INTEGER(now->tm_mday);
   r["hour"] = INTEGER(now->tm_hour);
   r["minutes"] = INTEGER(now->tm_min);
   r["seconds"] = INTEGER(now->tm_sec);
+  r["unix_time"] = INTEGER(_time);
+  r["timestamp"] = TEXT(timestamp);
+  r["iso_8601"] = TEXT(iso_8601);
+
   QueryData results;
   results.push_back(r);
   return results;
