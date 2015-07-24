af13c1b7ea645781aaf23adc802440b3e5f942fe
Teddy Reed <teddy@prosauce.org>
2015-07-24 09:52:29 -0700

    Silence google benchmark CMake output, remove benchmark tests

--- CMake/FindBenchmark.cmake
+++ CMake/FindBenchmark.cmake
@@ -1,2 +1,8 @@
-INCLUDE_DIRECTORIES("${CMAKE_SOURCE_DIR}/third-party/benchmark/include")
-ADD_SUBDIRECTORY("${CMAKE_SOURCE_DIR}/third-party/benchmark")
+set(BENCHMARK_SOURCE_DIR "${CMAKE_SOURCE_DIR}/third-party/benchmark")
+set(BENCHMARK_BUILD_DIR "${CMAKE_BINARY_DIR}/third-party/benchmark")
+
+# Only build the benchmark shared library for benchmark targets.
+set(BENCHMARK_ENABLE_TESTING OFF CACHE BOOL "")
+
+INCLUDE_DIRECTORIES("${BENCHMARK_SOURCE_DIR}/include")
+ADD_SUBDIRECTORY("${BENCHMARK_SOURCE_DIR}")
