861e0e4273a8834fb1964f0ee616d10a65f077d2
Javier Marcos <javier.marcos@gmail.com>
2015-08-13 00:53:44 +0200

    Using the code in lib.sh

--- tools/deployment/make_osx_package.sh
+++ tools/deployment/make_osx_package.sh
@@ -11,8 +11,9 @@ set -e
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 SOURCE_DIR="$SCRIPT_DIR/../.."
-BUILD_VERSION=`sw_vers -productVersion | awk -F '.' '{print $1 "." $2}'`
-if [[ "$BUILD_VERSION" == "10.10" ]]; then
+source $SOURCE_DIR/tools/lib.sh
+distro "darwin" DISTRO
+if [[ "$DISTRO" == "10.10" ]]; then
   BUILD_DIR="$SOURCE_DIR/build/darwin"
 else
   BUILD_DIR="$SOURCE_DIR/build/darwin$BUILD_VERSION"
