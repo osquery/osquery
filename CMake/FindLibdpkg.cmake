65738a73c1f6bd2d0583deca2e219293974125b4
Brandon Bennett <bbennett@fb.com>
2015-10-15 01:02:49 -0600

    Support for newer versions of libdpkg
    Libdpkg has some breaking changes in newer versions which prevented
    compiling the deb_packages table on Ubuntu 15.04.  This change looks for
    the libpkg version user pkg-config and adds some preprocessor magic to
    support the newer versions.

---
+++ CMake/FindLibdpkg.cmake
@@ -0,0 +1,21 @@
+# - Find libdpkg
+# This module defines
+# LIBPKG_VERSION, string representation of libpkg version
+# LIBPKG_VERSION_MAJOR, major version number
+# LIBPKG_VERSION_MINOR, minor version number
+# LIBPKG_VERSION_PATCH, patch version number
+# LIBPKG_VERSION_NUMBER, version as a compariable integer
+
+find_package(PkgConfig QUIET)
+pkg_check_modules(PC_LIBDPKG QUIET libdpkg)
+
+MESSAGE("LOG" ${PC_LIBDPKG_LIBRARY_DIRS})
+
+set(LIBDPKG_VERSION ${PC_LIBDPKG_VERSION})
+string(REGEX REPLACE "([0-9]+).[0-9]+.[0-9]+" "\\1" LIBDPKG_VERSION_MAJOR ${LIBDPKG_VERSION})
+string(REGEX REPLACE "[0-9]+.([0-9]+).[0-9]+" "\\1" LIBDPKG_VERSION_MINOR ${LIBDPKG_VERSION})
+string(REGEX REPLACE "[0-9]+.[0-9]+.([0-9]+)" "\\1" LIBDPKG_VERSION_PATCH ${LIBDPKG_VERSION})
+math(EXPR LIBDPKG_VERSION_NUMBER "${LIBDPKG_VERSION_MAJOR} * 1000000 + ${LIBDPKG_VERSION_MINOR} * 1000 + ${LIBDPKG_VERSION_PATCH}")
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Libdpkg DEFAULT_MSG LIBDPKG_VERSION)
