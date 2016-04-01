cebd103e9b3515f20ba4afd4024af1c565c79a3e
Zachary Wasserman <zachwass2000@gmail.com>
2016-04-01 14:59:24 -0700

    Add gmock into gtest CMake config

--- CMake/FindGtest.cmake
+++ CMake/FindGtest.cmake
@@ -1,2 +1,2 @@
-INCLUDE_DIRECTORIES("${CMAKE_SOURCE_DIR}/third-party/gtest-1.7.0/include")
-ADD_SUBDIRECTORY("${CMAKE_SOURCE_DIR}/third-party/gtest-1.7.0")
+INCLUDE_DIRECTORIES("${CMAKE_SOURCE_DIR}/third-party/gmock-1.7.0/gtest/include" "${CMAKE_SOURCE_DIR}/third-party/gmock-1.7.0/include")
+ADD_SUBDIRECTORY("${CMAKE_SOURCE_DIR}/third-party/gmock-1.7.0")
