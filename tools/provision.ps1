99bc905b63839f24eb701ad37e26d370456a6c4f
yying <andy@trailofbits.com>
2016-04-05 16:13:26 -0400

    Added Facebook copyright information.

--- tools/provision.ps1
+++ tools/provision.ps1
@@ -1,3 +1,10 @@
+#  Copyright (c) 2014-present, Facebook, Inc.
+#  All rights reserved.
+#
+#  This source code is licensed under the BSD-style license found in the
+#  LICENSE file in the root directory of this source tree. An additional grant
+#  of patent rights can be found in the PATENTS file in the same directory.
+
 # URL of where our pre-compiled third-party dependenices are archived
 $THIRD_PARTY_ARCHIVE_URL = 'https://s3.amazonaws.com/osquery-pkgs/third-party.zip'
 
