619cbb3cc14fab2ca12d870422a8239de2e25468
yying <andy@trailofbits.com>
2016-05-04 16:06:24 -0400

    Adding only the process abstraction code

---
+++ osquery/core/posix/process.cpp
@@ -0,0 +1,107 @@
+/*
+ *  Copyright (c) 2014-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#include <vector>
+
+#include <signal.h>
+#include <sys/types.h>
+
+#include <osquery/logger.h>
+
+#include "osquery/core/process.h"
+
+extern char **environ;
+
+namespace osquery {
+
+PlatformProcess::PlatformProcess(PlatformPidType id)
+  : id_(id) { }
+
+PlatformProcess::PlatformProcess(const PlatformProcess& src) = default;
+PlatformProcess::PlatformProcess(PlatformProcess&& src) = default;
+
+bool PlatformProcess::operator==(const PlatformProcess& process) const {
+  return (nativeHandle() == process.nativeHandle());
+}
+
+bool PlatformProcess::operator!=(const PlatformProcess& process) const {
+  return (nativeHandle() != process.nativeHandle());
+}
+
+PlatformProcess::~PlatformProcess() { }
+
+PlatformProcess& PlatformProcess::operator=(const PlatformProcess& process) = default;
+
+int PlatformProcess::pid() const {
+  return id_;
+}
+
+bool PlatformProcess::kill() const {
+  if (id_ == kInvalidPid) {
+    return false;
+  }
+  
+  int status = ::kill(id_, SIGKILL);
+  return (status == 0);
+}
+
+PlatformProcess PlatformProcess::launchWorker(const std::string& exec_path, const std::string& name) {
+  auto worker_pid = ::fork();
+  if (worker_pid < 0) {
+    return PlatformProcess(kInvalidPid);
+  } else if (worker_pid == 0) {
+    setEnvVar("OSQUERY_WORKER", std::to_string(::getpid()).c_str());
+    ::execle(exec_path.c_str(), name.c_str(), nullptr, ::environ);
+    
+    // Code should never reach this point
+    LOG(ERROR) << "osqueryd could not start worker process";
+    Initializer::shutdown(EXIT_CATASTROPHIC);
+    return PlatformProcess(kInvalidPid);
+  }
+  return PlatformProcess(worker_pid);
+}
+
+PlatformProcess PlatformProcess::launchExtension(const std::string& exec_path, 
+                                                 const std::string& extension, 
+                                                 const std::string& extensions_socket,
+                                                 const std::string& extensions_timeout,
+                                                 const std::string& extensions_interval,
+                                                 const std::string& verbose) {
+  auto ext_pid = ::fork();
+  if (ext_pid < 0) {
+    return PlatformProcess(kInvalidPid);
+  } else if (ext_pid == 0) {
+    setEnvVar("OSQUERY_EXTENSIONS", std::to_string(::getpid()).c_str());
+    ::execle(exec_path.c_str(),
+             ("osquery extension: " + extension).c_str(),
+             "--socket",
+             extensions_socket.c_str(),
+             "--timeout",
+             extensions_timeout.c_str(),
+             "--interval",
+             extensions_interval.c_str(),
+             (verbose == "true") ? "--verbose" : (char*)nullptr,
+             (char*)nullptr,
+             ::environ);
+    
+    // Code should never reach this point
+    VLOG(1) << "Could not start extension process: " << extension;
+    Initializer::shutdown(EXIT_FAILURE);
+    return PlatformProcess(kInvalidPid);
+  }
+
+  return PlatformProcess(ext_pid);
+}
+
+PlatformProcess PlatformProcess::fromPlatformPid(PlatformPidType id) {
+  return PlatformProcess(id);
+}
+}
+
---
+++ osquery/core/windows/process.cpp
@@ -0,0 +1,208 @@
+/*
+ *  Copyright (c) 2014-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#include <vector>
+#include <sstream>
+#include <signal.h>
+#include <sys/types.h>
+
+#include "osquery/core/process.h"
+
+extern char **environ;
+
+osquery::PlatformPidType __declspec(nothrow) duplicateHandle(osquery::PlatformPidType src) {
+  osquery::PlatformPidType handle = osquery::kInvalidPid;
+
+  if (src != osquery::kInvalidPid) {
+    if (!::DuplicateHandle(GetCurrentProcess(),
+                           src,
+                           GetCurrentProcess(),
+                           &handle,
+                           0,
+                           FALSE,
+                           DUPLICATE_SAME_ACCESS)) {
+      handle = osquery::kInvalidPid;
+    }
+  }
+  return handle;
+}
+
+namespace osquery {
+
+PlatformProcess::PlatformProcess(PlatformPidType id) { 
+  id_ = duplicateHandle(id);
+}
+
+PlatformProcess::PlatformProcess(const PlatformProcess& src) {
+  id_ = duplicateHandle(src.nativeHandle());
+}
+
+PlatformProcess::PlatformProcess(PlatformProcess&& src) {
+  id_ = kInvalidPid;
+  std::swap(id_, src.id_);
+}  
+
+PlatformProcess::~PlatformProcess() { 
+  if (id_ != kInvalidPid) {
+    ::CloseHandle(id_);
+    id_ = kInvalidPid;
+  }
+}
+
+PlatformProcess& PlatformProcess::operator=(const PlatformProcess& process) {
+  id_ = duplicateHandle(process.nativeHandle());
+  return *this;
+}
+
+bool PlatformProcess::operator==(const PlatformProcess& process) const {
+  return (::GetProcessId(nativeHandle()) == ::GetProcessId(process.nativeHandle()));
+}
+
+bool PlatformProcess::operator!=(const PlatformProcess& process) const {
+  return (::GetProcessId(nativeHandle()) != ::GetProcessId(process.nativeHandle()));
+}
+
+int PlatformProcess::pid() const {
+  return ::GetProcessId(id_);
+}
+
+bool PlatformProcess::kill() const {
+  if (id_ == kInvalidPid) {
+    return false;
+  }
+  
+  return ::TerminateProcess(id_, 0);
+}
+
+PlatformProcess PlatformProcess::launchWorker(const std::string& exec_path, const std::string& name) {
+  ::STARTUPINFOA si = { 0 };
+  ::PROCESS_INFORMATION pi = { 0 };
+  
+  si.cb = sizeof(si);
+  
+  // TODO(#1991): We currently do not sanitize or check for bad characters in for the worker name. Names
+  //              with double quotes have the potential of causing argument parsing issues. However, it
+  //              is not a huge concern for the worker process, for it does not pass any command line 
+  //              arguments.
+  std::stringstream args_stream;
+  args_stream << "\"" << name << "\"";
+  
+  std::stringstream handle_stream;
+  
+  // TODO(#1991): The HANDLE exposed to the child process only has SYNCHRONIZE and 
+  //              PROCESS_QUERY_LIMITED_INFORMATION privileges which is enough to cover the current use 
+  //              cases. This may not be the case in the future...
+  HANDLE hLauncherProcess = ::OpenProcess(SYNCHRONIZE | PROCESS_QUERY_LIMITED_INFORMATION, 
+                                          TRUE, 
+                                          GetCurrentProcessId());
+  if (hLauncherProcess == NULL) {
+    return PlatformProcess(kInvalidPid);
+  }
+  
+  handle_stream << hLauncherProcess;
+  std::string handle = handle_stream.str();
+  
+  if (!::SetEnvironmentVariableA("OSQUERY_WORKER", "1") ||
+      !::SetEnvironmentVariableA("OSQUERY_LAUNCHER", handle.c_str())) {
+    ::CloseHandle(hLauncherProcess);
+
+    return PlatformProcess(kInvalidPid);
+  }
+  
+  std::string args = args_stream.str();
+  std::vector<char> argv(args.begin(), args.end());
+  argv.push_back('\0');
+  
+  BOOL status = ::CreateProcessA(exec_path.c_str(),
+                                 &argv[0],
+                                 NULL,
+                                 NULL,
+                                 TRUE,
+                                 0,
+                                 NULL,
+                                 NULL,
+                                 &si,
+                                 &pi);
+  ::SetEnvironmentVariableA("OSQUERY_WORKER", NULL);
+  ::SetEnvironmentVariableA("OSQUERY_LAUNCHER", NULL);
+  ::CloseHandle(hLauncherProcess);
+  
+  if (!status) {
+    return PlatformProcess(kInvalidPid);
+  }
+  
+  PlatformProcess process(pi.hProcess);
+  ::CloseHandle(pi.hThread);
+  ::CloseHandle(pi.hProcess);
+  
+  return process;
+}
+
+PlatformProcess PlatformProcess::launchExtension(const std::string& exec_path, 
+                                                 const std::string& extension, 
+                                                 const std::string& extensions_socket,
+                                                 const std::string& extensions_timeout,
+                                                 const std::string& extensions_interval,
+                                                 const std::string& verbose) {
+  ::STARTUPINFOA si = { 0 };
+  ::PROCESS_INFORMATION pi = { 0 };
+  
+  si.cb = sizeof(si);
+  
+  // TODO(#1991): extension name should be sanitized or checked for invalid characters such as 
+  //              double quotes. An extension name with bad characters has the potential of affecting
+  //              command line argument parsing in the extension process which may lead to a dysfunctional
+  //              extension.
+  std::stringstream args_stream;
+  args_stream << "\"osquery extension: " << extension << "\" ";
+  args_stream << "--socket " << extensions_socket << " ";
+  args_stream << "--timeout " << extensions_timeout << " ";
+  args_stream << "--interval " << extensions_interval << " ";
+  
+  if (verbose == "true") {
+    args_stream << "--verbose";
+  }
+  
+  std::string args = args_stream.str();
+  std::vector<char> argv(args.begin(), args.end());
+  argv.push_back('\0');
+
+  if (!::SetEnvironmentVariableA("OSQUERY_EXTENSIONS", "1")) {
+    return PlatformProcess(kInvalidPid);
+  }
+  
+  BOOL status = ::CreateProcessA(exec_path.c_str(),
+                                 &argv[0],
+                                 NULL,
+                                 NULL,
+                                 TRUE,
+                                 0,
+                                 NULL,
+                                 NULL,
+                                 &si,
+                                 &pi);
+  ::SetEnvironmentVariableA("OSQUERY_EXTENSIONS", NULL);
+  
+  if (!status) {
+    return PlatformProcess(kInvalidPid);
+  }
+
+  PlatformProcess process(pi.hProcess);
+  ::CloseHandle(pi.hThread);
+  ::CloseHandle(pi.hProcess);
+  
+  return process;
+}
+
+PlatformProcess PlatformProcess::fromPlatformPid(PlatformPidType id) {
+  return PlatformProcess(id);
+}
+}
+
