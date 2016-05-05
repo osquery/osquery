3624ef36f648509d07c63c60e5237f2a38700548
yying <andy@trailofbits.com>
2016-05-05 13:59:54 -0400

    Build system integration of process abstraction and added a unit test

---
+++ osquery/core/tests/process_tests.cpp
@@ -0,0 +1,169 @@
+/*
+ *  Copyright (c) 2014-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+ 
+#include <osquery/core.h>
+#include <gtest/gtest.h>
+
+#include "osquery/core/process.h"
+#include "osquery/core/test_util.h"
+
+extern char *self_exec_path;
+
+bool getProcessExitCode(osquery::PlatformProcess& process, int& exitCode)
+{
+  if (!process.isValid()) {
+    return false;
+  }
+
+#ifdef WIN32
+  DWORD code = 0;
+  DWORD ret = 0;
+
+  while ((ret = ::WaitForSingleObject(process.nativeHandle(), INFINITE)) != WAIT_FAILED &&
+    ret != WAIT_OBJECT_0);
+  if (ret == WAIT_FAILED) {
+    return false;
+  }
+
+  if (!::GetExitCodeProcess(process.nativeHandle(), &code)) {
+    return false;
+  }
+
+  if (code != STILL_ACTIVE) {
+    exitCode = code;
+    return true;
+  }
+#else
+  int status = 0;
+  if (::waitpid(process.nativeHandle(), &status, 0) == -1) {
+    return false;
+  }
+  if (WIFEXITED(status)) {
+    exitCode = WEXITSTATUS(status);
+    return true;
+  }
+#endif
+  return false;
+}
+
+namespace osquery {
+
+class ProcessTests : public testing::Test { };
+
+TEST_F(ProcessTests, test_constructor) {
+  auto p = PlatformProcess(kInvalidPid);
+  EXPECT_FALSE(p.isValid());
+}
+
+#ifdef WIN32
+TEST_F(ProcessTests, test_constructorWin) {
+  HANDLE handle = ::OpenProcess(PROCESS_ALL_ACCESS, FALSE, ::GetCurrentProcessId());
+  EXPECT_NE(handle, reinterpret_cast<HANDLE>(NULL));
+
+  auto p = PlatformProcess(handle);
+  EXPECT_TRUE(p.isValid());
+  EXPECT_NE(p.nativeHandle(), handle);
+
+
+  if (handle) {
+    ::CloseHandle(handle);
+  }
+}
+#else
+TEST_F(ProcessTests, test_constructorPosix) {
+  auto p = PlatformProcess(getpid());
+  EXPECT_TRUE(p.isValid());
+  EXPECT_EQ(p.nativeHandle(), getpid());
+}
+#endif
+
+TEST_F(ProcessTests, test_getpid) {
+  int pid = -1;
+
+  PlatformProcess process = getCurrentProcess();
+  EXPECT_TRUE(process.isValid());
+
+#ifdef WIN32
+    pid = (int) ::GetCurrentProcessId();
+#else
+    pid = getpid();
+#endif
+
+  EXPECT_EQ(process.pid(), pid);
+}
+
+TEST_F(ProcessTests, test_assignment) {
+  PlatformProcess process(kInvalidPid);
+  EXPECT_FALSE(process.isValid());
+
+  PlatformProcess current = getCurrentProcess();
+  EXPECT_TRUE(current.isValid());
+
+  PlatformPidType old_type = current.nativeHandle();
+
+  process = current;
+  EXPECT_EQ(current.nativeHandle(), old_type);
+
+#ifdef WIN32
+  // We make sure that the HANDLE values are not the same
+  EXPECT_NE(current.nativeHandle(), process.nativeHandle());
+#endif
+
+  EXPECT_EQ(current, process);
+  EXPECT_FALSE(current != process);
+}
+
+TEST_F(ProcessTests, test_envVar) {
+  auto val = getEnvVar("GTEST_OSQUERY");
+  EXPECT_FALSE(val);
+  
+  EXPECT_TRUE(setEnvVar("GTEST_OSQUERY", "true"));
+  
+  val = getEnvVar("GTEST_OSQUERY");
+  EXPECT_EQ(*val, "true");
+  
+  EXPECT_TRUE(unsetEnvVar("GTEST_OSQUERY"));
+  
+  val = getEnvVar("GTEST_OSQUERY");
+  EXPECT_FALSE(val);
+}
+
+TEST_F(ProcessTests, test_launchExtension) {
+  {
+    osquery::PlatformProcess process = osquery::PlatformProcess::launchExtension(
+      self_exec_path,
+      "extension-test",
+      "socket-name",
+      "100",
+      "5",
+      "true"
+    );
+    EXPECT_TRUE(process.isValid());
+
+    int code = 0;
+    EXPECT_TRUE(getProcessExitCode(process, code));
+    EXPECT_EQ(code, EXTENSION_SUCCESS_CODE);
+  }
+}
+
+TEST_F(ProcessTests, test_launchWorker) {
+  {
+    osquery::PlatformProcess process = osquery::PlatformProcess::launchWorker(
+      self_exec_path,
+      "worker-test"
+    );
+    EXPECT_TRUE(process.isValid());
+
+    int code = 0;
+    EXPECT_TRUE(getProcessExitCode(process, code));
+    EXPECT_EQ(code, WORKER_SUCCESS_CODE);
+  }
+}
+}
