fcd00aff2c8f2ab73768813542a6a2c3c9781348
Teddy Reed <teddy@prosauce.org>
2016-05-11 14:17:12 -0700

    [Fix #2032] Remove manual Thrift library discovery (#2085)
    The Thrift library should be discovered simiarly to all others.
    When using the build-macros for library discovery the caller can choose
    how to discover static or dynamic links.

--- CMake/FindThrift.cmake
+++ CMake/FindThrift.cmake
@@ -11,28 +11,8 @@ if(APPLE AND NOT DEFINED $ENV{THRIFT_HOME})
   set(ENV{THRIFT_HOME} ${BREW_PREFIX})
 endif()
 
-# prefer the thrift version supplied in THRIFT_HOME
-find_path(THRIFT_INCLUDE_DIR thrift/Thrift.h HINTS
-  $ENV{THRIFT_HOME}/include/
-  /usr/local/include/
-  /opt/local/include/
-)
-
-find_path(THRIFT_CONTRIB_DIR share/fb303/if/fb303.thrift HINTS
-  $ENV{THRIFT_HOME}
-  /usr/local/
-)
-
-set(THRIFT_LIB_PATHS
-  $ENV{THRIFT_HOME}/lib
-  /usr/local/lib
-  /opt/local/lib)
-
-# prefer the thrift version supplied in THRIFT_HOME
+# Prefer the thrift version supplied in THRIFT_HOME
 if(NOT DEFINED THRIFT_FOUND)
-  find_library(THRIFT_LIBRARY NAMES thrift HINTS ${THRIFT_LIB_PATHS})
-  #find_library(THRIFT_STATIC_LIBRARY NAMES libthrift.a HINTS ${THRIFT_LIB_PATHS})
-
   find_program(THRIFT_COMPILER thrift
     $ENV{THRIFT_HOME}/bin
     /usr/local/bin
@@ -40,20 +20,15 @@ if(NOT DEFINED THRIFT_FOUND)
     NO_DEFAULT_PATH
   )
 
-  if (THRIFT_LIBRARY)
+  if (THRIFT_COMPILER)
     set(THRIFT_FOUND TRUE)
-    LOG_LIBRARY(thrift "${THRIFT_LIBRARY}")
     exec_program(${THRIFT_COMPILER}
       ARGS -version OUTPUT_VARIABLE THRIFT_VERSION RETURN_VALUE THRIFT_RETURN)
   else()
-    message(FATAL_ERROR "Thrift compiler/libraries NOT found.")
+    message(FATAL_ERROR "Thrift compiler NOT found.")
   endif()
 
-  mark_as_advanced(
-    THRIFT_LIBRARY
-    THRIFT_COMPILER
-    THRIFT_INCLUDE_DIR
-  )
+  mark_as_advanced(THRIFT_COMPILER)
 endif()
 
 if (NOT "${THRIFT_VERSION}" STREQUAL "Thrift version 0.9.3")
