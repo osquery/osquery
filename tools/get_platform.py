34221005788a869d091048ad8764ccf03a46bd65
Nick Anderson <PoppySeedPlehzr@users.noreply.github.com>
2016-05-31 09:10:53 -0700

    Updated the get_platform.py script to be Python 3 compatible (#2122)
    Added in future imports to make the script forwards compatible and updated print functions.

--- tools/get_platform.py
+++ tools/get_platform.py
@@ -7,6 +7,11 @@
 #  LICENSE file in the root directory of this source tree. An additional grant
 #  of patent rights can be found in the PATENTS file in the same directory.
 
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
 import os
 import re
 import sys
@@ -21,51 +26,51 @@ DEBIAN_VERSION = "/etc/debian_version"
 
 def _platform():
     osType, _, _, _, _, _ = platform.uname()
-    
+
     if osType == "Windows":
         return ("windows", "windows")
     elif osType == "Linux":
         if os.path.exists(ORACLE_RELEASE):
             return ("redhat", "oracle")
-        
+
         if os.path.exists(SYSTEM_RELEASE):
             with open(SYSTEM_RELEASE, "r") as fd:
                 fileContents = fd.read()
-        
+
                 if fileContents.find("CentOS") != -1:
                     return ("redhat", "centos")
-                
+
                 if fileContents.find("Red Hat Enterprise") != -1:
                     return ("redhat", "rhel")
-                
+
                 if fileContents.find("Amazon Linux") != -1:
                     return ("redhat", "amazon")
-                
+
                 if fileContents.find("Fedora") != -1:
                     return ("redhat", "fedora")
-                
+
         if os.path.exists(LSB_RELEASE):
             with open(LSB_RELEASE, "r") as fd:
                 fileContents = fd.read()
-                
+
                 if fileContents.find("DISTRIB_ID=Ubuntu") != -1:
                     return ("debian", "ubuntu")
-                    
+
         if os.path.exists(DEBIAN_VERSION):
             return ("debian", "debian")
     else:
         return (None, osType.lower())
-        
+
 def _distro(osType):
     def getRedhatDistroVersion(pattern):
         with open(SYSTEM_RELEASE, "r") as fd:
             contents = fd.read()
-            
+
             result = re.findall(pattern, contents)
             if result and len(result) == 1:
                 return result[0].replace("release ", osType)
         return None
-    
+
     def commandOutput(cmd):
         try:
             output = subprocess.check_output(cmd)
@@ -74,9 +79,9 @@ def _distro(osType):
             return None
         except WindowsError:
             return None
-            
+
     _, _, osVersion, _, _, _ = platform.uname()
-    
+
     if osType == "oracle":
         result = getRedhatDistroVersion(r'release [5-7]')
         if result is not None:
@@ -118,27 +123,27 @@ def _distro(osType):
           return results[0]
     elif osType == "windows":
         return "windows%s" % osVersion
-    
+
     return "unknown_version"
 
 def platformAction():
     family, osType = _platform()
-    print osType
-    
+    print(osType)
+
 def distroAction():
     family, osType = _platform()
-    print _distro(osType)
- 
+    print(_distro(osType))
+
 def familyAction():
     family, osType = _platform()
     if family:
-        print family
+        print(family)
 
 def defaultAction():
     family, osType = _platform()
     distro = _distro(osType)
-    print "%s;%s" % (osType, distro)
-    
+    print("%s;%s" % (osType, distro))
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Platform detection script for osquery")
     parser.add_argument("--platform", action="store_true", help="Outputs the detected platform")
@@ -146,7 +151,7 @@ if __name__ == "__main__":
     parser.add_argument("--family", action="store_true", help="Outputs the detected family")
 
     args = parser.parse_args()
-    
+
     if args.platform and \
         not args.distro and \
         not args.family:
@@ -161,5 +166,5 @@ if __name__ == "__main__":
       familyAction()
     else:
       defaultAction()
-      
+
     sys.exit(0)
