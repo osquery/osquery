79818c72abe314dfb2e8423ca9058d8a93d162aa
Zachary Wasserman <zachwass2000@gmail.com>
2016-06-14 16:50:28 -0700

    Add newlines in firehose records (#2166)

--- osquery/logger/plugins/tests/aws_firehose_tests.cpp
+++ osquery/logger/plugins/tests/aws_firehose_tests.cpp
@@ -59,7 +59,7 @@ TEST_F(FirehoseTests, test_send) {
   EXPECT_CALL(*client,
               PutRecordBatch(Property(
                   &Aws::Firehose::Model::PutRecordBatchRequest::GetRecords,
-                  ElementsAre(MatchesEntry("foo")))))
+                  ElementsAre(MatchesEntry("foo\n")))))
       .WillOnce(Return(outcome));
   EXPECT_EQ(Status(0), forwarder.send(logs, "results"));
 
@@ -74,7 +74,7 @@ TEST_F(FirehoseTests, test_send) {
   EXPECT_CALL(*client,
               PutRecordBatch(Property(
                   &Aws::Firehose::Model::PutRecordBatchRequest::GetRecords,
-                  ElementsAre(MatchesEntry("bar"), MatchesEntry("foo")))))
+                  ElementsAre(MatchesEntry("bar\n"), MatchesEntry("foo\n")))))
       .WillOnce(Return(outcome));
   EXPECT_EQ(Status(1, "Foo error"), forwarder.send(logs, "results"));
 }
