f6ead92d81972bcae0289264c656c2ee8f4d90e2
Artem Dinaburg <artem@trailofbits.com>
2016-06-29 11:55:42 -0700

    Use symbolic constant for 0700 file mode

--- osquery/filesystem/fileops.h
+++ osquery/filesystem/fileops.h
@@ -48,6 +48,23 @@ using PlatformTimeType = FILETIME;
 #define W_OK 2
 #define X_OK R_OK
 
+// Windows does not define these constants, and they are neater
+// than using raw octal for platformChmod, etc.
+#define S_IRUSR 0400
+#define S_IWUSR 0200
+#define S_IXUSR 0100
+#define S_IRWXU (S_IRUSR|S_IWUSR|S_IXUSR)
+
+#define S_IRGRP (S_IRUSR >> 3)
+#define S_IWGRP (S_IWUSR >> 3)
+#define S_IXGRP (S_IXUSR >> 3)
+#define S_IRWXG (S_IRWXU >> 3)
+
+#define S_IROTH (S_IRGRP >> 3)
+#define S_IWOTH (S_IWGRP >> 3)
+#define S_IXOTH (S_IXGRP >> 3)
+#define S_IRWXO (S_IRWXG >> 3)
+
 #else
 
 using PlatformHandle = int;
