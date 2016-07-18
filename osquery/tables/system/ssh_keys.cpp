1bb99eccb20f3e76d2af2fb71649bafe87df061a
nerddotcat <gkreick@unomaha.edu>
2016-07-18 01:50:46 -0500

    Fixed formatting

--- osquery/tables/system/ssh_keys.cpp
+++ osquery/tables/system/ssh_keys.cpp
@@ -23,40 +23,38 @@ namespace tables {
 const std::string kSSHUserKeysDir = ".ssh/";
 
 void genSSHkeyForHosts(const std::string& uid,
-                        const std::string& directory,
-                        QueryData& results) {
-  //get list of files in directory
+                       const std::string& directory,
+                       QueryData& results) {
+  // Get list of files in directory
   boost::filesystem::path keys_dir = directory;
   keys_dir /= kSSHUserKeysDir;
-  std::vector<std::string>  files_list;
+  std::vector<std::string> files_list;
   auto status = listFilesInDirectory(keys_dir, files_list, false);
   if (!status.ok()) {
     return;
   }
-  //go through each file
+  // Go through each file
   for (const auto& kfile : files_list) {
     std::string keys_content;
     if (!forensicReadFile(kfile, keys_content).ok()) {
       // Cannot read a specific keys file.
       continue;
     }
-    
-    //if the file is a private key
-    if (keys_content.find("PRIVATE KEY") != std::string::npos)
-    {
-      //file is private key, create record for it
+
+    // If the file is a private key
+    if (keys_content.find("PRIVATE KEY") != std::string::npos) {
+      // File is private key, create record for it
       Row r;
       r["uid"] = uid;
       r["key_file"] = kfile;
       r["encrypted"] = INTEGER(0);
 
-      // check to see if the file is encrypted
+      // Check to see if the file is encrypted
       if (keys_content.find("ENCRYPTED") != std::string::npos) {
         r["encrypted"] = INTEGER(1);
       }
       results.push_back(r);
     }
-    
   }
 }
 
