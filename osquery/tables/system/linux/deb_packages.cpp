53f056f73245fe3756411aa6a4397fa05aaf5e9e
Teddy Reed <teddy@prosauce.org>
2016-08-15 21:29:40 -0700

    Fix dep_packages and apply to all Linux

--- osquery/tables/system/linux/deb_packages.cpp
+++ osquery/tables/system/linux/deb_packages.cpp
@@ -8,84 +8,36 @@
 *
 */
 
-#include <boost/algorithm/string.hpp>
-#include <osquery/tables.h>
-
 // see README.api of libdpkg-dev
 #define LIBDPKG_VOLATILE_API
 
 extern "C" {
 #include <dpkg/dpkg-db.h>
-
-#if LIBDPKG_VERSION < 1017010
-// copy pasted from dpkg-db.h
-// Prior to libdpkg 1.7.10 these enums are inside struct pkginfo and are not
-// visible for other headers
-enum pkgwant {
-	PKG_WANT_UNKNOWN,
-	PKG_WANT_INSTALL,
-	PKG_WANT_HOLD,
-	PKG_WANT_DEINSTALL,
-	PKG_WANT_PURGE,
-	/** Not allowed except as special sentinel value in some places. */
-	PKG_WANT_SENTINEL,
-};
-
-enum pkgeflag {
-	PKG_EFLAG_OK		= 0,
-	PKG_EFLAG_REINSTREQ	= 1,
-};
-
-enum pkgstatus {
-	PKG_STAT_NOTINSTALLED,
-	PKG_STAT_CONFIGFILES,
-	PKG_STAT_HALFINSTALLED,
-	PKG_STAT_UNPACKED,
-	PKG_STAT_HALFCONFIGURED,
-	PKG_STAT_TRIGGERSAWAITED,
-	PKG_STAT_TRIGGERSPENDING,
-	PKG_STAT_INSTALLED,
-};
-
-enum pkgpriority {
-	PKG_PRIO_REQUIRED,
-	PKG_PRIO_IMPORTANT,
-	PKG_PRIO_STANDARD,
-	PKG_PRIO_OPTIONAL,
-	PKG_PRIO_EXTRA,
-	PKG_PRIO_OTHER,
-	PKG_PRIO_UNKNOWN,
-	PKG_PRIO_UNSET = -1,
-};
-#endif
-
-#if LIBDPKG_VERSION < 1017023
-// libdpkg versions 1.17.23 and newer defines a size_t for fieldinfo which is
-// automatically expanded using the FIELD macro found in dpkg/parsedump.h. For
-// versions prior we just need the name
-#define FIELD(name) name
-#endif
-
 #include <dpkg/dpkg.h>
 #include <dpkg/pkg-array.h>
 #include <dpkg/parsedump.h>
 }
 
+#include <boost/algorithm/string.hpp>
+
+#include <osquery/filesystem.h>
+#include <osquery/logger.h>
+#include <osquery/tables.h>
+
 namespace osquery {
 namespace tables {
 
-/**
-* @brief A comparator used to sort the packages array
-*/
+static const std::string kDPKGPath{"/var/lib/dpkg"};
+
+/// A comparator used to sort the packages array.
 int pkg_sorter(const void *a, const void *b) {
   const struct pkginfo *pa = *(const struct pkginfo **)a;
   const struct pkginfo *pb = *(const struct pkginfo **)b;
   const char *arch_a = pa->installed.arch->name;
   const char *arch_b = pb->installed.arch->name;
-  int res;
 
-  res = strcmp(pa->set->name, pb->set->name);
-  if (res) {
+  int res = strcmp(pa->set->name, pb->set->name);
+  if (res != 0) {
     return res;
   }
 
@@ -97,11 +49,11 @@ int pkg_sorter(const void *a, const void *b) {
 }
 
 /**
-* @brief A field extractor to fetch the revision of a package
-*
-* dpkg tracks the revision as part of version, but we need to
-* provde our own fwritefunction for fieldinfos to extract it.
-*/
+ * @brief A field extractor to fetch the revision of a package.
+ *
+ * dpkg tracks the revision as part of version, but we need to provide our own
+ * fwritefunction for fieldinfos to extract it.
+ */
 void w_revision(struct varbuf *vb,
                 const struct pkginfo *pkg,
                 const struct pkgbin *pkgbin,
@@ -123,6 +75,8 @@ void dpkg_setup(struct pkg_array *packages) {
   dpkg_set_progname("osquery");
   push_error_context();
 
+  dpkg_db_set_dir("/var/lib/dpkg/");
+  modstatdb_init();
   modstatdb_open(msdbrw_readonly);
 
   pkg_array_init_from_db(packages);
@@ -150,41 +104,46 @@ const std::map<std::string, std::string> kFieldMappings = {
     {"Revision", "revision"}};
 
 /**
-* @brief Field names and function references to extract information
+* @brief Field names and function references to extract information.
 *
 * These are taken from lib/dpkg/parse.c, with a slight modification to
 * add an fwritefunction for Revision. Additional fields can be taken
 * as needed.
 */
 const struct fieldinfo fieldinfos[] = {
-    {FIELD("Package"), f_name, w_name},
-    {FIELD("Installed-Size"), f_charfield, w_charfield, PKGIFPOFF(installedsize)},
-    {FIELD("Architecture"), f_architecture, w_architecture},
+    {FIELD("Package"), f_name, w_name, 0},
+    {FIELD("Installed-Size"),
+     f_charfield,
+     w_charfield,
+     PKGIFPOFF(installedsize)},
+    {FIELD("Architecture"), f_architecture, w_architecture, 0},
     {FIELD("Source"), f_charfield, w_charfield, PKGIFPOFF(source)},
     {FIELD("Version"), f_version, w_version, PKGIFPOFF(version)},
-    {FIELD("Revision"), f_revision, w_revision},
-    {NULL}};
+    {FIELD("Revision"), f_revision, w_revision, 0},
+    {}};
 
 void extractDebPackageInfo(const struct pkginfo *pkg, QueryData &results) {
   Row r;
-  const struct fieldinfo *fip;
+
   struct varbuf vb;
   varbuf_init(&vb, 20);
 
   // Iterate over the desired fieldinfos, calling their fwritefunctions
   // to extract the package's information.
+  const struct fieldinfo *fip = nullptr;
   for (fip = fieldinfos; fip->name; fip++) {
     fip->wcall(&vb, pkg, &pkg->installed, fw_printheader, fip);
 
     std::string line = vb.string();
     if (!line.empty()) {
-      std::size_t separator_position = line.find(':');
+      size_t separator_position = line.find(':');
+
       std::string key = line.substr(0, separator_position);
       std::string value = line.substr(separator_position + 1, line.length());
       auto it = kFieldMappings.find(key);
       if (it != kFieldMappings.end()) {
         boost::algorithm::trim(value);
-        r[it->second] = value;
+        r[it->second] = std::move(value);
       }
     }
     varbuf_reset(&vb);
@@ -194,16 +153,23 @@ void extractDebPackageInfo(const struct pkginfo *pkg, QueryData &results) {
   results.push_back(r);
 }
 
-QueryData genDebs(QueryContext &context) {
+QueryData genDebPackages(QueryContext &context) {
   QueryData results;
+
+  if (!osquery::isDirectory(kDPKGPath)) {
+    TLOG << "Cannot find DPKG database: " << kDPKGPath;
+    return results;
+  }
 
   struct pkg_array packages;
   dpkg_setup(&packages);
+
   for (int i = 0; i < packages.n_pkgs; i++) {
     struct pkginfo *pkg = packages.pkgs[i];
-	// Casted to int to allow the older enums that were embeded in the packages
-	// struct to be compared
-	if (static_cast<int>(pkg->status) == static_cast<int>(PKG_STAT_NOTINSTALLED)) {
+    // Casted to int to allow the older enums that were embeded in the packages
+    // struct to be compared
+    if (static_cast<int>(pkg->status) ==
+        static_cast<int>(PKG_STAT_NOTINSTALLED)) {
       continue;
     }
 
