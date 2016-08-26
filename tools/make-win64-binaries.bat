7915b579eef6282cc912a3f0ddcfe9bed1a2b54e
artemdinaburg <artem@dinaburg.org>
2016-08-26 06:42:18 -0400

    Load VS variables for amd64 native build prior to launching cmake (#2408)

--- tools/make-win64-binaries.bat
+++ tools/make-win64-binaries.bat
@@ -1,3 +1,6 @@
+call "%VS140COMNTOOLS%vcvarsqueryregistry.bat" 64bit
+call "%VCINSTALLDIR%vcvarsall.bat" amd64
+
 mkdir .\build\windows10
 cd .\build\windows10
 cmake ..\.. -G "Visual Studio 14 2015 Win64"
