1bc52f8a50c4f316735af4579579720819e7c5fd
Teddy Reed <teddy@prosauce.org>
2016-09-07 14:28:56 -0700

    [Fix #2443] Restore shellstaticFunc argument names (#2444)

--- osquery/devtools/shell.cpp
+++ osquery/devtools/shell.cpp
@@ -234,7 +234,7 @@ static char continuePrompt[20]; // Continuation prompt. default: "   ...> "
 // of work. Instead just use this hack, which is quite harmless.
 static const char* zShellStatic = 0;
 void shellstaticFunc(sqlite3_context* context,
-                     int /* argc */,
+                     int argc,
                      sqlite3_value** /* argv */) {
   assert(0 == argc);
   assert(zShellStatic);
