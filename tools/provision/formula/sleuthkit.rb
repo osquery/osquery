80de6e3d3b3046a640c426dc07a83ed9e6a6fc79
Daniel Bayerlein <danielbayerlein@users.noreply.github.com>
2016-09-28 21:00:07 +0200

    Update sleuthkit to v4.3.0 (#2545) (#2557)

--- tools/provision/formula/sleuthkit.rb
+++ tools/provision/formula/sleuthkit.rb
@@ -3,8 +3,8 @@ require File.expand_path("../Abstract/abstract-osquery-formula", __FILE__)
 class Sleuthkit < AbstractOsqueryFormula
   desc "Forensic toolkit"
   homepage "http://www.sleuthkit.org/"
-  url "https://github.com/sleuthkit/sleuthkit/archive/sleuthkit-4.2.0.tar.gz"
-  sha256 "d71414134c9f8ce8e193150dd478c063173ee7f3b01f8a2a5b18c09aaa956ba7"
+  url "https://github.com/sleuthkit/sleuthkit/archive/sleuthkit-4.3.0.tar.gz"
+  sha256 "64a57a44955e91300e1ae69b34e8702afda0fb5bd72e2116429875c9f5f28980"
   head "https://github.com/sleuthkit/sleuthkit.git"
 
   bottle do
