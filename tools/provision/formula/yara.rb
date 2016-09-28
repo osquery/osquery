a50899036b46b4252291f67cd0a95fe389f8d429
Lennart Espe <lennart@mooxmirror.io>
2016-09-28 21:33:22 +0200

    deps: Replace old patch hash

--- tools/provision/formula/yara.rb
+++ tools/provision/formula/yara.rb
@@ -11,15 +11,13 @@ class Yara < AbstractOsqueryFormula
     sha256 "b3a6f830beffcd68f610fa45d8c9eedb7006e9304fffd5fee52dc0cda79daa9f" => :x86_64_linux
   end
 
-  end
-
   stable do
     url "https://github.com/VirusTotal/yara/archive/v3.5.0.tar.gz"
     sha256 "4bc72ee755db85747f7e856afb0e817b788a280ab5e73dee42f159171a9b5299"
 
     patch do
       url "https://github.com/VirusTotal/yara/pull/529.diff"
-      sha256 "7d865ebbb1f31a5b5e0337001842d5aab42b2b9683b307c187425fb76dc9524f"
+      sha256 "c462efecbd2be2f582d64fd0cd493cb9ccc22ea42339b508488ddb20d63c4061"
     end
   end
 
