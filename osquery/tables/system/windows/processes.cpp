2845898b18d249dbbec5d7ab436ae2952b33b109
yying <yying@users.noreply.github.com>
2016-10-03 15:23:46 -0400

    Efficiency improvements to processes table on Windows (#2587)

--- osquery/tables/system/windows/processes.cpp
+++ osquery/tables/system/windows/processes.cpp
@@ -9,7 +9,6 @@
  */
 
 #include <map>
-#include <sstream>
 #include <string>
 
 #define _WIN32_DCOM
@@ -18,7 +17,9 @@
 #include <psapi.h>
 #include <stdlib.h>
 
+#include <boost/algorithm/string/join.hpp>
 #include <boost/algorithm/string/trim.hpp>
+#include <boost/lexical_cast.hpp>
 
 #include <osquery/core.h>
 #include <osquery/filesystem.h>
@@ -32,7 +33,7 @@
 namespace osquery {
 namespace tables {
 
-std::set<long> getProcList(const QueryContext& context) {
+std::set<long> getSelectedPids(const QueryContext& context) {
   std::set<long> pidlist;
   if (context.constraints.count("pid") > 0 &&
       context.constraints.at("pid").exists(EQUALS)) {
@@ -41,107 +42,110 @@ std::set<long> getProcList(const QueryContext& context) {
         pidlist.insert(pid);
       }
     }
-    return pidlist;
-  } else {
-    WmiRequest request("SELECT ProcessId FROM Win32_Process");
-    if (request.getStatus().ok()) {
-      long placeHolder;
-      Status s;
-      for (auto const& result : request.results()) {
-        s = result.GetLong("ProcessId", placeHolder);
-        if (!s.ok()) {
-          continue;
-        }
-        pidlist.insert(placeHolder);
-      }
-    }
-    return pidlist;
   }
+
+  /// If there are no constraints, pidlist will be an empty set
+  return pidlist;
 }
 
-void genProcess(long pid, QueryData& results_data) {
+void genProcess(const WmiResultItem& result, QueryData& results_data) {
+  Row r;
+  Status s;
+  long pid;
+  long lPlaceHolder;
+  std::string sPlaceHolder;
+  HANDLE hProcess = nullptr;
+
   // We store the current processes PID, as there are API calls which are more
   // efficient for populating process info for the current process.
   auto currentPid = GetCurrentProcessId();
 
-  std::stringstream ss;
-  ss << "SELECT * FROM Win32_Process WHERE ProcessId=" << pid;
+  s = result.GetLong("ProcessId", pid);
+  r["pid"] = s.ok() ? BIGINT(pid) : BIGINT(-1);
+  if (pid == currentPid) {
+    hProcess = GetCurrentProcess();
+  } else {
+    hProcess =
+        OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, false, pid);
+  }
 
-  WmiRequest request(ss.str());
-  if (request.getStatus().ok()) {
-    std::vector<WmiResultItem>& results = request.results();
-    if (results.size() == 1) {
-      Row r;
-      Status s;
-      long pid;
-      long lPlaceHolder;
-      std::string sPlaceHolder;
-      HANDLE hProcess;
+  result.GetString("Name", r["name"]);
+  result.GetString("ExecutablePath", r["path"]);
+  result.GetString("CommandLine", r["cmdline"]);
+  result.GetString("ExecutionState", r["state"]);
+  result.GetLong("ParentProcessId", lPlaceHolder);
+  r["parent"] = BIGINT(lPlaceHolder);
+  result.GetLong("Priority", lPlaceHolder);
+  r["nice"] = INTEGER(lPlaceHolder);
+  r["on_disk"] = osquery::pathExists(r["path"]).toString();
 
-      s = results[0].GetLong("ProcessId", pid);
-      r["pid"] = s.ok() ? BIGINT(pid) : BIGINT(-1);
-      if (pid == currentPid) {
-        hProcess = GetCurrentProcess();
-      } else {
-        hProcess = OpenProcess(
-            PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, false, pid);
-      }
+  std::vector<char> fileName(MAX_PATH);
+  fileName.assign(MAX_PATH + 1, '\0');
+  if (pid == currentPid) {
+    GetModuleFileName(nullptr, fileName.data(), MAX_PATH);
+  } else {
+    GetModuleFileNameEx(hProcess, nullptr, fileName.data(), MAX_PATH);
+  }
+  r["cwd"] = SQL_TEXT(fileName.data());
+  r["root"] = r["cwd"];
 
-      results[0].GetString("Name", r["name"]);
-      results[0].GetString("ExecutablePath", r["path"]);
-      results[0].GetString("CommandLine", r["cmdline"]);
-      results[0].GetString("ExecutionState", r["state"]);
-      results[0].GetLong("ParentProcessId", lPlaceHolder);
-      r["parent"] = BIGINT(lPlaceHolder);
-      results[0].GetLong("Priority", lPlaceHolder);
-      r["nice"] = INTEGER(lPlaceHolder);
-      r["on_disk"] = osquery::pathExists(r["path"]).toString();
+  r["pgroup"] = "-1";
+  r["uid"] = "-1";
+  r["euid"] = "-1";
+  r["suid"] = "-1";
+  r["gid"] = "-1";
+  r["egid"] = "-1";
+  r["sgid"] = "-1";
+  r["start_time"] = "0";
 
-      std::vector<char> fileName(MAX_PATH);
-      fileName.assign(MAX_PATH + 1, '\0');
-      if (pid == currentPid) {
-        GetModuleFileName(nullptr, fileName.data(), MAX_PATH);
-      } else {
-        GetModuleFileNameEx(hProcess, nullptr, fileName.data(), MAX_PATH);
-      }
-      r["cwd"] = SQL_TEXT(fileName.data());
-      r["root"] = r["cwd"];
+  // We pre-populate the info, in the event the API calls fail.
+  result.GetString("UserModeTime", sPlaceHolder);
+  long long llHolder;
+  osquery::safeStrtoll(sPlaceHolder, 10, llHolder);
+  r["user_time"] = BIGINT(llHolder / 10000000);
+  result.GetString("KernelModeTime", sPlaceHolder);
+  osquery::safeStrtoll(sPlaceHolder, 10, llHolder);
+  r["system_time"] = BIGINT(llHolder / 10000000);
 
-      r["pgroup"] = "-1";
-      r["uid"] = "-1";
-      r["euid"] = "-1";
-      r["suid"] = "-1";
-      r["gid"] = "-1";
-      r["egid"] = "-1";
-      r["sgid"] = "-1";
-      r["start_time"] = "0";
+  result.GetString("PrivatePageCount", sPlaceHolder);
+  r["wired_size"] = BIGINT(sPlaceHolder);
+  result.GetString("WorkingSetSize", sPlaceHolder);
+  r["resident_size"] = sPlaceHolder;
+  result.GetString("VirtualSize", sPlaceHolder);
+  r["total_size"] = BIGINT(sPlaceHolder);
+  results_data.push_back(r);
 
-      // We pre-populate the info, in the event the API calls fail.
-      results[0].GetString("UserModeTime", sPlaceHolder);
-      long long llHolder;
-      osquery::safeStrtoll(sPlaceHolder, 10, llHolder);
-      r["user_time"] = BIGINT(llHolder / 10000000);
-      results[0].GetString("KernelModeTime", sPlaceHolder);
-      osquery::safeStrtoll(sPlaceHolder, 10, llHolder);
-      r["system_time"] = BIGINT(llHolder / 10000000);
-
-      results[0].GetString("PrivatePageCount", sPlaceHolder);
-      r["wired_size"] = BIGINT(sPlaceHolder);
-      results[0].GetString("WorkingSetSize", sPlaceHolder);
-      r["resident_size"] = sPlaceHolder;
-      results[0].GetString("VirtualSize", sPlaceHolder);
-      r["total_size"] = BIGINT(sPlaceHolder);
-      results_data.push_back(r);
-    }
+  // OpenProcess returns nullptr on error, GetCurrentProcess returns a
+  // psuedo-handle of -1, which is the same as INVALID_HANDLE_VALUE
+  if (hProcess != nullptr && hProcess != INVALID_HANDLE_VALUE) {
+    CloseHandle(hProcess);
   }
 }
 
 QueryData genProcesses(QueryContext& context) {
   QueryData results;
 
-  auto pidlist = getProcList(context);
-  for (const auto& pid : pidlist) {
-    genProcess(pid, results);
+  std::string query = "SELECT * FROM Win32_Process";
+
+  auto pidlist = getSelectedPids(context);
+  if (pidlist.size() > 0) {
+    std::vector<std::string> constraints;
+    for (const auto& pid : pidlist) {
+      constraints.push_back("ProcessId=" + std::to_string(pid));
+    }
+    if (constraints.size() > 0) {
+      query += " WHERE " + boost::algorithm::join(constraints, " OR ");
+    }
+  }
+
+  WmiRequest request(query);
+  if (request.getStatus().ok()) {
+    for (const auto& item : request.results()) {
+      long pid = 0;
+      if (item.GetLong("ProcessId", pid).ok()) {
+        genProcess(item, results);
+      }
+    }
   }
 
   return results;
