2cb69abafca53a79283b38cdac109ce6d766fa7d
Michael McGrew <mcgrew@fb.com>
2016-10-12 15:11:35 -0700

    formatting

--- osquery/tables/system/windows/appcompat_shims.cpp
+++ osquery/tables/system/windows/appcompat_shims.cpp
@@ -39,10 +39,10 @@ QueryData genShims(QueryContext& context) {
     QueryData regResults;
     sdb sdb;
     std::string subkey = rKey.at("subkey");
-	auto start = subkey.find("{");
-	if (start == std::string::npos) {
-		continue;
-	}
+    auto start = subkey.find("{");
+    if (start == std::string::npos) {
+      continue;
+    }
     std::string sdbId = subkey.substr(start, subkey.length());
     // make sure it's a sane uninstall key
     queryKey("HKEY_LOCAL_MACHINE", subkey, regResults);
@@ -72,22 +72,23 @@ QueryData genShims(QueryContext& context) {
   for (const auto& rKey : shimResults) {
     QueryData appResults;
     std::string subkey = rKey.at("subkey");
-	auto start = rKey.at("subkey").rfind("\\") + 1;
-	if (start == std::string::npos) {
-		continue;
-	}
-    std::string executable = rKey.at("subkey").substr(start, rKey.at("subkey").length());
+    auto start = rKey.at("subkey").rfind("\\") + 1;
+    if (start == std::string::npos) {
+      continue;
+    }
+    std::string executable =
+        rKey.at("subkey").substr(start, rKey.at("subkey").length());
     // make sure it's a sane uninstall key
     queryKey("HKEY_LOCAL_MACHINE", subkey, appResults);
     for (const auto& aKey : appResults) {
       Row r;
-	  std::string sdbId;
-	  if (aKey.at("name").length() > 4) {
-		 sdbId = aKey.at("name").substr(0, aKey.at("name").length() - 4);
-	  }
-	  if (sdbs.count(sdbId) == 0) {
-		  continue;
-	  }
+      std::string sdbId;
+      if (aKey.at("name").length() > 4) {
+        sdbId = aKey.at("name").substr(0, aKey.at("name").length() - 4);
+      }
+      if (sdbs.count(sdbId) == 0) {
+        continue;
+      }
       r["executable"] = executable;
       r["path"] = sdbs.at(sdbId).path;
       r["description"] = sdbs.at(sdbId).description;
