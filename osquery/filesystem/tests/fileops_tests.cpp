138b821341479b09023bc846aa3fd82d8df9b020
yying <andy@trailofbits.com>
2016-11-07 20:22:46 -0500

    Modified tests to work on Windows and POSIX

--- osquery/filesystem/tests/fileops_tests.cpp
+++ osquery/filesystem/tests/fileops_tests.cpp
@@ -416,11 +416,11 @@ TEST_F(FileOpsTests, test_chmod_no_write) {
   }
 }
 
-#ifdef WIN32
-// Windows-specific test for file "immutability"
-TEST_F(FileOpsTests, test_immutable) {
+TEST_F(FileOpsTests, test_safe_permissions) {
   const auto root_dir = (fs::temp_directory_path() / "immutable-test").string();
   const auto temp_file = root_dir + "/test";
+  const int all_access = S_IRUSR | S_IWUSR | S_IXUSR | S_IRGRP | S_IWGRP |
+                         S_IXGRP | S_IROTH | S_IWOTH | S_IXOTH;
 
   fs::create_directories(root_dir);
 
@@ -436,38 +436,67 @@ TEST_F(FileOpsTests, test_immutable) {
     EXPECT_FALSE(status.ok());
     EXPECT_EQ(1, status.getCode());
 
+    if (isPlatform(PlatformType::TYPE_POSIX)) {
+      // On POSIX, chmod on a file requires +x on the parent directory
+      EXPECT_TRUE(platformChmod(root_dir, all_access));
+    }
+
     EXPECT_TRUE(platformChmod(temp_file, S_IRUSR | S_IRGRP | S_IROTH));
     EXPECT_TRUE(platformChmod(root_dir,
                               S_IRUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH));
 
     status = fd.hasSafePermissions();
-    EXPECT_FALSE(status.ok());
-    EXPECT_EQ(1, status.getCode());
+
+    if (isPlatform(PlatformType::TYPE_WINDOWS)) {
+      EXPECT_FALSE(status.ok());
+      EXPECT_EQ(1, status.getCode());
+    } else {
+      // On POSIX, we only check to see if temp_file has S_IWOTH
+      EXPECT_TRUE(status.ok());
+    }
+
+    if (isPlatform(PlatformType::TYPE_POSIX)) {
+      // On POSIX, chmod on a file requires +x on the parent directory
+      EXPECT_TRUE(platformChmod(root_dir, all_access));
+    }
 
     EXPECT_TRUE(platformChmod(temp_file, S_IRUSR | S_IRGRP | S_IROTH));
     EXPECT_TRUE(platformChmod(root_dir, S_IRUSR | S_IRGRP | S_IWGRP | S_IROTH));
 
     status = fd.hasSafePermissions();
-    EXPECT_FALSE(status.ok());
-    EXPECT_EQ(1, status.getCode());
+
+    if (isPlatform(PlatformType::TYPE_WINDOWS)) {
+      EXPECT_FALSE(status.ok());
+      EXPECT_EQ(1, status.getCode());
+    } else {
+      // On POSIX, we only check to see if temp_file has S_IWOTH
+      EXPECT_TRUE(status.ok());
+    }
+
+    if (isPlatform(PlatformType::TYPE_POSIX)) {
+      // On POSIX, chmod on a file requires +x on the parent directory
+      EXPECT_TRUE(platformChmod(root_dir, all_access));
+    }
 
     EXPECT_TRUE(platformChmod(temp_file, 0));
     EXPECT_TRUE(platformChmod(root_dir, 0));
     EXPECT_TRUE(fd.hasSafePermissions().ok());
+
+    if (isPlatform(PlatformType::TYPE_POSIX)) {
+      // On POSIX, chmod on a file requires +x on the parent directory
+      EXPECT_TRUE(platformChmod(root_dir, all_access));
+    }
 
     EXPECT_TRUE(platformChmod(temp_file, S_IRUSR | S_IRGRP | S_IROTH));
     EXPECT_TRUE(platformChmod(root_dir, S_IRUSR | S_IRGRP | S_IROTH));
     EXPECT_TRUE(fd.hasSafePermissions().ok());
   }
 
-  EXPECT_TRUE(platformChmod(
-      temp_file, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH));
-  EXPECT_TRUE(platformChmod(
-      root_dir, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH));
+  EXPECT_TRUE(platformChmod(temp_file, all_access));
+  EXPECT_TRUE(platformChmod(root_dir, all_access));
 
   fs::remove_all(root_dir);
 }
-#endif
 
 TEST_F(FileOpsTests, test_glob) {
   {
