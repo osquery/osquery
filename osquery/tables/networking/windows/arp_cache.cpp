987ef67ffb4628f104c3048aa2bade861a566632
Daniel Sedlacek <dansedlacek@fb.com>
2016-12-10 21:47:56 -0800

    reworked table to align with posix

--- osquery/tables/networking/windows/arp_cache.cpp
+++ osquery/tables/networking/windows/arp_cache.cpp
@@ -8,11 +8,14 @@
 *
 */
 
+#include <boost/range/algorithm/find.hpp>
 #include <osquery/tables.h>
 #include <string>
 
+#include "boost/algorithm/string/replace.hpp"
 #include "osquery/core/conversions.h"
 #include "osquery/core/windows/wmi.h"
+#include "osquery/tables/networking/windows/interfaces.h"
 
 namespace osquery {
 namespace tables {
@@ -37,32 +40,65 @@ const std::map<unsigned char, const std::string> mapOfState = {
 };
 
 QueryData genArpCache(QueryContext& context) {
+  Row r;
   QueryData results;
-
+  QueryData interfaces = genInterfaceDetails(context);
   WmiRequest wmiSystemReq("select * from MSFT_NetNeighbor",
                           L"ROOT\\StandardCimv2");
   std::vector<WmiResultItem>& wmiResults = wmiSystemReq.results();
+  std::map<long, std::string> mapOfInterfaces = {
+      {1, ""}, // loopback
+  };
+  unsigned short usiPlaceHolder;
+  unsigned char cPlaceHolder;
+  unsigned int uiPlaceHolder;
+  std::string strPlaceHolder;
 
-  if (wmiResults.size() != 0) {
-    Row r;
+  for (auto& iface : interfaces) {
+    long interfaceIndex = atol(iface["interface"].c_str());
+    std::string macAddress = iface["mac"];
+    mapOfInterfaces.insert(
+        std::pair<long, std::string>(interfaceIndex, macAddress));
+  }
+  for (const auto& item : wmiResults) {
+    item.GetUnsignedShort("AddressFamily", usiPlaceHolder);
+    r["address_family"] = SQL_TEXT(mapOfAddressFamily.at(usiPlaceHolder));
+    item.GetUChar("Store", cPlaceHolder);
+    r["store"] = SQL_TEXT(mapOfStore.at(cPlaceHolder));
+    item.GetUChar("State", cPlaceHolder);
+    r["state"] = SQL_TEXT(mapOfState.at(cPlaceHolder));
+    item.GetUnsignedInt32("InterfaceIndex", uiPlaceHolder);
+    r["interface"] = SQL_TEXT(mapOfInterfaces.at(uiPlaceHolder));
+    item.GetString("IPAddress", r["ip_address"]);
+    item.GetString("InterfaceAlias", r["interface_alias"]);
+    item.GetString("LinkLayerAddress", strPlaceHolder);
+    r["link_layer_address"] =
+        SQL_TEXT(boost::replace_all_copy(strPlaceHolder, "-", ":"));
 
-    for (const auto& item : wmiResults) {
-      unsigned short usiPlaceHolder;
-      unsigned char cPlaceHolder;
-      unsigned int uiPlaceHolder;
+    results.push_back(r);
+  }
 
-      item.GetUnsignedShort("AddressFamily", usiPlaceHolder);
-      r["address_family"] = SQL_TEXT(mapOfAddressFamily.at(usiPlaceHolder));
-      item.GetUChar("Store", cPlaceHolder);
-      r["store"] = SQL_TEXT(mapOfStore.at(cPlaceHolder));
-      item.GetUChar("State", cPlaceHolder);
-      r["state"] = SQL_TEXT(mapOfState.at(cPlaceHolder));
-      item.GetUnsignedInt32("InterfaceIndex", uiPlaceHolder);
-      r["interface_index"] = INTEGER(uiPlaceHolder);
-      item.GetString("IPAddress", r["ip_address"]);
-      item.GetString("InterfaceAlias", r["interface_alias"]);
-      item.GetString("LinkLayerAddress", r["link_layer_address"]);
+  return results;
+}
 
+QueryData genCrossPlatformArpCache(QueryContext& context) {
+  Row r;
+  QueryData results;
+  QueryData winArpCache = genArpCache(context);
+
+  for (const auto& item : winArpCache) {
+    if (item.at("link_layer_address") == "" ||
+        item.at("link_layer_address") == "00:00:00:00:00:00") {
+      continue;
+    }
+    if (item.at("address_family") == "IPv4") {
+      r["address"] = item.at("ip_address");
+      r["mac"] = item.at("link_layer_address");
+      r["interface"] = item.at("interface");
+      r["permanent"] = SQL_TEXT("0");
+      if (item.at("state") == "Permanent") {
+        r["permanent"] = SQL_TEXT("1");
+      }
       results.push_back(r);
     }
   }
