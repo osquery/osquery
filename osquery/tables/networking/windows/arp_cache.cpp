1dc78f13d2e779a593532fd0500a0c52d1673c1e
Daniel Sedlacek <dansedlacek@fb.com>
2016-12-31 12:09:50 -0800

    review feedback. PLEASE GOD LET ME PASS THE CODE AUDIT!

--- osquery/tables/networking/windows/arp_cache.cpp
+++ osquery/tables/networking/windows/arp_cache.cpp
@@ -56,13 +56,14 @@ QueryData genArpCache(QueryContext& context) {
   unsigned int uiPlaceHolder;
   std::string strPlaceHolder;
 
-  for (auto& iface : interfaces) {
+  for (const auto& iface : interfaces) {
     long interfaceIndex;
+    if (iface.count("interface") > 0) {
+      safeStrtol(iface.at("interface"), 10, interfaceIndex);
+      std::string macAddress = iface.at("mac");
 
-    safeStrtol(iface["interface"], 10, interfaceIndex);
-    std::string macAddress = iface["mac"];
-    mapOfInterfaces.insert(
-        std::pair<long, std::string>(interfaceIndex, macAddress));
+      mapOfInterfaces.insert(std::make_pair(interfaceIndex, macAddress));
+    }
   }
   for (const auto& item : wmiResults) {
     item.GetUnsignedShort("AddressFamily", usiPlaceHolder);
@@ -86,23 +87,20 @@ QueryData genArpCache(QueryContext& context) {
 }
 
 QueryData genCrossPlatformArpCache(QueryContext& context) {
-  Row r;
   QueryData results;
   QueryData winArpCache = genArpCache(context);
 
   for (const auto& item : winArpCache) {
-    if (item.at("link_layer_address") == "" ||
+    if (item.at("link_layer_address").empty() ||
         item.at("link_layer_address") == "00:00:00:00:00:00") {
       continue;
     }
     if (item.at("address_family") == "IPv4") {
+      Row r;
       r["address"] = item.at("ip_address");
       r["mac"] = item.at("link_layer_address");
       r["interface"] = item.at("interface");
-      r["permanent"] = SQL_TEXT("0");
-      if (item.at("state") == "Permanent") {
-        r["permanent"] = SQL_TEXT("1");
-      }
+      r["permanent"] = SQL_TEXT(("Permanent" == item.at("state")) ? "1" : "0");
       results.push_back(r);
     }
   }
