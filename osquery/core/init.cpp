780b7f30383f6fe5e995f946847b37e023ce5a07
Teddy Reed <teddy@prosauce.org>
2017-01-19 21:07:26 -0800

    Emit only ERROR logs to osqueryd stderr

--- osquery/core/init.cpp
+++ osquery/core/init.cpp
@@ -344,6 +344,11 @@ Initializer::Initializer(int& argc, char**& argv, ToolType tool)
     }
     // Initialize the shell after setting modified defaults and parsing flags.
     initShell();
+  } else {
+    // The daemon will only output ERROR logs to stderr.
+    if (Flag::isDefault("stderrthreshold")) {
+      Flag::updateValue("stderrthreshold", "2");
+    }
   }
 
 #ifndef WIN32
