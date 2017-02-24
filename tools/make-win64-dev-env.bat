d9817c70982a564b8da3b7e16c7b234034bea680
Ravi Buddi <raviteja7@users.noreply.github.com>
2017-02-24 18:57:38 -0500

    Update the path for provision.ps1
    The powershell script, provision.ps1 is already present in the tools directory. The batch script is searching for it in the 'tools\' directory and it produces an error.

--- tools/make-win64-dev-env.bat
+++ tools/make-win64-dev-env.bat
@@ -1,5 +1,5 @@
 @echo off
-%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Bypass .\tools\provision.ps1
+%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Bypass .\provision.ps1
 IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
 %ALLUSERSPROFILE%\chocolatey\bin\refreshenv.cmd
 
