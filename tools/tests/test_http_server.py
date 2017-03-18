9715fdbd849206f1e0e71994770927944f9ed4c4
Mitchell Grenier <mitchell.wm.grenier@gmail.com>
2017-03-17 22:00:45 -0700

    Adding discovery query support to distributed queries (#3049)

--- tools/tests/test_http_server.py
+++ tools/tests/test_http_server.py
@@ -45,6 +45,17 @@ EXAMPLE_DISTRIBUTED = {
     }
 }
 
+EXAMPLE_DISTRIBUTED_DISCOVERY = {
+    "queries": {
+        "windows_info": "select * from system_info",
+        "darwin_chrome_ex": "select users.username, ce.* from users join chrome_extensions ce using (uid)",
+    },
+    "discovery": {
+        "windows_info": "select * from os_version where platform='windows'",
+        "darwin_chrome_ex": "select * from os_version where platform='darwin'"
+    }
+}
+
 EXAMPLE_DISTRIBUTED_ACCELERATE = {
     "queries": {
         "info": "select * from osquery_info",
