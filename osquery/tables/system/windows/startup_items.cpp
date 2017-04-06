f9d9bf57a2ccd8d77875446fe7860f128897c953
Ryan Heffernan <ryanheffernan@fb.com>
2017-04-06 09:23:51 -0700

    Adding more startup run keys

--- osquery/tables/system/windows/startup_items.cpp
+++ osquery/tables/system/windows/startup_items.cpp
@@ -31,7 +31,11 @@ namespace tables {
 
 const std::set<std::string> kStartupRegKeys = {
     "HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run%",
+    "HKEY_LOCAL_MACHINE\\SOFTWARE\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Run%",
+    "HKEY_LOCAL_MACHINE\\Software\\Microsoft\Windows\\CurrentVersion\\Policies\\Explorer\\Run%",
     "HKEY_USERS\\%\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run%",
+    "HKEY_USERS\\%\\SOFTWARE\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Run%",
+    "HKEY_USERS\\%\\Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer\\Run%",
 };
 const std::set<std::string> kStartupFolderDirectories = {
     "C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\%%",
