733e996599dbf51cab6785e37ecad617ec042312
Gregory Heimbuecher <gjh@fb.com>
2017-04-21 10:22:17 -0700

    Made requested changes.

--- osquery/tables/system/darwin/event_taps.mm
+++ osquery/tables/system/darwin/event_taps.mm
@@ -16,56 +16,57 @@
 
 namespace osquery {
 namespace tables {
-  
-const std::map <CGEventType, std::string> kEventMap =
-{
-  {kCGEventNull, "EventNull"},
-  {kCGEventLeftMouseDown, "LeftMouseDown"},
-  {kCGEventLeftMouseUp, "EventLeftMouseUp"},
-  {kCGEventRightMouseDown, "EventRightMouseDown"},
-  {kCGEventRightMouseUp, "EventRightMouseUp"},
-  {kCGEventMouseMoved, "EventMouseMoved"},
-  {kCGEventLeftMouseDragged, "EventLeftMouseDragged"},
-  {kCGEventKeyDown, "EventKeyDown"},
-  {kCGEventKeyUp, "EventKeyUp"},
-  {kCGEventFlagsChanged, "EventFlagsChanged"},
-  {kCGEventScrollWheel, "EventScrollWheel"},
-  {kCGEventTabletPointer, "EventTabletPointer"},
-  {kCGEventTabletPointer, "EventTabletPointer"},
-  {kCGEventOtherMouseDown, "EventOtherMouseDown"},
-  {kCGEventOtherMouseUp, "EventOtherMouseUp"},
-  {kCGEventOtherMouseDragged, "EventOtherMouseDragged"},
+
+const std::map<CGEventType, std::string> kEventMap = {
+    {kCGEventNull, "EventNull"},
+    {kCGEventLeftMouseDown, "LeftMouseDown"},
+    {kCGEventLeftMouseUp, "EventLeftMouseUp"},
+    {kCGEventRightMouseDown, "EventRightMouseDown"},
+    {kCGEventRightMouseUp, "EventRightMouseUp"},
+    {kCGEventMouseMoved, "EventMouseMoved"},
+    {kCGEventLeftMouseDragged, "EventLeftMouseDragged"},
+    {kCGEventKeyDown, "EventKeyDown"},
+    {kCGEventKeyUp, "EventKeyUp"},
+    {kCGEventFlagsChanged, "EventFlagsChanged"},
+    {kCGEventScrollWheel, "EventScrollWheel"},
+    {kCGEventTabletPointer, "EventTabletPointer"},
+    {kCGEventTabletPointer, "EventTabletPointer"},
+    {kCGEventOtherMouseDown, "EventOtherMouseDown"},
+    {kCGEventOtherMouseUp, "EventOtherMouseUp"},
+    {kCGEventOtherMouseDragged, "EventOtherMouseDragged"},
 };
 
-  QueryData genEventTaps(QueryContext &context) {
-    QueryData results;
-    uint32_t tapCount = 0;
-    CGError err;
-    err = CGGetEventTapList(NULL, NULL, &tapCount);
-    if(err != 0) {
-      return results;
-    }
-    CGEventTapInformation taps[tapCount];
-    err = CGGetEventTapList(tapCount, taps, &tapCount);
-    if(err != 0) {
-      return results;
-    }
-    for (size_t i = 0; i < tapCount; ++i) {
-      for (const auto &type: kEventMap) {
-        if ((taps[i].eventsOfInterest & CGEventMaskBit(type.first)) == 0) {
-          continue;
-        }
-        Row r;
-        r["enabled"] = std::to_string(taps[i].enabled);
-        r["event_tap_id"] = std::to_string(taps[i].eventTapID);
-        r["event_tapped"] = type.second;
-        r["process_being_tapped"] = std::to_string(taps[i].processBeingTapped);
-        r["tapping_process"] = std::to_string(taps[i].tappingProcess);
-        results.push_back(r);
-      }
-    }
+QueryData genEventTaps(QueryContext& context) {
+  QueryData results;
+  uint32_t tapCount = 0;
+  CGError err;
+  err = CGGetEventTapList(0, nullptr, &tapCount);
+  if (err != kCGErrorSuccess) {
     return results;
   }
-
- }
+  CGEventTapInformation* taps =
+      (CGEventTapInformation*)malloc(sizeof(CGEventTapInformation) * tapCount);
+  err = CGGetEventTapList(tapCount, taps, &tapCount);
+  if (err != kCGErrorSuccess) {
+    free(taps);
+    return results;
+  }
+  for (size_t i = 0; i < tapCount; ++i) {
+    for (const auto& type : kEventMap) {
+      if ((taps[i].eventsOfInterest & CGEventMaskBit(type.first)) == 0) {
+        continue;
+      }
+      Row r;
+      r["enabled"] = std::to_string(taps[i].enabled);
+      r["event_tap_id"] = std::to_string(taps[i].eventTapID);
+      r["event_tapped"] = type.second;
+      r["process_being_tapped"] = std::to_string(taps[i].processBeingTapped);
+      r["tapping_process"] = std::to_string(taps[i].tappingProcess);
+      results.push_back(r);
+    }
+  }
+  free(taps);
+  return results;
+}
+}
 }
