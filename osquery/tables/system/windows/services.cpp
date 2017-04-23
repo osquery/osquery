d35f69e9d83f374199f1bae5a33ad46d4de8676c
Ryan Heffernan <ryanheffernan@fb.com>
2017-04-22 19:26:37 -0700

    Moving Windows Services table to RAII

--- osquery/tables/system/windows/services.cpp
+++ osquery/tables/system/windows/services.cpp
@@ -1,12 +1,12 @@
 /*
-*  Copyright (c) 2014-present, Facebook, Inc.
-*  All rights reserved.
-*
-*  This source code is licensed under the BSD-style license found in the
-*  LICENSE file in the root directory of this source tree. An additional grant
-*  of patent rights can be found in the PATENTS file in the same directory.
-*
-*/
+ *  Copyright (c) 2014-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
 
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
@@ -42,86 +42,135 @@ const std::map<int, std::string> kServiceType = {
     {0x00000110, "OWN_PROCESS(Interactive)"},
     {0x00000120, "SHARE_PROCESS(Interactive)"}};
 
-bool QuerySvcInfo(const SC_HANDLE& schScManager,
-                  ENUM_SERVICE_STATUS_PROCESS& svc,
-                  Row& r) {
-  DWORD cbBufSize = 0;
+class WinSvc : private only_movable {
+ public:
+  explicit WinSvc(SC_HANDLE scmHandle, ENUM_SERVICE_STATUS_PROCESS serviceName);
+  WinSvc(WinSvc&&) = default;
+  WinSvc& operator=(WinSvc&&) = default;
+  ~WinSvc();
 
-  auto schService =
-      OpenService(schScManager, svc.lpServiceName, SERVICE_QUERY_CONFIG);
+ public:
+  Status status();
+  bool ok();
+  Row result();
 
-  if (schService == nullptr) {
-    TLOG << "OpenService failed (" << GetLastError() << ")";
-    return FALSE;
+ private:
+  SC_HANDLE scHandle_;
+  Row result_;
+  Status status_;
+};
+
+WinSvc::WinSvc(SC_HANDLE scmHandle, ENUM_SERVICE_STATUS_PROCESS svc) {
+  scHandle_ = OpenService(scmHandle, svc.lpServiceName, SERVICE_QUERY_CONFIG);
+  if (scHandle_ == nullptr) {
+    status_ = Status(GetLastError(), "Failed to open service handle");
+    return;
   }
 
-  QueryServiceConfig(schService, nullptr, 0, &cbBufSize);
-  auto lpsc = (LPQUERY_SERVICE_CONFIG)malloc(cbBufSize);
-  if (!QueryServiceConfig(schService, lpsc, cbBufSize, &cbBufSize)) {
-    TLOG << "QueryServiceConfig failed (" << GetLastError() << ")";
+  DWORD cbBufSize;
+  (void)QueryServiceConfig(scHandle_, nullptr, 0, &cbBufSize);
+  std::unique_ptr<QUERY_SERVICE_CONFIG> lpsc(
+      static_cast<LPQUERY_SERVICE_CONFIG>(malloc(cbBufSize)));
+  if (0 == QueryServiceConfig(scHandle_, lpsc.get(), cbBufSize, &cbBufSize)) {
+    status_ = Status(GetLastError(), "Failed to query service config");
+    return;
   }
 
-  QueryServiceConfig2(
-      schService, SERVICE_CONFIG_DESCRIPTION, nullptr, 0, &cbBufSize);
-  auto lpsd = (LPSERVICE_DESCRIPTION)malloc(cbBufSize);
-  if (!QueryServiceConfig2(schService,
-                           SERVICE_CONFIG_DESCRIPTION,
-                           (LPBYTE)lpsd,
-                           cbBufSize,
-                           &cbBufSize)) {
-    TLOG << "QueryServiceConfig2 failed (" << GetLastError() << ")";
+  (void)QueryServiceConfig2(
+      scHandle_, SERVICE_CONFIG_DESCRIPTION, nullptr, 0, &cbBufSize);
+  std::unique_ptr<SERVICE_DESCRIPTION> lpsd(
+      static_cast<LPSERVICE_DESCRIPTION>(malloc(cbBufSize)));
+  if (0 == QueryServiceConfig2(scHandle_,
+                               SERVICE_CONFIG_DESCRIPTION,
+                               (LPBYTE)lpsd.get(),
+                               cbBufSize,
+                               &cbBufSize)) {
+    status_ = Status(GetLastError(), "Failed to query service description");
+    return;
   }
 
-  r["name"] = SQL_TEXT(svc.lpServiceName);
-  r["display_name"] = SQL_TEXT(svc.lpDisplayName);
-  r["status"] = SQL_TEXT(kSvcStatus[svc.ServiceStatusProcess.dwCurrentState]);
-  r["pid"] = INTEGER(svc.ServiceStatusProcess.dwProcessId);
-  r["win32_exit_code"] = INTEGER(svc.ServiceStatusProcess.dwWin32ExitCode);
-  r["service_exit_code"] =
+  result_["name"] = SQL_TEXT(svc.lpServiceName);
+  result_["display_name"] = SQL_TEXT(svc.lpDisplayName);
+  result_["status"] =
+      SQL_TEXT(kSvcStatus[svc.ServiceStatusProcess.dwCurrentState]);
+  result_["pid"] = INTEGER(svc.ServiceStatusProcess.dwProcessId);
+  result_["win32_exit_code"] =
+      INTEGER(svc.ServiceStatusProcess.dwWin32ExitCode);
+  result_["service_exit_code"] =
       INTEGER(svc.ServiceStatusProcess.dwServiceSpecificExitCode);
-  r["start_type"] = SQL_TEXT(kSvcStartType[lpsc->dwStartType]);
-  r["path"] = SQL_TEXT(lpsc->lpBinaryPathName);
-  r["user_account"] = SQL_TEXT(lpsc->lpServiceStartName);
+  result_["start_type"] = SQL_TEXT(kSvcStartType[lpsc->dwStartType]);
+  result_["path"] = SQL_TEXT(lpsc->lpBinaryPathName);
+  result_["user_account"] = SQL_TEXT(lpsc->lpServiceStartName);
 
   if (lpsd->lpDescription != nullptr) {
-    r["description"] = SQL_TEXT(lpsd->lpDescription);
+    result_["description"] = SQL_TEXT(lpsd->lpDescription);
   }
 
   if (kServiceType.count(lpsc->dwServiceType) > 0) {
-    r["service_type"] = SQL_TEXT(kServiceType.at(lpsc->dwServiceType));
+    result_["service_type"] = SQL_TEXT(kServiceType.at(lpsc->dwServiceType));
   } else {
-    r["service_type"] = SQL_TEXT("UNKNOWN");
+    result_["service_type"] = SQL_TEXT("UNKNOWN");
   }
 
   QueryData regResults;
   queryKey("HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Services\\" +
-               r["name"] + "\\Parameters",
+               result_["name"] + "\\Parameters",
            regResults);
   for (const auto& aKey : regResults) {
     if (aKey.at("name") == "ServiceDll") {
-      r["module_path"] = SQL_TEXT(aKey.at("data"));
+      result_["module_path"] = SQL_TEXT(aKey.at("data"));
     }
   }
-
-  free(lpsc);
-  free(lpsd);
-  CloseServiceHandle(schService);
-  return TRUE;
 }
 
-QueryData genServices(QueryContext& context) {
-  void* buf = nullptr;
-  DWORD bytesNeeded = 0;
-  DWORD serviceCount = 0;
-  QueryData results;
+WinSvc::~WinSvc() {
+  CloseServiceHandle(scHandle_);
+}
 
-  auto schScManager = OpenSCManager(nullptr, nullptr, GENERIC_READ);
-  if (schScManager == nullptr) {
-    TLOG << "EnumServiceStatusEx failed (" << GetLastError() << ")";
-    return {};
+Row WinSvc::result() {
+  return result_;
+}
+
+Status WinSvc::status() {
+  return status_;
+}
+
+class WinSvcQuery : private only_movable {
+ public:
+  WinSvcQuery();
+  WinSvcQuery(WinSvcQuery&&) = default;
+  WinSvcQuery& operator=(WinSvcQuery&&) = default;
+  ~WinSvcQuery();
+
+ public:
+  QueryData results();
+  Status status();
+
+ private:
+  SC_HANDLE scmHandle_;
+  QueryData results_;
+  Status status_;
+};
+
+QueryData WinSvcQuery::results() {
+  return results_;
+}
+
+Status WinSvcQuery::status() {
+  return status_;
+}
+
+WinSvcQuery::WinSvcQuery() {
+  scmHandle_ = OpenSCManager(nullptr, nullptr, GENERIC_READ);
+  if (scmHandle_ == nullptr) {
+    status_ = Status(GetLastError(),
+                     "Failed to connect to Service Connection Manager");
+    return;
   }
 
-  (void)EnumServicesStatusEx(schScManager,
+  DWORD bytesNeeded = 0;
+  DWORD serviceCount = 0;
+  (void)EnumServicesStatusEx(scmHandle_,
                              SC_ENUM_PROCESS_INFO,
                              SERVICE_WIN32,
                              SERVICE_STATE_ALL,
@@ -132,31 +181,38 @@ QueryData genServices(QueryContext& context) {
                              nullptr,
                              nullptr);
 
-  buf = malloc(bytesNeeded);
-  if (EnumServicesStatusEx(schScManager,
-                           SC_ENUM_PROCESS_INFO,
-                           SERVICE_WIN32,
-                           SERVICE_STATE_ALL,
-                           (LPBYTE)buf,
-                           bytesNeeded,
-                           &bytesNeeded,
-                           &serviceCount,
-                           nullptr,
-                           nullptr)) {
-    ENUM_SERVICE_STATUS_PROCESS* services = (ENUM_SERVICE_STATUS_PROCESS*)buf;
-    for (DWORD i = 0; i < serviceCount; ++i) {
-      Row r;
-      if (QuerySvcInfo(schScManager, services[i], r)) {
-        results.push_back(r);
-      }
-    }
-  } else {
-    TLOG << "EnumServiceStatusEx failed (" << GetLastError() << ")";
+  std::unique_ptr<ENUM_SERVICE_STATUS_PROCESS[]> buf(
+      static_cast<ENUM_SERVICE_STATUS_PROCESS*>(malloc(bytesNeeded)));
+  if (0 == EnumServicesStatusEx(scmHandle_,
+                                SC_ENUM_PROCESS_INFO,
+                                SERVICE_WIN32,
+                                SERVICE_STATE_ALL,
+                                (LPBYTE)buf.get(),
+                                bytesNeeded,
+                                &bytesNeeded,
+                                &serviceCount,
+                                nullptr,
+                                nullptr)) {
+    status_ = Status(GetLastError(), "Failed to enumerate services");
+    return;
   }
+  for (size_t i = 0; i < serviceCount; i++) {
+    results_.push_back(WinSvc(scmHandle_, buf[i]).result());
+  }
+}
 
-  free(buf);
-  CloseServiceHandle(schScManager);
-  return results;
+WinSvcQuery::~WinSvcQuery() {
+  CloseServiceHandle(scmHandle_);
 }
+
+QueryData genServices(QueryContext& context) {
+  WinSvcQuery q;
+  if (q.status().ok()) {
+    return q.results();
+  } else {
+    LOG(WARNING) << q.status().getMessage();
+    return QueryData();
+  }
 }
-}
+} // namespace tables
+} // namespace osquery
