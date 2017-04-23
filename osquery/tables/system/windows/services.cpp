1b6e358d8f66a3850d724a63b84551aa6e2a6b9c
Ryan Heffernan <ryanheffernan@fb.com>
2017-04-22 20:59:16 -0700

    Adding nullptr checks and typedefs

--- osquery/tables/system/windows/services.cpp
+++ osquery/tables/system/windows/services.cpp
@@ -42,12 +42,20 @@ const std::map<int, std::string> kServiceType = {
     {0x00000110, "OWN_PROCESS(Interactive)"},
     {0x00000120, "SHARE_PROCESS(Interactive)"}};
 
+typedef std::unique_ptr<SC_HANDLE__, std::function<void(SC_HANDLE)>>
+    svc_handle_t;
+
+void closeServiceHandle(SC_HANDLE sch) {
+  if (sch != nullptr)
+    CloseServiceHandle(sch);
+}
+
 static inline Status getService(const SC_HANDLE& scmHandle,
                                 const ENUM_SERVICE_STATUS_PROCESS& svc,
                                 Row& result) {
-  std::unique_ptr<SC_HANDLE__, std::function<void(SC_HANDLE)>> svcHandle(
+  svc_handle_t svcHandle(
       OpenService(scmHandle, svc.lpServiceName, SERVICE_QUERY_CONFIG),
-      [](SC_HANDLE sch) { CloseServiceHandle(sch); });
+      closeServiceHandle);
   if (svcHandle == nullptr) {
     return Status(GetLastError(), "Failed to open service handle");
   }
@@ -56,22 +64,26 @@ static inline Status getService(const SC_HANDLE& scmHandle,
   (void)QueryServiceConfig(svcHandle.get(), nullptr, 0, &cbBufSize);
   std::unique_ptr<QUERY_SERVICE_CONFIG> lpsc(
       static_cast<LPQUERY_SERVICE_CONFIG>(malloc(cbBufSize)));
+  if (lpsc == nullptr)
+    return Status(1, "Failed to malloc service config buffer");
+
   if (0 ==
-      QueryServiceConfig(svcHandle.get(), lpsc.get(), cbBufSize, &cbBufSize)) {
+      QueryServiceConfig(svcHandle.get(), lpsc.get(), cbBufSize, &cbBufSize))
     return Status(GetLastError(), "Failed to query service config");
-  }
 
   (void)QueryServiceConfig2(
       svcHandle.get(), SERVICE_CONFIG_DESCRIPTION, nullptr, 0, &cbBufSize);
   std::unique_ptr<SERVICE_DESCRIPTION> lpsd(
       static_cast<LPSERVICE_DESCRIPTION>(malloc(cbBufSize)));
+  if (lpsd == nullptr)
+    return Status(1, "Failed to malloc service description buffer");
+
   if (0 == QueryServiceConfig2(svcHandle.get(),
                                SERVICE_CONFIG_DESCRIPTION,
                                (LPBYTE)lpsd.get(),
                                cbBufSize,
-                               &cbBufSize)) {
+                               &cbBufSize))
     return Status(GetLastError(), "Failed to query service description");
-  }
 
   result["name"] = SQL_TEXT(svc.lpServiceName);
   result["display_name"] = SQL_TEXT(svc.lpDisplayName);
@@ -104,17 +116,16 @@ static inline Status getService(const SC_HANDLE& scmHandle,
       result["module_path"] = SQL_TEXT(aKey.at("data"));
     }
   }
+
   return Status();
 }
 
 static inline Status getServices(QueryData& results) {
-  std::unique_ptr<SC_HANDLE__, std::function<void(SC_HANDLE)>> scmHandle(
-      OpenSCManager(nullptr, nullptr, GENERIC_READ),
-      [](SC_HANDLE sch) { CloseServiceHandle(sch); });
-  if (scmHandle == nullptr) {
+  svc_handle_t scmHandle(OpenSCManager(nullptr, nullptr, GENERIC_READ),
+                         closeServiceHandle);
+  if (scmHandle == nullptr)
     return Status(GetLastError(),
                   "Failed to connect to Service Connection Manager");
-  }
 
   DWORD bytesNeeded = 0;
   DWORD serviceCount = 0;
@@ -128,25 +139,28 @@ static inline Status getServices(QueryData& results) {
                              &serviceCount,
                              nullptr,
                              nullptr);
-
-  std::unique_ptr<ENUM_SERVICE_STATUS_PROCESS[]> buf(
+  std::unique_ptr<ENUM_SERVICE_STATUS_PROCESS[]> lpSvcBuf(
       static_cast<ENUM_SERVICE_STATUS_PROCESS*>(malloc(bytesNeeded)));
+  if (lpSvcBuf == nullptr)
+    return Status(1, "Failed to malloc service buffer");
+
   if (0 == EnumServicesStatusEx(scmHandle.get(),
                                 SC_ENUM_PROCESS_INFO,
                                 SERVICE_WIN32,
                                 SERVICE_STATE_ALL,
-                                (LPBYTE)buf.get(),
+                                (LPBYTE)lpSvcBuf.get(),
                                 bytesNeeded,
                                 &bytesNeeded,
                                 &serviceCount,
                                 nullptr,
-                                nullptr)) {
+                                nullptr))
     return Status(GetLastError(), "Failed to enumerate services");
-  }
+
   for (size_t i = 0; i < serviceCount; i++) {
     Row r;
-    auto s = getService(scmHandle.get(), buf[i], r);
-    if (!s.ok()) return s;
+    auto s = getService(scmHandle.get(), lpSvcBuf[i], r);
+    if (!s.ok())
+      return s;
     results.push_back(r);
   }
 
@@ -157,7 +171,9 @@ QueryData genServices(QueryContext& context) {
   QueryData results;
   auto status = getServices(results);
   if (!status.ok()) {
+    // Prefer no results to incomplete results
     LOG(WARNING) << status.getMessage();
+    results = QueryData();
   }
   return results;
 }
