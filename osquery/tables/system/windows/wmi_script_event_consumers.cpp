a01bf8bc94240b94ef58d825bd0ab89cce43ddc9
Mitchell Grenier <obelisk@fb.com>
2017-05-26 15:45:13 -0700

    Trying to apply xiangfan-ms' patch

--- osquery/tables/system/windows/wmi_script_event_consumers.cpp
+++ osquery/tables/system/windows/wmi_script_event_consumers.cpp
@@ -23,7 +23,10 @@ QueryData genScriptConsumers(QueryContext& context) {
   std::stringstream ss;
   ss << "SELECT * FROM ActiveScriptEventConsumer";
 
-  WmiRequest request(ss.str(), static_cast<BSTR>(L"ROOT\\Subscription"));
+  BSTR bstr = ::SysAllocString(L"ROOT\\Subscription");
+  WmiRequest request(ss.str(), bstr);
+  ::SysFreeString(bstr);
+
   if (request.getStatus().ok()) {
     auto& results = request.results();
     for (const auto& result : results) {
