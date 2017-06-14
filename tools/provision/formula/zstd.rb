2e056a8d7da86be98e98a6173685e04e53437fb8
Mitchell Grenier <obelisk@fb.com>
2017-06-13 19:19:36 -0700

    Add a patch to remove AVX instructions on linux

--- tools/provision/formula/zstd.rb
+++ tools/provision/formula/zstd.rb
@@ -5,22 +5,14 @@ class Zstd < Formula
   homepage "http://zstd.net/"
   url "https://github.com/facebook/zstd/archive/v1.2.0.tar.gz"
   sha256 "4a7e4593a3638276ca7f2a09dc4f38e674d8317bbea51626393ca73fc047cbfb"
-  revision 101
-
-  #bottle do
-  #  cellar :any
-  #  sha256 "bc2ff6cb7373b79d53896ded9b54fbaf225dc24621dee26dbe9d482ff592274a" => :sierra
-  #  sha256 "70cd43a12ae9a638221725bb3ca770d8080ed437c3f9f34a8789e44642e40e65" => :el_capitan
-  #  sha256 "2d0e998725164b7cee9197f5cf5d254370250528adfb1b0328b65683fd2a1ce5" => :yosemite
-  #end
-  #option "without-pzstd", "Build without parallel (de-)compression tool"
+  revision 103
 
   depends_on "cmake" => :build
 
   def install
-    ENV.append_to_cflags(" -fPIC")
+    ENV.append_to_cflags(" -fPIC -mno-avx")
 
-    system "make", "lib"
+    system "make", "lib-release"
     system "make", "install", "PREFIX=#{prefix}/"
   end
 end
