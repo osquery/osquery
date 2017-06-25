9026a314e6708e696fc963c46f6b308df9ace8f2
Teddy Reed <teddy@prosauce.org>
2017-06-25 15:41:16 -0700

    freebsd: Some refactoring to processes table

--- osquery/tables/system/freebsd/procstat.h
+++ osquery/tables/system/freebsd/procstat.h
@@ -12,6 +12,7 @@
 
 #include <sys/queue.h>
 #include <sys/user.h>
+
 #include <libprocstat.h>
 
 #include <osquery/tables.h>
@@ -19,11 +20,23 @@
 namespace osquery {
 namespace tables {
 
+/**
+ * A helper function to retrieve processes using libprocstat(3). It is the
+ * responsibility of the caller to call procstat_freeprocs() and
+ * procstat_close() when done.
+ *
+ * Returns the number of processes in the "procs" list. On failure, returns 0
+ * and sets pstat and procs to NULL.
+ *
+ * Errors are not well exposed in some of the calls in libprocstat(3). This is
+ * a bit of a bummer as libkvm(3) is much better in that respect but I would
+ * rather use libprocstat(3) as it provides a nicer abstraction.
+ */
 unsigned int getProcesses(QueryContext& context,
                           struct procstat** pstat,
                           struct kinfo_proc** procs);
 
+/// Helper function to cleanup the libprocstat(3) pointers used.
 void procstatCleanup(struct procstat* pstat, struct kinfo_proc* procs);
-
 }
 }
