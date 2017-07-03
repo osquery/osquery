99675fdbb26c035960713c293e3e62727cde560b
Teddy Reed <teddy@prosauce.org>
2017-07-02 17:18:40 -0700

    audit: Increase speed when using socket_events (#3449)

--- osquery/tables/events/linux/socket_events.cpp
+++ osquery/tables/events/linux/socket_events.cpp
@@ -28,6 +28,11 @@ FLAG(bool,
      false,
      "Allow the audit publisher to install socket-related rules");
 
+HIDDEN_FLAG(bool,
+            audit_allow_unix,
+            false,
+            "Allow socket events to collect domain sockets");
+
 // Depend on the external getUptime table method.
 namespace tables {
 extern long getUptime();
@@ -58,17 +63,17 @@ REGISTER(SocketEventSubscriber, "event_subscriber", "socket_events");
 inline std::string ip4FromSaddr(const std::string& saddr, ushort offset) {
   long result{0};
   safeStrtol(saddr.substr(offset, 8), 16, result);
-  return std::to_string((result & 0xff000000) >> 24) + "." +
-         std::to_string((result & 0x00ff0000) >> 16) + "." +
-         std::to_string((result & 0x0000ff00) >> 8) + "." +
+  return std::to_string((result & 0xff000000) >> 24) + '.' +
+         std::to_string((result & 0x00ff0000) >> 16) + '.' +
+         std::to_string((result & 0x0000ff00) >> 8) + '.' +
          std::to_string((result & 0x000000ff));
 }
 
-void parseSockAddr(const std::string& saddr, AuditFields& r) {
+bool parseSockAddr(const std::string& saddr, AuditFields& r) {
   // The protocol is not included in the audit message.
   if (saddr[0] == '0' && saddr[1] == '2') {
     // IPv4
-    r["family"] = "2";
+    r["family"] = '2';
     long result{0};
     safeStrtol(saddr.substr(4, 4), 16, result);
     r["remote_port"] = INTEGER(result);
@@ -83,16 +88,20 @@ void parseSockAddr(const std::string& saddr, AuditFields& r) {
     for (size_t i = 0; i < 8; ++i) {
       address += saddr.substr(16 + (i * 4), 4);
       if (i == 0 || i % 7 != 0) {
-        address += ":";
+        address += ':';
       }
     }
     boost::algorithm::to_lower(address);
     r["remote_address"] = std::move(address);
   } else if (saddr[0] == '0' && saddr[1] == '1' && saddr.size() > 6) {
-    // Unix domain
-    r["family"] = "1";
-    r["local_port"] = "0";
-    r["remote_port"] = "0";
+    // Unix domain socket.
+    if (!FLAGS_audit_allow_unix) {
+      return false;
+    }
+
+    r["family"] = '1';
+    r["local_port"] = '0';
+    r["remote_port"] = '0';
     off_t begin = (saddr[4] == '0' && saddr[5] == '0') ? 6 : 4;
     auto end = saddr.substr(begin).find("00");
     end = (end == std::string::npos) ? saddr.size() : end + 4;
@@ -102,11 +111,10 @@ void parseSockAddr(const std::string& saddr, AuditFields& r) {
       r["socket"] = "unknown";
     }
   } else {
-    r["family"] = "-1";
     // No idea!
-    r["local_address"] = "unknown";
-    r["remote_address"] = "unknown";
+    return false;
   }
+  return true;
 }
 
 bool SocketUpdate(size_t type, const AuditFields& fields, AuditFields& r) {
@@ -116,11 +124,13 @@ bool SocketUpdate(size_t type, const AuditFields& fields, AuditFields& r) {
       return false;
     }
 
-    r["protocol"] = "0";
-    r["local_port"] = "0";
-    r["remote_port"] = "0";
+    r["protocol"] = '0';
+    r["local_port"] = '0';
+    r["remote_port"] = '0';
     // Parse the struct and emit the row.
-    parseSockAddr(saddr, r);
+    if (!parseSockAddr(saddr, r)) {
+      return false;
+    }
     return true;
   }
 
