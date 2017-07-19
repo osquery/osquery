951b0090693e82a04ef36197903d9a846b21712a
Mitchell Grenier <mitchell.wm.grenier@gmail.com>
2017-07-18 18:38:01 -0700

    Fix a crash in interface_details on windows (#3478)

--- osquery/tables/networking/windows/interfaces.cpp
+++ osquery/tables/networking/windows/interfaces.cpp
@@ -62,6 +62,9 @@ void genInterfaceDetail(const WmiResultItem& adapter, QueryData& results) {
   WmiRequest irequest(query);
   if (irequest.getStatus().ok()) {
     auto& iresults = irequest.results();
+    if (iresults.size() == 0) {
+      return;
+    }
     iresults[0].GetLong("MTU", lPlaceHolder);
     r["mtu"] = INTEGER(lPlaceHolder);
 
