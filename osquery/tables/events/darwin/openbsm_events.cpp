ad822c9baba6f34500a3cfbcd31a1edceb585632
Mitchell Grenier <obelisk@fb.com>
2017-08-03 10:06:07 -0700

    Merging process_execution_events and process_events
    This also moved the SSH Local Logins to user_events which is now available on macOS. We might need to change the descriptions of some of these fields though.

--- osquery/tables/events/darwin/openbsm_events.cpp
+++ osquery/tables/events/darwin/openbsm_events.cpp
@@ -7,19 +7,26 @@
  *  of patent rights can be found in the PATENTS file in the same directory.
  *
  */
+#include <arpa/inet.h>
 
 #include <bsm/libbsm.h>
 
 #include <osquery/events.h>
+#include <osquery/logger.h>
 
 #include "osquery/events/darwin/openbsm.h"
 
 namespace osquery {
 
+namespace tables {
+extern long getUptime();
+}
+
 static inline void OpenBSM_AUT_SUBJECT32_EX(Row& r, const tokenstr_t& tok) {
   if (tok.id != AUT_SUBJECT32_EX) {
     return;
   }
+  r["auid"] = INTEGER(tok.tt.subj32_ex.auid);
   r["pid"] = INTEGER(tok.tt.subj32_ex.pid);
   r["uid"] = INTEGER(tok.tt.subj32_ex.ruid);
   r["gid"] = INTEGER(tok.tt.subj32_ex.rgid);
@@ -61,12 +68,8 @@ class OpenBSMSSHLoginSubscriber
                   const OpenBSMSubscriptionContextRef& sc);
 };
 
-REGISTER(OpenBSMExecVESubscriber,
-         "event_subscriber",
-         "process_execution_events");
-REGISTER(OpenBSMSSHLoginSubscriber,
-         "event_subscriber",
-         "local_ssh_login_events");
+REGISTER(OpenBSMExecVESubscriber, "event_subscriber", "process_events");
+REGISTER(OpenBSMSSHLoginSubscriber, "event_subscriber", "user_events");
 
 void OpenBSMExecVESubscriber::configure() {
   auto sc = createSubscriptionContext();
@@ -90,18 +93,20 @@ Status OpenBSMExecVESubscriber::Callback(
       r["time"] = BIGINT(tok.tt.hdr64_ex.s);
       break;
     case AUT_SUBJECT32:
+      r["auid"] = INTEGER(tok.tt.subj32.auid);
       r["pid"] = INTEGER(tok.tt.subj32.pid);
       r["euid"] = INTEGER(tok.tt.subj32.euid);
       r["egid"] = INTEGER(tok.tt.subj32.egid);
-      r["ruid"] = INTEGER(tok.tt.subj32.ruid);
-      r["rgid"] = INTEGER(tok.tt.subj32.rgid);
+      r["uid"] = INTEGER(tok.tt.subj32.ruid);
+      r["gid"] = INTEGER(tok.tt.subj32.rgid);
       break;
     case AUT_SUBJECT64:
+      r["auid"] = INTEGER(tok.tt.subj64.auid);
       r["pid"] = INTEGER(tok.tt.subj64.pid);
       r["euid"] = INTEGER(tok.tt.subj64.euid);
       r["egid"] = INTEGER(tok.tt.subj64.egid);
-      r["ruid"] = INTEGER(tok.tt.subj64.ruid);
-      r["rgid"] = INTEGER(tok.tt.subj64.rgid);
+      r["uid"] = INTEGER(tok.tt.subj64.ruid);
+      r["gid"] = INTEGER(tok.tt.subj64.rgid);
       break;
     case AUT_SUBJECT32_EX:
       OpenBSM_AUT_SUBJECT32_EX(r, tok);
@@ -115,22 +120,32 @@ Status OpenBSMExecVESubscriber::Callback(
     case AUT_EXEC_ARGS:
       for (auto i = static_cast<unsigned int>(0); i < tok.tt.execarg.count;
            ++i) {
-        r["args"] += std::string(tok.tt.execarg.text[i]) + " ";
+        r["cmdline"] += std::string(tok.tt.execarg.text[i]) + " ";
       }
+      r["cmdline_size"] = INTEGER(r["cmdline"].length());
       break;
     case AUT_PATH:
       r["path"] = std::string(tok.tt.path.path);
       break;
     case AUT_ATTR32:
-      r["file_mode"] = INTEGER(decimalIntToOctInt(tok.tt.attr32.mode));
+      r["mode"] = INTEGER(decimalIntToOctInt(tok.tt.attr32.mode));
       r["owner_uid"] = INTEGER(tok.tt.attr32.uid);
       r["owner_gid"] = INTEGER(tok.tt.attr32.gid);
       r["fsid"] = INTEGER(tok.tt.attr32.fsid);
       r["nid"] = INTEGER(tok.tt.attr32.nid);
       r["dev"] = INTEGER(tok.tt.attr32.dev);
       break;
+    case AUT_EXEC_ENV:
+      for (auto i = static_cast<unsigned int>(0); i < tok.tt.execarg.count;
+           ++i) {
+        r["env"] += std::string(tok.tt.execenv.text[i]) + " ";
+      }
+      r["env_count"] = INTEGER(tok.tt.execarg.count);
+      r["env_size"] = INTEGER(r["env"].length());
+      break;
     }
   }
+  r["uptime"] = INTEGER(tables::getUptime());
   add(r);
   return Status(0, "OK");
 }
@@ -156,11 +171,34 @@ Status OpenBSMSSHLoginSubscriber::Callback(
     case AUT_HEADER64_EX:
       r["time"] = BIGINT(tok.tt.hdr64_ex.s);
       break;
+    case AUT_SUBJECT32:
+      r["auid"] = INTEGER(tok.tt.subj32.auid);
+      break;
     case AUT_SUBJECT32_EX:
       OpenBSM_AUT_SUBJECT32_EX(r, tok);
+      char ip_str[INET6_ADDRSTRLEN];
+      if (tok.tt.subj32_ex.tid.addr == nullptr) {
+        break;
+      }
+      if (tok.tt.proc32_ex.tid.type == AU_IPv4) {
+        struct in_addr ipv4;
+        ipv4.s_addr = static_cast<in_addr_t>(*tok.tt.subj32_ex.tid.addr);
+        r["address"] =
+            std::string(inet_ntop(AF_INET, &ipv4, ip_str, INET6_ADDRSTRLEN));
+      } else {
+        struct in6_addr ipv6;
+        bcopy(tok.tt.subj32_ex.tid.addr, &ipv6, sizeof(ipv6));
+        r["address"] =
+            std::string(inet_ntop(AF_INET6, &ipv6, ip_str, INET6_ADDRSTRLEN));
+      }
+      break;
+    case AUT_TEXT:
+      r["message"] = std::string(tok.tt.text.text, tok.tt.text.len);
       break;
     }
   }
+
+  r["uptime"] = INTEGER(tables::getUptime());
   add(r);
   return Status(0);
 }
