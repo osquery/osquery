04b51fd450b699cc24f46604fc4576240b7a06e1
Mark Ignacio <mark@ignacio.io>
2017-08-15 10:33:54 -0700

    add LVM and parental contexts to block_devices and disk_encryption on Linux (#3551)

--- osquery/tables/system/linux/disk_encryption.cpp
+++ osquery/tables/system/linux/disk_encryption.cpp
@@ -22,20 +22,21 @@ extern "C" {
 namespace osquery {
 namespace tables {
 
-void genFDEStatusForBlockDevice(const std::string &name,
-                                const std::string &uuid,
-                                QueryData &results) {
+void genFDEStatusForBlockDevice(const std::string& name,
+                                const std::string& uuid,
+                                const std::string& parent_name,
+                                std::map<std::string, Row>& encrypted_rows,
+                                QueryData& results) {
   Row r;
   r["name"] = name;
   r["uuid"] = uuid;
 
-  struct crypt_device *cd = nullptr;
+  struct crypt_device* cd = nullptr;
   struct crypt_active_device cad;
   crypt_status_info ci;
   std::string type;
   std::string cipher;
   std::string cipher_mode;
-
   ci = crypt_status(cd, name.c_str());
   switch (ci) {
   case CRYPT_ACTIVE:
@@ -52,26 +53,33 @@ void genFDEStatusForBlockDevice(const std::string &name,
 
     if (crypt_init < 0) {
       VLOG(1) << "Unable to initialize crypt device for " << name;
-      crypt_free(cd);
       break;
     }
 
     type = crypt_get_type(cd);
     if (crypt_get_active_device(cd, name.c_str(), &cad) < 0) {
       VLOG(1) << "Unable to get active device for " << name;
-      crypt_free(cd);
       break;
     }
     cipher = crypt_get_cipher(cd);
     cipher_mode = crypt_get_cipher_mode(cd);
     r["type"] = type + "-" + cipher + "-" + cipher_mode;
+    encrypted_rows[name] = r;
     break;
   }
 
   default:
-    r["encrypted"] = "0";
+    if (encrypted_rows.count(parent_name)) {
+      auto parent_row = encrypted_rows[parent_name];
+      r["encrypted"] = "1";
+      r["type"] = parent_row["type"];
+    } else {
+      r["encrypted"] = "0";
+    }
   }
-
+  if (cd != nullptr) {
+    crypt_free(cd);
+  }
   results.push_back(r);
 }
 
@@ -83,11 +91,14 @@ QueryData genFDEStatus(QueryContext &context) {
     return results;
   }
 
+  std::map<std::string, Row> encrypted_rows;
   auto block_devices = SQL::selectAllFrom("block_devices");
   for (const auto &row : block_devices) {
     const auto name = (row.count("name") > 0) ? row.at("name") : "";
     const auto uuid = (row.count("uuid") > 0) ? row.at("uuid") : "";
-    genFDEStatusForBlockDevice(name, uuid, results);
+    const auto parent_name = (row.count("parent") > 0 ? row.at("parent") : "");
+    genFDEStatusForBlockDevice(
+        name, uuid, parent_name, encrypted_rows, results);
   }
   return results;
 }
