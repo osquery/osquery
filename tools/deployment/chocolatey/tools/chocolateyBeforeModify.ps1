9b15f0022e5dc194749902e8e5378620dc6aeb03
Nick Anderson <thor@fb.com>
2017-08-18 08:52:36 -0700

    deploy: adding sanity checks to deployment powershell scripts

--- tools/deployment/chocolatey/tools/chocolateyBeforeModify.ps1
+++ tools/deployment/chocolatey/tools/chocolateyBeforeModify.ps1
@@ -7,16 +7,26 @@
 . "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\\osquery_utils.ps1"
 
 $serviceName = 'osqueryd'
-$progData =  [System.Environment]::GetEnvironmentVariable('ProgramData')
+$progData = [System.Environment]::GetEnvironmentVariable('ProgramData')
 $targetFolder = Join-Path $progData "osquery"
 $daemonFolder = Join-Path $targetFolder $serviceName
+$extensionsFolder = Join-Path $targetFolder 'extensions'
 
-# Before modifying we ensure to stop the service, if it exists
-if ((Get-Service $serviceName -ErrorAction SilentlyContinue) -and (Get-Service $serviceName).Status -eq 'Running') {
+# Ensure the service is stopped and processes are not running if exists.
+if ((Get-Service $serviceName -ErrorAction SilentlyContinue) -and `
+  (Get-Service $serviceName).Status -eq 'Running') {
   Stop-Service $serviceName
+  # If we find zombie processes, ensure they're termintated
+  $proc = Get-Process | Where-Object { $_.ProcessName -eq 'osqueryd' }
+  if ($proc -ne $null) {
+    Stop-Process -Force $proc -ErrorAction SilentlyContinue
+  }
 }
 
 # Lastly, ensure that the Deny Write ACLs have been removed before modifying
 if (Test-Path $daemonFolder) {
   Set-DenyWriteAcl $daemonFolder 'Remove'
+}
+if (Test-Path $extensionsFolder) {
+  Set-DenyWriteAcl $extensionsFolder 'Remove'
 }
