57b6263c6db1a4ddd02e4f307c4fe688d5e66197
Garret Reece <GarretReece@users.noreply.github.com>
2017-08-18 10:19:46 -0500

    formatting cleanup

--- osquery/tables/system/windows/logical_drives.cpp
+++ osquery/tables/system/windows/logical_drives.cpp
@@ -1,55 +1,51 @@
-#include <osquery/tables.h>
 #include "osquery/core/windows/wmi.h"
+#include <osquery/tables.h>
 
 namespace osquery {
-	namespace tables {
-		QueryData genLogicalDrives(QueryContext &context) {
-			Row r;
-			QueryData results;
+namespace tables {
 
-			WmiRequest wmiSystemReq("select DeviceID, DriveType, FreeSpace, Size from Win32_LogicalDisk");
-			std::vector<WmiResultItem>& wmiResults = wmiSystemReq.results();
-			for (unsigned int i = 0; i < wmiResults.size(); ++i) 
-			{
-				unsigned int driveType = 0;
-				wmiResults[i].GetString("DeviceID", r["device_id"]);
-				wmiResults[i].GetUnsignedInt32("DriveType", driveType);
-				wmiResults[i].GetString("FreeSpace", r["freespace"]);
-				wmiResults[i].GetString("Size", r["size"]);
-				
-				switch (driveType)
-				{
-				case 0:
-					r["type"] = TEXT("UNKNOWN");
-					break;
+QueryData genLogicalDrives(QueryContext& context) {
+  Row r;
+  QueryData results;
 
-				case 1:
-					r["type"] = TEXT("No Root Directory");
-					break;
+  WmiRequest wmiSystemReq(
+      "select DeviceID, DriveType, FreeSpace, Size from Win32_LogicalDisk");
+  std::vector<WmiResultItem>& wmiResults = wmiSystemReq.results();
+  for (unsigned int i = 0; i < wmiResults.size(); ++i) {
+    unsigned int driveType = 0;
+    wmiResults[i].GetString("DeviceID", r["device_id"]);
+    wmiResults[i].GetUnsignedInt32("DriveType", driveType);
+    wmiResults[i].GetString("FreeSpace", r["freespace"]);
+    wmiResults[i].GetString("Size", r["size"]);
 
-				case 2:
-					r["type"] = TEXT("Removable Disk");
-					break;
+    switch (driveType) {
+    case 0:
+      r["type"] = TEXT("Unknown");
+      break;
+    case 1:
+      r["type"] = TEXT("No Root Directory");
+      break;
+    case 2:
+      r["type"] = TEXT("Removable Disk");
+      break;
+    case 3:
+      r["type"] = TEXT("Local Disk");
+      break;
+    case 4:
+      r["type"] = TEXT("Network Drive");
+      break;
+    case 5:
+      r["type"] = TEXT("Compact Disc");
+      break;
+    case 6:
+      r["type"] = TEXT("RAM Disk");
+      break;
+    }
 
-				case 3:
-					r["type"] = TEXT("Local Disk");
-					break;
-
-				case 4:
-					r["type"] = TEXT("Network Drive");
-					break;
-
-				case 5:
-					r["type"] = TEXT("Compact Disc");
-					break;
-
-				case 6:
-					r["type"] = TEXT("RAM Disk");
-					break;
-				}
-				results.push_back(r);
-			}
-			return results;
-		}
-	}
+    results.push_back(r);
+  }
+  return results;
 }
+} // namespace tables
+} // namespace osquery
+
