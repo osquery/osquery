3f59181911bbb227e793ffed714a805af7ecb477
Atyansh Jaiswal <atyansh@fb.com>
2017-08-23 17:46:10 -0700

    Can make basic GET request and get results back

--- osquery/tables/networking/posix/curl.cpp
+++ osquery/tables/networking/posix/curl.cpp
@@ -8,24 +8,54 @@
  *
  */
 
+#include <boost/network/include/http/client.hpp>
+#include <boost/numeric/conversion/cast.hpp>
+#include <chrono>
 #include <osquery/tables.h>
+
+#include <stdio.h>
+
+using namespace boost::network::http;
+using namespace std::chrono;
 
 namespace osquery {
 namespace tables {
 
-void processRequest(const std::string& request, const QueryData& results) {
+void processRequest(const std::string& request_str, QueryData& results) {
+  Row r;
+  r["url"] = request_str;
+  r["method"] = "GET";
+  r["ua"] = "osquery";
+
+  try {
+    client client_;
+    client::request request_(request_str);
+    time_point<system_clock> start = std::chrono::system_clock::now();
+    client::response response_ = client_.get(request_);
+    time_point<system_clock> end = std::chrono::system_clock::now();
+    r["response_code"] = BIGINT(static_cast<int>(status(response_)));
+    r["rtt"] = BIGINT(microseconds(end - start).count());
+
+    // This usually destroys the UI since responses are long
+    // r["result"] = static_cast<std::string>(body(response_));
+
+    r["bytes"] = BIGINT((static_cast<std::string>(body(response_))).size());
+    results.push_back(r);
+  } catch (std::exception& e) {
+    std::cerr << "Exception: " << e.what() << std::endl;
+  }
 }
 
 QueryData genCurl(QueryContext& context) {
-	QueryData results;
+  QueryData results;
 
-	auto requests = context.constraints["url"].getAll(EQUALS);
+  auto requests = context.constraints["url"].getAll(EQUALS);
 
   for (const auto& request : requests) {
     processRequest(request, results);
   }
 
-	return results;
+  return results;
 }
-}
-}
+} // namespace tables
+} // namespace osquery
