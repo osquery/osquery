1cd4ed949f9077c66ecf374bd63d2c6a0a0e6e7b
Allan Liu <allanliu55@gmail.com>
2017-09-09 13:38:01 -0400

    kafka logger: Kafka producer implemented as a Logger plugin (#3155)

--- tools/provision.sh
+++ tools/provision.sh
@@ -146,6 +146,7 @@ function platform_darwin_main() {
   brew_dependency osquery/osquery-local/linenoise-ng
   brew_dependency osquery/osquery-local/augeas
   brew_dependency osquery/osquery-local/lldpd
+  brew_dependency osquery/osquery-local/librdkafka
 
   # POSIX-shared locally-managed tools.
   brew_dependency osquery/osquery-local/zzuf
---
+++ tools/provision/formula/librdkafka.rb
@@ -0,0 +1,45 @@
+require File.expand_path("../Abstract/abstract-osquery-formula", __FILE__)
+
+class Librdkafka < AbstractOsqueryFormula
+  desc "The Apache Kafka C/C++ library"
+  homepage "https://github.com/edenhill/librdkafka"
+  url "https://github.com/edenhill/librdkafka/archive/v0.9.5.tar.gz"
+  sha256 "dd395ffca89c9591e567366f3ad2517cee76578a10d0a16a93f990c33f553179"
+
+  depends_on "openssl"
+  depends_on "pkg-config" => :build
+  depends_on "lzlib"
+  depends_on "lz4" => :optional
+
+  def install
+    args = [
+      "--disable-dependency-tracking",
+      "--prefix=#{prefix}",
+      "--disable-sasl",
+      "--disable-lz4",
+    ]
+
+    if OS.linux?
+      ENV.append "LIBS", "-lpthread -lz -lssl -lcrypto -lrt"
+    end
+
+    system "./configure", *args
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<-EOS.undent
+      #include <librdkafka/rdkafka.h>
+
+      int main (int argc, char **argv)
+      {
+        int partition = RD_KAFKA_PARTITION_UA; /* random */
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-L#{lib}", "-lrdkafka", "-lz", "-lpthread", "-o", "test"
+    system "./test"
+  end
+
+end
