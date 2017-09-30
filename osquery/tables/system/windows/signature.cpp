4cda0ab9590ab10e7e3891b358055a27eba3a9ce
Alessandro Gario <alessandro.gario@gmail.com>
2017-09-30 03:47:43 -0700

    signature table (Win): Get the original program name from the signature.

--- osquery/tables/system/windows/signature.cpp
+++ osquery/tables/system/windows/signature.cpp
@@ -32,13 +32,46 @@
 #include <osquery/tables.h>
 
 namespace osquery {
-// This is defined in osquery/core/windows/wmi.cpp
+// These are defined in osquery/core/windows/wmi.cpp
 std::wstring stringToWstring(const std::string& src);
+std::string wstringToString(const wchar_t* src);
+
+template <typename T, typename DeleterType, DeleterType deleter>
+struct CustomUniquePtr final {
+  using pointer = T;
+  using deleter_type = CustomUniquePtr<T, DeleterType, deleter>;
+  using type = std::unique_ptr<T, deleter_type>;
+
+  void operator()(pointer p) {
+    deleter(p);
+  }
+};
+
+void CertStoreDeleter(HCERTSTORE h) {
+  CertCloseStore(h, 0);
+}
+
+using unique_hcertstore =
+    CustomUniquePtr<HCERTSTORE, decltype(&CertStoreDeleter), CertStoreDeleter>;
+
+using unique_certcontext =
+    CustomUniquePtr<PCCERT_CONTEXT,
+                    decltype(&CertFreeCertificateContext),
+                    CertFreeCertificateContext>;
+
+using unique_hcryptmsg =
+    CustomUniquePtr<HCRYPTMSG, decltype(&CryptMsgClose), CryptMsgClose>;
 
 struct SignatureInformation final {
   enum class Result { Valid, Trusted, Invalid, Missing, Distrusted, Untrusted };
 
+  /// Executable path
   std::string path;
+
+  /// The original program name, when the publisher has signed the program
+  std::string original_program_name;
+
+  /// Signature verification result
   Result result;
 };
 
@@ -46,6 +79,7 @@ void generateRow(Row& row, const SignatureInformation& signature_info) {
   row.clear();
 
   row["path"] = signature_info.path;
+  row["original_program_name"] = signature_info.original_program_name;
 
   switch (signature_info.result) {
   case SignatureInformation::Result::Valid: {
@@ -86,22 +120,15 @@ void generateRow(Row& row, const SignatureInformation& signature_info) {
   }
 }
 
-Status querySignatureInformation(SignatureInformation& signature_info,
-                                 const std::string& path) {
-  std::wstring utf16_path = stringToWstring(path);
-  if (utf16_path.empty()) {
-    return Status(1, "Invalid path");
-  }
-
-  signature_info.path = path;
-
+Status verifySignature(SignatureInformation::Result& result,
+                       const std::wstring& path) {
   WINTRUST_DATA trust_provider_settings = {};
   trust_provider_settings.cbStruct = sizeof(WINTRUST_DATA);
 
   // Set the input file
   WINTRUST_FILE_INFO file_info = {};
   file_info.cbStruct = sizeof(WINTRUST_FILE_INFO);
-  file_info.pcwszFilePath = utf16_path.data();
+  file_info.pcwszFilePath = path.data();
   trust_provider_settings.pFile = &file_info;
 
   // Do revocation checking
@@ -116,18 +143,18 @@ Status querySignatureInformation(SignatureInformation& signature_info,
 
   // Do the actual verification
   GUID authenticode_policy_provider = WINTRUST_ACTION_GENERIC_VERIFY_V2;
-  Status exit_status;
 
   auto verification_status =
       WinVerifyTrust(static_cast<HWND>(INVALID_HANDLE_VALUE),
                      &authenticode_policy_provider,
                      &trust_provider_settings);
 
+  bool verification_error = false;
+
   switch (verification_status) {
   // Signatures that have been distrusted by the administrator
   case TRUST_E_EXPLICIT_DISTRUST: {
-    signature_info.result = SignatureInformation::Result::Distrusted;
-    exit_status = Status(0, "Ok");
+    result = SignatureInformation::Result::Distrusted;
     break;
   }
 
@@ -135,37 +162,32 @@ Status querySignatureInformation(SignatureInformation& signature_info,
   // or not accessible.
   case TRUST_E_NOSIGNATURE: {
     auto error_code = GetLastError();
-    if (error_code == TRUST_E_NOSIGNATURE ||
-        error_code == TRUST_E_SUBJECT_FORM_UNKNOWN ||
-        error_code == TRUST_E_PROVIDER_UNKNOWN) {
-      signature_info.result = SignatureInformation::Result::Missing;
-      exit_status = Status(0, "Ok");
-    } else {
-      exit_status =
-          Status(1, std::string("Failed to verify the file signature") + path);
+    if (error_code != TRUST_E_NOSIGNATURE &&
+        error_code != TRUST_E_SUBJECT_FORM_UNKNOWN &&
+        error_code != TRUST_E_PROVIDER_UNKNOWN) {
+      verification_error = true;
+      break;
     }
 
+    result = SignatureInformation::Result::Missing;
     break;
   }
 
   case ERROR_SUCCESS: {
-    signature_info.result = SignatureInformation::Result::Trusted;
-    exit_status = Status(0, "Ok");
+    result = SignatureInformation::Result::Trusted;
     break;
   }
 
   // Although this signature is perfectly valid, it wasn't explicitly trusted
   // by the system administrator
   case CRYPT_E_SECURITY_SETTINGS: {
-    signature_info.result = SignatureInformation::Result::Valid;
-    exit_status = Status(0, "Ok");
+    result = SignatureInformation::Result::Valid;
     break;
   }
 
   case TRUST_E_SUBJECT_NOT_TRUSTED:
   default: {
-    signature_info.result = SignatureInformation::Result::Untrusted;
-    exit_status = Status(0, "Ok");
+    result = SignatureInformation::Result::Untrusted;
     break;
   }
   }
@@ -175,7 +197,176 @@ Status querySignatureInformation(SignatureInformation& signature_info,
                  &authenticode_policy_provider,
                  &trust_provider_settings);
 
-  return exit_status;
+  if (verification_error) {
+    return Status(1, "Failed to verify the file signature");
+  }
+
+  return Status(0, "Ok");
+}
+
+Status getOriginalProgramName(SignatureInformation& signature_info,
+                              PCMSG_SIGNER_INFO signer_info) {
+  PCRYPT_ATTRIBUTE publisher_info_ptr = nullptr;
+
+  for (DWORD i = 0; i < signer_info->AuthAttrs.cAttr; i++) {
+    if (std::strcmp(SPC_SP_OPUS_INFO_OBJID,
+                    signer_info->AuthAttrs.rgAttr[i].pszObjId) == 0) {
+      publisher_info_ptr = &signer_info->AuthAttrs.rgAttr[i];
+      break;
+    }
+  }
+
+  if (publisher_info_ptr == nullptr) {
+    return Status(1, "The publisher information could not be found");
+  }
+
+  DWORD publisher_info_size;
+  if (!CryptDecodeObject(X509_ASN_ENCODING | PKCS_7_ASN_ENCODING,
+                         SPC_SP_OPUS_INFO_OBJID,
+                         publisher_info_ptr->rgValue[0].pbData,
+                         publisher_info_ptr->rgValue[0].cbData,
+                         0,
+                         nullptr,
+                         &publisher_info_size)) {
+    VLOG(1) << GetLastError();
+    return Status(1, "Failed to access the publisher information");
+  }
+
+  std::vector<std::uint8_t> publisher_info_blob_buffer;
+  publisher_info_blob_buffer.resize(static_cast<size_t>(publisher_info_size));
+  if (publisher_info_blob_buffer.size() !=
+      static_cast<size_t>(publisher_info_size)) {
+    return Status(1, "Memory allocation failure");
+  }
+
+  PSPC_SP_OPUS_INFO publisher_info_blob_ptr =
+      reinterpret_cast<PSPC_SP_OPUS_INFO>(publisher_info_blob_buffer.data());
+
+  if (!CryptDecodeObject(X509_ASN_ENCODING | PKCS_7_ASN_ENCODING,
+                         SPC_SP_OPUS_INFO_OBJID,
+                         publisher_info_ptr->rgValue[0].pbData,
+                         publisher_info_ptr->rgValue[0].cbData,
+                         0,
+                         publisher_info_ptr,
+                         &publisher_info_size)) {
+    return Status(1, "Failed to decode the publisher information");
+  }
+
+  if (publisher_info_blob_ptr->pwszProgramName != nullptr) {
+    signature_info.original_program_name =
+        wstringToString(publisher_info_blob_ptr->pwszProgramName);
+  }
+
+  return Status(0, "Ok");
+}
+
+Status getCertificateInformation(SignatureInformation& signature_info,
+                                 const std::wstring& path) {
+  unique_hcryptmsg::type message;
+  unique_hcertstore::type certificate_store;
+
+  {
+    HCERTSTORE certificate_store_handle;
+    HCRYPTMSG message_handle;
+    if (!CryptQueryObject(CERT_QUERY_OBJECT_FILE,
+                          path.data(),
+                          CERT_QUERY_CONTENT_FLAG_PKCS7_SIGNED_EMBED,
+                          CERT_QUERY_FORMAT_FLAG_BINARY,
+                          0,
+                          nullptr,
+                          nullptr,
+                          nullptr,
+                          &certificate_store_handle,
+                          &message_handle,
+                          nullptr)) {
+      return Status(1,
+                    "Failed to query the Authenticode signature information");
+    }
+
+    certificate_store.reset(certificate_store_handle);
+    message.reset(message_handle);
+  }
+
+  DWORD signer_info_size;
+  if (!CryptMsgGetParam(message.get(),
+                        CMSG_SIGNER_INFO_PARAM,
+                        0,
+                        nullptr,
+                        &signer_info_size)) {
+    return Status(1, "Failed to get the signer information size");
+  }
+
+  std::vector<std::uint8_t> signer_information;
+  signer_information.resize(static_cast<size_t>(signer_info_size));
+  if (signer_information.size() != static_cast<size_t>(signer_info_size)) {
+    return Status(1, "Memory allocation error");
+  }
+
+  PCMSG_SIGNER_INFO signer_information_ptr =
+      reinterpret_cast<PCMSG_SIGNER_INFO>(signer_information.data());
+  if (!CryptMsgGetParam(message.get(),
+                        CMSG_SIGNER_INFO_PARAM,
+                        0,
+                        signer_information_ptr,
+                        &signer_info_size)) {
+    return Status(1, "Failed to acquire the signer information");
+  }
+
+  auto status = getOriginalProgramName(signature_info, signer_information_ptr);
+  if (!status.ok()) {
+    return status;
+  }
+
+  unique_certcontext::type certificate_context;
+
+  {
+    CERT_INFO cert_search_params = {};
+    cert_search_params.Issuer = signer_information_ptr->Issuer;
+    cert_search_params.SerialNumber = signer_information_ptr->SerialNumber;
+
+    auto cert_context =
+        CertFindCertificateInStore(certificate_store.get(),
+                                   X509_ASN_ENCODING | PKCS_7_ASN_ENCODING,
+                                   0,
+                                   CERT_FIND_SUBJECT_CERT,
+                                   &cert_search_params,
+                                   nullptr);
+    if (!cert_context) {
+      return Status(1, "Lookup failed in the temporary certificate store");
+    }
+
+    certificate_context.reset(cert_context);
+  }
+
+  return Status(0, "Ok");
+}
+
+Status querySignatureInformation(SignatureInformation& signature_info,
+                                 const std::string& path) {
+  signature_info = {};
+
+  std::wstring utf16_path = stringToWstring(path);
+  if (utf16_path.empty()) {
+    return Status(1, "Invalid path");
+  }
+
+  signature_info.path = path;
+
+  auto status = verifySignature(signature_info.result, utf16_path);
+  if (!status.ok()) {
+    return status;
+  }
+
+  if (signature_info.result == SignatureInformation::Result::Missing) {
+    return Status(0, "Ok");
+  }
+
+  status = getCertificateInformation(signature_info, utf16_path);
+  if (!status.ok()) {
+    return status;
+  }
+
+  return Status(0, "Ok");
 }
 
 namespace tables {
@@ -220,6 +411,11 @@ QueryData generateQueryResults(QueryContext& context) {
 
   QueryData results;
   for (const auto& path_string : paths) {
+    if (path_string.empty()) {
+      LOG(WARNING) << "Empty path received";
+      continue;
+    }
+
     Row r;
     auto status = generateRow(r, path_string);
     if (status.ok()) {
