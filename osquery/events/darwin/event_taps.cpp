bce2ed5f0aac63c016fff9a8c3e53e60cf1d7d31
Mitchell Grenier <obelisk@fb.com>
2017-10-03 13:53:43 -0700

    Publisher and Table for Event Taps (KeyDown)

---
+++ osquery/events/darwin/event_taps.cpp
@@ -0,0 +1,103 @@
+/*
+ *  Copyright (c) 2014-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#include <ApplicationServices/ApplicationServices.h>
+
+#include <osquery/flags.h>
+
+#include "osquery/events/darwin/event_taps.h"
+
+namespace osquery {
+
+/// Flag that turns the eventing system for event taps on or off
+FLAG(bool,
+	disable_event_tapping,
+	true,
+	"Disable receiving and subscribing to events from the event taps subsystem");
+
+REGISTER(EventTappingEventPublisher, "event_publisher", "event_tapping");
+
+CGEventRef EventTappingEventPublisher::eventCallback(CGEventTapProxy proxy,
+                                                     CGEventType type,
+                                                     CGEventRef event,
+                                                     void* refcon) {
+  auto ec = createEventContext();
+  EventFactory::fire<EventTappingEventPublisher>(ec);
+  return event;
+}
+
+Status EventTappingEventPublisher::setUp() {
+  if (FLAGS_disable_event_tapping) {
+    return Status(1, "Publisher disabled via configuration");
+  }
+  return Status(0);
+}
+
+void EventTappingEventPublisher::configure() {
+  WriteLock lock(mutex_);
+  restart();
+}
+
+void EventTappingEventPublisher::tearDown() {
+  stop();
+
+  WriteLock lock(mutex_);
+  run_loop_ = nullptr;
+}
+
+void EventTappingEventPublisher::stop() {
+  WriteLock lock(mutex_);
+
+  if (run_loop_source_ != nullptr) {
+    CFRunLoopRemoveSource(
+        CFRunLoopGetCurrent(), run_loop_source_, kCFRunLoopCommonModes);
+    run_loop_source_ = nullptr;
+  }
+  if (run_loop_ != nullptr) {
+    CFRunLoopStop(run_loop_);
+  }
+}
+
+void EventTappingEventPublisher::restart() {
+  if (run_loop_ == nullptr) {
+    return;
+  }
+  stop();
+  WriteLock lock(mutex_);
+  CGEventMask eventMask = (1 << kCGEventKeyDown);
+  CFMachPortRef eventTap = CGEventTapCreate(kCGSessionEventTap,
+                                            kCGHeadInsertEventTap,
+                                            kCGEventTapOptionListenOnly,
+                                            eventMask,
+                                            eventCallback,
+                                            NULL);
+  run_loop_source_ =
+      CFMachPortCreateRunLoopSource(kCFAllocatorDefault, eventTap, 0);
+  CFRunLoopAddSource(
+      CFRunLoopGetCurrent(), run_loop_source_, kCFRunLoopCommonModes);
+  CGEventTapEnable(eventTap, true);
+}
+
+Status EventTappingEventPublisher::run() {
+  if (run_loop_ == nullptr) {
+    run_loop_ = CFRunLoopGetCurrent();
+    restart();
+  }
+
+  CFRunLoopRun();
+  return Status(0);
+}
+
+bool EventTappingEventPublisher::shouldFire(
+    const EventTappingSubscriptionContextRef& mc,
+    const EventTappingEventContextRef& ec) const {
+  return true;
+}
+}
---
+++ osquery/events/darwin/event_taps.h
@@ -0,0 +1,67 @@
+/*
+ *  Copyright (c) 2014-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#pragma once
+
+#include <osquery/events.h>
+
+namespace osquery {
+
+struct EventTappingSubscriptionContext : public SubscriptionContext {};
+struct EventTappingEventContext : public EventContext {};
+
+using EventTappingEventContextRef = std::shared_ptr<EventTappingEventContext>;
+using EventTappingSubscriptionContextRef =
+    std::shared_ptr<EventTappingSubscriptionContext>;
+
+/// This is a dispatched service that handles published EventTapping events.
+class EventTappingConsumerRunner;
+
+class EventTappingEventPublisher
+    : public EventPublisher<EventTappingSubscriptionContext, EventTappingEventContext> {
+  DECLARE_PUBLISHER("event_tapping");
+
+ public:
+  Status setUp() override;
+
+  void configure() override;
+
+  void tearDown() override;
+
+  void stop() override;
+
+  void restart();
+
+  Status run() override;
+
+  EventTappingEventPublisher() : EventPublisher() {}
+
+  virtual ~EventTappingEventPublisher() {
+    tearDown();
+  }
+
+ private:
+  /// Apply normal subscription to event matching logic.
+  bool shouldFire(const EventTappingSubscriptionContextRef& mc,
+                  const EventTappingEventContextRef& ec) const override;
+
+  static CGEventRef eventCallback(CGEventTapProxy proxy,
+                                  CGEventType type,
+                                  CGEventRef event,
+                                  void* refcon);
+
+  /// This publisher thread's runloop.
+  CFRunLoopSourceRef run_loop_source_{nullptr};
+  CFRunLoopRef run_loop_{nullptr};
+
+  /// Storage/container operations protection mutex.
+  mutable Mutex mutex_;
+};
+} // namespace osquery
