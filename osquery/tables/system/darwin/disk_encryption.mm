57cb9bf6737de383d015340242d62bce6c718485
Zachary Wasserman <zachwass2000@gmail.com>
2017-10-04 13:20:40 -0700

    Formatting

--- osquery/tables/system/darwin/disk_encryption.mm
+++ osquery/tables/system/darwin/disk_encryption.mm
@@ -8,8 +8,8 @@
  *
  */
 
-#include <Foundation/Foundation.h>
 #include <DiskArbitration/DiskArbitration.h>
+#include <Foundation/Foundation.h>
 
 #include <functional>
 
@@ -17,11 +17,11 @@
 
 #include <osquery/core.h>
 #include <osquery/logger.h>
-#include <osquery/tables.h>
 #include <osquery/sql.h>
+#include <osquery/tables.h>
 
-#include "osquery/events/darwin/iokit.h"
 #include "osquery/events/darwin/diskarbitration.h"
+#include "osquery/events/darwin/iokit.h"
 
 namespace osquery {
 namespace tables {
@@ -40,11 +40,11 @@ const std::string kAPFSFileSystem = "apfs";
 const std::string kDeviceNamePrefix = "/dev/";
 
 const std::set<std::string> kHardcodedDiskUUIDs = {
-  "EBC6C064-0000-11AA-AA11-00306543ECA",
-  "64C0C6EB-0000-11AA-AA11-00306543ECAC",
-  "C064EBC6-0000-11AA-AA11-00306543ECAC",
-  "ec1c2ad9-b618-4ed6-bd8d-50f361c27507",
-  "2fa31400-baff-4de7-ae2a-c3aa6e1fd340",
+    "EBC6C064-0000-11AA-AA11-00306543ECA",
+    "64C0C6EB-0000-11AA-AA11-00306543ECAC",
+    "C064EBC6-0000-11AA-AA11-00306543ECAC",
+    "ec1c2ad9-b618-4ed6-bd8d-50f361c27507",
+    "2fa31400-baff-4de7-ae2a-c3aa6e1fd340",
 };
 
 // kCoreStorageIsEncryptedKey is not publicly defined
@@ -159,209 +159,208 @@ void genFDEStatusForAPFS(Row& r) {
 
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Warc-performSelector-leaks"
-    // DMManager *man = [DMManager sharedManager]
-    id cls = NSClassFromString(@"DMManager");
-    if (cls == nullptr) {
-      LOG(ERROR) << "Could not load DMManager class";
-      cleanup();
-      return;
-    }
-    SEL sel = @selector(sharedManager);
-    if (![cls respondsToSelector:sel]) {
-      LOG(ERROR) << "DMManager does not respond to sharedManager selector";
-      cleanup();
-      return;
-    }
-    id man = [cls performSelector:sel];
-    if (man == nullptr) {
-      LOG(ERROR) << "[DMManager sharedManager] returned null";
-      cleanup();
-      return;
-    }
+  // DMManager *man = [DMManager sharedManager]
+  id cls = NSClassFromString(@"DMManager");
+  if (cls == nullptr) {
+    LOG(ERROR) << "Could not load DMManager class";
+    cleanup();
+    return;
+  }
+  SEL sel = @selector(sharedManager);
+  if (![cls respondsToSelector:sel]) {
+    LOG(ERROR) << "DMManager does not respond to sharedManager selector";
+    cleanup();
+    return;
+  }
+  id man = [cls performSelector:sel];
+  if (man == nullptr) {
+    LOG(ERROR) << "[DMManager sharedManager] returned null";
+    cleanup();
+    return;
+  }
 
-    // DMAPFS * apfs = [[DMAPFS alloc] initWithManager:man];
-    cls = NSClassFromString(@"DMAPFS");
-    if (cls == nullptr) {
-      LOG(ERROR) << "Could not load DMAPFS class";
-      cleanup();
-      return;
-    }
-    sel = @selector(alloc);
-    if (![cls respondsToSelector:sel]) {
-      LOG(ERROR) << "DMAPFS does not respond to alloc selector";
-      cleanup();
-      return;
-    }
-    id apfs = [cls performSelector:sel];
-    if (apfs == nullptr) {
-      LOG(ERROR) << "Could not allocate DMAPFS object";
-      cleanup();
-      return;
-    }
+  // DMAPFS * apfs = [[DMAPFS alloc] initWithManager:man];
+  cls = NSClassFromString(@"DMAPFS");
+  if (cls == nullptr) {
+    LOG(ERROR) << "Could not load DMAPFS class";
+    cleanup();
+    return;
+  }
+  sel = @selector(alloc);
+  if (![cls respondsToSelector:sel]) {
+    LOG(ERROR) << "DMAPFS does not respond to alloc selector";
+    cleanup();
+    return;
+  }
+  id apfs = [cls performSelector:sel];
+  if (apfs == nullptr) {
+    LOG(ERROR) << "Could not allocate DMAPFS object";
+    cleanup();
+    return;
+  }
 
-    sel = @selector(initWithManager:);
-    if (![apfs respondsToSelector:sel]) {
-      LOG(ERROR) << "DMAPFS does not respond to initWithManager: selector";
-      cleanup();
-      return;
-    }
-    [apfs performSelector:sel withObject:man];
+  sel = @selector(initWithManager:);
+  if (![apfs respondsToSelector:sel]) {
+    LOG(ERROR) << "DMAPFS does not respond to initWithManager: selector";
+    cleanup();
+    return;
+  }
+  [apfs performSelector:sel withObject:man];
 
-    cleanup = [&]() {
-      CFRelease((__bridge CFTypeRef)apfs);
-      CFRelease(session);
-      CFBundleUnloadExecutable(bundle);
-      CFRelease(bundle);
-    };
+  cleanup = [&]() {
+    CFRelease((__bridge CFTypeRef)apfs);
+    CFRelease(session);
+    CFBundleUnloadExecutable(bundle);
+    CFRelease(bundle);
+  };
 
 #pragma clang diagnostic pop
 
-    DADiskRef targetVol = DADiskCreateFromBSDName(nullptr, session, r["name"].c_str());
-    if (targetVol == nullptr) {
-      LOG(ERROR) << "Error creating target volume from BSD name";
-      cleanup();
-      return;
-    }
-    cleanup = [&]() {
-      CFRelease(targetVol);
-      CFRelease((__bridge CFTypeRef)apfs);
-      CFRelease(session);
-      CFBundleUnloadExecutable(bundle);
-      CFRelease(bundle);
-    };
-
-    // err = [apfs isEncryptedVolume:targetVol encrypted:&isEncrypted];
-    char* typeEncodings = nullptr;
-    asprintf(&typeEncodings,
-             "%s%s%s%s%s",
-             @encode(int), // return
-             @encode(id), // self
-             @encode(SEL), // _cmd
-             @encode(DADiskRef),
-             @encode(char*));
-    if (typeEncodings == nullptr) {
-      LOG(ERROR) << "asprintf returned nullptr for typeEncodings";
-      cleanup();
-      return;
-    }
-    NSMethodSignature* signature =
-        [NSMethodSignature signatureWithObjCTypes:typeEncodings];
-    free(typeEncodings);
-    if (signature == nullptr) {
-      LOG(ERROR) << "Got null NSMethodSignature";
-      cleanup();
-      return;
-    }
-
-    char isEncrypted = 0;
-    char *isEncryptedPtr = &isEncrypted;
-    int err = 0;
-
-    @try {
-      NSInvocation* inv =
-          [NSInvocation invocationWithMethodSignature:signature];
-      if (inv == nullptr) {
-        LOG(ERROR)
-            << "Failed to create NSInvocation for isEncryptedVolume:encrypted:";
-        cleanup();
-        return;
-      }
-      [inv setSelector:@selector(isEncryptedVolume:encrypted:)];
-      [inv setReturnValue:&err];
-      [inv setArgument:&targetVol atIndex:2];
-      [inv setArgument:&isEncryptedPtr atIndex:3];
-      [inv invokeWithTarget:apfs];
-    } @catch (NSException* exception) {
-      LOG(ERROR) << "NSInvocation threw for isEncryptedVolume:encrypted:";
-      cleanup();
-      return;
-    }
-    if (err != 0) {
-      LOG(ERROR) << "Error calling isEncryptedVolume:encrypted:";
-      cleanup();
-      return;
-    }
-
-    // err = [apfs cryptoUsersForVolume:targetVol users:&cryptoUsers];
-    typeEncodings = nullptr;
-    asprintf(&typeEncodings,
-             "%s%s%s%s%s",
-             @encode(int), // return
-             @encode(id), // self
-             @encode(SEL), // _cmd
-             @encode(DADiskRef),
-             @encode(void*));
-    if (typeEncodings == nullptr) {
-      LOG(ERROR) << "asprintf returned nullptr for typeEncodings";
-      cleanup();
-      return;
-    }
-    signature = [NSMethodSignature signatureWithObjCTypes:typeEncodings];
-    free(typeEncodings);
-    if (signature == nullptr) {
-      LOG(ERROR) << "Got null NSMethodSignature";
-      cleanup();
-      return;
-    }
-
-    NSArray* cryptoUsers = nullptr;
-    void *cryptoUsersPtr = &cryptoUsers;
-    @try {
-      NSInvocation* inv =
-          [NSInvocation invocationWithMethodSignature:signature];
-      if (inv == nullptr) {
-        LOG(ERROR)
-            << "Failed to create NSInvocation for cryptoUsersForVolume:users:";
-        cleanup();
-        return;
-      }
-      [inv setSelector:@selector(cryptoUsersForVolume:users:)];
-      [inv setReturnValue:&err];
-      [inv setArgument:&targetVol atIndex:2];
-      [inv setArgument:&cryptoUsersPtr atIndex:3];
-      [inv invokeWithTarget:apfs];
-    } @catch (NSException* exception) {
-      LOG(ERROR) << "NSInvocation threw for cryptoUsersForVolume:users:";
-      cleanup();
-      return;
-    }
-
-    // We can perform this cleanup before analyzing the results of the calls.
+  DADiskRef targetVol =
+      DADiskCreateFromBSDName(nullptr, session, r["name"].c_str());
+  if (targetVol == nullptr) {
+    LOG(ERROR) << "Error creating target volume from BSD name";
     cleanup();
+    return;
+  }
+  cleanup = [&]() {
+    CFRelease(targetVol);
+    CFRelease((__bridge CFTypeRef)apfs);
+    CFRelease(session);
+    CFBundleUnloadExecutable(bundle);
+    CFRelease(bundle);
+  };
 
-    if (err != 0) {
-      LOG(ERROR) << "Error calling cryptoUsersForVolume:users:";
+  // err = [apfs isEncryptedVolume:targetVol encrypted:&isEncrypted];
+  char* typeEncodings = nullptr;
+  asprintf(&typeEncodings,
+           "%s%s%s%s%s",
+           @encode(int), // return
+           @encode(id), // self
+           @encode(SEL), // _cmd
+           @encode(DADiskRef),
+           @encode(char*));
+  if (typeEncodings == nullptr) {
+    LOG(ERROR) << "asprintf returned nullptr for typeEncodings";
+    cleanup();
+    return;
+  }
+  NSMethodSignature* signature =
+      [NSMethodSignature signatureWithObjCTypes:typeEncodings];
+  free(typeEncodings);
+  if (signature == nullptr) {
+    LOG(ERROR) << "Got null NSMethodSignature";
+    cleanup();
+    return;
+  }
+
+  char isEncrypted = 0;
+  char* isEncryptedPtr = &isEncrypted;
+  int err = 0;
+
+  @try {
+    NSInvocation* inv = [NSInvocation invocationWithMethodSignature:signature];
+    if (inv == nullptr) {
+      LOG(ERROR)
+          << "Failed to create NSInvocation for isEncryptedVolume:encrypted:";
+      cleanup();
       return;
     }
+    [inv setSelector:@selector(isEncryptedVolume:encrypted:)];
+    [inv setReturnValue:&err];
+    [inv setArgument:&targetVol atIndex:2];
+    [inv setArgument:&isEncryptedPtr atIndex:3];
+    [inv invokeWithTarget:apfs];
+  } @catch (NSException* exception) {
+    LOG(ERROR) << "NSInvocation threw for isEncryptedVolume:encrypted:";
+    cleanup();
+    return;
+  }
+  if (err != 0) {
+    LOG(ERROR) << "Error calling isEncryptedVolume:encrypted:";
+    cleanup();
+    return;
+  }
 
-    if (cryptoUsers != nullptr) {
-      @autoreleasepool {
-        for (id arrObj in cryptoUsers) {
-          if (![arrObj isKindOfClass:[NSString class]]) {
-            continue;
-          }
+  // err = [apfs cryptoUsersForVolume:targetVol users:&cryptoUsers];
+  typeEncodings = nullptr;
+  asprintf(&typeEncodings,
+           "%s%s%s%s%s",
+           @encode(int), // return
+           @encode(id), // self
+           @encode(SEL), // _cmd
+           @encode(DADiskRef),
+           @encode(void*));
+  if (typeEncodings == nullptr) {
+    LOG(ERROR) << "asprintf returned nullptr for typeEncodings";
+    cleanup();
+    return;
+  }
+  signature = [NSMethodSignature signatureWithObjCTypes:typeEncodings];
+  free(typeEncodings);
+  if (signature == nullptr) {
+    LOG(ERROR) << "Got null NSMethodSignature";
+    cleanup();
+    return;
+  }
 
-          const char* cStr = [(NSString*)arrObj UTF8String];
-          if (cStr == nullptr) {
-            continue;
-          }
-          std::string uuidStr(cStr);
+  NSArray* cryptoUsers = nullptr;
+  void* cryptoUsersPtr = &cryptoUsers;
+  @try {
+    NSInvocation* inv = [NSInvocation invocationWithMethodSignature:signature];
+    if (inv == nullptr) {
+      LOG(ERROR)
+          << "Failed to create NSInvocation for cryptoUsersForVolume:users:";
+      cleanup();
+      return;
+    }
+    [inv setSelector:@selector(cryptoUsersForVolume:users:)];
+    [inv setReturnValue:&err];
+    [inv setArgument:&targetVol atIndex:2];
+    [inv setArgument:&cryptoUsersPtr atIndex:3];
+    [inv invokeWithTarget:apfs];
+  } @catch (NSException* exception) {
+    LOG(ERROR) << "NSInvocation threw for cryptoUsersForVolume:users:";
+    cleanup();
+    return;
+  }
 
-          if (kHardcodedDiskUUIDs.count(uuidStr) == 0) {
-            QueryData rows = SQL::selectAllFrom("users");
-            for (const auto& row : rows) {
-              if (row.count("uuid") > 0 && row.at("uuid") == uuidStr) {
-                r["user_uuid"] = row.at("uuid");
-                r["uid"] = row.count("uuid") > 0 ? row.at("uid") : "";
-              }
+  // We can perform this cleanup before analyzing the results of the calls.
+  cleanup();
+
+  if (err != 0) {
+    LOG(ERROR) << "Error calling cryptoUsersForVolume:users:";
+    return;
+  }
+
+  if (cryptoUsers != nullptr) {
+    @autoreleasepool {
+      for (id arrObj in cryptoUsers) {
+        if (![arrObj isKindOfClass:[NSString class]]) {
+          continue;
+        }
+
+        const char* cStr = [(NSString*)arrObj UTF8String];
+        if (cStr == nullptr) {
+          continue;
+        }
+        std::string uuidStr(cStr);
+
+        if (kHardcodedDiskUUIDs.count(uuidStr) == 0) {
+          QueryData rows = SQL::selectAllFrom("users");
+          for (const auto& row : rows) {
+            if (row.count("uuid") > 0 && row.at("uuid") == uuidStr) {
+              r["user_uuid"] = row.at("uuid");
+              r["uid"] = row.count("uuid") > 0 ? row.at("uid") : "";
             }
           }
         }
       }
     }
+  }
 
-    r["encrypted"] = isEncrypted ? "1" : "0";
-    r["type"] = isEncrypted ? "APFS Encryption" : "";
+  r["encrypted"] = isEncrypted ? "1" : "0";
+  r["type"] = isEncrypted ? "APFS Encryption" : "";
 }
 
 void genFDEStatusForBSDName(const std::string& bsd_name,
