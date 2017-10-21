16dc81a6e7b0318fa67ac2a626380a049102f9ad
Nick Anderson <nanderson7@gmail.com>
2017-10-21 14:11:28 -0700

    build: installing Windows 10 SDK during provisioning

--- tools/provision.ps1
+++ tools/provision.ps1
@@ -443,6 +443,7 @@ function Main {
   $out = Install-ChocoPackage '7zip.commandline'
   $out = Install-ChocoPackage 'vswhere'
   $out = Install-ChocoPackage 'cmake.portable'
+  $out = Install-ChocoPackage 'windows-sdk-10.0'
 
   # Only install python if it's not needed
   $pythonInstall = Test-PythonInstalled
