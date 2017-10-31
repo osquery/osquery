573cf5fc113cf0e38e8a63767627998c67b51a19
Nick Anderson <nanderson7@gmail.com>
2017-10-30 22:46:45 -0700

    tables: porting the ssl cert to Windows (#3894)

--- osquery/tables/networking/ssl_cert.cpp
+++ osquery/tables/networking/ssl_cert.cpp
@@ -22,24 +22,23 @@
 namespace osquery {
 namespace tables {
 
-static void fillRow(Row& r, X509* cert) {
+void fillRow(Row& r, X509* cert) {
   std::vector<char> temp(256, 0x0);
-  auto temp_data = temp.data();
-  auto temp_size = temp.size();
+  int temp_size = static_cast<int>(temp.size());
 
   // set certificate subject information
   auto subject_name = X509_get_subject_name(cert);
   auto ret = X509_NAME_get_text_by_NID(
-      subject_name, NID_commonName, temp_data, temp_size);
-  r["issued_common_name"] = ret == -1 ? "-1" : std::string(temp_data);
+      subject_name, NID_commonName, temp.data(), temp_size);
+  r["issued_common_name"] = ret == -1 ? "-1" : std::string(temp.data());
 
   ret = X509_NAME_get_text_by_NID(
-      subject_name, NID_organizationName, temp_data, temp_size);
-  r["issued_organization"] = ret == -1 ? "-1" : std::string(temp_data);
+      subject_name, NID_organizationName, temp.data(), temp_size);
+  r["issued_organization"] = ret == -1 ? "-1" : std::string(temp.data());
 
   ret = X509_NAME_get_text_by_NID(
-      subject_name, NID_organizationalUnitName, temp_data, temp_size);
-  r["issued_organization_unit"] = ret == -1 ? "-1" : std::string(temp_data);
+      subject_name, NID_organizationalUnitName, temp.data(), temp_size);
+  r["issued_organization_unit"] = ret == -1 ? "-1" : std::string(temp.data());
 
   auto serial = X509_get_serialNumber(cert);
   auto bn = ASN1_INTEGER_to_BN(serial, NULL);
@@ -51,16 +50,16 @@ static void fillRow(Row& r, X509* cert) {
   // set certificate issuer information
   auto issuer_name = X509_get_issuer_name(cert);
   ret = X509_NAME_get_text_by_NID(
-      issuer_name, NID_commonName, temp_data, temp_size);
-  r["issuer_cn"] = ret == -1 ? "-1" : std::string(temp_data);
+      issuer_name, NID_commonName, temp.data(), temp_size);
+  r["issuer_cn"] = ret == -1 ? "-1" : std::string(temp.data());
 
   ret = X509_NAME_get_text_by_NID(
-      issuer_name, NID_organizationName, temp_data, temp_size);
-  r["issuer_organization"] = ret == -1 ? "-1" : std::string(temp_data);
+      issuer_name, NID_organizationName, temp.data(), temp_size);
+  r["issuer_organization"] = ret == -1 ? "-1" : std::string(temp.data());
 
   ret = X509_NAME_get_text_by_NID(
-      issuer_name, NID_organizationalUnitName, temp_data, temp_size);
-  r["issuer_organization_unit"] = ret == -1 ? "-1" : std::string(temp_data);
+      issuer_name, NID_organizationalUnitName, temp.data(), temp_size);
+  r["issuer_organization_unit"] = ret == -1 ? "-1" : std::string(temp.data());
 
   // set period of validity
   auto valid_from = X509_get_notBefore(cert);
@@ -68,27 +67,25 @@ static void fillRow(Row& r, X509* cert) {
   auto b = BIO_new(BIO_s_mem());
 
   ASN1_TIME_print(b, valid_from);
-  ret = BIO_gets(b, temp_data, temp_size);
-  r["valid_from"] = ret == 0 ? "-1" : std::string(temp_data);
+  ret = BIO_gets(b, temp.data(), temp_size);
+  r["valid_from"] = ret == 0 ? "-1" : std::string(temp.data());
 
   ASN1_TIME_print(b, valid_to);
-  ret = BIO_gets(b, temp_data, temp_size);
-  r["valid_to"] = ret == 0 ? "-1" : std::string(temp_data);
+  ret = BIO_gets(b, temp.data(), temp_size);
+  r["valid_to"] = ret == 0 ? "-1" : std::string(temp.data());
   BIO_free(b);
 
   // set sha 256 & 1 fingerprint
   std::vector<unsigned char> temp_c(256, 0x0);
-  auto temp_c_data = temp_c.data();
-  auto temp_c_size = temp_c.size();
+  unsigned int len = 0;
   auto digest = const_cast<EVP_MD*>(EVP_sha256());
-  unsigned len = temp_c_size;
-  ret = X509_digest(cert, digest, temp_c_data, &len);
+  ret = X509_digest(cert, digest, temp_c.data(), &len);
 
   std::stringstream ss;
   if (ret != 0) {
-    for (unsigned i = 0; i < len; i++) {
+    for (size_t i = 0; i < len; i++) {
       ss << std::uppercase << std::hex << std::setfill('0') << std::setw(2)
-         << static_cast<unsigned>(temp_c_data[i]) << ':';
+         << static_cast<unsigned>(temp_c[i]) << ':';
     }
 
     r["sha256_fingerprint"] = ss.str().substr(0, ss.str().size() - 1);
@@ -98,86 +95,79 @@ static void fillRow(Row& r, X509* cert) {
 
   temp_c.clear();
   digest = const_cast<EVP_MD*>(EVP_sha1());
-  ret = X509_digest(cert, digest, temp_c_data, &len);
+  ret = X509_digest(cert, digest, temp_c.data(), &len);
 
   if (ret != 0) {
     ss.str("");
-    for (unsigned i = 0; i < len; i++) {
+    for (size_t i = 0; i < len; i++) {
       ss << std::uppercase << std::hex << std::setfill('0') << std::setw(2)
-         << static_cast<unsigned>(temp_c_data[i]) << ':';
+         << static_cast<unsigned>(temp_c[i]) << ':';
     }
 
     r["sha1_fingerprint"] = ss.str().substr(0, ss.str().size() - 1);
   } else {
     r["sha1_fingerprint"] = "-1";
   }
-
-  return;
 }
 
-static void getSslCert(const std::string domain, QueryData& results) {
-  std::string port = ":443";
-  SSL* ssl = NULL;
-
+Status getSslCert(const std::string domain, QueryData& results) {
+  SSL* ssl = nullptr;
   SSL_library_init();
 
   const auto method = SSLv23_method();
   if (!method) {
-    VLOG(1) << "Failed to create SSL_method object";
-    return;
+    return Status(1, "Failed to create SSL_method object");
   }
 
   auto ctx = SSL_CTX_new(method);
   if (!ctx) {
-    VLOG(1) << "Failed to create SSL_CTX object";
-    return;
+    return Status(1, "Failed to create SSL_CTX object");
   }
 
   auto server = BIO_new_ssl_connect(ctx);
   if (!server) {
-    VLOG(1) << "Failed to create SSL Bio object";
-    return;
+    return Status(1, "Failed to create SSL Bio object");
   }
 
+  std::string port = ":443";
   auto ret = BIO_set_conn_hostname(server, (domain + port).c_str());
   if (ret != 1) {
-    VLOG(1) << "Failed to set SSL domain and port " << domain << port << ": "
-            << std::to_string(ret);
-    return;
+    return Status(1,
+                  "Failed to set SSL domain and port " + domain + port + ": " +
+                      std::to_string(ret));
   }
 
   BIO_get_ssl(server, &ssl);
   if (!ssl) {
-    VLOG(1) << "Failed to retrieve SSL object from Bio";
-    return;
+    return Status(1, "Failed to retrieve SSL object from Bio");
   }
 
   ret = SSL_set_tlsext_host_name(ssl, domain.c_str());
   if (ret != 1) {
-    VLOG(1) << "Failed to set SSL server name " << domain << ": "
-            << std::to_string(ret);
-    return;
+    return Status(
+        1,
+        "Failed to set SSL server name " + domain + ": " + std::to_string(ret));
   }
 
   ret = BIO_do_connect(server);
   if (ret != 1) {
-    VLOG(1) << "Failed to establish SSL connection with " << domain << ": "
-            << std::to_string(ret);
-    return;
+    return Status(1,
+                  "Failed to establish SSL connection with " + domain + ": " +
+                      std::to_string(ret));
   }
 
   ret = BIO_do_handshake(server);
   if (ret != 1) {
-    VLOG(1) << "Failed to complete SSL/TLS handshake with " << domain << ": "
-            << std::to_string(ret);
-    return;
+    return Status(1,
+                  "Failed to complete SSL/TLS handshake with " + domain + ": " +
+                      std::to_string(ret));
   }
 
   auto cert = SSL_get_peer_certificate(ssl);
   if (!cert) {
-    VLOG(1) << "No certificate from " << domain;
-    return;
+    return Status(1, "No certificate from " + domain);
   }
+
   Row r;
   r["domain"] = domain;
   fillRow(r, cert);
@@ -192,7 +182,7 @@ static void getSslCert(const std::string domain, QueryData& results) {
   if (ctx) {
     SSL_CTX_free(ctx);
   }
-  return;
+  return Status();
 }
 
 QueryData genSslCert(QueryContext& context) {
@@ -200,7 +190,10 @@ QueryData genSslCert(QueryContext& context) {
   auto domains = context.constraints["domain"].getAll(EQUALS);
 
   for (const auto& domain : domains) {
-    getSslCert(domain, results);
+    auto s = getSslCert(domain, results);
+    if (!s.ok()) {
+      LOG(INFO) << s.getMessage();
+    }
   }
 
   return results;
