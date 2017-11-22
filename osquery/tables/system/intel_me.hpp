1ee1d390f1959e2e530cb9b95e199ff28c6ea2d0
Nick Anderson <thor@fb.com>
2017-11-22 11:33:20 -0800

    tables: adding support for intel_me virtual table on Windows

---
+++ osquery/tables/system/intel_me.hpp
@@ -0,0 +1,52 @@
+/*
+ *  Copyright (c) 2014-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#pragma once
+
+#include <vector>
+
+namespace osquery {
+namespace tables {
+
+#define INTEL_ME_LINUX_IOCTL 0xC0104801
+#define INTEL_ME_WINDOWS_IOCTL 0x8000E004
+
+/**
+ * @brief Intel HECI GUID for the update application.
+ *
+ * This GUID can be used on every OS over the HECI protocol to inspect the
+ * FW version.
+ */
+const std::vector<uint8_t> kMEIUpdateGUID{
+    232, 205, 157, 48, 177, 204, 98, 64, 143, 120, 96, 1, 21, 163, 67, 39,
+};
+
+struct mei_response {
+  uint32_t maxlen;
+  uint8_t version;
+};
+
+struct mei_version {
+  uint32_t important_details[7];
+  uint16_t major;
+  uint16_t minor;
+  uint16_t hotfix;
+  uint16_t build;
+  uint16_t r_major;
+  uint16_t r_minor;
+  uint16_t r_hotfix;
+  uint16_t r_build;
+  uint16_t codes[6];
+#ifdef WIN32
+  uint16_t codes2[506];
+#endif
+};
+}
+}
---
+++ osquery/tables/system/windows/intel_me.cpp
@@ -0,0 +1,169 @@
+/*
+ *  Copyright (c) 2014-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+#define WIN32_LEAN_AND_MEAN
+// clang-format off
+#include <Windows.h>
+#include <SetupAPI.h>
+// clang-format on
+#include <initguid.h>
+#include <tchar.h>
+
+#include <osquery/core.h>
+#include <osquery/tables.h>
+#include <osquery/logger.h>
+
+#include "osquery/core/conversions.h"
+#include "osquery/tables/system/intel_me.hpp"
+
+namespace osquery {
+namespace tables {
+
+DEFINE_GUID(HECI_INTERFACE_GUID,
+            0xE2D1FF34,
+            0x3458,
+            0x49A9,
+            0x88,
+            0xDA,
+            0x8E,
+            0x69,
+            0x15,
+            0xCE,
+            0x9B,
+            0xE5);
+
+void getHECIDriverVersion(QueryData& results) {
+  // Find all devices that have our interface handle for device info
+  auto guid = const_cast<LPGUID>(&HECI_INTERFACE_GUID);
+  HDEVINFO deviceInfo = SetupDiGetClassDevs(
+      guid, nullptr, nullptr, DIGCF_PRESENT | DIGCF_DEVICEINTERFACE);
+
+  if (deviceInfo == INVALID_HANDLE_VALUE) {
+    VLOG(1) << "Failed to open MEI device with " << GetLastError();
+    return;
+  }
+
+  SP_DEVICE_INTERFACE_DATA interfaceData;
+  interfaceData.cbSize = sizeof(SP_DEVICE_INTERFACE_DATA);
+
+  // This device path is the output from the driver search logic here.
+  std::string devPath;
+
+  unsigned long index = 0;
+  auto ret = SetupDiEnumDeviceInterfaces(
+      deviceInfo, nullptr, guidPtr, index, &interfaceData);
+  while (ret == TRUE) {
+    unsigned long detailSize = 0;
+    if (!SetupDiGetDeviceInterfaceDetail(
+            deviceInfo, &interfaceData, nullptr, 0, &detailSize, nullptr)) {
+      if (GetLastError() != ERROR_INSUFFICIENT_BUFFER) {
+        continue;
+      }
+    }
+
+    auto deviceDetails = static_cast<PSP_DEVICE_INTERFACE_DETAIL_DATA>(
+        LocalAlloc(LPTR, detailSize));
+
+    if (deviceDetails != nullptr) {
+      deviceDetails->cbSize = sizeof(SP_DEVICE_INTERFACE_DETAIL_DATA);
+      if (SetupDiGetDeviceInterfaceDetail(deviceInfo,
+                                          &interfaceData,
+                                          deviceDetails,
+                                          detailSize,
+                                          nullptr,
+                                          nullptr)) {
+        devPath = deviceDetails->DevicePath;
+      }
+      LocalFree(deviceDetails);
+    }
+
+    ret = SetupDiEnumDeviceInterfaces(
+        deviceInfo, nullptr, guidPtr, ++index, &interfaceData);
+  }
+
+  SetupDiDestroyDeviceInfoList(deviceInfo);
+
+  // HECI driver was not found
+  if (devPath.empty()) {
+    VLOG(1) << "Could not locate HECI driver";
+    return;
+  }
+
+  HANDLE driver = CreateFile(devPath.c_str(),
+                             GENERIC_READ | GENERIC_WRITE,
+                             FILE_SHARE_READ | FILE_SHARE_WRITE,
+                             nullptr,
+                             OPEN_EXISTING,
+                             0,
+                             nullptr);
+  if (driver == INVALID_HANDLE_VALUE) {
+    VLOG(1) << "Failed to open handle to device path with " << GetLastError();
+    return;
+  }
+
+  unsigned long ioctlConnectClient = INTEL_ME_WINDOWS_IOCTL;
+
+  // Response data from driver open.
+  struct mei_response response;
+  // Response from FirmwareUpdate HECI GUID.
+  struct mei_version version;
+
+  ret = DeviceIoControl(driver,
+                        ioctlConnectClient,
+                        static_cast<LPVOID>(kMEIUpdateGUID.data()),
+                        kMEIUpdateGUID.size(),
+                        &response,
+                        sizeof(response),
+                        nullptr,
+                        nullptr);
+
+  if (ret == 0) {
+    VLOG(1) << "Device IOCTL call failed with " << GetLastError();
+    CloseHandle(driver);
+    return;
+  }
+
+  if (response.maxlen < sizeof(version)) {
+    VLOG(1) << "HECI driver does not support FirmwareUpdate responses";
+    CloseHandle(driver);
+    return;
+  }
+
+  unsigned char fw_cmd[4] = {0};
+  ret = WriteFile(
+      driver, static_cast<void*>(fw_cmd), sizeof(fw_cmd), nullptr, nullptr);
+  if (ret != TRUE) {
+    VLOG(1) << "HECI driver write failed with " << GetLastError();
+  }
+
+  memset(&version, 0, sizeof(version));
+  ret = ReadFile(driver, &version, sizeof(version), nullptr, nullptr);
+  if (ret != TRUE) {
+    VLOG(1) << "HECI driver read failed with " << GetLastError();
+  }
+
+  CloseHandle(driver);
+
+  Row r;
+  r["version"] = std::to_string(version.major) + '.' +
+                 std::to_string(version.minor) + '.' +
+                 std::to_string(version.hotfix) + '.' +
+                 std::to_string(version.build);
+
+  results.push_back(r);
+}
+
+QueryData getIntelMEInfo(QueryContext& context) {
+  QueryData results;
+  getHECIDriverVersion(results);
+  return results;
+}
+}
+}
