25780992f93204447cdf5b475acdb0d7a8038366
Diptanu Choudhury <diptanuc@gmail.com>
2017-12-06 14:02:40 -0800

    Only pre-declaring constants in header

--- osquery/tables/system/linux/deb.cpp
+++ osquery/tables/system/linux/deb.cpp
@@ -13,6 +13,26 @@
 namespace osquery {
 namespace tables {
 
+const std::vector<struct fieldinfo> fieldinfos = {
+    {FIELD("Package"), f_name, w_name, 0},
+    {FIELD("Installed-Size"),
+     f_charfield,
+     w_charfield,
+     PKGIFPOFF(installedsize)},
+    {FIELD("Architecture"), f_architecture, w_architecture, 0},
+    {FIELD("Source"), f_charfield, w_charfield, PKGIFPOFF(source)},
+    {FIELD("Version"), f_version, w_version, PKGIFPOFF(version)},
+    {FIELD("Revision"), f_revision, w_revision, 0}};
+
+const std::map<std::string, std::string> kFieldMappings = {
+    {"Package", "name"},
+    {"Version", "version"},
+    {"Installed-Size", "size"},
+    {"Architecture", "arch"},
+    {"Source", "source"},
+    {"Revision", "revision"}};
+
+
 int pkg_sorter(const void* a, const void* b) {
   const struct pkginfo* pa = *(const struct pkginfo**)a;
   const struct pkginfo* pb = *(const struct pkginfo**)b;
--- osquery/tables/system/linux/deb.h
+++ osquery/tables/system/linux/deb.h
@@ -46,24 +46,9 @@ void w_revision(struct varbuf* vb,
 * add an fwritefunction for Revision. Additional fields can be taken
 * as needed.
 */
-const std::vector<struct fieldinfo> fieldinfos = {
-    {FIELD("Package"), f_name, w_name, 0},
-    {FIELD("Installed-Size"),
-     f_charfield,
-     w_charfield,
-     PKGIFPOFF(installedsize)},
-    {FIELD("Architecture"), f_architecture, w_architecture, 0},
-    {FIELD("Source"), f_charfield, w_charfield, PKGIFPOFF(source)},
-    {FIELD("Version"), f_version, w_version, PKGIFPOFF(version)},
-    {FIELD("Revision"), f_revision, w_revision, 0}};
+extern const std::vector<struct fieldinfo> fieldinfos;
 
-const std::map<std::string, std::string> kFieldMappings = {
-    {"Package", "name"},
-    {"Version", "version"},
-    {"Installed-Size", "size"},
-    {"Architecture", "arch"},
-    {"Source", "source"},
-    {"Revision", "revision"}};
+extern const std::map<std::string, std::string> kFieldMappings;
 #endif
 
 /**
