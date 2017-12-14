cdadfd7df67cc020f9ab7ca55c5620d12ebaae15
Teddy Reed <teddy@prosauce.org>
2017-12-14 14:23:46 -0800

    deps: Include script to auto-build bottles (#3992)

--- tools/provision/darwin.sh
+++ tools/provision/darwin.sh
@@ -7,7 +7,24 @@
 #  LICENSE file in the root directory of this source tree. An additional grant
 #  of patent rights can be found in the PATENTS file in the same directory.
 
+DARWIN_SETUP="\
+if [[ ! -f /var/.osquery_build ]]; then \
+touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress; \
+PROD=\$(softwareupdate -l | grep \"\\*.*Command Line\" | \
+  head -n 1 | awk -F\"*\" '{print \$2}' | sed -e 's/^ *//' | tr -d '\n' \
+); \
+softwareupdate -i \"\$PROD\" --verbose; \
+sudo touch /var/.osquery_build; \
+fi; \
+"
+
+function vagrant_setup() {
+  sudo bash -c "$DARWIN_SETUP"
+}
+
 function distro_main() {
   GEM=`which gem`
   do_sudo $GEM install --no-ri --no-rdoc fpm
 }
+
+[ "$0" = "$BASH_SOURCE" ] && vagrant_setup || true
