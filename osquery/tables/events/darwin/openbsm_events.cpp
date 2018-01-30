1a63bd96bc30b661a1ec61ad5148e9fea91bb328
Filipe Manco <fmanco@fb.com>
2018-01-30 11:13:25 +0000

    darwin: Add parent info to proc events [3/3]
    Capture fork events to construct a map of parent child relationships. On
    a exec look at the map to get the parent information. Use exit events to
    garbage collect the map.

--- osquery/tables/events/darwin/openbsm_events.cpp
+++ osquery/tables/events/darwin/openbsm_events.cpp
@@ -15,6 +15,8 @@
 #include <osquery/events.h>
 #include <osquery/logger.h>
 
+#include <unordered_map>
+
 #include "osquery/events/darwin/openbsm.h"
 
 namespace osquery {
@@ -60,6 +62,10 @@ class OpenBSMProcEvSubscriber : public EventSubscriber<OpenBSMEventPublisher> {
 
  private:
   Status handleExec(const OpenBSMEventContextRef& ec);
+  Status handleFork(const OpenBSMEventContextRef& ec);
+  Status handleExit(const OpenBSMEventContextRef& ec);
+
+  std::unordered_map<uint32_t, uint32_t> ppid_map;
 };
 
 class OpenBSMSSHLoginSubscriber
@@ -80,7 +86,14 @@ REGISTER(OpenBSMSSHLoginSubscriber, "event_subscriber", "user_events");
 
 void OpenBSMProcEvSubscriber::configure() {
   std::vector<size_t> event_ids{
-      AUE_EXECVE, AUE_POSIX_SPAWN,
+      AUE_EXECVE,
+      AUE_POSIX_SPAWN,
+      AUE_FORK,
+      AUE_VFORK,
+      AUE_FORK1,
+      AUE_DARWIN_RFORK,
+      AUE_RFORK,
+      AUE_EXIT,
   };
   for (const auto& evid : event_ids) {
     auto sc = createSubscriptionContext();
@@ -91,11 +104,29 @@ void OpenBSMProcEvSubscriber::configure() {
 
 Status OpenBSMProcEvSubscriber::Callback(
     const OpenBSMEventContextRef& ec, const OpenBSMSubscriptionContextRef& sc) {
-  return handleExec(ec);
+  switch (sc->event_id) {
+  case AUE_EXECVE:
+  case AUE_POSIX_SPAWN:
+    return handleExec(ec);
+
+  case AUE_FORK:
+  case AUE_VFORK:
+  case AUE_FORK1:
+  case AUE_DARWIN_RFORK:
+  case AUE_RFORK:
+    return handleFork(ec);
+
+  case AUE_EXIT:
+    return handleExit(ec);
+  }
+
+  return Status(1, "Unexpected event");
 }
 
 Status OpenBSMProcEvSubscriber::handleExec(const OpenBSMEventContextRef& ec) {
   Row r;
+  uint32_t pid = 0;
+
   for (const auto& tok : ec->tokens) {
     switch (tok.id) {
     case AUT_HEADER32:
@@ -115,6 +146,7 @@ Status OpenBSMProcEvSubscriber::handleExec(const OpenBSMEventContextRef& ec) {
       r["egid"] = INTEGER(tok.tt.subj32.egid);
       r["uid"] = INTEGER(tok.tt.subj32.ruid);
       r["gid"] = INTEGER(tok.tt.subj32.rgid);
+      pid = tok.tt.subj32.pid;
       break;
     case AUT_SUBJECT64:
       r["auid"] = INTEGER(tok.tt.subj64.auid);
@@ -123,6 +155,7 @@ Status OpenBSMProcEvSubscriber::handleExec(const OpenBSMEventContextRef& ec) {
       r["egid"] = INTEGER(tok.tt.subj64.egid);
       r["uid"] = INTEGER(tok.tt.subj64.ruid);
       r["gid"] = INTEGER(tok.tt.subj64.rgid);
+      pid = tok.tt.subj32.pid;
       break;
     case AUT_SUBJECT32_EX:
       OpenBSM_AUT_SUBJECT32_EX(r, tok);
@@ -163,8 +196,104 @@ Status OpenBSMProcEvSubscriber::handleExec(const OpenBSMEventContextRef& ec) {
     }
   }
   r["uptime"] = INTEGER(tables::getUptime());
+
+  auto ppid = ppid_map.find(pid);
+  if (ppid != ppid_map.end()) {
+    r["parent"] = INTEGER(ppid->second);
+  }
+  /* If mapping doesn't exist no fork was captured. Not fatal. Ignoring. */
+
   add(r);
   return Status(0, "OK");
+}
+
+Status OpenBSMProcEvSubscriber::handleFork(const OpenBSMEventContextRef& ec) {
+  uint32_t ppid = 0; // Parent PID
+  uint32_t cpid = 0; // Child PID
+
+  for (const auto& tok : ec->tokens) {
+    switch (tok.id) {
+    /* The parent is the process issuing the syscall. Its PID is given on the
+     * subject token.
+     */
+    case AUT_SUBJECT32:
+      ppid = tok.tt.subj32.pid;
+      break;
+    case AUT_SUBJECT64:
+      ppid = tok.tt.subj64.pid;
+      break;
+    case AUT_SUBJECT32_EX:
+      ppid = tok.tt.subj32_ex.pid;
+      break;
+    case AUT_SUBJECT64_EX:
+      ppid = tok.tt.subj64_ex.pid;
+      break;
+
+    /* Child PID is given as the first argument here. This could also be
+     * extract from the return token as the child PID should be the return
+     * value of the fork syscall given to the parent.
+     */
+    case AUT_ARG32:
+      if (tok.tt.arg32.no == 0) {
+        cpid = tok.tt.arg32.val;
+      }
+      break;
+    case AUT_ARG64:
+      if (tok.tt.arg64.no == 0) {
+        cpid = tok.tt.arg64.val;
+      }
+      break;
+
+    /* Check whether fork succeeded. Upon failure stop processing as no child
+     * is created.
+     */
+    case AUT_RETURN32:
+      if (tok.tt.ret32.status != 0) {
+        return Status(0);
+      }
+      break;
+    case AUT_RETURN64:
+      if (tok.tt.ret64.err != 0) {
+        return Status(0);
+      }
+    }
+  }
+
+  /* If we succeeded to to capture both parent and child PIDs add them to the
+   * mapping. This information should always be available, if not the event is
+   * malformed.
+   */
+  if (ppid != 0 && cpid != 0) {
+    ppid_map[cpid] = ppid;
+    return Status(0);
+  } else {
+    return Status(1, "Malformed event");
+  }
+}
+
+Status OpenBSMProcEvSubscriber::handleExit(const OpenBSMEventContextRef& ec) {
+  /* When the process exits the mapping is no longer relevant as there won't be
+   * more syscalls issued by it. The PID most come in the subject header.
+   */
+  for (const auto& tok : ec->tokens) {
+    switch (tok.id) {
+    case AUT_SUBJECT32:
+      ppid_map.erase(tok.tt.subj32.pid);
+      return Status(0);
+    case AUT_SUBJECT64:
+      ppid_map.erase(tok.tt.subj64.pid);
+      return Status(0);
+    case AUT_SUBJECT32_EX:
+      ppid_map.erase(tok.tt.subj32_ex.pid);
+      return Status(0);
+    case AUT_SUBJECT64_EX:
+      ppid_map.erase(tok.tt.subj64_ex.pid);
+      return Status(0);
+    }
+  }
+
+  /* If the PID wasn't found this event is malformed. */
+  return Status(1, "Malformed event");
 }
 
 void OpenBSMSSHLoginSubscriber::configure() {
