d922bab390f86db2bc10b0e01b584a9e8c734aee
Alessandro Gario <alessandro.gario@gmail.com>
2018-04-27 13:31:55 +0200

    CMake: Add support for extension bundling

--- CMake/CMakeLibs.cmake
+++ CMake/CMakeLibs.cmake
@@ -271,15 +271,162 @@ macro(ADD_OSQUERY_EXTENSION TARGET)
   set_target_properties(${TARGET} PROPERTIES OUTPUT_NAME "${TARGET}.ext")
 endmacro(ADD_OSQUERY_EXTENSION)
 
+function(add_osquery_extension_ex class_name extension_type extension_name ${ARGN})
+  # Make sure the extension type is valid
+  if(NOT "${extension_type}" STREQUAL "config" AND NOT "${extension_type}" STREQUAL "table")
+    message(FATAL_ERROR "Invalid extension type specified")
+  endif()
+
+  # Update the initializer list; this will be added to the main.cpp file of the extension
+  # group
+  set_property(GLOBAL APPEND_STRING
+    PROPERTY OSQUERY_EXTENSION_GROUP_INITIALIZERS
+    "REGISTER_EXTERNAL(${class_name}, \"${extension_type}\", \"${extension_name}\");\n"
+  )
+
+  # Loop through each argument
+  foreach(argument ${ARGN})
+    if("${argument}" STREQUAL "SOURCES" OR "${argument}" STREQUAL "LIBRARIES" OR
+      "${argument}" STREQUAL "INCLUDEDIRS" OR "${argument}" STREQUAL "MAININCLUDES")
+
+      set(current_scope "${argument}")
+      continue()
+    endif()
+
+    if("${current_scope}" STREQUAL "SOURCES")
+      if(NOT IS_ABSOLUTE "${argument}")
+        set(argument "${CMAKE_CURRENT_SOURCE_DIR}/${argument}")
+      endif()
+
+      list(APPEND source_file_list "${argument}")
+
+    elseif("${current_scope}" STREQUAL "INCLUDEDIRS")
+      if(NOT IS_ABSOLUTE "${argument}")
+        set(argument "${CMAKE_CURRENT_SOURCE_DIR}/${argument}")
+      endif()
+
+      list(APPEND include_folder_list "${argument}")
+
+    elseif("${current_scope}" STREQUAL "LIBRARIES")
+      list(APPEND library_list "${argument}")
+    elseif("${current_scope}" STREQUAL "MAININCLUDES")
+      list(APPEND main_include_list "${argument}")
+    else()
+      message(FATAL_ERROR "Invalid scope")
+    endif()
+  endforeach()
+
+  # Validate the arguments
+  if("${source_file_list}" STREQUAL "")
+    message(FATAL_ERROR "Source files are missing")
+  endif()
+
+  if("${main_include_list}" STREQUAL "")
+    message(FATAL_ERROR "The main include list is missing")
+  endif()
+
+  # Update the global properties
+  set_property(GLOBAL APPEND
+    PROPERTY OSQUERY_EXTENSION_GROUP_SOURCES
+    ${source_file_list}
+  )
+
+  set_property(GLOBAL APPEND
+    PROPERTY OSQUERY_EXTENSION_GROUP_MAIN_INCLUDES
+    ${main_include_list}
+  )
+
+  if(NOT "${library_list}" STREQUAL "")
+    set_property(GLOBAL APPEND
+      PROPERTY OSQUERY_EXTENSION_GROUP_LIBRARIES
+      ${library_list}
+    )
+  endif()
+
+  if(NOT "${include_folder_list}" STREQUAL "")
+    set_property(GLOBAL APPEND
+      PROPERTY OSQUERY_EXTENSION_GROUP_INCLUDE_FOLDERS
+      ${include_folder_list}
+    )
+  endif()
+endfunction()
+
+# This function takes the global properties saved by add_osquery_extension_ex and generates
+# a single extenion executable containing all the user code
+function(generate_osquery_extension_group)
+  get_property(extension_source_files GLOBAL PROPERTY OSQUERY_EXTENSION_GROUP_SOURCES)
+  if("${extension_source_files}" STREQUAL "")
+    return()
+  endif()
+
+  # Allow the user to customize the extension name and version using
+  # environment variables
+  if(DEFINED ENV{OSQUERY_EXTENSION_GROUP_NAME})
+    set(OSQUERY_EXTENSION_GROUP_NAME $ENV{OSQUERY_EXTENSION_GROUP_NAME})
+  else()
+    set(OSQUERY_EXTENSION_GROUP_NAME "osquery_extension_group")
+  endif()
+
+  if(DEFINED ENV{OSQUERY_EXTENSION_GROUP_VERSION})
+    set(OSQUERY_EXTENSION_GROUP_VERSION $ENV{OSQUERY_EXTENSION_GROUP_VERSION})
+  else()
+    set(OSQUERY_EXTENSION_GROUP_VERSION "1.0")
+  endif()
+
+  # Build the include list; this contains the files required to declare
+  # the classes used in the REGISTER_EXTERNAL directives
+  #
+  # Note: The variables in uppercase are used by the template
+  get_property(main_include_list GLOBAL PROPERTY OSQUERY_EXTENSION_GROUP_MAIN_INCLUDES)
+  foreach(include_file ${main_include_list})
+    set(OSQUERY_EXTENSION_GROUP_INCLUDES "${OSQUERY_EXTENSION_GROUP_INCLUDES}\n#include <${include_file}>")
+  endforeach()
+
+  # We need to generate the main.cpp file, containing all the required
+  # REGISTER_EXTERNAL directives
+  get_property(OSQUERY_EXTENSION_GROUP_INITIALIZERS GLOBAL PROPERTY OSQUERY_EXTENSION_GROUP_INITIALIZERS)
+  configure_file(
+    "${CMAKE_CURRENT_SOURCE_DIR}/osquery_main.cpp.template"
+    "${CMAKE_CURRENT_BINARY_DIR}/osquery_main.cpp"
+  )
+
+  # Generate the extension target
+  add_executable("${OSQUERY_EXTENSION_GROUP_NAME}"
+    "${CMAKE_CURRENT_BINARY_DIR}/osquery_main.cpp"
+    ${extension_source_files}
+  )
+
+  set_target_properties("${OSQUERY_EXTENSION_GROUP_NAME}" PROPERTIES SUFFIX "")
+  set_target_properties("${OSQUERY_EXTENSION_GROUP_NAME}" PROPERTIES
+    OUTPUT_NAME "${OSQUERY_EXTENSION_GROUP_NAME}.ext"
+  )
+
+  get_property(library_list GLOBAL PROPERTY OSQUERY_EXTENSION_GROUP_LIBRARIES)
+  if(NOT "${library_list}" STREQUAL "")
+    target_link_libraries("${OSQUERY_EXTENSION_GROUP_NAME}" PRIVATE
+      ${library_list}
+    )
+  endif()
+
+  get_property(include_folder_list GLOBAL PROPERTY OSQUERY_EXTENSION_GROUP_INCLUDE_FOLDERS)
+  if(NOT "${include_folder_list}" STREQUAL "")
+    target_include_directories("${OSQUERY_EXTENSION_GROUP_NAME}" PRIVATE
+      ${include_folder_list}
+    )
+  endif()
+
+  TARGET_OSQUERY_LINK_WHOLE("${OSQUERY_EXTENSION_GROUP_NAME}" libosquery)
+endfunction()
+
 # Helper to abstract OS/Compiler whole linking.
 macro(TARGET_OSQUERY_LINK_WHOLE TARGET OSQUERY_LIB)
   if(WINDOWS)
-      target_link_libraries(${TARGET} "${OS_WHOLELINK_PRE}$<TARGET_FILE_NAME:${OSQUERY_LIB}>")
-      target_link_libraries(${TARGET} ${OSQUERY_LIB})
+      target_link_libraries(${TARGET} PRIVATE "${OS_WHOLELINK_PRE}$<TARGET_FILE_NAME:${OSQUERY_LIB}>")
+      target_link_libraries(${TARGET} PRIVATE ${OSQUERY_LIB})
   else()
-      target_link_libraries(${TARGET} "${OS_WHOLELINK_PRE}")
-      target_link_libraries(${TARGET} ${OSQUERY_LIB})
-      target_link_libraries(${TARGET} "${OS_WHOLELINK_POST}")
+      target_link_libraries(${TARGET} PRIVATE "${OS_WHOLELINK_PRE}")
+      target_link_libraries(${TARGET} PRIVATE ${OSQUERY_LIB})
+      target_link_libraries(${TARGET} PRIVATE "${OS_WHOLELINK_POST}")
   endif()
 endmacro(TARGET_OSQUERY_LINK_WHOLE)
 
