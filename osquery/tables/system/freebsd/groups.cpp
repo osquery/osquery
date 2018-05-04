e8e950a73263236e71100e1485538c83166e82e5
Max Kareta <mkareta@fb.com>
2018-05-04 13:23:53 +0100

    tuned code

--- osquery/tables/system/freebsd/groups.cpp
+++ osquery/tables/system/freebsd/groups.cpp
@@ -1,1 +1,48 @@
-../linux/groups.cpp
+/**
+ *  Copyright (c) 2014-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under both the Apache 2.0 license (found in the
+ *  LICENSE file in the root directory of this source tree) and the GPLv2 (found
+ *  in the COPYING file in the root directory of this source tree).
+ *  You may select, at your option, one of the above-listed licenses.
+ */
+
+#include <mutex>
+#include <set>
+
+#include <grp.h>
+
+#include <osquery/core.h>
+#include <osquery/tables.h>
+
+namespace osquery {
+namespace tables {
+
+Mutex grpEnumerationMutex;
+
+QueryData genGroups(QueryContext& context) {
+  QueryData results;
+  struct group* grp = nullptr;
+  std::set<long> groups_in;
+
+  WriteLock lock(grpEnumerationMutex);
+  setgrent();
+  while ((grp = getgrent()) != nullptr) {
+    if (std::find(groups_in.begin(), groups_in.end(), grp->gr_gid) ==
+        groups_in.end()) {
+      Row r;
+      r["gid"] = INTEGER(grp->gr_gid);
+      r["gid_signed"] = INTEGER((int32_t)grp->gr_gid);
+      r["groupname"] = TEXT(grp->gr_name);
+      results.push_back(r);
+      groups_in.insert(grp->gr_gid);
+    }
+  }
+  endgrent();
+  groups_in.clear();
+
+  return results;
+}
+} // namespace tables
+} // namespace osquery
--- osquery/tables/system/freebsd/users.cpp
+++ osquery/tables/system/freebsd/users.cpp
@@ -1,1 +1,92 @@
-../linux/users.cpp
+/**
+ *  Copyright (c) 2014-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under both the Apache 2.0 license (found in the
+ *  LICENSE file in the root directory of this source tree) and the GPLv2 (found
+ *  in the COPYING file in the root directory of this source tree).
+ *  You may select, at your option, one of the above-listed licenses.
+ */
+
+#include <pwd.h>
+
+#include <mutex>
+
+#include <osquery/core.h>
+#include <osquery/tables.h>
+
+#include "osquery/core/conversions.h"
+
+namespace osquery {
+namespace tables {
+
+Mutex pwdEnumerationMutex;
+
+void genUser(const struct passwd* pwd, QueryData& results) {
+  Row r;
+  r["uid"] = BIGINT(pwd->pw_uid);
+  r["gid"] = BIGINT(pwd->pw_gid);
+  r["uid_signed"] = BIGINT((int32_t)pwd->pw_uid);
+  r["gid_signed"] = BIGINT((int32_t)pwd->pw_gid);
+
+  if (pwd->pw_name != nullptr) {
+    r["username"] = TEXT(pwd->pw_name);
+  }
+
+  if (pwd->pw_gecos != nullptr) {
+    r["description"] = TEXT(pwd->pw_gecos);
+  }
+
+  if (pwd->pw_dir != nullptr) {
+    r["directory"] = TEXT(pwd->pw_dir);
+  }
+
+  if (pwd->pw_shell != nullptr) {
+    r["shell"] = TEXT(pwd->pw_shell);
+  }
+  results.push_back(r);
+}
+
+QueryData genUsers(QueryContext& context) {
+  QueryData results;
+
+  struct passwd* pwd = nullptr;
+  if (context.constraints["uid"].exists(EQUALS)) {
+    auto uids = context.constraints["uid"].getAll(EQUALS);
+    for (const auto& uid : uids) {
+      long auid{0};
+      if (safeStrtol(uid, 10, auid)) {
+        WriteLock lock(pwdEnumerationMutex);
+        pwd = getpwuid(auid);
+        if (pwd != nullptr) {
+          genUser(pwd, results);
+        }
+      }
+    }
+  } else if (context.constraints["username"].exists(EQUALS)) {
+    auto usernames = context.constraints["username"].getAll(EQUALS);
+    for (const auto& username : usernames) {
+      WriteLock lock(pwdEnumerationMutex);
+      pwd = getpwnam(username.c_str());
+      if (pwd != nullptr) {
+        genUser(pwd, results);
+      }
+    }
+  } else {
+    WriteLock lock(pwdEnumerationMutex);
+    pwd = getpwent();
+    while (pwd != nullptr) {
+      genUser(pwd, results);
+      pwd = getpwent();
+    }
+    endpwent();
+  }
+
+  return results;
+}
+
+QueryData genStartupItems(QueryContext& context) {
+  return QueryData();
+}
+} // namespace tables
+} // namespace osquery
