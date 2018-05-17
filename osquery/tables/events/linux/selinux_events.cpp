c0b7d4d41f7784087da78afc80b2d14539db4400
Alessandro Gario <alessandro.gario@gmail.com>
2018-05-17 12:01:32 +0200

    Code review changes

--- osquery/tables/events/linux/selinux_events.cpp
+++ osquery/tables/events/linux/selinux_events.cpp
@@ -30,34 +30,6 @@ namespace tables {
 extern long getUptime();
 }
 
-namespace {
-// clang-format off
-// This map must contain exactly the same elements that
-// SELinuxEventSubscriber::GetEventSet() returns!
-const std::map<int, std::string> record_type_to_label = {
-  {AUDIT_USER_AVC, "USER_AVC"},
-  {AUDIT_AVC, "AVC"},
-  {AUDIT_SELINUX_ERR, "SELINUX_ERR"},
-  {AUDIT_AVC_PATH, "AVC_PATH"},
-  {AUDIT_MAC_POLICY_LOAD, "MAC_POLICY_LOAD"},
-  {AUDIT_MAC_STATUS, "MAC_STATUS"},
-  {AUDIT_MAC_CONFIG_CHANGE, "MAC_CONFIG_CHANGE"},
-  {AUDIT_MAC_UNLBL_ALLOW, "MAC_UNLBL_ALLOW"},
-  {AUDIT_MAC_CIPSOV4_ADD, "MAC_CIPSOV4_ADD"},
-  {AUDIT_MAC_CIPSOV4_DEL, "MAC_CIPSOV4_DEL"},
-  {AUDIT_MAC_MAP_ADD, "MAC_MAP_ADD"},
-  {AUDIT_MAC_MAP_DEL, "MAC_MAP_DEL"},
-  {AUDIT_MAC_IPSEC_ADDSA, "MAC_IPSEC_ADDSA"},
-  {AUDIT_MAC_IPSEC_DELSA, "MAC_IPSEC_DELSA"},
-  {AUDIT_MAC_IPSEC_ADDSPD, "MAC_IPSEC_ADDSPD"},
-  {AUDIT_MAC_IPSEC_DELSPD, "MAC_IPSEC_DELSPD"},
-  {AUDIT_MAC_IPSEC_EVENT, "MAC_IPSEC_EVENT"},
-  {AUDIT_MAC_UNLBL_STCADD, "MAC_UNLBL_STCADD"},
-  {AUDIT_MAC_UNLBL_STCDEL, "MAC_UNLBL_STCDEL"}
-};
-// clang-format on
-} // namespace
-
 Status SELinuxEventSubscriber::init() {
   if (!FLAGS_audit_allow_selinux_events) {
     return Status(1, "Subscriber disabled via configuration");
@@ -96,8 +68,14 @@ Status SELinuxEventSubscriber::ProcessEvents(
     for (const auto& record : event.record_list) {
       Row r;
 
-      r["type"] = record_type_to_label.at(record.type);
-      r["msg"] = record.raw_data;
+      auto record_pretty_name_it = kSELinuxRecordLabels.find(record.type);
+      if (record_pretty_name_it == kSELinuxRecordLabels.end()) {
+        r["type"] = std::to_string(record.type);
+      } else {
+        r["type"] = record_pretty_name_it->second;
+      }
+
+      r["message"] = record.raw_data;
       r["uptime"] = std::to_string(tables::getUptime());
       emitted_row_list.push_back(r);
     }
@@ -107,34 +85,6 @@ Status SELinuxEventSubscriber::ProcessEvents(
 }
 
 const std::set<int>& SELinuxEventSubscriber::GetEventSet() noexcept {
-  // Documented events that could not be found in the headers:
-  // - USER_SELINUX_ERR
-  // - USER_MAC_POLICY_LOAD
-  // - USER_ROLE_CHANGE
-  // - USER_LABEL_EXPORT
-  static const std::set<int> selinux_event_list = {
-      // This is outside the documented numeric range (1400-1499)
-      AUDIT_USER_AVC,
-
-      AUDIT_AVC,
-      AUDIT_SELINUX_ERR,
-      AUDIT_AVC_PATH,
-      AUDIT_MAC_POLICY_LOAD,
-      AUDIT_MAC_STATUS,
-      AUDIT_MAC_CONFIG_CHANGE,
-      AUDIT_MAC_UNLBL_ALLOW,
-      AUDIT_MAC_CIPSOV4_ADD,
-      AUDIT_MAC_CIPSOV4_DEL,
-      AUDIT_MAC_MAP_ADD,
-      AUDIT_MAC_MAP_DEL,
-      AUDIT_MAC_IPSEC_ADDSA,
-      AUDIT_MAC_IPSEC_DELSA,
-      AUDIT_MAC_IPSEC_ADDSPD,
-      AUDIT_MAC_IPSEC_DELSPD,
-      AUDIT_MAC_IPSEC_EVENT,
-      AUDIT_MAC_UNLBL_STCADD,
-      AUDIT_MAC_UNLBL_STCDEL};
-
-  return selinux_event_list;
+  return kSELinuxEventList;
 }
 } // namespace osquery
