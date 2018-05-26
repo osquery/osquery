882bd42253d6b13d6a94c4248b67ff31c8f8b499
micheal-o <micheal.okutubo@gmail.com>
2018-05-26 14:30:39 -0400

    fix for BSD and macos

--- osquery/tables/system/posix/ulimit_info.cpp
+++ osquery/tables/system/posix/ulimit_info.cpp
@@ -17,26 +17,63 @@
 #include <osquery/logger.h>
 #include <osquery/tables.h>
 
+#ifndef RLIMIT_LOCKS
+#define RLIMIT_LOCKS -1
+#endif
+#ifndef RLIMIT_SIGPENDING
+#define RLIMIT_SIGPENDING -1
+#endif
+#ifndef RLIMIT_MSGQUEUE
+#define RLIMIT_MSGQUEUE -1
+#endif
+#ifndef RLIMIT_NICE
+#define RLIMIT_NICE -1
+#endif
+#ifndef RLIMIT_RTPRIO
+#define RLIMIT_RTPRIO -1
+#endif
+#ifndef RLIMIT_SBSIZE
+#define RLIMIT_SBSIZE -1
+#endif
+#ifndef RLIMIT_NPTS
+#define RLIMIT_NPTS -1
+#endif
+#ifndef RLIMIT_SWAP
+#define RLIMIT_SWAP -1
+#endif
+#ifndef RLIMIT_KQUEUES
+#define RLIMIT_KQUEUES -1
+#endif
+#ifndef RLIMIT_UMTXP
+#define RLIMIT_UMTXP -1
+#endif
+
 namespace osquery {
 namespace tables {
 
 void getLimit(QueryData& results) {
-  std::map<std::string, int> resource_map;
-  resource_map["cpu"] = RLIMIT_CPU;
-  resource_map["fsize"] = RLIMIT_FSIZE;
-  resource_map["data"] = RLIMIT_DATA;
-  resource_map["stack"] = RLIMIT_STACK;
-  resource_map["core"] = RLIMIT_CORE;
-  resource_map["rss"] = RLIMIT_RSS;
-  resource_map["memlock"] = RLIMIT_MEMLOCK;
-  resource_map["nproc"] = RLIMIT_NPROC;
-  resource_map["nofile"] = RLIMIT_NOFILE;
-  resource_map["as"] = RLIMIT_AS;
-  resource_map["locks"] = RLIMIT_LOCKS;
-  resource_map["sigpending"] = RLIMIT_SIGPENDING;
-  resource_map["msgqueue"] = RLIMIT_MSGQUEUE;
-  resource_map["nice"] = RLIMIT_NICE;
-  resource_map["rtprio"] = RLIMIT_RTPRIO;
+  std::map<std::string, int> resource_map = {
+      {"cpu", RLIMIT_CPU},
+      {"fsize", RLIMIT_FSIZE},
+      {"data", RLIMIT_DATA},
+      {"stack", RLIMIT_STACK},
+      {"core", RLIMIT_CORE},
+      {"nofile", RLIMIT_NOFILE},
+      {"as", RLIMIT_AS},
+      {"rss", RLIMIT_RSS},
+      {"memlock", RLIMIT_MEMLOCK},
+      {"nproc", RLIMIT_NPROC},
+      {"locks", RLIMIT_LOCKS},
+      {"sigpending", RLIMIT_SIGPENDING},
+      {"msgqueue", RLIMIT_MSGQUEUE},
+      {"nice", RLIMIT_NICE},
+      {"rtprio", RLIMIT_RTPRIO},
+      {"sbsize", RLIMIT_SBSIZE},
+      {"npts", RLIMIT_NPTS},
+      {"swap", RLIMIT_SWAP},
+      {"kqueues", RLIMIT_KQUEUES},
+      {"umtxp", RLIMIT_UMTXP},
+  };
 
   for (const auto& it : resource_map) {
     struct rlimit rlp;
