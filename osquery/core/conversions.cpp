56cb16118fbeb75ad69b7a4d19591b404017cb5b
George Guliashvili <gguli@fb.com>
2018-06-08 10:56:21 +0100

    unused split(string,string,size_t) split contained bug, it was joining on every delimiter, which would result to unusual outcome. However, test could not detect this problem as delim.size() was 1. It turned out, that this split is not used anywhere, so completely removing it from the project.

--- osquery/core/conversions.cpp
+++ osquery/core/conversions.cpp
@@ -384,28 +384,6 @@ std::vector<std::string> split(const std::string& s, const std::string& delim) {
   return elems;
 }
 
-std::vector<std::string> split(const std::string& s,
-                               const std::string& delim,
-                               size_t occurences) {
-  // Split the string normally with the required delimiter.
-  auto content = split(s, delim);
-  // While the result split exceeds the number of requested occurrences, join.
-  std::vector<std::string> accumulator;
-  std::vector<std::string> elems;
-  for (size_t i = 0; i < content.size(); i++) {
-    if (i < occurences) {
-      elems.push_back(content.at(i));
-    } else {
-      accumulator.push_back(content.at(i));
-    }
-  }
-  // Join the optional accumulator.
-  if (accumulator.size() > 0) {
-    elems.push_back(join(accumulator, delim));
-  }
-  return elems;
-}
-
 std::string join(const std::vector<std::string>& s, const std::string& tok) {
   return boost::algorithm::join(s, tok);
 }
