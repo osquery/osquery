70153644706f83bde9eb19a3d856dd75514ea0bc
Filipe Manco <fmanco@fb.com>
2018-06-13 17:35:32 +0100

    Removed runner.waitForShutdown()

--- osquery/main/main.cpp
+++ osquery/main/main.cpp
@@ -141,7 +141,6 @@ int startShell(osquery::Initializer& runner, int argc, char* argv[]) {
     retcode = osquery::launchIntoShell(argc, argv);
     // Finally shutdown.
     runner.requestShutdown();
-    runner.waitForShutdown();
   } else {
     retcode = profile(argc, argv);
   }
