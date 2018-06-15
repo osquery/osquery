da88ea28b3be037e2257c0080ee5f11a20403bc8
Filipe Manco <fmanco@fb.com>
2018-06-15 21:39:30 +0100

    Implement make format_check
    The tool tools/formatting/format-check.py is a wrapper around
    tools/formatting/git-clang-format.py which (1) caculates the merge-base
    between your current branch and master or the passed branch (2) runs
    git-clang-format.py with the result and (3) analyses the output and
    exits with an appropriate code, printing an message and if necessary the
    diff representing the changes required.
    
    This will make code auditing performance code format checks only for the
    commits on your feature branch, so they won't be affected in case master
    advanced.

---
+++ tools/formatting/format-check.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+
+import argparse
+import os
+import subprocess
+import sys
+
+
+def check(base_commit):
+    try:
+        p = subprocess.Popen(
+                [
+                    "python",
+                    os.path.join("tools", "formatting", "git-clang-format.py"),
+                    "--style=file",
+                    "--diff",
+                    "--commit",
+                    base_commit,
+                    ],
+                stdout=subprocess.PIPE, stderr=subprocess.PIPE
+                )
+        out, err = p.communicate()
+    except OSError as e:
+        print("{}\n\n{}".format("Failed to call git-clang-format.py", str(e)))
+        return False
+
+    if p.returncode:
+        print("{}\n\n{}\n{}".format(
+            "Failed to run formatting script", out, err
+            ))
+        return False
+    elif out.startswith("no modified files to format"):
+        print("No code changes found!")
+        return True
+    elif out.startswith("clang-format did not modify any files"):
+        print("Code passes formatting tests!")
+        return True
+    else:
+        print("{}\n\n{}".format(
+            "Modifications failed code formatting requirements", out
+            ))
+        return False
+
+def get_base_commit(base_branch):
+    try:
+        return subprocess.check_output(
+                ["git", "merge-base", "HEAD", base_branch]
+                ).strip()
+    except OSError as e:
+        print("{}\n\n{}".format("Failed to execut git", str(e)))
+    except subprocess.CalledProcessError as e:
+        print("{}\n\n{}".format("Failed to determine merge-base", str(e)))
+
+    return None
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Check code changes formatting.")
+    parser.add_argument(
+            "base_branch",
+            metavar="base_branch",
+            type=str,
+            nargs="?",
+            default="master",
+            help="The base branch to compare to.",
+            )
+
+    args = parser.parse_args()
+
+    base_commit = get_base_commit(args.base_branch)
+
+    return check(base_commit) if base_commit is not None else False
+
+if __name__ == "__main__":
+    sys.exit(not main())
