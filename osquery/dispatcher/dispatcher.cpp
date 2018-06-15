984df16fbfa1c1dbb47e30cb9769087c3d1deea8
George Guliashvili <gguli@fb.com>
2018-06-15 15:22:24 +0100

    typo

--- osquery/dispatcher/dispatcher.cpp
+++ osquery/dispatcher/dispatcher.cpp
@@ -143,7 +143,7 @@ void Dispatcher::joinServices() {
   DLOG(INFO) << "Thread: " << std::this_thread::get_id()
              << " requesting a join";
 
-  // Stops when service_threads_ is empty. Befor stopping and releasing of the lock, empties services_ .
+  // Stops when service_threads_ is empty. Before stopping and releasing of the lock, empties services_ .
   while (1) {
     InternalThreadRef thread = nullptr;
     {
