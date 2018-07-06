eb019189dbf0d2b94db7681dce36258cba844527
akindyakov <akindyakov@fb.com>
2018-07-06 16:35:42 +0100

    Added pre-aggregation buffer to numeric monitoring points dispatcher registry

--- osquery/numeric_monitoring/numeric_monitoring.cpp
+++ osquery/numeric_monitoring/numeric_monitoring.cpp
@@ -12,10 +12,12 @@
 
 #include <boost/format.hpp>
 
+#include <osquery/dispatcher.h>
 #include <osquery/flags.h>
 #include <osquery/logger.h>
 #include <osquery/numeric_monitoring.h>
 #include <osquery/numeric_monitoring/plugin_interface.h>
+#include <osquery/numeric_monitoring/pre_aggregation_cache.h>
 #include <osquery/registry_factory.h>
 
 namespace osquery {
@@ -28,9 +30,20 @@ FLAG(string,
      numeric_monitoring_plugins,
      "filesystem",
      "Coma separated numeric monitoring plugins names");
+FLAG(uint64,
+     numeric_monitoring_pre_aggregation_time,
+     60,
+     "Time period in _seconds_ for numeric monitoring pre-aggreagation buffer. "
+     "During this period of time monitoring points are going to be "
+     "pre-aggregated and accumulated in buffer. At the end of this period "
+     "aggregated points will be flushed to [numeric_monitoring_plugins]. "
+     "0 means work without buffer at all. "
+     "For the most of monitoring data some aggregation will be applied on the "
+     "user side. It means for such monitoring particular points means not "
+     "much. And to reduce a disk usage and a network traffic some "
+     "pre-aggregation is applied on osquery side.");
 
 namespace {
-
 using monitoring::PreAggregationType;
 
 template <typename KeyType, typename ValueType>
@@ -89,26 +102,89 @@ tryTo<monitoring::PreAggregationType>(const std::string& from) {
 
 namespace monitoring {
 
+namespace {
+
+class PreAggregationBuffer final : public InternalRunnable {
+ public:
+  explicit PreAggregationBuffer(const std::string& name,
+                                std::chrono::milliseconds time_quantum)
+      : InternalRunnable(name), time_quantum_(std::move(time_quantum)) {}
+
+  static std::shared_ptr<PreAggregationBuffer> create() {
+    auto self = std::make_shared<PreAggregationBuffer>(
+        "numeric_monitoring_pre_aggregation_buffer",
+        std::chrono::seconds(FLAGS_numeric_monitoring_pre_aggregation_time));
+    Dispatcher::addService(self);
+    return self;
+  }
+
+  void start() override {
+    while (!interrupted()) {
+      auto points = takeCachedPoints();
+      for (const auto& pt : points) {
+        unbufferedRecord(
+            pt.path_, pt.value_, pt.pre_aggregation_type_, pt.time_point_);
+      }
+      pauseMilli(time_quantum_);
+    }
+  }
+
+  void record(const std::string& path,
+              const ValueType& value,
+              const PreAggregationType& pre_aggregation,
+              const TimePoint& time_point) {
+    if (time_quantum_ == std::chrono::milliseconds::zero()) {
+      unbufferedRecord(path, value, pre_aggregation, time_point);
+    } else {
+      std::lock_guard<std::mutex> lock(cache_lock_);
+      cache_.addPoint(Point(path, value, pre_aggregation, time_point));
+    }
+  }
+
+ private:
+  std::vector<Point> takeCachedPoints() {
+    std::lock_guard<std::mutex> lock(cache_lock_);
+    auto points = cache_.takePoints();
+    return points;
+  }
+
+  void unbufferedRecord(const std::string& path,
+                        const ValueType& value,
+                        const PreAggregationType& pre_aggregation,
+                        const TimePoint& time_point) {
+    auto status = Registry::call(
+        registryName(),
+        FLAGS_numeric_monitoring_plugins,
+        {
+            {recordKeys().path, path},
+            {recordKeys().value, std::to_string(value)},
+            {recordKeys().pre_aggregation, to<std::string>(pre_aggregation)},
+            {recordKeys().timestamp,
+             std::to_string(time_point.time_since_epoch().count())},
+        });
+    if (!status.ok()) {
+      LOG(ERROR) << "Data loss. Numeric monitoring point dispatch failed: "
+                 << status.what();
+    }
+  }
+
+ private:
+  const std::chrono::milliseconds time_quantum_;
+  PreAggregationCache cache_;
+  std::mutex cache_lock_;
+};
+
+} // namespace
+
 void record(const std::string& path,
-            ValueType value,
-            PreAggregationType pre_aggregation,
-            TimePoint time_point) {
+            const ValueType value,
+            const PreAggregationType pre_aggregation,
+            const TimePoint time_point) {
   if (!FLAGS_enable_numeric_monitoring) {
     return;
   }
-  auto status = Registry::call(
-      registryName(),
-      FLAGS_numeric_monitoring_plugins,
-      {
-          {recordKeys().path, path},
-          {recordKeys().value, std::to_string(value)},
-          {recordKeys().timestamp,
-           std::to_string(time_point.time_since_epoch().count())},
-          {recordKeys().pre_aggregation, to<std::string>(pre_aggregation)},
-      });
-  if (!status.ok()) {
-    LOG(ERROR) << "Failed to send numeric monitoring record: " << status.what();
-  }
+  static auto buffer = PreAggregationBuffer::create();
+  buffer->record(path, value, pre_aggregation, std::move(time_point));
 }
 
 } // namespace monitoring
