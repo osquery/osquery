e5bb481e6bd716a2dca097606ae8d728ed31d634
Teddy Reed <teddy@prosauce.org>
2018-07-11 16:31:57 -0400

    Update utils.py

--- tools/tests/utils.py
+++ tools/tests/utils.py
@@ -180,9 +180,8 @@ def profile_cmd(cmd, proc=None, shell=False, timeout=0, count=1):
             proc.kill()
             break
 
-    # there are two sleep(1) calls in the profiling code, so we subtract 2 here to account for it.
+    # account for sleep(1) in profiling code
     duration = time.time() - start_time - 2
-    # occasionally this can lead to negative results, so max up to zero in case that happens
     duration = max(duration, 0)
 
     utilization = [percent for percent in percents if percent != 0]
