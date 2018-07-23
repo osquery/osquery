68e6d77050d5c5ced0834c8d9bc496ebdf1c4a77
Alexander <akindyakov@gmail.com>
2018-07-23 16:15:10 +0100

    to<Status>(const Expected&) implementation (#4725)
    * to<Status>(Expected) implementation
    
    To make a transition from Status to Expected a bit easier.
    For transition time there must be a lot of conversions from Expected to Status and back.
    Conversion from Status to Expected is not simple, for many reasons.
    Conversion from Expected to Status is trivial, but a bit verbose:
    ```c++
    if (exp.isError()) {
      return Status::failure(
        exp.getError().getFullMessage()
      );
    } else {
      return Status::success();
    }
    ```
    I'd suggest using more laconic, clear and explicit way to convert.

--- osquery/core/tests/status_tests.cpp
+++ osquery/core/tests/status_tests.cpp
@@ -10,6 +10,8 @@
 
 #include <osquery/status.h>
 
+#include <boost/algorithm/string.hpp>
+
 #include <gtest/gtest.h>
 
 namespace osquery {
@@ -72,5 +74,33 @@ TEST_F(StatusTests, test_failure_with_success_code) {
   ASSERT_DEATH(Status::failure(Status::kSuccessCode, "message"),
                "Using 'failure' to create Status object with a kSuccessCode");
 #endif
+}
+
+namespace {
+
+enum class TestError {
+  Semantic = 1,
+};
+
+bool stringContains(const std::string& where, const std::string& what) {
+  return boost::contains(where, what);
+};
+
+} // namespace
+
+TEST_F(StatusTests, test_expected_to_status_failure) {
+  const auto expected = Expected<std::string, TestError>(
+      TestError::Semantic, "The ultimate failure reason");
+  auto s = to<Status>(expected);
+  EXPECT_FALSE(s.ok());
+  EXPECT_PRED2(stringContains, s.toString(), "The ultimate failure reason");
+}
+
+TEST_F(StatusTests, test_expected_to_status_success) {
+  const auto expected =
+      Expected<std::string, TestError>("This is not a failure");
+  auto s = to<Status>(expected);
+  EXPECT_TRUE(s.ok());
+  EXPECT_EQ(s, Status::success());
 }
 }
