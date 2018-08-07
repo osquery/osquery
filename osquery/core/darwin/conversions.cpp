a979df602d0e8ab61ddbc5f42a7fb8d9cf897516
akindyakov <akindyakov@fb.com>
2018-08-07 18:08:31 +0100

    Fix up by George comment

--- osquery/core/darwin/conversions.cpp
+++ osquery/core/darwin/conversions.cpp
@@ -21,7 +21,11 @@ std::string stringFromCFString(const CFStringRef& cf_string) {
   auto const wlength = CFStringGetLength(cf_string);
   auto const length =
       CFStringGetMaximumSizeForEncoding(wlength, kCFStringEncodingUTF8);
+  if (length == kCFNotFound) {
+    return "";
+  }
   auto result = std::string(length + 1, '\0');
+  // According to documentation: "if there is an error in conversion, the buffer contains only partial results". And because of that we don't need to check up the return value.
   CFStringGetCString(
       cf_string, &result.front(), result.size(), kCFStringEncodingUTF8);
   result.resize(result.find('\0'));
