b7079f41f55f61804f7b9f088db570b73aef5360
Teddy Reed <teddy@casualhacking.io>
2018-08-11 12:28:38 -0400

    deps: Add bottle hashes for ssdeep-cpp (#4851)

--- tools/provision/formula/ssdeep-cpp.rb
+++ tools/provision/formula/ssdeep-cpp.rb
@@ -8,6 +8,13 @@ class SsdeepCpp < AbstractOsqueryFormula
   sha256 "ff2eabc78106f009b4fb2def2d76fb0ca9e12acf624cbbfad9b3eb390d931313"
   revision 200
 
+  bottle do
+    root_url "https://osquery-packages.s3.amazonaws.com/bottles"
+    cellar :any_skip_relocation
+    sha256 "00ccb3820f19ca73e5cf553da5623d118843fad1516d5bf283eae403ae55298d" => :x86_64_linux
+    sha256 "64a6e3794335f0b24813ad3b22d40ed2bb32ad8040414e59022dfeb148e0c0e1" => :sierra
+  end
+
   def install
     append "CXXFLAGS", "-stdlib=libc++"
     system "./configure --prefix=#{prefix} --enable-static --enable-shared=no"
