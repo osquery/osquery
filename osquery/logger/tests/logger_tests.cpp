9c33466fea85964d3e36c2f8c9e16772805b4159
George Guliashvili <gguli@fb.com>
2018-08-17 07:57:40 +0100

    stop test directly configuring GLOG minloglevel

--- osquery/logger/tests/logger_tests.cpp
+++ osquery/logger/tests/logger_tests.cpp
@@ -17,7 +17,6 @@
 #include <osquery/logger.h>
 #include <osquery/registry_factory.h>
 
-DECLARE_int32(minloglevel);
 DECLARE_int32(stderrthreshold);
 
 namespace osquery {
@@ -196,30 +195,30 @@ TEST_F(LoggerTests, test_logger_log_status) {
 }
 
 TEST_F(LoggerTests, test_logger_status_level) {
-  auto minloglevel = FLAGS_minloglevel;
-  FLAGS_minloglevel = 0;
+  const auto logger_min_status = FLAGS_logger_min_status;
+  FLAGS_logger_min_status = 0;
   // This will be printed to stdout.
   LOG(INFO) << "Logger test is generating an info status";
   EXPECT_EQ(1U, LoggerTests::statuses_logged);
 
-  FLAGS_minloglevel = 1;
+  FLAGS_logger_min_status = 1;
   setVerboseLevel();
 
   LOG(INFO) << "Logger test is generating an info status";
   EXPECT_EQ(1U, LoggerTests::statuses_logged);
   LOG(WARNING) << "Logger test is generating a warning status";
   EXPECT_EQ(2U, LoggerTests::statuses_logged);
-  FLAGS_minloglevel = minloglevel;
+  FLAGS_logger_min_status = logger_min_status;
 
   auto stderrthreshold = FLAGS_stderrthreshold;
   FLAGS_stderrthreshold = 2;
   setVerboseLevel();
+  FLAGS_logger_min_status = logger_min_status;
 
   LOG(WARNING) << "Logger test is generating a warning status";
   EXPECT_EQ(3U, LoggerTests::statuses_logged);
   FLAGS_stderrthreshold = stderrthreshold;
 
-  auto logger_min_status = FLAGS_logger_min_status;
   FLAGS_logger_min_status = 1;
   setVerboseLevel();
 
