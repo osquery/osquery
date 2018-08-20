3acffc01e1a18efdfd12bea4cdb12d36e4274a1c
George Guliashvili <george.giorgi.guliashvili@gmail.com>
2018-08-20 15:31:32 +0100

    remove relayStatusLogs from scheduler

--- osquery/dispatcher/scheduler.cpp
+++ osquery/dispatcher/scheduler.cpp
@@ -209,10 +209,6 @@ void SchedulerRunner::start() {
       resetDatabase();
     }
 
-    // GLog is not re-entrant, so logs must be flushed in a dedicated thread.
-    if ((i % 3) == 0) {
-      relayStatusLogs();
-    }
     auto loop_step_duration =
         std::chrono::duration_cast<std::chrono::milliseconds>(
             std::chrono::steady_clock::now() - start_time_point);
