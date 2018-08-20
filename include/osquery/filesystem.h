2d91c84f7d3ca57750bc12ebba9173860e058cae
George Guliashvili <george.giorgi.guliashvili@gmail.com>
2018-08-20 21:23:53 +0100

    remove doc

--- include/osquery/filesystem.h
+++ include/osquery/filesystem.h
@@ -92,7 +92,6 @@ Status readFile(const boost::filesystem::path& path,
  * @param path the path of the file that you would like to write.
  * @param content the text that should be written exactly to disk.
  * @param permissions the filesystem permissions to request when opening.
- * @param force_permissions always `chmod` the path after opening.
  *
  * @return an instance of Status, indicating success or failure.
  */
