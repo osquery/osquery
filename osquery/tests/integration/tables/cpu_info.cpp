8c7f6a032eb5300aab88f70250cea084f48bf176
George Guliashvili <gguli@fb.com>
2018-08-31 13:27:32 +0100

    cpu info inital commit

--- osquery/tests/integration/tables/cpu_info.cpp
+++ osquery/tests/integration/tables/cpu_info.cpp
@@ -19,31 +19,25 @@ namespace osquery {
 class cpuInfo : public IntegrationTableTest {};
 
 TEST_F(cpuInfo, test_sanity) {
-  // 1. Query data
-  // QueryData data = execute_query("select * from cpu_info");
-  // 2. Check size before validation
-  // ASSERT_GE(data.size(), 0ul);
-  // ASSERT_EQ(data.size(), 1ul);
-  // ASSERT_EQ(data.size(), 0ul);
-  // 3. Build validation map
-  // See IntegrationTableTest.cpp for avaialbe flags
-  // Or use custom DataCheck object
-  // ValidatatioMap row_map = {
-  //      {"device_id", NormalType}
-  //      {"model", NormalType}
-  //      {"manufacturer", NormalType}
-  //      {"processor_type", NormalType}
-  //      {"availability", NormalType}
-  //      {"cpu_status", IntType}
-  //      {"number_of_cores", NormalType}
-  //      {"logical_processors", IntType}
-  //      {"address_width", NormalType}
-  //      {"current_clock_speed", IntType}
-  //      {"max_clock_speed", IntType}
-  //      {"socket_designation", NormalType}
-  //}
-  // 4. Perform validation
-  // validate_rows(data, row_map);
+  QueryData data = execute_query("select * from cpu_info");
+  ASSERT_GE(data.size(), 0ul);
+  ASSERT_EQ(data.size(), 1ul);
+  ASSERT_EQ(data.size(), 0ul);
+  ValidatatioMap row_map = {
+       {"device_id", NormalType}
+       {"model", NormalType}
+       {"manufacturer", NormalType}
+       {"processor_type", NormalType}
+       {"availability", NormalType}
+       {"cpu_status", IntType}
+       {"number_of_cores", NormalType}
+       {"logical_processors", IntType}
+       {"address_width", NormalType}
+       {"current_clock_speed", IntType}
+       {"max_clock_speed", IntType}
+       {"socket_designation", NormalType}
+  }
+  validate_rows(data, row_map);
 }
 
 } // namespace osquery
