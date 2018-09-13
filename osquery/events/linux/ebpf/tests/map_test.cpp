9bacff61fa69575c9313e1d58231b510245ec293
akindyakov <akindyakov@fb.com>
2018-09-13 11:49:00 +0000

    Use error message from Expected as a test ASSERT_* message

--- osquery/events/linux/ebpf/tests/map_test.cpp
+++ osquery/events/linux/ebpf/tests/map_test.cpp
@@ -23,7 +23,8 @@ class EbpfMapTests : public testing::Test {};
 TEST_F(EbpfMapTests, key_int_value_int) {
   auto const size = std::size_t{12};
   auto map_exp = ebpf::createMap<int, int, BPF_MAP_TYPE_HASH>(size);
-  ASSERT_TRUE(map_exp.isValue());
+  ASSERT_TRUE(map_exp.isValue())
+      << map_exp.getError().getFullMessageRecursive();
   auto map = map_exp.take();
   ASSERT_EQ(map.size(), size);
   {
@@ -38,27 +39,27 @@ TEST_F(EbpfMapTests, key_int_value_int) {
   }
   {
     auto exp = map.updateElement(5, 53);
-    ASSERT_TRUE(exp.isValue());
+    ASSERT_TRUE(exp.isValue()) << exp.getError().getFullMessageRecursive();
   }
   {
     auto exp = map.lookupElement(5);
-    ASSERT_TRUE(exp.isValue());
+    ASSERT_TRUE(exp.isValue()) << exp.getError().getFullMessageRecursive();
     ASSERT_EQ(exp.get(), 53);
   }
   {
     // key could be greater a size, because it is a hash map
     auto exp = map.updateElement(207, 8042);
-    ASSERT_TRUE(exp.isValue());
+    ASSERT_TRUE(exp.isValue()) << exp.getError().getFullMessageRecursive();
   }
   {
     auto exp = map.lookupElement(207);
-    ASSERT_TRUE(exp.isValue());
+    ASSERT_TRUE(exp.isValue()) << exp.getError().getFullMessageRecursive();
     ASSERT_EQ(exp.get(), 8042);
   }
   {
     // let's try to delete some existing key
     auto exp = map.deleteElement(207);
-    ASSERT_TRUE(exp.isValue());
+    ASSERT_TRUE(exp.isValue()) << exp.getError().getFullMessageRecursive();
   }
   {
     auto exp = map.lookupElement(207);
@@ -83,11 +84,11 @@ TEST_F(EbpfMapTests, key_int_value_struct) {
         .right = 2781,
     };
     auto exp = map.updateElement(72, v);
-    ASSERT_TRUE(exp.isValue());
+    ASSERT_TRUE(exp.isValue()) << exp.getError().getFullMessageRecursive();
   }
   {
     auto exp = map.lookupElement(72);
-    ASSERT_TRUE(exp.isValue());
+    ASSERT_TRUE(exp.isValue()) << exp.getError().getFullMessageRecursive();
     EXPECT_EQ(exp.get().left, -9287);
     EXPECT_EQ(exp.get().right, 2781);
   }
