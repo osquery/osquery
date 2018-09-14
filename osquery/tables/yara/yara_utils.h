bf93fcf2f9172de726a637ae9b12c192612849c6
Nick Anderson <nanderson7@gmail.com>
2018-09-14 11:36:24 -0700

    database: changing default path of the database for pathing uniformity (#4832)

--- osquery/tables/yara/yara_utils.h
+++ osquery/tables/yara/yara_utils.h
@@ -24,7 +24,7 @@ namespace pt = boost::property_tree;
 
 namespace osquery {
 
-const std::string kYARAHome{OSQUERY_HOME "/yara/"};
+const std::string kYARAHome{OSQUERY_HOME "yara/"};
 
 void YARACompilerCallback(int error_level,
                           const char* file_name,
