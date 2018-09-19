0c50ced401adfe11eccb4be3a861bfbce90a844a
Sharvil Shah <sharvil@sharvilshah.com>
2018-09-19 14:29:01 +0530

    tweaks and better error handling

--- osquery/tables/system/darwin/ibridge.cpp
+++ osquery/tables/system/darwin/ibridge.cpp
@@ -8,9 +8,9 @@
  *  You may select, at your option, one of the above-listed licenses.
  */
 
+#include <osquery/core/map_take.h>
 #include <osquery/logger.h>
 #include <osquery/tables.h>
-#include <osquery/core/map_take.h>
 
 #include "osquery/core/conversions.h"
 #include "osquery/core/darwin/iokit.hpp"
@@ -19,6 +19,7 @@ namespace osquery {
 namespace tables {
 
 #define kIODeviceEfiPath_ ":/efi/platform"
+#define kIODeviceChosenPath_ ":/chosen"
 #define kEmbeddedOSClass_ "AppleEmbeddedOSSupportHost"
 #define kAppleCoprocessorVersionKey_ "apple-coprocessor-version"
 
@@ -30,11 +31,32 @@ static const std::unordered_map<uint32_t, std::string> kCoprocessorVersions = {
     {0x00020000, "Apple T2 Chip"},
 };
 
+static inline void genBootUuid(Row& r) {
+  auto chosen = IORegistryEntryFromPath(
+      kIOMasterPortDefault, kIODeviceTreePlane kIODeviceChosenPath_);
+  if (chosen == MACH_PORT_NULL) {
+    return;
+  }
+
+  CFMutableDictionaryRef properties = nullptr;
+  auto kr = IORegistryEntryCreateCFProperties(
+      chosen, &properties, kCFAllocatorDefault, kNilOptions);
+  IOObjectRelease(chosen);
+
+  if (kr != KERN_SUCCESS) {
+    LOG(WARNING) << "Cannot get EFI properties";
+    return;
+  }
+
+  r["boot_uuid"] = getIOKitProperty(properties, "boot-uuid");
+  CFRelease(properties);
+}
+
 static inline void genAppleCoprocessorVersion(Row& r) {
   auto asoc = IORegistryEntryFromPath(kIOMasterPortDefault,
                                       kIODeviceTreePlane kIODeviceEfiPath_);
   if (asoc == MACH_PORT_NULL) {
-    VLOG(1) << "Cannot open EFI Device Tree";
+    LOG(WARNING) << "Cannot open EFI Device Tree";
     return;
   }
 
@@ -44,7 +66,7 @@ static inline void genAppleCoprocessorVersion(Row& r) {
   IOObjectRelease(asoc);
 
   if (kr != KERN_SUCCESS) {
-    VLOG(1) << "Cannot get EFI properties";
+    LOG(WARNING) << "Cannot get EFI properties";
     return;
   }
 
@@ -53,22 +75,14 @@ static inline void genAppleCoprocessorVersion(Row& r) {
     auto version_data = (CFDataRef)CFDictionaryGetValue(
         properties, CFSTR(kAppleCoprocessorVersionKey_));
     auto range = CFRangeMake(0, CFDataGetLength(version_data));
-    auto buffer = (unsigned char*)malloc(range.length + 1);
 
-    if (buffer == nullptr) {
-      return;
-    }
-    memset(buffer, 0, range.length + 1);
-    CFDataGetBytes(version_data, range, buffer);
+    auto buffer = std::vector<unsigned char>(range.length + 1, 0);
+    CFDataGetBytes(version_data, range, &buffer[0]);
 
-    uint32_t version = {0};
-    memcpy(&version, buffer, 4);
-
-    //r["coprocessor_version"] = (kCoprocessorVersions.count(version) > 0)
-    //                               ? kCoprocessorVersions.at(version)
-    //                               : "unknown";
-    r["coprocessor_version"] = tryTakeCopy(kCoprocessorVersions, version).takeOr(std::string{"unknown"});
-    free(buffer);
+    uint32_t version = 0;
+    memcpy(&version, buffer.data(), 4);
+    r["coprocessor_version"] = tryTakeCopy(kCoprocessorVersions, version)
+                                   .takeOr(std::string{"unknown"});
   }
   CFRelease(properties);
 }
@@ -78,10 +92,11 @@ QueryData genIBridgeInfo(QueryContext& context) {
   Row r;
 
   genAppleCoprocessorVersion(r);
+  genBootUuid(r);
 
   auto eos = IOServiceNameMatching(kEmbeddedOSClass_);
   if (eos == nullptr) {
-    VLOG(1)
+    LOG(WARNING)
         << "EmbeddedOS class not found. Perhaps this mac doesn't have T* chip";
     return results;
   }
@@ -93,19 +108,18 @@ QueryData genIBridgeInfo(QueryContext& context) {
   IOObjectRelease(service);
 
   if (kr != KERN_SUCCESS) {
-    VLOG(1) << "Cannot get EmbeddedOS properties";
+    LOG(WARNING) << "Cannot get EmbeddedOS properties";
     IOObjectRelease(service);
     return results;
   }
 
   r["unique_chip_id"] = getIOKitProperty(properties, "DeviceUniqueChipID");
   r["firmware_version"] = getIOKitProperty(properties, "DeviceBuildVersion");
-  r["boot_uuid"] = getIOKitProperty(properties, "DeviceBootUUID");
 
   CFRelease(properties);
   IOObjectRelease(service);
 
-  results.push_back(r);
+  results.push_back(std::move(r));
   return results;
 }
 } // namespace tables
