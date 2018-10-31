bf4c0a3117889325fc560075bbe96e0a606a32e8
Filipe Manco <fmanco@fb.com>
2018-10-31 06:16:47 -0700

    Move iptables querying to a C based proxy
    The iptables uapi kernel headers are incompatible with C++. To avoid
    depending on modified kernel headers query iptables from a C based
    proxy. This is a temporary solution until the problem is fixed on
    upstream Linux.

--- osquery/tables/networking/linux/iptables.cpp
+++ osquery/tables/networking/linux/iptables.cpp
@@ -11,7 +11,7 @@
 #include <sstream>
 
 #include <arpa/inet.h>
-#include <libiptc/libiptc.h>
+#include <net/if.h>
 #include <netinet/in.h>
 
 #include <boost/algorithm/string/split.hpp>
@@ -24,6 +24,10 @@
 #include "osquery/core/conversions.h"
 #include "osquery/tables/networking/utils.h"
 
+extern "C" {
+#include <osquery/tables/networking/linux/iptc_proxy.h>
+}
+
 namespace osquery {
 namespace tables {
 
@@ -33,131 +37,114 @@ static const std::string kHexMap = "0123456789ABCDEF";
 static const int kMaskHighBits = 4;
 static const int kMaskLowBits = 15;
 
-void parseIpEntry(const ipt_ip *ip, Row &r) {
-  r["protocol"] = INTEGER(ip->proto);
-  if (strlen(ip->iniface)) {
-    r["iniface"] = TEXT(ip->iniface);
-  } else {
-    r["iniface"] = "all";
-  }
-
-  if (strlen(ip->outiface)) {
-    r["outiface"] = TEXT(ip->outiface);
-  } else {
-    r["outiface"] = "all";
-  }
-
-  r["src_ip"] = ipAsString(&ip->src);
-  r["dst_ip"] = ipAsString(&ip->dst);
-  r["src_mask"] = ipAsString(&ip->smsk);
-  r["dst_mask"] = ipAsString(&ip->dmsk);
-
-  char aux_char[2] = {0};
-  std::string iniface_mask;
-  for (int i = 0; i < IFNAMSIZ && ip->iniface_mask[i] != 0x00; i++) {
-    aux_char[0] = kHexMap[(int)ip->iniface_mask[i] >> kMaskHighBits];
-    aux_char[1] = kHexMap[(int)ip->iniface_mask[i] & kMaskLowBits];
-    iniface_mask += aux_char[0];
-    iniface_mask += aux_char[1];
-  }
-
-  r["iniface_mask"] = TEXT(iniface_mask);
-  std::string outiface_mask = "";
-  for (int i = 0; i < IFNAMSIZ && ip->outiface_mask[i] != 0x00; i++) {
-    aux_char[0] = kHexMap[(int)ip->outiface_mask[i] >> kMaskHighBits];
-    aux_char[1] = kHexMap[(int)ip->outiface_mask[i] & kMaskLowBits];
-    outiface_mask += aux_char[0];
-    outiface_mask += aux_char[1];
-  }
-  r["outiface_mask"] = TEXT(outiface_mask);
-}
-
-void parseEntryMatch(const struct ipt_entry* en, Row& r) {
-  // Get rule port details from the xt_entry_match object
-
-  // m will never be NULL, elems is an array
-  auto m = (struct xt_entry_match*)en->elems;
-
-  if (en->ip.proto == IPPROTO_TCP) {
-    // m_data will never be NULL if ip.proto is set to TCP/UDP
-    auto m_data = (struct ipt_tcp*)m->data;
-    r["src_port"] =
-        std::to_string(m_data->spts[0]) + ':' + std::to_string(m_data->spts[1]);
-    r["dst_port"] =
-        std::to_string(m_data->dpts[0]) + ':' + std::to_string(m_data->dpts[1]);
-  } else if (en->ip.proto == IPPROTO_UDP) {
-    auto m_data = (struct ipt_udp*)m->data;
-    r["src_port"] =
-        std::to_string(m_data->spts[0]) + ':' + std::to_string(m_data->spts[1]);
-    r["dst_port"] =
-        std::to_string(m_data->dpts[0]) + ':' + std::to_string(m_data->dpts[1]);
-  } else {
-    r["src_port"] = "";
-    r["dst_port"] = "";
-  }
-}
-
 void genIPTablesRules(const std::string &filter, QueryData &results) {
   Row r;
   r["filter_name"] = filter;
 
   // Initialize the access to iptc
-  auto handle = (struct iptc_handle *)iptc_init(filter.c_str());
+  auto handle = iptcproxy_init(filter.c_str());
   if (handle == nullptr) {
     return;
   }
 
   // Iterate through chains
-  for (auto chain = iptc_first_chain(handle); chain != nullptr;
-       chain = iptc_next_chain(handle)) {
-    r["chain"] = TEXT(chain);
+  for (auto chain = iptcproxy_first_chain(handle);
+      chain != nullptr;
+      chain = iptcproxy_next_chain(handle)) {
+    r["chain"] = TEXT(chain->chain);
 
-    struct ipt_counters counters;
-    auto policy = iptc_get_policy(chain, &counters, handle);
-
-    if (policy != nullptr) {
-      r["policy"] = TEXT(policy);
-      r["packets"] = INTEGER(counters.pcnt);
-      r["bytes"] = INTEGER(counters.bcnt);
+    if (chain->policy != nullptr) {
+      r["policy"] = TEXT(chain->policy);
+      r["packets"] = INTEGER(chain->policy_data.pcnt);
+      r["bytes"] = INTEGER(chain->policy_data.bcnt);
     } else {
       r["policy"] = "";
       r["packets"] = "0";
       r["bytes"] = "0";
     }
 
-    const struct ipt_entry *prev_rule = nullptr;
     // Iterating through all the rules per chain
-    for (const struct ipt_entry *chain_rule = iptc_first_rule(chain, handle);
-         chain_rule;
-         chain_rule = iptc_next_rule(prev_rule, handle)) {
-      prev_rule = chain_rule;
+    for (auto rule = iptcproxy_first_rule(chain->chain, handle);
+         rule != nullptr;
+         rule = iptcproxy_next_rule(handle)) {
 
-      auto target = iptc_get_target(chain_rule, handle);
-      if (target != nullptr) {
-        r["target"] = TEXT(target);
+      if (rule->target != nullptr) {
+        r["target"] = TEXT(rule->target);
       } else {
         r["target"] = "";
       }
 
-      if (chain_rule->target_offset) {
+      if (rule->match) {
         r["match"] = "yes";
-        // fill protocol port details
-        parseEntryMatch(chain_rule, r);
       } else {
         r["match"] = "no";
+      }
+
+      if (rule->match && rule->match_data.valid) {
+        r["src_port"] =
+          std::to_string(rule->match_data.spts[0])
+          + ':'
+          + std::to_string(rule->match_data.spts[1]);
+        r["dst_port"] =
+          std::to_string(rule->match_data.dpts[0])
+          + ':'
+          + std::to_string(rule->match_data.dpts[1]);
+      } else {
         r["src_port"] = "";
         r["dst_port"] = "";
       }
 
-      const struct ipt_ip *ip = &chain_rule->ip;
-      parseIpEntry(ip, r);
+      r["protocol"] = INTEGER(rule->ip_data.proto);
+      if (strlen(rule->ip_data.iniface)) {
+        r["iniface"] = TEXT(rule->ip_data.iniface);
+      } else {
+        r["iniface"] = "all";
+      }
+
+      if (strlen(rule->ip_data.outiface)) {
+        r["outiface"] = TEXT(rule->ip_data.outiface);
+      } else {
+        r["outiface"] = "all";
+      }
+
+      r["src_ip"] = ipAsString(&rule->ip_data.src);
+      r["dst_ip"] = ipAsString(&rule->ip_data.dst);
+      r["src_mask"] = ipAsString(&rule->ip_data.smsk);
+      r["dst_mask"] = ipAsString(&rule->ip_data.dmsk);
+
+      char aux_char[2] = {0};
+      std::string iniface_mask;
+      for (int i = 0;
+          i < IFNAMSIZ && rule->ip_data.iniface_mask[i] != 0x00;
+          i++) {
+        aux_char[0] = kHexMap[
+          (int)rule->ip_data.iniface_mask[i] >> kMaskHighBits];
+        aux_char[1] = kHexMap[
+          (int)rule->ip_data.iniface_mask[i] & kMaskLowBits];
+        iniface_mask += aux_char[0];
+        iniface_mask += aux_char[1];
+      }
+
+      r["iniface_mask"] = TEXT(iniface_mask);
+      std::string outiface_mask = "";
+      for (int i = 0;
+          i < IFNAMSIZ && rule->ip_data.outiface_mask[i] != 0x00;
+          i++) {
+        aux_char[0] = kHexMap[
+          (int)rule->ip_data.outiface_mask[i] >> kMaskHighBits];
+        aux_char[1] = kHexMap[
+          (int)rule->ip_data.outiface_mask[i] & kMaskLowBits];
+        outiface_mask += aux_char[0];
+        outiface_mask += aux_char[1];
+      }
+      r["outiface_mask"] = TEXT(outiface_mask);
 
       results.push_back(r);
     } // Rule iteration
     results.push_back(r);
   } // Chain iteration
 
-  iptc_free(handle);
+  iptcproxy_free(handle);
 }
 
 QueryData genIptables(QueryContext &context) {
