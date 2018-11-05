d8252a154645d19f1ee1ba425b2f9ab6fe5b7c55
William Woodruff <william.woodruff@trailofbits.com>
2018-11-05 14:51:24 -0500

    system/windows: Add default environment table

---
+++ osquery/tables/system/windows/default_environment.cpp
@@ -0,0 +1,26 @@
+#include <osquery/core.h>
+#include <osquery/logger.h>
+#include <osquery/sql.h>
+#include <osquery/tables.h>
+
+namespace osquery {
+namespace tables {
+  static std::string kEnvironmentKey =
+    "HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment";
+
+  QueryData genEnvironment(QueryContext& context) {
+    QueryData results;
+    auto environment = SQL::selectAllFrom("registry", "key", EQUALS, kEnvironmentKey);
+
+    for (const auto& env : environment) {
+      Row r;
+      r["variable"] = env.at("name");
+      r["value"] = env.at("data");
+      r["expand"] = INTEGER(env.at("type") == "REG_EXPAND_SZ");
+      results.push_back(r);
+    }
+
+    return results;
+  }
+}
+}
