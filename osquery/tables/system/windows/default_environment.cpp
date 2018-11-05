1b22e9456de267b3f5c7a7128e77e31b78ac72f0
William Woodruff <william.woodruff@trailofbits.com>
2018-11-05 14:59:39 -0500

    system/windows: clang-format

--- osquery/tables/system/windows/default_environment.cpp
+++ osquery/tables/system/windows/default_environment.cpp
@@ -5,22 +5,24 @@
 
 namespace osquery {
 namespace tables {
-  static std::string kEnvironmentKey =
-    "HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment";
+static std::string kEnvironmentKey =
+    "HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Control\\Session "
+    "Manager\\Environment";
 
-  QueryData genEnvironment(QueryContext& context) {
-    QueryData results;
-    auto environment = SQL::selectAllFrom("registry", "key", EQUALS, kEnvironmentKey);
+QueryData genEnvironment(QueryContext& context) {
+  QueryData results;
+  auto environment =
+      SQL::selectAllFrom("registry", "key", EQUALS, kEnvironmentKey);
 
-    for (const auto& env : environment) {
-      Row r;
-      r["variable"] = env.at("name");
-      r["value"] = env.at("data");
-      r["expand"] = INTEGER(env.at("type") == "REG_EXPAND_SZ");
-      results.push_back(r);
-    }
-
-    return results;
+  for (const auto& env : environment) {
+    Row r;
+    r["variable"] = env.at("name");
+    r["value"] = env.at("data");
+    r["expand"] = INTEGER(env.at("type") == "REG_EXPAND_SZ");
+    results.push_back(r);
   }
+
+  return results;
 }
-}
+} // namespace tables
+} // namespace osquery
