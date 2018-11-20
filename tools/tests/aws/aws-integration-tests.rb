01b9c589365fcdef78821ec226dd856c9a901f40
AlexM <20775507+packetzero@users.noreply.github.com>
2018-11-20 16:21:32 -0600

    update aws-integration-tests.rb

--- tools/tests/aws/aws-integration-tests.rb
+++ tools/tests/aws/aws-integration-tests.rb
@@ -2,18 +2,47 @@ require 'ostruct'
 require 'json'
 require 'optparse'
 
+# Usage:
+#
+# $ ruby tools/tests/aws/aws-integration-tests.rb -s osquery-kinesis-stream -r "arn:aws:iam::123456789012:role/osquery-kinesis-streams"
+# -------------------------------------
+# Running 'local_user_creds'
+# :SUCCESS
+# -------------------------------------
+# Running 'osquery_assume_role'
+# :SUCCESS
+# -------------------------------------
+# Running 'provided_session_token'
+# :SUCCESS
+# -------------------------------------
+# :SKIPPING endpoint_override_provided_session (no local proxy)
+# -------------------------------------
+# :SKIPPING endpoint_override (no local proxy)
+# -------------------------------------
+# Running 'fail_user_creds_invalid'
+# :SUCCESS
+# -------------------------------------
+# Running 'fail_invalid_sts_role'
+# :SUCCESS
+
+
 TEST_NAMES=[
   'local_user_creds',
   'osquery_assume_role',
   'provided_session_token',
   'fail_user_creds_invalid',
-  'fail_invalid_sts_role'
+  'fail_invalid_sts_role',
+  'endpoint_override',
+  'endpoint_override_provided_session'
 ]
 
 class AwsIntegrationTestRunner
-
-  def initialize()
-    @exe="./build/darwin/osquery/aws_logger_integration_tests"
+  attr_reader :options, :creds, :have_local_proxy
+  def initialize(options)
+    @options = options
+    @exe = options.test_exe_path
+    @creds = get_aws_config
+    check_local_proxy
   end
 
   #---------------------------------------------------------
@@ -41,6 +70,16 @@ class AwsIntegrationTestRunner
     end
 
     return items
+  end
+
+  #---------------------------------------------------------
+  # In order to try the endpoint_override tests,
+  # need the local reverse proxy.  See nginx-proxy in
+  # https://github.com/packetzero/osquery_aws_notes
+  #---------------------------------------------------------
+  def check_local_proxy
+    tmp = `curl --head --insecure --stderr - -o - https://localhost:443/`
+    @have_local_proxy = tmp.include?("x-amzn-RequestId:")
   end
 
   #---------------------------------------------------------
@@ -91,24 +130,16 @@ class AwsIntegrationTestRunner
   #---------------------------------------------------------
   # returns a hashmap matching names in osquery config 'options'
   #---------------------------------------------------------
-  def get_config_hash(region, access_key_id="", secret_access_key="", logger_plugin="aws_kinesis")
-    options = OpenStruct.new(:aws_region => region, :aws_access_key_id => access_key_id,
-      :aws_secret_access_key => secret_access_key, :logger_plugin => logger_plugin,
+  def get_config_hash()
+    options = OpenStruct.new(:aws_region => @creds[:region], :aws_access_key_id => @creds[:access_key],
+      :aws_secret_access_key => @creds[:secret_key], :logger_plugin => "aws_kinesis",
       :aws_session_token => "", :aws_endpoint_override => "",
-      :aws_kinesis_stream => "", :aws_kinesis_period => 1,
+      :aws_kinesis_stream => @options.kinesis_stream_name,
+      :aws_kinesis_period => 1,
       :aws_kinesis_max_retry => 2,
       :disable_logging => false, :utc => true
     )
     return options
-  end
-
-  #---------------------------------------------------------
-  # alternative signature
-  #---------------------------------------------------------
-  def get_config_hash2(creds, options)
-    cfg = get_config_hash(creds[:region], creds[:access_key], creds[:secret_key])
-    cfg.aws_kinesis_stream = options.kinesis_stream_name
-    return cfg
   end
 
   #---------------------------------------------------------
@@ -119,9 +150,26 @@ class AwsIntegrationTestRunner
     return doc.to_json
   end
 
+  def was_successful output
+    was_sent = false
+    err_count = 0
+    output.lines.each do |line|
+      if line.start_with?("E")
+        err_count += 1
+      end
+      if line.include?("Successfully sent") && line.include?("aws_kinesis")
+        was_sent = true
+      end
+    end
+    return was_sent && err_count == 0
+  end
+
 end
 
 #### script execution starts here
+if ARGV.count == 0
+  ARGV.push "--help"
+end
 
 # defaults
 
@@ -130,7 +178,8 @@ options = OpenStruct.new(
   :kinesis_stream_name => 'osquery-kinesis-stream',
   :role_arn => '',
   :test_exe_path => "./build/#{platform_name}/osquery/aws_logger_integration_tests",
-  :test_names => TEST_NAMES
+  :test_names => TEST_NAMES,
+  :verbose => false
 )
 
 # parse arguments
@@ -139,19 +188,21 @@ OptionParser.new do |opt|
   opt.on('-s VALUE','--stream STREAM_NAME','e.g. "osquery-kinesis-stream"') { |value| options.kinesis_stream_name = value }
   opt.on('-r VALUE','--role_arn ROLE_ARN_VALUE','e.g. "arn:aws:iam::123456789012:role/osquery-kinesis-streams"') { |value| options.role_arn = value }
   opt.on('-p VALUE','--path_to_exe PATH','OPTIONAL e.g. ./build/darwin/osquery/aws_logger_integration_tests') { |value| options.test_exe_path = value }
+  opt.on('-v','--verbose','Write each test output and config to stdout') { |value| options.verbose = true }
   opt.on('-f VALUE','--filter TEST_LIST', "OPTIONAL Comma-delimited list of test names to run.\n\t\tFull list: #{TEST_NAMES.join(',')}") { |value| options.test_names = value.split(',') }
 end.parse!
 
+unless File.exists?(options.test_exe_path)
+  puts "ERROR: Test exe not found:#{options.test_exe_path}"
+  exit 2
+end
+
 osquery_configs = {}
-obj = AwsIntegrationTestRunner.new
+obj = AwsIntegrationTestRunner.new(options)
 
-# get aws command-line config
-creds = obj.get_aws_config
+# basic user creds test
 
-# build a hash map we can use to build JSON config
-
-cfg = obj.get_config_hash(creds[:region], creds[:access_key], creds[:secret_key])
-cfg.aws_kinesis_stream = options.kinesis_stream_name
+cfg = obj.get_config_hash()
 
 test_name = 'local_user_creds'
 if options.test_names.include?(test_name)
@@ -162,9 +213,9 @@ end
 # assume role
 
 test_name = 'osquery_assume_role'
-cfg = obj.get_config_hash2(creds,options)
-cfg.aws_sts_arn_role = options.role_arn
 if options.test_names.include?(test_name)
+  cfg = obj.get_config_hash()
+  cfg.aws_sts_arn_role = options.role_arn
   osquery_configs[test_name] = obj.make_config_json(cfg)
 end
 
@@ -172,6 +223,8 @@ end
 
 test_name = 'provided_session_token'
 if options.test_names.include?(test_name)
+
+  cfg = obj.get_config_hash()
 
   result = obj.assume_role options.role_arn
   if result.nil? || result[:session_token].nil?
@@ -187,11 +240,49 @@ if options.test_names.include?(test_name)
 
 end
 
+# endpoint override - with provided session
+
+test_name = 'endpoint_override_provided_session'
+if options.test_names.include?(test_name)
+
+  cfg = obj.get_config_hash()
+
+  result = obj.assume_role options.role_arn
+  if result.nil? || result[:session_token].nil?
+    puts "ERROR: assume role did not provide session token"
+    exit 3
+  end
+  cfg.aws_session_token = result[:session_token]
+  cfg.aws_sts_arn_role = options.role_arn
+  cfg.aws_access_key_id = result[:access_key]
+  cfg.aws_secret_access_key = result[:secret_key]
+  cfg.aws_endpoint_override = "localhost:443"
+
+  # get path to local server case
+  # assuming this ruby script lives in osquery/tools/tests/aws/
+  cfg.tls_server_certs = "#{File.dirname(__FILE__)}/../test_server_ca.pem"
+  osquery_configs[test_name] = obj.make_config_json(cfg)
+end
+
+# endpoint override
+
+test_name = 'endpoint_override'
+if options.test_names.include?(test_name)
+  cfg = obj.get_config_hash()
+#  cfg.aws_sts_arn_role = options.role_arn
+
+  cfg.aws_endpoint_override = "localhost:443"
+  # get path to local server case
+  # assuming this ruby script lives in osquery/tools/tests/aws/
+  cfg.tls_server_certs = "#{File.dirname(__FILE__)}/../test_server_ca.pem"
+  osquery_configs[test_name] = obj.make_config_json(cfg)
+end
+
 # invalid secret
 
 test_name = 'fail_user_creds_invalid'
 if options.test_names.include?(test_name)
-  cfg = obj.get_config_hash2(creds,options)
+  cfg = obj.get_config_hash()
   cfg.aws_secret_access_key = 'xxxxxx'
   # this may or may not succeed, depending on AWS user settings
   osquery_configs[test_name] = obj.make_config_json(cfg)
@@ -201,7 +292,7 @@ end
 
 test_name = 'fail_invalid_sts_role'
 if options.test_names.include?(test_name)
-  cfg = obj.get_config_hash2(creds,options)
+  cfg = obj.get_config_hash()
   cfg.aws_sts_arn_role = "#{options.role_arn}-nosuch"
   # this may or may not succeed, depending on AWS user settings
   osquery_configs[test_name] = obj.make_config_json(cfg)
@@ -213,17 +304,30 @@ if osquery_configs.empty?
 end
 
 # run them
-
 osquery_configs.each do |name, config_content|
+
+  puts "-------------------------------------"
+  if name.start_with?("endpoint_override") && !obj.have_local_proxy
+    puts ":SKIPPING #{name} (no local proxy)"
+    next
+  end
 
   path="/tmp/aws_int_test.conf"
 
   File.open(path, "w") { |f| f.write(config_content) }
-  puts "-------------------------------------"
   puts "Running '#{name}'"
+  STDOUT.flush
+
+  puts "\n  Config:#{config_content}\n\n" if (options.verbose)
 
   output = obj.run_test path
-  puts output
+  if obj.was_successful(output) || name.start_with?("fail")
+    puts ":SUCCESS"
+    puts output if options.verbose
+  else
+    puts ":FAILED"
+    puts output
+  end
 
   File.unlink path
 end
