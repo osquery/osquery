0f15c5fc6a4f1c62fc10054a571d376d12869923
Alexander Kindyakov <akindyakov@fb.com>
2019-01-15 05:35:34 -0800

    C++ enum wrapper for posix general error codes (#5342)
    Summary:
    Pull Request resolved: https://github.com/facebook/osquery/pull/5342
    
    This sort of enumeration is required to pass posix system errors with `Expected<>`
    
    Reviewed By: mkareta, fmanco
    
    Differential Revision: D13608306
    
    fbshipit-source-id: 0b787b12fac915f097dc310b616df507772da455

---
+++ osquery/utils/system/tests/errno.cpp
@@ -0,0 +1,7 @@
+/**
+ *  Copyright (c) 2018-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed as defined on the LICENSE file found in the
+ *  root directory of this source tree.
+ */
