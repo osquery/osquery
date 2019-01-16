794684798cf019255ec50c3f9bda79852b6142ab
Alexander Kindyakov <akindyakov@fb.com>
2019-01-16 05:32:43 -0800

    Integration test for the users table (#5357)
    Summary:
    Pull Request resolved: https://github.com/facebook/osquery/pull/5357
    
    Basically rebase PR #5134 on top of experimental branch
    
    Reviewed By: guliashvili
    
    Differential Revision: D13675072
    
    fbshipit-source-id: 3e5798dfddf7fcfb50d9366349b40d8980976c8f

--- tests/integration/tables/users.cpp
+++ tests/integration/tables/users.cpp
@@ -1,4 +1,3 @@
-
 /**
  *  Copyright (c) 2014-present, Facebook, Inc.
  *  All rights reserved.
@@ -14,41 +13,52 @@
 
 #include <osquery/tests/integration/tables/helper.h>
 
+#include <osquery/utils/info/platform_type.h>
+
 namespace osquery {
 namespace table_tests {
+namespace {
 
-class users : public testing::Test {
-  protected:
-    void SetUp() override {
-      setUpEnvironment();
-    }
+class UsersTest : public testing::Test {
+ protected:
+  void SetUp() override {
+    setUpEnvironment();
+  }
 };
 
-TEST_F(users, test_sanity) {
-  // 1. Query data
-  auto const data = execute_query("select * from users");
-  // 2. Check size before validation
-  // ASSERT_GE(data.size(), 0ul);
-  // ASSERT_EQ(data.size(), 1ul);
-  // ASSERT_EQ(data.size(), 0ul);
-  // 3. Build validation map
-  // See helper.h for avaialbe flags
-  // Or use custom DataCheck object
-  // ValidatatioMap row_map = {
-  //      {"uid", IntType}
-  //      {"gid", IntType}
-  //      {"uid_signed", IntType}
-  //      {"gid_signed", IntType}
-  //      {"username", NormalType}
-  //      {"description", NormalType}
-  //      {"directory", NormalType}
-  //      {"shell", NormalType}
-  //      {"uuid", NormalType}
-  //      {"type", NormalType}
-  //}
-  // 4. Perform validation
-  // validate_rows(data, row_map);
+TEST_F(UsersTest, test_sanity) {
+  auto const rows = execute_query("select * from users");
+  ASSERT_GE(rows.size(), 1ul); // There must be at least one user
+  auto row_map = ValidatatioMap{
+      {"uid", NonNegativeInt},
+      {"uid_signed", IntType},
+      {"gid_signed", IntType},
+      {"description", NormalType},
+      {"shell", NonEmptyString},
+  };
+  if (isPlatform(PlatformType::TYPE_WINDOWS)) {
+    row_map.emplace("gid", IntType);
+    row_map.emplace("username", NormalType);
+  } else {
+    row_map.emplace("gid", NonNegativeInt);
+    row_map.emplace("username", NonEmptyString);
+  }
+  if (isPlatform(PlatformType::TYPE_WINDOWS)) {
+    row_map.emplace("directory", NormalType);
+  } else {
+    row_map.emplace("directory", NonEmptyString);
+  }
+  if (isPlatform(PlatformType::TYPE_WINDOWS)) {
+    row_map.emplace("type", NormalType);
+  }
+  if (isPlatform(PlatformType::TYPE_OSX)) {
+    row_map.emplace("uuid", ValidUUID);
+  } else {
+    row_map.emplace("uuid", NormalType);
+  }
+  validate_rows(rows, row_map);
 }
 
+} // namespace
 } // namespace table_tests
 } // namespace osquery
