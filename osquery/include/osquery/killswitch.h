a67525fae1172fa47306865c98a65561279c58f8
Filipe Manco <fmanco@fb.com>
2019-01-21 11:48:49 -0800

    Fix LICENSE information on file headers (#5375)
    Summary:
    Pull Request resolved: https://github.com/facebook/osquery/pull/5375
    
    LICENSE is now defined in a single file on the root of the project, update the
    header to contain that information.
    
    **Project LICENSE did not change.**
    
    Reviewed By: akindyakov
    
    Differential Revision: D13750575
    
    fbshipit-source-id: 1e608a81b260b8395f9d008fc67f463160c1fc2b

--- osquery/include/osquery/killswitch.h
+++ osquery/include/osquery/killswitch.h
@@ -4,7 +4,6 @@
  *
  *  This source code is licensed under both the Apache 2.0 license (found in
  * the LICENSE file in the root directory of this source tree) and the GPLv2
- * (found in the COPYING file in the root directory of this source tree). You
  * may select, at your option, one of the above-listed licenses.
  */
 
