760e877ae5bdde1d6abff20dfbb52a7f0a92e655
Alexander Kindyakov <akindyakov@fb.com>
2019-02-11 02:29:59 -0800

    prototype of osquery/plugin_sdk
    Summary:
    Build osquery extensions/plugins against all osquery is an apparently bad idea.
    Because plugins usually are not require full set of thirdparty libs of osquery. Most of osquery code is also not necesarry for plugins. And the main thing, clear definition of public interface of osquery plugins will force us to keep it clear and short, change it carefully and consciously, crate a tests for it.
    
    Reviewed By: fmanco
    
    Differential Revision: D13990668
    
    fbshipit-source-id: ed6ed3f6f75178d829fc6bcbd1af20ef2e268fa8

---
+++ sdk/plugin_sdk.h
@@ -0,0 +1,9 @@
+/**
+ *  Copyright (c) 2014-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed as defined on the LICENSE file found in the
+ *  root directory of this source tree.
+ */
+
+#pragma once
