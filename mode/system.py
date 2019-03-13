e5aaeda8b99f132d967bdff176a6457c4b0ae060
Filipe Manco <fmanco@fb.com>
2019-03-13 05:36:09 -0700

    Fix formatting of oss/mode/system.py
    Summary: Reduce differences to internal version to make it easier to diff. These will eventually be the same script but we need to do this step by step.
    
    Reviewed By: SAlexandru
    
    Differential Revision: D14282634
    
    fbshipit-source-id: 322c0bf1b1750bf234d4d14b813c6c5aea3becbd

--- mode/system.py
+++ mode/system.py
@@ -8,21 +8,61 @@ import itertools
 
 
 PLATFORM_CXX_FLAGS = {
-    'linux': {
-        'release': ['//tools/buckconfigs/linux-x86_64/toolchain/ubuntu-18.04-clang.bcfg', '//tools/buckconfigs/linux-x86_64/type/release.bcfg', '//tools/buckconfigs/linux-x86_64/base.bcfg', '//tools/buckconfigs/base.bcfg'],
-        'debug': ['//tools/buckconfigs/linux-x86_64/toolchain/ubuntu-18.04-clang.bcfg', '//tools/buckconfigs/linux-x86_64/type/debug.bcfg', '//tools/buckconfigs/linux-x86_64/base.bcfg', '//tools/buckconfigs/base.bcfg'],
+    "linux": {
+        "release": [
+            "//tools/buckconfigs/linux-x86_64/toolchain/ubuntu-18.04-clang.bcfg",
+            "//tools/buckconfigs/linux-x86_64/type/release.bcfg",
+            "//tools/buckconfigs/linux-x86_64/base.bcfg",
+            "//tools/buckconfigs/base.bcfg",
+        ],
+        "debug": [
+            "//tools/buckconfigs/linux-x86_64/toolchain/ubuntu-18.04-clang.bcfg",
+            "//tools/buckconfigs/linux-x86_64/type/debug.bcfg",
+            "//tools/buckconfigs/linux-x86_64/base.bcfg",
+            "//tools/buckconfigs/base.bcfg",
+        ],
     },
-    'freebsd': {
-        'release': ['//tools/buckconfigs/freebsd-x86_64/toolchain/freebsd-11.2-clang.bcfg', '//tools/buckconfigs/freebsd-x86_64/type/release.bcfg', '//tools/buckconfigs/freebsd-x86_64/base.bcfg', '//tools/buckconfigs/base.bcfg'],
-        'debug': ['//tools/buckconfigs/freebsd-x86_64/toolchain/freebsd-11.2-clang.bcfg', '//tools/buckconfigs/freebsd-x86_64/type/debug.bcfg', '//tools/buckconfigs/freebsd-x86_64/base.bcfg', '//tools/buckconfigs/base.bcfg'],
+    "freebsd": {
+        "release": [
+            "//tools/buckconfigs/freebsd-x86_64/toolchain/freebsd-11.2-clang.bcfg",
+            "//tools/buckconfigs/freebsd-x86_64/type/release.bcfg",
+            "//tools/buckconfigs/freebsd-x86_64/base.bcfg",
+            "//tools/buckconfigs/base.bcfg",
+        ],
+        "debug": [
+            "//tools/buckconfigs/freebsd-x86_64/toolchain/freebsd-11.2-clang.bcfg",
+            "//tools/buckconfigs/freebsd-x86_64/type/debug.bcfg",
+            "//tools/buckconfigs/freebsd-x86_64/base.bcfg",
+            "//tools/buckconfigs/base.bcfg",
+        ],
     },
-    'darwin': {
-        'release': ['//tools/buckconfigs/macos-x86_64/type/release.bcfg', '//tools/buckconfigs/macos-x86_64/base.bcfg', '//tools/buckconfigs/base.bcfg'],
-        'debug': ['//tools/buckconfigs/macos-x86_64/type/debug.bcfg', '//tools/buckconfigs/macos-x86_64/base.bcfg', '//tools/buckconfigs/base.bcfg'],
+    "darwin": {
+        "release": [
+            "//tools/buckconfigs/macos-x86_64/type/release.bcfg",
+            "//tools/buckconfigs/macos-x86_64/base.bcfg",
+            "//tools/buckconfigs/base.bcfg",
+        ],
+        "debug": [
+            "//tools/buckconfigs/macos-x86_64/type/debug.bcfg",
+            "//tools/buckconfigs/macos-x86_64/base.bcfg",
+            "//tools/buckconfigs/base.bcfg",
+        ],
     },
-    'windows': {
-        'release': ['//tools/buckconfigs/windows-x86_64/toolchain/vsToolchainFlags.bcfg', '//tools/buckconfigs/windows-x86_64/type/release.bcfg', '//tools/buckconfigs/windows-x86_64/python/default.bcfg', '//tools/buckconfigs/windows-x86_64/base.bcfg', '//tools/buckconfigs/base.bcfg'],
-        'debug': ['//tools/buckconfigs/windows-x86_64/toolchain/vsToolchainFlags.bcfg', '//tools/buckconfigs/windows-x86_64/type/debug.bcfg', '//tools/buckconfigs/windows-x86_64/python/default.bcfg', '//tools/buckconfigs/windows-x86_64/base.bcfg', '//tools/buckconfigs/base.bcfg'],
+    "windows": {
+        "release": [
+            "//tools/buckconfigs/windows-x86_64/toolchain/vsToolchainFlags.bcfg",
+            "//tools/buckconfigs/windows-x86_64/type/release.bcfg",
+            "//tools/buckconfigs/windows-x86_64/python/default.bcfg",
+            "//tools/buckconfigs/windows-x86_64/base.bcfg",
+            "//tools/buckconfigs/base.bcfg",
+        ],
+        "debug": [
+            "//tools/buckconfigs/windows-x86_64/toolchain/vsToolchainFlags.bcfg",
+            "//tools/buckconfigs/windows-x86_64/type/debug.bcfg",
+            "//tools/buckconfigs/windows-x86_64/python/default.bcfg",
+            "//tools/buckconfigs/windows-x86_64/base.bcfg",
+            "//tools/buckconfigs/base.bcfg",
+        ],
     },
 }
 
@@ -33,23 +73,37 @@ def generate(flavors):
     osType, _, _, _, _, _ = platform.uname()
     osType = osType.lower()
     if osType not in PLATFORM_CXX_FLAGS:
-        raise Exception('Platform {} not supported!'.format(osType))
-    return itertools.chain.from_iterable([PLATFORM_CXX_FLAGS[osType][flavor] for flavor in flavors])
+        raise Exception("Platform {} not supported!".format(osType))
+    return itertools.chain.from_iterable(
+        [PLATFORM_CXX_FLAGS[osType][flavor] for flavor in flavors]
+    )
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Automatically set the proper config files for buck. This is selected based on the platform')
-    parser.add_argument('--flavors', dest='flavors', action='store', type=str, help='comma seperated list of flavors. Currently supported: release and debug')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Automatically set the proper config files for buck. This is selected based on the platform"
+    )
+    parser.add_argument(
+        "--flavors",
+        dest="flavors",
+        action="store",
+        type=str,
+        help="comma seperated list of flavors. Currently supported: release and debug",
+    )
 
     if len(sys.argv) == 1:
         parser.print_help(sys.stderr)
         sys.exit(1)
 
     args = parser.parse_args()
-    flavors = {flavor.lower() for flavor in args.flavors.split(',')}
+    flavors = {flavor.lower() for flavor in args.flavors.split(",")}
     if len(flavors - SUPPORTED_FLAVORS) > 0:
-        print('Invalid flavors were given: {}\n'.format(','.join(flavors - SUPPORTED_FLAVORS)))
+        print(
+            "Invalid flavors were given: {}\n".format(
+                ",".join(flavors - SUPPORTED_FLAVORS)
+            )
+        )
         parser.print_help(sys.stderr)
     else:
-        configs = ['--config-file\n{}'.format(config) for config in generate(flavors)]
-        print('\n'.join(configs))
+        configs = ["--config-file\n{}".format(config) for config in generate(flavors)]
+        print("\n".join(configs))
