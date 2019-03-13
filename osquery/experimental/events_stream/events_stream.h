da12514fb9463a0ec012f440bf3efe308b81f731
Alexander Kindyakov <akindyakov@fb.com>
2019-03-13 05:01:19 -0700

    Introduce events stream registry
    Summary:
    It is experimental registry to stream events from osquery. It is separated from logger registry, which is semantically very similar, on purpose.
    
    Firstly, because we are still not sure about exact way to process, filter, deliver events. I think the best way to test things is to implement pipeline for the events separately from the logger for now and may be merge them later. It's easier to merge things than split them.
    
    Secondary, current implementation of logger is not very performant with plenty of additional functionality. Since we going to evaluate performance hit of new type of events (linux syscall tracing) it is not a good idea to connect it to sophisticated system with well known performance problems.
    
    Reviewed By: jessek
    
    Differential Revision: D14404665
    
    fbshipit-source-id: c6d8b509f2364cca7e431146ee48dc6268c32242

---
+++ osquery/experimental/events_stream/events_stream.h
@@ -0,0 +1,21 @@
+/**
+ *  Copyright (c) 2014-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed in accordance with the terms specified in
+ *  the LICENSE file found in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include <string>
+
+namespace osquery {
+namespace experimental {
+namespace events {
+
+void dispatchSerializedEvent(const std::string& event);
+
+} // namespace events
+} // namespace experimental
+} // namespace osquery
