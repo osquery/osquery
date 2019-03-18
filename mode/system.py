9646d00f05230a8336c5bd16b884dcc20b7a2216
Filipe Manco <fmanco@fb.com>
2019-03-18 14:31:24 -0700

    Make system.py more generic
    Summary: system.py will need to support different distros. Make it more generic to simplify that.
    
    Reviewed By: marekcirkos
    
    Differential Revision: D14282633
    
    fbshipit-source-id: e333ac5f29ad8ff074b4c1132ee27c3bfdfda7d8

--- mode/system.py
+++ mode/system.py
@@ -4,77 +4,77 @@
 import sys
 import argparse
 import platform
-import itertools
 
 
-PLATFORM_CXX_FLAGS = {
-    "linux": {
-        "release": [
-            "//tools/buckconfigs/linux-x86_64/toolchain/ubuntu-18.04-clang.bcfg",
-            "//tools/buckconfigs/linux-x86_64/type/release.bcfg",
-            "//tools/buckconfigs/linux-x86_64/base.bcfg",
-            "//tools/buckconfigs/base.bcfg",
-        ],
-        "debug": [
-            "//tools/buckconfigs/linux-x86_64/toolchain/ubuntu-18.04-clang.bcfg",
-            "//tools/buckconfigs/linux-x86_64/type/debug.bcfg",
-            "//tools/buckconfigs/linux-x86_64/base.bcfg",
-            "//tools/buckconfigs/base.bcfg",
-        ],
+class Platform:
+    LINUX_X86_64 = "linux-x86_64"
+    MACOS_X86_64 = "macos-x86_64"
+    FREEBSD_X86_64 = "freebsd-x86_64"
+    WINDOWS_X86_64 = "windows-x86_64"
+
+
+class BuildType:
+    RELEASE = "release"
+    DEBUG = "debug"
+
+
+BCFG_TOOLCHAIN = {
+    Platform.LINUX_X86_64: "//tools/buckconfigs/linux-x86_64/toolchain/ubuntu-18.04-clang.bcfg",
+    Platform.MACOS_X86_64: None,
+    Platform.FREEBSD_X86_64: "//tools/buckconfigs/freebsd-x86_64/toolchain/freebsd-11.2-clang.bcfg",
+    Platform.WINDOWS_X86_64: "//tools/buckconfigs/windows-x86_64/toolchain/vsToolchainFlags.bcfg",
+}
+
+
+BCFG_TYPE = {
+    Platform.LINUX_X86_64: {
+        BuildType.RELEASE: "//tools/buckconfigs/linux-x86_64/type/release.bcfg",
+        BuildType.DEBUG: "//tools/buckconfigs/linux-x86_64/type/debug.bcfg",
     },
-    "freebsd": {
-        "release": [
-            "//tools/buckconfigs/freebsd-x86_64/toolchain/freebsd-11.2-clang.bcfg",
-            "//tools/buckconfigs/freebsd-x86_64/type/release.bcfg",
-            "//tools/buckconfigs/freebsd-x86_64/base.bcfg",
-            "//tools/buckconfigs/base.bcfg",
-        ],
-        "debug": [
-            "//tools/buckconfigs/freebsd-x86_64/toolchain/freebsd-11.2-clang.bcfg",
-            "//tools/buckconfigs/freebsd-x86_64/type/debug.bcfg",
-            "//tools/buckconfigs/freebsd-x86_64/base.bcfg",
-            "//tools/buckconfigs/base.bcfg",
-        ],
+    Platform.MACOS_X86_64: {
+        BuildType.RELEASE: "//tools/buckconfigs/macos-x86_64/type/release.bcfg",
+        BuildType.DEBUG: "//tools/buckconfigs/macos-x86_64/type/debug.bcfg",
     },
-    "darwin": {
-        "release": [
-            "//tools/buckconfigs/macos-x86_64/type/release.bcfg",
-            "//tools/buckconfigs/macos-x86_64/base.bcfg",
-            "//tools/buckconfigs/base.bcfg",
-        ],
-        "debug": [
-            "//tools/buckconfigs/macos-x86_64/type/debug.bcfg",
-            "//tools/buckconfigs/macos-x86_64/base.bcfg",
-            "//tools/buckconfigs/base.bcfg",
-        ],
+    Platform.FREEBSD_X86_64: {
+        BuildType.RELEASE: "//tools/buckconfigs/freebsd-x86_64/type/release.bcfg",
+        BuildType.DEBUG: "//tools/buckconfigs/freebsd-x86_64/type/debug.bcfg",
     },
-    "windows": {
-        "release": [
-            "//tools/buckconfigs/windows-x86_64/toolchain/vsToolchainFlags.bcfg",
-            "//tools/buckconfigs/windows-x86_64/type/release.bcfg",
-            "//tools/buckconfigs/windows-x86_64/python/default.bcfg",
-            "//tools/buckconfigs/windows-x86_64/base.bcfg",
-            "//tools/buckconfigs/base.bcfg",
-        ],
-        "debug": [
-            "//tools/buckconfigs/windows-x86_64/toolchain/vsToolchainFlags.bcfg",
-            "//tools/buckconfigs/windows-x86_64/type/debug.bcfg",
-            "//tools/buckconfigs/windows-x86_64/python/default.bcfg",
-            "//tools/buckconfigs/windows-x86_64/base.bcfg",
-            "//tools/buckconfigs/base.bcfg",
-        ],
+    Platform.WINDOWS_X86_64: {
+        BuildType.RELEASE: "//tools/buckconfigs/windows-x86_64/type/release.bcfg",
+        BuildType.DEBUG: "//tools/buckconfigs/windows-x86_64/type/debug.bcfg",
     },
 }
 
-SUPPORTED_FLAVORS = ["debug", "release"]
+
+BCFG_PLATFORM_BASE = {
+    Platform.LINUX_X86_64: "//tools/buckconfigs/linux-x86_64/base.bcfg",
+    Platform.MACOS_X86_64: "//tools/buckconfigs/macos-x86_64/base.bcfg",
+    Platform.FREEBSD_X86_64: "//tools/buckconfigs/freebsd-x86_64/base.bcfg",
+    Platform.WINDOWS_X86_64: "//tools/buckconfigs/windows-x86_64/base.bcfg",
+}
 
 
-def generate(flavor):
-    osType, _, _, _, _, _ = platform.uname()
-    osType = osType.lower()
-    if osType not in PLATFORM_CXX_FLAGS:
-        raise Exception("Platform {} not supported!".format(osType))
-    return PLATFORM_CXX_FLAGS[osType][flavor]
+BCFG_PLATFORM = "//tools/buckconfigs/base.bcfg"
+
+
+def generate_config_file_flag(bcfg):
+    return "--config-file\n" + bcfg if bcfg else ""
+
+
+def generate_toolchain(build_platform, build_type):
+    return generate_config_file_flag(BCFG_TOOLCHAIN[build_platform])
+
+
+def generate_type(build_platform, build_type):
+    return generate_config_file_flag(BCFG_TYPE[build_platform][build_type])
+
+
+def generate_platform_base(build_platform):
+    return generate_config_file_flag(BCFG_PLATFORM_BASE[build_platform])
+
+
+def generate_base():
+    return generate_config_file_flag(BCFG_PLATFORM)
 
 
 # Buck does not allow this script to fail, so lets pass an invalid flag ---- to
@@ -83,6 +83,31 @@ def generate(flavor):
 def fail(message):
     print("---- || {} || ----".format(message))
     sys.exit(1)
+
+
+def get_platform():
+    os = platform.system().lower()
+    arch = platform.machine().lower()
+
+    if os == "linux" and arch == "x86_64":
+        return Platform.LINUX_X86_64
+    elif os == "darwin" and arch == "x86_64":
+        return Platform.MACOS_X86_64
+    elif os == "freebsd" and arch == "x86_64":
+        return Platform.FREEBSD_X86_64
+    elif os == "windows" and arch == "x86_64":
+        return Platform.WINDOWS_X86_64
+    else:
+        fail("Unsupported platform {} {}".format(os, arch))
+
+
+def get_build_type(build_type):
+    if build_type == "release":
+        return BuildType.RELEASE
+    elif build_type == "debug":
+        return BuildType.DEBUG
+    else:
+        fail("Unsupported build type {}".format(build_type))
 
 
 if __name__ == "__main__":
@@ -100,10 +125,19 @@ if __name__ == "__main__":
         action="store",
         type=str,
         help="comma seperated list of flavors. Currently supported: release and debug",
-        choices=SUPPORTED_FLAVORS,
-        default="release",
+        choices=[BuildType.RELEASE, BuildType.DEBUG],
+        default=BuildType.RELEASE,
     )
 
     args = parser.parse_args()
-    configs = ["--config-file\n{}".format(config) for config in generate(args.flavors)]
+
+    build_platform = get_platform()
+    build_type = get_build_type(args.flavors)
+
+    configs = [
+        generate_toolchain(build_platform, build_type),
+        generate_type(build_platform, build_type),
+        generate_platform_base(build_platform),
+        generate_base(),
+    ]
     print("\n".join(configs))
