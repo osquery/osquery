e6fe15eb49660725e65dba1549932ed96e0a8c6e
Teddy Reed <teddy@casualhacking.io>
2019-06-23 01:05:42 -0400

    macos: Add hack for boost asio string_view detection (#5592)

--- osquery/remote/http_client.h
+++ osquery/remote/http_client.h
@@ -19,6 +19,12 @@
 #define OPENSSL_NO_MD5 1
 #define OPENSSL_NO_DEPRECATED 1
 
+// TODO(5591) Remove this when addressed by Boost's ASIO config.
+// https://www.boost.org/doc/libs/1_67_0/boost/asio/detail/config.hpp
+// Standard library support for std::string_view.
+#define BOOST_ASIO_HAS_STD_STRING_VIEW 1
+#define BOOST_ASIO_DISABLE_STD_STRING_VIEW 1
+
 // clang-format off
 // Keep it on top of all other includes to fix double include WinSock.h header file
 // which is windows specific boost build problem
--- osquery/tables/applications/posix/docker.cpp
+++ osquery/tables/applications/posix/docker.cpp
@@ -16,6 +16,12 @@
 #include <boost/algorithm/string/split.hpp>
 #include <boost/property_tree/json_parser.hpp>
 
+// TODO(5591) Remove this when addressed by Boost's ASIO config.
+// https://www.boost.org/doc/libs/1_67_0/boost/asio/detail/config.hpp
+// Standard library support for std::string_view.
+#define BOOST_ASIO_HAS_STD_STRING_VIEW 1
+#define BOOST_ASIO_DISABLE_STD_STRING_VIEW 1
+
 #include <boost/asio.hpp>
 #include <boost/foreach.hpp>
 
