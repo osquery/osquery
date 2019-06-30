13faab396ab03d56e9dbc77af38b8346c66e3dff
Stefano Bonicatti <stefano.bonicatti@gmail.com>
2019-06-30 19:17:12 +0200

    Fix detection of some headers on some IDEs
    Some IDE require that the headers are assigned to a target, so they know
    which TU uses them and also that they are part of the project.
    The option ADD_HEADERS_AS_SOURCES has been added, if it's ON,
    we assign those headers as INTERFACE sources of the target that publish
    them.

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -15,6 +15,8 @@ endif()
 # This may be useful to speed up development builds
 option(BUILD_SHARED_LIBS "Whether to build shared libraries (like *.dll or *.so) or static ones (like *.a)" ${BUILD_SHARED_LIBS_DEFAULT_VALUE})
 
+option(ADD_HEADERS_AS_SOURCES "Whether to add headers as sources of a target or not. This is needed for some IDEs which wouldn't detect headers properly otherwise")
+
 # This is the default S3 storage used by Facebook to store 3rd party dependencies; it
 # is provided here as a configuration option
 if("${THIRD_PARTY_REPOSITORY_URL}" STREQUAL "")
