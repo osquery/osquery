7529da2624eb8052826bcdf9617c7ad9732a00c2
puffyCid <puffycyan@gmail.com>
2019-07-27 17:54:45 -0400

    Added tests, removed diagnostic files

---
+++ tests/integration/tables/userassist.cpp
@@ -0,0 +1,23 @@
+#include <osquery/tests/integration/tables/helper.h>
+
+namespace osquery {
+namespace table_tests {
+class UserassistTest : public testing::Test {
+ protected:
+  void SetUp() override {
+    setUpEnvironment();
+  }
+};
+
+TEST_F(UserassistTest, test_sanity) {
+  QueryData const rows = execute_query("select * from userassist");
+  ValidatatioMap row_map = {
+      {"path", NonEmptyString},
+      {"last_execution_time", NormalType},
+      {"count", NormalType},
+      {"sid", NonEmptyString},
+  };
+  validate_rows(rows, row_map);
+}
+} // namespace table_tests
+} // namespace osquery
