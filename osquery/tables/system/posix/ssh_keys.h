675cdd9658cd53b7caaaaa2458b5594faaf06972
Robbie Ostrow <ostrowr@gmail.com>
2019-08-26 02:28:21 -0700

    Use OpenSSL to parse SSH private keys.
    Also add some tests. The ed25519 tests require a new version of openssl so are currently
    failing.

---
+++ osquery/tables/system/posix/ssh_keys.h
@@ -0,0 +1,32 @@
+/**
+ *  Copyright (c) 2014-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed in accordance with the terms specified in
+ *  the LICENSE file found in the root directory of this source tree.
+ */
+
+#include <osquery/query.h>
+#include <osquery/tables.h>
+
+#include <string>
+
+namespace osquery {
+namespace tables {
+
+const std::string kSSHUserKeysDir = ".ssh/";
+
+void genSSHkeyForHosts(const std::string& uid,
+                       const std::string& gid,
+                       const std::string& directory,
+                       QueryData& results);
+
+QueryData getUserSshKeys(QueryContext& context);
+
+} // namespace tables
+} // namespace osquery
+
+// TODOS
+// - Copyright header still Facebook?
+// - Include tests in buck build?
+// - Would like a third-party ssh key parser
