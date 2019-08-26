caff105c7566011535aa8bf58f6afad4f3508b08
Alessandro Gario <alessandro.gario@gmail.com>
2019-08-26 16:25:31 +0200

    Update the license header used on smart_drives and windows_crashes

--- osquery/tables/system/windows/windows_crashes.cpp
+++ osquery/tables/system/windows/windows_crashes.cpp
@@ -1,12 +1,10 @@
-/*
-*  Copyright (c) 2014-present, Facebook, Inc.
-*  All rights reserved.
-*
-*  This source code is licensed under the BSD-style license found in the
-*  LICENSE file in the root directory of this source tree. An additional grant
-*  of patent rights can be found in the PATENTS file in the same directory.
-*
-*/
+/**
+ *  Copyright (c) 2014-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed in accordance with the terms specified in
+ *  the LICENSE file found in the root directory of this source tree.
+ */
 
 #include <osquery/utils/system/system.h>
 
