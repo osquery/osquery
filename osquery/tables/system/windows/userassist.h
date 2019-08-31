a270bd73d5cbaa1acc98b4e5205c275f22edaf21
puffyCid <puffycyan@gmail.com>
2019-08-30 23:48:51 -0400

    Updated tests

---
+++ osquery/tables/system/windows/userassist.h
@@ -0,0 +1,18 @@
+/**
+ *  Copyright (c) 2014-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed in accordance with the terms specified in
+ *  the LICENSE file found in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include <osquery/core.h>
+#include <osquery/tables.h>
+namespace osquery {
+namespace tables {
+
+std::string rot_decode(std::string& value_key_reg);
+} // namespace tables
+} // namespace osquery
--- tests/integration/tables/userassist.cpp
+++ tests/integration/tables/userassist.cpp
@@ -1,3 +1,4 @@
+#include <osquery/tables/system/windows/userassist.h>
 #include <osquery/tests/integration/tables/helper.h>
 
 namespace osquery {
@@ -9,32 +10,13 @@ class UserassistTest : public testing::Test {
   }
 };
 
-std::string rot_decode(std::string& value_key_reg) {
-  std::string decoded_value_key;
-
-  for (std::size_t i = 0; i < value_key_reg.size(); i++) {
-    if (isalpha(value_key_reg[i])) {
-      if (value_key_reg[i] >= 'a' && value_key_reg[i] <= 'm') {
-        decoded_value_key.append(1, value_key_reg[i] + 13);
-      } else if (value_key_reg[i] >= 'm' && value_key_reg[i] <= 'z') {
-        decoded_value_key.append(1, value_key_reg[i] - 13);
-      } else if (value_key_reg[i] >= 'A' && value_key_reg[i] <= 'M') {
-        decoded_value_key.append(1, value_key_reg[i] + 13);
-      } else if (value_key_reg[i] >= 'M' && value_key_reg[i] <= 'Z') {
-        decoded_value_key.append(1, value_key_reg[i] - 13);
-      }
-    } else {
-      decoded_value_key.append(1, value_key_reg[i]);
-    }
-  }
-  return decoded_value_key;
-}
-
+namespace tables {
 TEST(Rot13Test, DecodeData) {
   std::string encoded_data = "Gur dhvpx oebja sbk whzcf bire gur ynml qbt";
   std::string decoded_data = rot_decode(encoded_data);
   ASSERT_TRUE(decoded_data == "The quick brown fox jumps over the lazy dog");
 }
+} // namespace tables
 
 TEST_F(UserassistTest, test_sanity) {
   QueryData const rows = execute_query("select * from userassist");
