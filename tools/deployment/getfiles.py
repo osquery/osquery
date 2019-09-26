31e6dfdfebeb237149aa8e76fc1b225995aea2bd
grrrrrrrrrr <grantseltzer@gmail.com>
2019-09-26 07:32:21 -0400

    Fix make rpm script to not break because of symlinks (#5780)
    Signed-off-by: grant <grantseltzer@gmail.com>

--- tools/deployment/getfiles.py
+++ tools/deployment/getfiles.py
@@ -49,5 +49,5 @@ if __name__ == "__main__":
             continue
         if file['file'].find("gtest") > 0:
             continue
-        print(file['file'].replace(args.base, ""))
+        print(file['file'])
         pass
