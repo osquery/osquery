3fbbc9a1fd8d0d56cfdc401c53c4abf7ead38d1d
William Woodruff <william@trailofbits.com>
2019-10-01 17:08:18 -0400

    osquery, specs: Tweak error message, move alias code

--- osquery/tables/system/darwin/firewall.cpp
+++ osquery/tables/system/darwin/firewall.cpp
@@ -98,7 +98,7 @@ QueryData parseALFExceptionsTree(const pt::ptree& tree) {
       auto status = pathFromNestedPlistAliasData(alias_data, path);
 
       if (!status.ok()) {
-        TLOG << status.getMessage();
+        TLOG << "Could not parse nested plist for applications: " << status.getMessage();
         continue;
       }
 
