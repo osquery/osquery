1737f756a79d2769425b76bdefd9600dc65b3d67
puffyCid <puffycyan@gmail.com>
2019-10-06 18:00:33 -0400

    Formatting

--- tests/integration/tables/bam.cpp
+++ tests/integration/tables/bam.cpp
@@ -20,13 +20,15 @@ class BamTest : public testing::Test {
 TEST_F(BamTest, test_sanity) {
   QueryData const rows = execute_query("select * from bam;");
   QueryData const specific_query_rows =
-      execute_query("select * from bam where path is 'SequenceNumber'; ");
+      execute_query("select * from bam where path is 'SequenceNumber';");
   ASSERT_GT(rows.size(), 0ul);
-  ASSERT_GT(rows.size(), 1ul);
+  ASSERT_GT(specific_query_rows.size(), 1ul);
 
-  ValidatatioMap row_map = {{"path", NonEmptyString},
-                            {"last_execution_time", NormalType},
-                            {"sid", NonEmptyString}};
+  ValidatationMap row_map = {
+      {"path", NonEmptyString},
+      {"last_execution_time", NormalType},
+      {"sid", NonEmptyString},
+  };
   validate_rows(rows, row_map);
   validate_rows(specific_query_rows, row_map);
 }
