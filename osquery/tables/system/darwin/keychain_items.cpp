f6c2bb740bd6309a66f32cb83f73f7a8d0aeccb8
Will Newton <will.newton@gmail.com>
2019-10-08 15:30:15 +0100

    Make macOS keychain handling more robust
    When requesting kSecClassIdentity items from the keychain sometimes
    invalid items are returned. These cause errSecInvalidItemRef to
    be returned from SecKeychainItemCopyAttributesAndData and result
    in an empty row in the table. Catch the error and avoid returning
    empty rows.

--- osquery/tables/system/darwin/keychain_items.cpp
+++ osquery/tables/system/darwin/keychain_items.cpp
@@ -55,8 +55,16 @@ void genKeychainItem(const SecKeychainItemRef& item, QueryData& results) {
   // Any tag that does not exist for the item will prevent the entire result.
   for (const auto& attr_tag : kKeychainItemAttrs) {
     tags[0] = attr_tag.first;
-    SecKeychainItemCopyAttributesAndData(
+    auto status = SecKeychainItemCopyAttributesAndData(
         item, &info, &item_class, &attr_list, 0, nullptr);
+
+    if (status == errSecNoSuchAttr) {
+      // This attr does not exist, skip it.
+      continue;
+    } else if (status != errSecSuccess) {
+      // If this keychain item is not valid then don't add it to results.
+      return;
+    }
 
     if (attr_list != nullptr) {
       // Expect each specific tag to return string data.
