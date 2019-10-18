5236fd4040ac7e9f708bd088a0477c4a9e2bf098
santhoshnathan211 <56686978+santhoshnathan211@users.noreply.github.com>
2019-10-18 21:31:13 +0630

    Create something2.cpp

---
+++ santhoshnathan/something2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>  
+using namespace std;  
+int main()  
+{  
+   int i,fact=1,number;    
+  cout<<"Enter any Number: ";    
+ cin>>number;    
+  for(i=1;i<=number;i++){    
+      fact=fact*i;    
+  }    
+  cout<<"Factorial of " <<number<<" is: "<<fact<<endl;  
+  return 0;  
+}  
