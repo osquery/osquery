8c3e85be8c358206709c6c4dff382f9a76e718d6
Stefano Bonicatti <stefano.bonicatti@gmail.com>
2019-10-25 00:20:33 +0200

    Fix again osqueryi link

--- tools/tests/winexpect.py
+++ tools/tests/winexpect.py
@@ -47,7 +47,6 @@ class REPLWrapper(object):
             return res
         try:
             command = command + '\r\n'
-            print(command)
             self.child.proc.stdin.write(command.encode())
             self.child.proc.stdin.flush()
 
