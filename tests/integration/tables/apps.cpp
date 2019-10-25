c15a908996eeae216bc78fbf64288dafaa141d2b
milkmix <julien.bachmann@gmail.com>
2019-10-25 23:51:37 +0200

    corrected formatting

--- tests/integration/tables/apps.cpp
+++ tests/integration/tables/apps.cpp
@@ -25,25 +25,25 @@ TEST_F(AppsTest, test_sanity) {
   auto const data = execute_query("select * from apps");
   ASSERT_GE(data.size(), 1ul);
   ValidationMap row_map = {
-       {"name", NonEmptyString}
-       {"path", DirectoryOnDisk}
-       {"bundle_executable", NormalType}
-       {"bundle_identifier", NormalType}
-       {"bundle_name", NormalType}
-       {"bundle_short_version", NormalType}
-       {"bundle_version", NormalType}
-       {"bundle_package_type", NonEmptyString}
-       {"environment", NormalType}
-       {"element", IntType}
-       {"compiler", NormalType}
-       {"development_region", NormalType}
-       {"display_name", NormalType}
-       {"info_string", NormalType}
-       {"minimum_system_version", NormalType}
-       {"category", NormalType}
-       {"applescript_enabled", IntType}
-       {"copyright", NormalType}
-       {"last_opened_time", NormalType}
+      {"name", NonEmptyString},
+      {"path", DirectoryOnDisk},
+      {"bundle_executable", NormalType},
+      {"bundle_identifier", NormalType},
+      {"bundle_name", NormalType},
+      {"bundle_short_version", NormalType},
+      {"bundle_version", NormalType},
+      {"bundle_package_type", NonEmptyString},
+      {"environment", NormalType},
+      {"element", IntType},
+      {"compiler", NormalType},
+      {"development_region", NormalType},
+      {"display_name", NormalType},
+      {"info_string", NormalType},
+      {"minimum_system_version", NormalType},
+      {"category", NormalType},
+      {"applescript_enabled", IntType},
+      {"copyright", NormalType},
+      {"last_opened_time", NormalType},
   };
   validate_rows(data, row_map);
 }
