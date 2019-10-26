9cd499981c311ac50682cc7ef3bce5f2777140e8
Teddy Reed <teddy@casualhacking.io>
2019-10-26 12:24:06 -0400

    tests: Remove json-exponent test

--- osquery/utils/json/tests/json.cpp
+++ osquery/utils/json/tests/json.cpp
@@ -240,11 +240,4 @@ TEST_F(ConversionsTests, test_json_iterativeparsing) {
 
   EXPECT_FALSE(doc.fromString(json).ok());
 }
-
-TEST_F(ConversionsTests, test_json_largeexp) {
-  std::string json("0.0000074836628E-2147483636");
-  auto doc = JSON::newObject();
-
-  EXPECT_TRUE(doc.fromString(json).ok());
-}
 } // namespace osquery
