a7d744a16977acafe0c10fc4573977e1a49b8d5f
howar6hill <howar6hill@yahoo.com>
2019-10-28 17:29:13 +0800

    improve etc_protocols test

--- tests/integration/tables/etc_protocols.cpp
+++ tests/integration/tables/etc_protocols.cpp
@@ -26,7 +26,7 @@ TEST_F(EtcProtocolsTest, test_sanity) {
   auto const row_map = ValidationMap{
       {"name", NonEmptyString},
       {"number", NonNegativeInt},
-      {"alias", NonEmptyString},
+      {"alias", NonNull},
       {"comment", NormalType},
   };
   validate_rows(rows, row_map);
