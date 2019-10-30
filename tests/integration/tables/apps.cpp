99a0db55c10db7974a590ea1a01aabbd5c589d00
milkmix <julien.bachmann@gmail.com>
2019-10-30 17:02:44 +0100

    changed tests for 2 values to be more concrete

--- tests/integration/tables/apps.cpp
+++ tests/integration/tables/apps.cpp
@@ -10,6 +10,7 @@
 // Spec file: specs/darwin/apps.table
 
 #include <osquery/tests/integration/tables/helper.h>
+#include <osquery/utils/conversions/tryto.h>
 
 namespace osquery {
 namespace table_tests {
@@ -23,6 +24,18 @@ class AppsTest : public testing::Test {
 
 TEST_F(AppsTest, test_sanity) {
   auto const data = execute_query("select * from apps");
+
+  auto verify_zero_one_or_empty = [](std::string const& value) {
+    if (value.empty()) {
+      return true;
+    }
+    auto cast_result = tryTo<int64_t>(value);
+    if (!cast_result) {
+      return false;
+    }
+    return ((cast_result.get() == 0) || (cast_result.get() == 1));
+  };
+
   ASSERT_GE(data.size(), 1ul);
   ValidationMap row_map = {
       {"name", NonEmptyString},
@@ -34,14 +47,14 @@ TEST_F(AppsTest, test_sanity) {
       {"bundle_version", NormalType},
       {"bundle_package_type", NormalType},
       {"environment", NormalType},
-      {"element", NormalType},
+      {"element", verify_zero_one_or_empty},
       {"compiler", NormalType},
       {"development_region", NormalType},
       {"display_name", NormalType},
       {"info_string", NormalType},
       {"minimum_system_version", NormalType},
       {"category", NormalType},
-      {"applescript_enabled", NormalType},
+      {"applescript_enabled", verify_zero_one_or_empty},
       {"copyright", NormalType},
       {"last_opened_time", NormalType},
   };
