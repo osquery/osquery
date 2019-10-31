dee0212433dfcd6d0885e54679fbcc7d9edd653e
Stefano Bonicatti <stefano.bonicatti@gmail.com>
2019-10-31 13:38:46 +0100

    Fix a race condition between Thrift server stop() and listen() (#5937)
    If a Thrift server is requested to stop before everything is
    properly initialized a race condition could happen.
    Specifically if stopping during a listen() a race condition will happen
    accessing childInterruptSockWriter_, which is a socket initialized by listen(),
    in a non thread-safe way, and used by stop() to notify other threads
    that they should shutdown.

--- osquery/extensions/impl_thrift.cpp
+++ osquery/extensions/impl_thrift.cpp
@@ -8,6 +8,7 @@
 
 #include <osquery/core.h>
 #include <osquery/filesystem/filesystem.h>
+#include <osquery/logger.h>
 #include <osquery/system.h>
 
 #include <thrift/concurrency/ThreadManager.h>
@@ -30,6 +31,11 @@
 
 #include <limits>
 
+#include <boost/chrono/include.hpp>
+#include <boost/thread/condition_variable.hpp>
+#include <boost/thread/lock_types.hpp>
+#include <boost/thread/locks.hpp>
+
 namespace osquery {
 
 using namespace apache::thrift::protocol;
@@ -44,6 +50,46 @@ using TPlatformSocket = TPipe;
 using TPlatformServerSocket = TServerSocket;
 using TPlatformSocket = TSocket;
 #endif
+
+class ThriftServerEventHandler : public TServerEventHandler,
+                                 boost::noncopyable {
+ public:
+  ThriftServerEventHandler() : server_is_listening_{false} {}
+
+  void preServe() override {
+    boost::unique_lock<boost::mutex> lock(m_);
+    server_is_listening_ = true;
+    server_is_listening_cv_.notify_all();
+  }
+
+  /**
+    Waits either until the preServe() function is called,
+    so until the TServerSocket has exited from its listen(),
+    or a one minute timeout is reached.
+    The one minute timeout is there only to mitigate a possible developer error.
+  */
+  void waitUntilServerIsListening() {
+    boost::unique_lock<boost::mutex> lock(m_);
+    if (!server_is_listening_cv_.wait_for(
+            lock, boost::chrono::minutes(1), [this] {
+              return server_is_listening_;
+            })) {
+      VLOG(1)
+          << "Wait for Thrift server listen() has timed out. A tentative of "
+             "stopping a server that will never start has been attempted";
+    }
+  }
+
+  void reset() {
+    boost::unique_lock<boost::mutex> lock(m_);
+    server_is_listening_ = false;
+  }
+
+ private:
+  boost::condition_variable server_is_listening_cv_;
+  boost::mutex m_;
+  bool server_is_listening_;
+};
 
 class ExtensionHandler : virtual public extensions::ExtensionIf,
                          public ExtensionInterface {
@@ -119,6 +165,7 @@ struct ImplExtensionRunner {
   std::shared_ptr<TServerTransport> transport;
   std::shared_ptr<TThreadedServer> server;
   std::shared_ptr<TProcessor> processor;
+  std::shared_ptr<ThriftServerEventHandler> server_event_handler;
 };
 
 struct ImplExtensionClient {
@@ -277,6 +324,9 @@ void ExtensionRunnerInterface::connect() {
 
   server_->server = std::make_shared<TThreadedServer>(
       server_->processor, server_->transport, transport_fac, protocol_fac);
+
+  server_->server_event_handler = std::make_shared<ThriftServerEventHandler>();
+  server_->server->setServerEventHandler(server_->server_event_handler);
 }
 
 void ExtensionRunnerInterface::init(RouteUUID uuid, bool manager) {
@@ -297,6 +347,8 @@ void ExtensionRunnerInterface::init(RouteUUID uuid, bool manager) {
 void ExtensionRunnerInterface::stopServer() {
   // In most cases the service thread has started before the stop request.
   if (server_->server != nullptr) {
+    server_->server_event_handler->waitUntilServerIsListening();
+    server_->server_event_handler->reset();
     server_->server->stop();
   }
 }
