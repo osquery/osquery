7fee358178e98c75a7853dc73eb2aa403ce81672
Alessandro Gario <5714290+alessandrogario@users.noreply.github.com>
2019-11-02 02:25:14 +0100

    CMake: Update the OpenSSL formula (#5976)

--- tools/ci/scripts/install_openssl_formula_dependencies.ps1
+++ tools/ci/scripts/install_openssl_formula_dependencies.ps1
@@ -13,10 +13,3 @@
 # It does not seem that they have HTTPS: https://github.com/StrawberryPerl/strawberryperl.com/issues/11
 Invoke-WebRequest "http://strawberryperl.com/download/5.30.0.1/strawberry-perl-5.30.0.1-64bit.zip" -OutFile "$env:TEMP\strawberry_perl.zip"
 Expand-Archive "$env:TEMP\strawberry_perl.zip" -DestinationPath "C:\Strawberry" -Force
-
-#
-# Download and install the NASM assembler
-#
-
-Invoke-WebRequest "https://www.nasm.us/pub/nasm/releasebuilds/2.14.02/win64/nasm-2.14.02-installer-x64.exe" -OutFile "$env:TEMP\nasm_installer.exe"
-Start-Process "$env:TEMP\nasm_installer.exe" -Wait -ArgumentList "/S", "/D=C:\Program Files\NASM"
