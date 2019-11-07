0db80210be6b9642472cafd0195f13a63c418da0
AlexM <20775507+packetzero@users.noreply.github.com>
2019-11-06 19:34:54 -0600

    update to latest master

--- plugins/logger/aws_kinesis.cpp
+++ plugins/logger/aws_kinesis.cpp
@@ -23,18 +23,36 @@
 #include <boost/uuid/uuid_io.hpp>
 
 #include <osquery/flags.h>
+#include <osquery/logger.h>
 #include <osquery/process/process.h>
 #include <osquery/registry.h>
 #include <osquery/system.h>
 
+#include <osquery/utils/aws/aws_util.h>
+
+namespace fs = boost::filesystem;
+
+#ifdef WIN32
+#undef GetMessage
+#endif
+
+// If there are AWS errors, refresh the handle, which may pull in newer
+// FLAGS_aws_xx config such as the sts token.  But not more often than
+// AWS_HANDLE_REFRESH_SECONDS
+
+#define AWS_HANDLE_REFRESH_SECONDS 450 // 7.5 minutes (config refresh 5 mins)
+
 namespace osquery {
+
+DECLARE_string(aws_access_key_id);  // from aws_util.cpp
+DECLARE_string(aws_session_token);
 
 REGISTER(KinesisLoggerPlugin, "logger", "aws_kinesis");
 
 FLAG(uint64,
      aws_kinesis_period,
-     10,
-     "Seconds between flushing logs to Kinesis (default 10)");
+     8,
+     "Seconds between flushing logs to Kinesis (default 8)");
 
 FLAG(string, aws_kinesis_stream, "", "Name of Kinesis stream for logging")
 
@@ -43,25 +61,280 @@ FLAG(bool,
      false,
      "Enable random kinesis partition keys");
 
-Status KinesisLoggerPlugin::setUp() {
-  initAwsSdk();
-  forwarder_ = std::make_shared<KinesisLogForwarder>(
-      "aws_kinesis", FLAGS_aws_kinesis_period, 500);
-  Status s = forwarder_->setUp();
-  if (!s.ok()) {
-    LOG(ERROR) << "Error initializing Kinesis logger: " << s.getMessage();
-    return s;
+// Kinesis stream limit of 1000 records per second
+FLAG(uint64,
+     aws_kinesis_batch_max_records,
+     500U,
+     "Number of records per period");
+
+FLAG(uint64, aws_kinesis_batch_max_bytes, 5000000U, "Max bytes per batch");
+
+static bool gHaveSetup = false;
+
+class KinesisForwarder : public Forwarder {
+ public:
+  KinesisForwarder(const LoggerBounds bounds)
+      : Forwarder(bounds), linebuf_(bounds.max_bytes_per_record), _condition_lock(), _condition() {
+    partition_key_ = getHostIdentifier();
   }
-  Dispatcher::addService(forwarder_);
+  virtual ~KinesisForwarder() {}
+
+  Status send(std::string file_path) override {
+    Aws::Vector<Aws::Kinesis::Model::PutRecordsRequestEntry> records;
+
+    if (aws_id_changed() || client_ == nullptr) {
+      if (!shouldResetAwsHandle()) {
+        return Status(FORWARDER_STATUS_NO_CONNECTION);
+      }
+    }
+
+    // parse file into records
+
+    int readerr = _load(file_path, records);
+    if (readerr != 0) {
+      return Status(readerr, "Error reading file");
+    }
+
+    // build request
+
+    Aws::Kinesis::Model::PutRecordsRequest request;
+    request.WithStreamName(FLAGS_aws_kinesis_stream).SetRecords(records);
+
+    // do the send, with some retries
+
+    int retry;
+    for (retry = 0; retry < 3; retry++) {
+      if (retry > 0) {
+	    _pause(std::chrono::milliseconds(100));
+      }
+      auto outcome = client_->PutRecords(request);
+
+      if (outcome.IsSuccess()) {
+        break;
+      }
+
+      int failedCount = outcome.GetResult().GetFailedRecordCount();
+      if (failedCount > 0 && failedCount < (int)records.size()) {
+        LOG(WARNING) << "partial failure: " << failedCount << " of " << records.size();
+
+        // remove successful entries
+        const std::vector<Aws::Kinesis::Model::PutRecordsResultEntry> &refResults = outcome.GetResult().GetRecords();
+        auto it = records.begin();
+        int numRecords = (int)records.size();
+        for (int i = 0; i < numRecords; i++) {
+          if (i >= (int)refResults.size()) {
+            break; // just in case. results should match records.size
+          }
+          const Aws::Kinesis::Model::PutRecordsResultEntry &entry = refResults[i];
+          const Aws::String & refEntryErrCode = entry.GetErrorCode();
+          if (refEntryErrCode.empty()) {
+            records.erase(it++);  // remove successful
+          } else {
+            it++;
+          }
+        }
+
+        continue; // will retry
+      }
+
+      const Aws::Kinesis::KinesisErrors code = outcome.GetError().GetErrorType();
+
+      // network connection down?
+
+      if (code == Aws::Kinesis::KinesisErrors::NETWORK_CONNECTION) {
+        return Status(FORWARDER_STATUS_NO_CONNECTION);
+      }
+      if (code == Aws::Kinesis::KinesisErrors::SLOW_DOWN) {
+        LOG(WARNING) << "Received Kinesis SLOW_DOWN";
+        _pause(std::chrono::milliseconds(500));
+        return Status(FORWARDER_STATUS_NO_CONNECTION);
+      }
+
+      if (code == Aws::Kinesis::KinesisErrors::ACCESS_DENIED
+          || code == Aws::Kinesis::KinesisErrors::MISSING_AUTHENTICATION_TOKEN) {
+        client_ = nullptr;
+        LOG(WARNING) << "kinesis access denied. clearing handle";
+        return Status(FORWARDER_STATUS_NO_CONNECTION);
+      }
+
+      // anything we can retry?
+
+      if (code == Aws::Kinesis::KinesisErrors::REQUEST_EXPIRED
+          || code == Aws::Kinesis::KinesisErrors::INTERNAL_FAILURE) {
+        LOG(WARNING) << "kinesis failed with code:" << std::to_string((uint32_t)code);
+        continue;
+      }
+
+      // any other error probably can't be fixed by retry
+      LOG(ERROR) << "send fail. Code:" << (uint32_t)code << " msg:" << outcome.GetError().GetMessage();
+
+      // invalidate handle because:
+      //   Sometimes get code==UNKNOWN error with ExpiredTokenException in the
+      //   message string rather than code == ACCESS_DENIED
+
+      client_ = nullptr;
+
+      return Status((int)code, "error in send");
+    }
+
+    if (retry >= 3) {
+      return Status(1, "Failed with retries");
+    }
+
+    return Status();
+  }
+
+  /**
+   * keeps track of FLAGS_aws_access_key_id, return true if changed.
+   */
+  bool aws_id_changed() {
+    static std::string last_aws_access_key;
+    static std::string last_aws_session_token;
+
+    if (last_aws_access_key != FLAGS_aws_access_key_id
+        || last_aws_session_token != FLAGS_aws_session_token ) {
+      VLOG(1) << "using new aws access key:" << FLAGS_aws_access_key_id;
+      last_aws_access_key = FLAGS_aws_access_key_id;
+      last_aws_session_token = FLAGS_aws_session_token;
+      return true;
+    }
+
+    return false;
+  }
+
+  /*
+   * This should only be called when there was a send error.
+   * This will call makeAWSClient(client_) to obtain a new
+   * handle with latest FLAGS , in case there was a config change
+   * for IAM role, etc.
+   */
+  bool shouldResetAwsHandle() {
+    static time_t tLastReset = 0;
+    time_t now = time(NULL);
+    if ((now - tLastReset) >= AWS_HANDLE_REFRESH_SECONDS) {
+      tLastReset = now;
+      VLOG(1) << "obtaining new AWS client handle.";
+      Status s = makeAWSClient<Aws::Kinesis::KinesisClient>(client_);
+      return s.ok();
+    }
+    return false;
+  }
+
+  /*
+   * read file lines, populate records.
+   * returns false on success, true on error.
+   */
+  int _load(
+      std::string file_path,
+      Aws::Vector<Aws::Kinesis::Model::PutRecordsRequestEntry>& records) {
+    file_path = fs::path(file_path).make_preferred().string();
+
+    FILE* fp = FOPEN(file_path.c_str(), "r");
+    if (fp == nullptr) {
+      LOG(ERROR) << "unable to open cached log:" << file_path;
+      return FORWARDER_STATUS_READ_ERROR;
+    }
+
+    while (nullptr != fgets(linebuf_.data(), (int)linebuf_.size(), fp)) {
+      size_t len = strlen(linebuf_.data());
+      if (len <= 1)
+        continue;
+
+      if (linebuf_.data()[0] == '#') {
+        // metadata (TODO: check against MD5 of previous file)
+        continue;
+      }
+
+      // adjust len to ignore trailing newline
+      len--;
+
+      // check for truncated msgs - expect valid JSON object
+      if (linebuf_[len-1] != '}') {
+        LOG(WARNING) << "Drop Truncated : (possibly due to watchdog kill):" << std::string(linebuf_.data());
+        continue;
+      }
+
+      // Initialize and store the new log record
+      auto buffer = Aws::Utils::ByteBuffer(
+          reinterpret_cast<unsigned char*>(linebuf_.data()), len);
+
+      auto idx = records.size();
+      if (idx > bounds_.max_records_per_batch) {
+        LOG(ERROR) << "cached log file failed bounds check idx:" << idx
+                   << " max_records:" << bounds_.max_records_per_batch;
+        break;
+      }
+
+      Aws::Kinesis::Model::PutRecordsRequestEntry aws_record;
+      initializeRecord(aws_record, buffer);
+      records.emplace_back(std::move(aws_record));
+    }
+
+    fclose(fp);
+
+    if (records.empty()) {
+      VLOG(1) << "no records in cached file? " << file_path;
+      return FORWARDER_STATUS_EMPTY_FILE;
+    }
+
+    return 0;
+  }
+
+  Status setUp() {
+    return Status();
+  }
+
+ protected:
+  void initializeRecord(Aws::Kinesis::Model::PutRecordsRequestEntry& record,
+                        Aws::Utils::ByteBuffer& buffer) const {
+    std::string record_partition_key;
+    if (FLAGS_aws_kinesis_random_partition_key) {
+      // Generate a random partition key for each record, ensuring that
+      // records are spread evenly across shards.
+      boost::uuids::uuid uuid = boost::uuids::random_generator()();
+      record_partition_key = boost::uuids::to_string(uuid);
+    } else {
+      record_partition_key = partition_key_;
+    }
+
+    record.WithPartitionKey(record_partition_key).WithData(buffer);
+  }
+
+  void _pause(std::chrono::milliseconds milli) {
+	  std::unique_lock<std::mutex> lock(_condition_lock);
+	  _condition.wait_for(lock, milli);
+  }
+
+  std::vector<char> linebuf_;
+  std::shared_ptr<Aws::Kinesis::KinesisClient> client_{nullptr};
+  std::mutex _condition_lock;
+  std::condition_variable _condition;
+  std::string partition_key_;
+};
+
+Status KinesisLoggerPlugin::setUp() {
+  if (!gHaveSetup) {
+    gHaveSetup = true;
+    LoggerBounds bounds;
+    bounds.max_records_per_batch = FLAGS_aws_kinesis_batch_max_records;
+    bounds.max_bytes_per_record = 1000000 - 256;
+    bounds.max_bytes_per_batch = FLAGS_aws_kinesis_batch_max_bytes;
+
+    setProps("aws_kinesis", true, bounds);
+
+    initAwsSdk();
+
+    auto forwarder = std::make_shared<KinesisForwarder>(bounds);
+    Status s = forwarder->setUp();
+    if (!s.ok()) {
+      LOG(ERROR) << "Error initializing Kinesis logger: " << s.getMessage();
+      return s;
+    }
+
+    start(forwarder, (uint32_t)FLAGS_aws_kinesis_period, 4, 1250);
+  }
+
   return Status(0, "OK");
-}
-
-Status KinesisLoggerPlugin::logString(const std::string& s) {
-  return forwarder_->logString(s);
-}
-
-Status KinesisLoggerPlugin::logStatus(const std::vector<StatusLogLine>& log) {
-  return forwarder_->logStatus(log);
 }
 
 void KinesisLoggerPlugin::init(const std::string& name,
@@ -69,72 +342,4 @@ void KinesisLoggerPlugin::init(const std::string& name,
   logStatus(log);
 }
 
-Status KinesisLogForwarder::internalSetup() {
-  partition_key_ = getHostIdentifier();
-
-  if (FLAGS_aws_kinesis_stream.empty()) {
-    return Status(1, "Stream name must be specified with --aws_kinesis_stream");
-  }
-
-  VLOG(1) << "Kinesis logging initialized with stream: "
-          << FLAGS_aws_kinesis_stream;
-
-  return Status(0, "OK");
-}
-
-KinesisLogForwarder::Outcome KinesisLogForwarder::internalSend(
-    const Batch& batch) {
-  Aws::Kinesis::Model::PutRecordsRequest request;
-  request.WithStreamName(FLAGS_aws_kinesis_stream).SetRecords(batch);
-  return client_->PutRecords(request);
-}
-
-void KinesisLogForwarder::initializeRecord(
-    Record& record, Aws::Utils::ByteBuffer& buffer) const {
-  std::string record_partition_key;
-  if (FLAGS_aws_kinesis_random_partition_key) {
-    // Generate a random partition key for each record, ensuring that
-    // records are spread evenly across shards.
-    boost::uuids::uuid uuid = boost::uuids::random_generator()();
-    record_partition_key = boost::uuids::to_string(uuid);
-  } else {
-    record_partition_key = partition_key_;
-  }
-
-  record.WithPartitionKey(record_partition_key).WithData(buffer);
-}
-
-size_t KinesisLogForwarder::getMaxBytesPerRecord() const {
-  // Max size of log + partition key is 1MB. Max size of partition key is 256B.
-  return (1000000U - 256U);
-}
-
-size_t KinesisLogForwarder::getMaxRecordsPerBatch() const {
-  return 500U;
-}
-
-size_t KinesisLogForwarder::getMaxBytesPerBatch() const {
-  return 5000000U;
-}
-
-size_t KinesisLogForwarder::getMaxRetryCount() const {
-  return 100U;
-}
-
-size_t KinesisLogForwarder::getInitialRetryDelay() const {
-  return 3000U;
-}
-
-bool KinesisLogForwarder::appendNewlineSeparators() const {
-  return false;
-}
-
-size_t KinesisLogForwarder::getFailedRecordCount(Outcome& outcome) const {
-  return static_cast<size_t>(outcome.GetResult().GetFailedRecordCount());
-}
-
-KinesisLogForwarder::Result KinesisLogForwarder::getResult(
-    Outcome& outcome) const {
-  return outcome.GetResult().GetRecords();
-}
-}
+} // namespace osquery
---
+++ plugins/logger/cached_logger.h
@@ -0,0 +1,229 @@
+#pragma once
+
+#include <osquery/plugins/logger.h>
+
+namespace osquery {
+
+/*
+ * All files for this logger reside in database_path and are named
+ * starting with CACHE_FILE_PREFIX.  Includes an 'S_' or 'R_',
+ * depending on status or results files, includes the epoch timestamp
+ * and logger name. e.g. 'z_R_aws_kinesis_1553117765.json'.
+ *
+ * The cache files are newline-delimited text-files.  Each line containing
+ * one JSON status or result record.
+ *
+ * Each forwarder may have it's own limits on now many lines or records
+ * and bytes it can handle.  The goal is to package each cache file
+ * according to the LoggerBounds, so that it's ready for sending.
+ */
+
+#define CACHE_FILE_PREFIX "z_"
+
+struct LoggerBounds {
+  uint64_t max_records_per_batch;
+  uint64_t max_bytes_per_record;
+  uint64_t max_bytes_per_batch;
+};
+
+#define FORWARDER_STATUS_NO_CONNECTION 999
+#define FORWARDER_STATUS_EMPTY_FILE    998
+#define FORWARDER_STATUS_READ_ERROR    997
+
+class Forwarder {
+ public:
+  /*
+   * Even though CachedLoggerPlugin checks the bounds when files are
+   * written, the bounds are supplied so send() can double-check them
+   * if desired.
+   */
+  Forwarder(const LoggerBounds bounds) : bounds_(bounds) {}
+  virtual ~Forwarder() {}
+
+  /*
+   * Seconds between checking for cached logs to send.
+   */
+  virtual uint32_t getIntervalSeconds() {
+    return 10;
+  }
+
+  /*
+   * Max number of files to send per interval.
+   */
+  virtual uint32_t getMaxFilesPerInterval() {
+    return 2;
+  }
+
+  /*
+   * If multiple files per interval, delay between each.
+   */
+  virtual uint32_t getBurstDelayMillis() {
+    return 1500;
+  }
+
+  /*
+   * Perform actual send.
+   * Implementation is responsible for retries.
+   * If unable to connect to server, return FORWARDER_STATUS_NO_CONNECTION.
+   * The file will be deleted after return, unless
+   *   status == FORWARDER_STATUS_NO_CONNECTION.
+   */
+  virtual Status send(std::string file_path) = 0;
+
+ protected:
+  const LoggerBounds bounds_;
+};
+
+// How long to wait before closing a file and adding to send queue
+#define ROTATE_INTERVAL_SEC 25
+
+struct LogChannel {
+  FILE* fp;
+  std::string filepath;
+  time_t ts;
+  uint32_t subid;
+
+  uint64_t num_bytes;
+  uint32_t num_lines;
+
+  std::string prefix;
+  std::string last_md5;
+  bool isResult;
+};
+
+struct CacheFileInfo {
+  std::string filepath;
+  uint32_t num_lines;
+  std::string md5;
+  bool isResult;
+  CacheFileInfo(std::string path, uint32_t num = 0, std::string filemd5 = "")
+      : filepath(path), num_lines(num), md5(filemd5), isResult(false) {}
+};
+
+/**
+ * This logger writes result and status into newline-delimited text files.
+ * Cached Format:
+ *  - Every line beginning with a '{' is a JSON object
+ *  - Lines beginning with '#' include metadata, and should be ignored by
+ *    forwarder or application ingesting logs.
+ * This is designed to be subclassed, and used in conjunction with a forwarder.
+ * Subclass can call configure() to set the cache file parameters specific
+ * to the transport.  For example, aws kinesis has 1000 records per second
+ * limit, and record size of 1MB.
+ *
+ * The subclasses of CachedLoggerPlugin should call start() from setUp() with
+ * an instance of the Forwarder.  The start() method will create a
+ * dispatcher thread and call Forwarder.send() at the desired interval.
+ *
+ */
+class CachedLoggerPlugin : public LoggerPlugin {
+ public:
+  /**
+   * Call this first from from setUp() of any inherited classes.
+   * If useSeparateStatusChannel == true, status log entries will be
+   * cached and sent separately from results.
+   */
+  void setProps(std::string logname,
+                bool useSeparateStatusChannel,
+                const LoggerBounds bounds);
+
+  /**
+   * Call this at end of setUp() in any inherited classes.
+   */
+  void start(std::shared_ptr<Forwarder> spForwarder,
+             uint32_t interval_seconds,
+             uint32_t burst_file_count,
+             uint32_t burst_sleep_millis);
+
+  /*
+   * Returns one or more log cache files waiting to be sent.
+   * Called internally by ForwarderThread.  Consider this private.
+   */
+  void getCachedFiles(std::vector<std::string>& cached_file_paths, int count);
+
+  /*
+   * Called internally by ForwarderThread.  Consider this private.
+   */
+  void removeCachedFile(std::string& file_path, bool wasSentSuccessfully);
+
+  // override standard logger entrypoints ... writes to cached text files
+
+  Status logString(const std::string& s) override;
+
+  Status logStatus(const std::vector<StatusLogLine>& log) override;
+
+  /**
+   * Internally, this just calls logString() for each.
+   */
+  //Status logStringBatch(std::vector<std::string>& items) override;
+
+  /**
+   * important : override tearDown so forwarderThread_ can be interrupted.
+   * Otherwise, agent may not exit.  Also closes channel file handles.
+   */
+  void tearDown() override;
+
+  /**
+   * @returns true if the channel file should be closed, and a new file started.
+   * Considers:
+   *   - channel.num_bytes + line_length > bounds.max_bytes_per_batch
+   *   - channel.num_lines + 1 > bounds.max_records_per_batch
+   *   - (now - channel.ts > ROTATE_INTERVAL_SEC) and num_files_queued < 6
+   * This function should be treated as private. (static + public for unit test)
+   */
+  static bool _needsRotate(LoggerBounds bounds,
+                           LogChannel& channel,
+                           time_t now,
+                           size_t line_length,
+                           size_t num_files_queued);
+
+ protected:
+  /**
+   * _logStringInternal() is used by logString() and logStatus().
+   * It holds the mutex, checks bounds, does _rotateLog if needed,
+   * increments channel counters, and writes to file.
+   */
+  Status _logStringInternal(LogChannel& channel, const std::string& s, bool isResult);
+
+  /**
+   * _enumerateExistingFiles is called from setProps()
+   * to populate files_to_forward_ with any cached log
+   * files in cache directory.
+   */
+  void _enumerateExistingFiles();
+
+  /**
+   * Does a fclose(channel.fp), adds channel.filepath to files_to_forward_[],
+   * calls _clearAndNameLog(),
+   * And does fopen(channel.filepath,"w"), and adds
+   */
+  void _rotateLog(LogChannel& channel);
+
+  /**
+   * Resets counters, sets ts=now, updates filepath with timestamp.
+   */
+  void _clearAndNameLog(LogChannel& channel, time_t now);
+
+  bool useSeparateStatusChannel_;
+  LoggerBounds bounds_;
+  std::shared_ptr<InternalRunnable> forwarderThread_;
+  std::string cache_path_;
+  LogChannel results_channel_;
+  LogChannel status_channel_;
+  std::vector<CacheFileInfo> files_to_forward_;
+  bool cacheLimitReached_ {false};
+};
+
+#ifdef WIN32
+inline FILE* FOPEN(const char *FILEPATH, const char *MODE) {
+  FILE* fp;
+  if (0 != fopen_s(&fp, FILEPATH, MODE)) {
+    return nullptr;
+  }
+  return fp;
+}
+#else
+#define FOPEN(FILEPATH, MODE) fopen(FILEPATH, MODE)
+#endif
+
+} // namespace osquery
