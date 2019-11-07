ed996c66af8edbb80443dd88d39363c83265a46f
AlexM <20775507+packetzero@users.noreply.github.com>
2019-11-07 14:01:42 -0600

    get tp conveyor static lib built - at least on MacOS

---
+++ libraries/cmake/source_migration/modules/Findconveyor.cmake
@@ -0,0 +1,9 @@
+# Copyright (c) 2014-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed in accordance with the terms specified in
+# the LICENSE file found in the root directory of this source tree.
+
+cmake_minimum_required(VERSION 3.14.6)
+include("${CMAKE_SOURCE_DIR}/libraries/cmake/source/modules/Findconveyor.cmake")
+
