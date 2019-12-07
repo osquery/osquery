be3939c097c32e0a3eea6919956addc36a654e54
seph <seph@directionless.org>
2019-12-06 21:43:07 -0500

    tweak keychain items

--- tests/integration/tables/keychain_items.cpp
+++ tests/integration/tables/keychain_items.cpp
@@ -23,7 +23,7 @@ class KeychainItemsTest : public testing::Test {
 
 TEST_F(KeychainItemsTest, test_sanity) {
   auto const data = execute_query("select * from keychain_items");
-  ASSERT_GT(data.size(), 0ul);
+  ASSERT_GT(data.size(), 1ul);
   ValidationMap row_map = {
       {"label", NormalType},
       {"description", NormalType},
