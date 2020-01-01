e79e76eb8eadc4def485836727ed9915ffe74ab4
William Woodruff <william@trailofbits.com>
2019-12-31 19:22:44 -0500

    (Windows) New table: ntfs_journal_events (#5371)
    Co-authored-by: Garret Reece <GarretReece@users.noreply.github.com>
    Co-authored-by: Alessandro Gario <5714290+alessandrogario@users.noreply.github.com>

--- osquery/utils/system/windows/errno.cpp
+++ osquery/utils/system/windows/errno.cpp
@@ -14,6 +14,7 @@
 #define MAX_BUFFER_SIZE 256
 
 namespace osquery {
+const auto kWindowsLanguageId = MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT);
 
 std::string platformStrerr(int errnum) {
   std::vector<char> buffer;
@@ -27,4 +28,31 @@ std::string platformStrerr(int errnum) {
   return std::string(buffer.data());
 }
 
+Status getWindowsErrorDescription(std::string& error_message, DWORD error_id) {
+  error_message.clear();
+  LPSTR buffer = nullptr;
+
+  auto message_size = FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER |
+                                         FORMAT_MESSAGE_FROM_SYSTEM |
+                                         FORMAT_MESSAGE_IGNORE_INSERTS,
+                                     nullptr,
+                                     error_id,
+                                     kWindowsLanguageId,
+                                     reinterpret_cast<LPSTR>(&buffer),
+                                     0,
+                                     nullptr);
+
+  if (message_size == 0U) {
+    return Status(1,
+                  "Failed to fetch the Windows error message for the following "
+                  "error code: " +
+                      std::to_string(error_id));
+  }
+
+  error_message.assign(buffer, static_cast<size_t>(message_size));
+  LocalFree(buffer);
+
+  return Status(0);
 }
+
+} // namespace osquery
