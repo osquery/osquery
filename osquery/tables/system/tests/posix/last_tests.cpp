7833eadcc955ec3f4cd848dca10f4dc0a74c67d8
João Godinho <joaogodinho@users.noreply.github.com>
2020-01-06 11:40:27 +0000

    Update osquery/tables/system/tests/posix/last_tests.cpp
    Co-Authored-By: Stefano Bonicatti <smjert@gmail.com>

--- osquery/tables/system/tests/posix/last_tests.cpp
+++ osquery/tables/system/tests/posix/last_tests.cpp
@@ -38,7 +38,7 @@ TEST_F(LastImplTests, gen_row_from_utmpx) {
   ut_logout.ut_pid = 1337;
   ut_logout.ut_tv.tv_sec = 1577836900;
 
-  impl::genLastAccessForRow(&ut_login, results);
+  impl::genLastAccessForRow(ut_login, results);
   impl::genLastAccessForRow(ut_badtype, results);
   impl::genLastAccessForRow(&ut_logout, results);
 
