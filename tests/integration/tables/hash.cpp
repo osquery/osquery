b1a55c31ea258462a45780ba08023614f7cce905
Teddy Reed <teddy@casualhacking.io>
2020-01-18 15:38:32 -0500

    tests: Improve hash table integration tests (#6184)
    Co-Authored-By: Ryan Wilson <ryantimwilson@fb.com>

--- tests/integration/tables/hash.cpp
+++ tests/integration/tables/hash.cpp
@@ -9,8 +9,8 @@
 // Sanity check integration test for hash
 // Spec file: specs/hash.table
 
+#include <osquery/filesystem/filesystem.h>
 #include <osquery/tests/integration/tables/helper.h>
-
 #include <osquery/utils/info/platform_type.h>
 
 namespace osquery {
@@ -23,10 +23,12 @@ class Hash : public testing::Test {
   void SetUp() override {
     setUpEnvironment();
     path = fs::temp_directory_path() /
-           fs::unique_path("osquery.tests.file.hashes");
+           fs::unique_path("osquery.tests.file.hashes.%%%%.%%%%.%%%%.%%%%");
 
-    auto fout = std::ofstream(path.native(), std::ios::out | std::ios::binary);
-    fout << "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+    EXPECT_TRUE(
+        writeTextFile(
+            path, "Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
+            .ok());
   }
 
   void TearDown() override {
