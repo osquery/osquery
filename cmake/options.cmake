7f21ccfe6745f03e612f6e23234d1666d98cf643
Stefano Bonicatti <stefano.bonicatti@gmail.com>
2020-06-01 15:58:56 +0200

    Drop the facebook and source_migration layers (#6473)
    We have ported all platforms to use the source layer.
    The facebook layer is not needed anymore since we build everything from source now,
    the same is for the transitional source_migration layer.
    
    Minor cleanups about the recently removed python modules.

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -79,13 +79,7 @@ set(OSQUERY_CLANG_TIDY_CHECKS "-checks=cert-*,cppcoreguidelines-*,performance-*,
 # Unfortunately, due glog always enabling BUILD_TESTING, we have to force it off, so that tests won't be built
 overwrite_cache_variable("BUILD_TESTING" "BOOL" "OFF")
 
-# Linux can use source and formula modules to link dependencies; this
-# feature is not yet available on Windows and macOS
-if(DEFINED PLATFORM_LINUX)
-  set(third_party_source_list "source;formula;facebook")
-else()
-  set(third_party_source_list "source_migration;facebook")
-endif()
+set(third_party_source_list "source;formula")
 
 set(CMAKE_MODULE_PATH "${CMAKE_SOURCE_DIR}/cmake/modules" CACHE STRING "A list of paths containing CMake module files")
 set(OSQUERY_THIRD_PARTY_SOURCE "${third_party_source_list}" CACHE STRING "Sources used to acquire third-party dependencies")
