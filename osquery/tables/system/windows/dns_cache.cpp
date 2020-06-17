a7586cadc58b073238cf082b56bc17de016fb264
Nat Meo <nmeo@cisco.com>
2020-06-17 10:35:10 -0400

    Fix spacing of case statements

--- osquery/tables/system/windows/dns_cache.cpp
+++ osquery/tables/system/windows/dns_cache.cpp
@@ -30,99 +30,99 @@ typedef int(WINAPI *DNS_GET_CACHE_DATA_TABLE)(PDNSCACHEENTRY);
 std::string dnsTypeToString(unsigned short wType) {
   switch (wType)
   {
-	case 1:
+  case 1:
     return "A";
-	case 2:
+  case 2:
     return "NS";
-	case 5:
+  case 5:
     return "CNAME";
-	case 6:
+  case 6:
     return "SOA";
-	case 12:
+  case 12:
     return "PTR";
-	case 13:
+  case 13:
     return "HINFO";
-	case 15:
+  case 15:
     return "MX";
-	case 16:
+  case 16:
     return "TXT";
-	case 17:
+  case 17:
     return "RP";
-	case 18:
+  case 18:
     return "AFSDB";
-	case 24:
+  case 24:
     return "SIG";
-	case 25:
+  case 25:
     return "KEY";
-	case 28:
+  case 28:
     return "AAAA";
-	case 29:
+  case 29:
     return "LOC";
-	case 33:
+  case 33:
     return "SRV";
-	case 35:
+  case 35:
     return "NAPTR";
-	case 36:
+  case 36:
     return "KX";
-	case 37:
+  case 37:
     return "CERT";
-	case 39:
+  case 39:
     return "DNAME";
-	case 41:
+  case 41:
     return "OPT";
-	case 42:
+  case 42:
     return "APL";
-	case 43:
+  case 43:
     return "DS";
-	case 44:
+  case 44:
     return "SSHFP";
-	case 45:
+  case 45:
     return "IPSECKEY";
-	case 46:
+  case 46:
     return "RRSIG";
-	case 47:
+  case 47:
     return "NSEC";
-	case 48:
+  case 48:
     return "DNSKEY";
-	case 49:
+  case 49:
     return "DHCID";
-	case 50:
+  case 50:
     return "NSEC3";
-	case 51:
+  case 51:
     return "NSEC3PARAM";
-	case 52:
+  case 52:
     return "TLSA";
-	case 53:
+  case 53:
     return "SMIMEA";
-	case 55:
+  case 55:
     return "HIP";
-	case 59:
+  case 59:
     return "CDS";
-	case 60:
+  case 60:
     return "CDNSKEY";
-	case 61:
+  case 61:
     return "OPENPGPKEY";
-	case 62:
+  case 62:
     return "CSYNC";
-	case 63:
+  case 63:
     return "ZONEMD";
-	case 249:
+  case 249:
     return "TKEY";
-	case 250:
+  case 250:
     return "TSIG";
-	case 251:
+  case 251:
     return "IXFR";
-	case 252:
+  case 252:
     return "AXFR";
-	case 255:
+  case 255:
     return "*";
-	case 256:
+  case 256:
     return "URI";
-	case 257:
+  case 257:
     return "CAA";
-	case 32768:
+  case 32768:
     return "TA";
-	case 32769:
+  case 32769:
     return "DLV";
   }
 
