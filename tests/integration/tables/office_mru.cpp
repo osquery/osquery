4b515a503923445c8683c3cd22e910d970d3af47
puffycid <puffycyan@gmail.com>
2020-06-28 19:13:32 -0400

    formatting

--- tests/integration/tables/office_mru.cpp
+++ tests/integration/tables/office_mru.cpp
@@ -6,9 +6,6 @@
  *  the LICENSE file found in the root directory of this source tree.
  */
 
-// Sanity check integration test for carbon_black_info
-// Spec file: specs/carbon_black_info.table
-
 #include <osquery/tests/integration/tables/helper.h>
 
 namespace osquery {
