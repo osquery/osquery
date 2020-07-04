327a6a3e247efc6420ac6b541cc5ec4349e2e879
Teddy Reed <teddy@casualhacking.io>
2020-07-03 23:01:53 -0400

    tables: Refactor shell_history to use generators

--- osquery/tables/system/posix/shell_history.cpp
+++ osquery/tables/system/posix/shell_history.cpp
@@ -12,76 +12,119 @@
 
 #include <osquery/core.h>
 #include <osquery/filesystem/filesystem.h>
-#include <osquery/logger.h>
 #include <osquery/tables.h>
-#include <osquery/tables/system/system_utils.h>
 #include <osquery/tables/system/posix/shell_history.h>
-#include <osquery/utils/conversions/split.h>
+#include <osquery/tables/system/system_utils.h>
 #include <osquery/utils/system/system.h>
 
 namespace osquery {
 namespace tables {
 
 const std::vector<std::string> kShellHistoryFiles = {
-    ".bash_history", ".zsh_history", ".zhistory", ".history", ".sh_history",
+    ".bash_history",
+    ".zsh_history",
+    ".zhistory",
+    ".history",
+    ".sh_history",
 };
 
-void genShellHistoryFromFile(const std::string& uid,
-                             const boost::filesystem::path& history_file,
-                             QueryData& results) {
-  std::string history_content;
-  if (forensicReadFile(history_file, history_content).ok()) {
-    std::regex bash_timestamp_rx("^#([0-9]+)$");
-    std::regex zsh_timestamp_rx("^: {0,10}([0-9]{1,11}):[0-9]+;(.*)$");
+struct HistoryState {
+  std::string content;
+  std::regex bash_timestamp_rx{"^#([0-9]+)$"};
+  std::regex zsh_timestamp_rx{"^: {0,10}([0-9]{1,11}):[0-9]+;(.*)$"};
+  std::string prev_bash_timestamp;
+};
 
-    std::string prev_bash_timestamp;
-    for (const auto& line : split(history_content, "\n")) {
-      std::smatch bash_timestamp_matches;
-      std::smatch zsh_timestamp_matches;
+void genShellHistoryFromFile(
+    const std::string& uid,
+    const boost::filesystem::path& history_file,
+    std::function<void(DynamicTableRowHolder& row)> predicate) {
+  struct HistoryState hState;
 
-      if (prev_bash_timestamp.empty() &&
-          std::regex_search(line, bash_timestamp_matches, bash_timestamp_rx)) {
-        prev_bash_timestamp = bash_timestamp_matches[1];
-        continue;
-      }
+  auto parseLine =
+      [&hState, &uid, &history_file, &predicate](std::string& line) {
+        std::smatch bash_timestamp_matches;
+        std::smatch zsh_timestamp_matches;
 
-      Row r;
+        if (hState.prev_bash_timestamp.empty() &&
+            std::regex_search(
+                line, bash_timestamp_matches, hState.bash_timestamp_rx)) {
+          hState.prev_bash_timestamp = bash_timestamp_matches[1];
+          return;
+        }
 
-      if (!prev_bash_timestamp.empty()) {
-        r["time"] = INTEGER(prev_bash_timestamp);
-        r["command"] = line;
-        prev_bash_timestamp.clear();
-      } else if (std::regex_search(
-                     line, zsh_timestamp_matches, zsh_timestamp_rx)) {
-        std::string timestamp = zsh_timestamp_matches[1];
-        r["time"] = INTEGER(timestamp);
-        r["command"] = zsh_timestamp_matches[2];
-      } else {
-        r["time"] = INTEGER(0);
-        r["command"] = line;
-      }
+        auto r = make_table_row();
 
-      r["uid"] = uid;
-      r["history_file"] = history_file.string();
-      results.push_back(r);
+        if (!hState.prev_bash_timestamp.empty()) {
+          r["time"] = INTEGER(hState.prev_bash_timestamp);
+          r["command"] = std::move(line);
+          hState.prev_bash_timestamp.clear();
+        } else if (std::regex_search(
+                       line, zsh_timestamp_matches, hState.zsh_timestamp_rx)) {
+          std::string timestamp = zsh_timestamp_matches[1];
+          r["time"] = INTEGER(timestamp);
+          r["command"] = zsh_timestamp_matches[2];
+        } else {
+          r["time"] = INTEGER(0);
+          r["command"] = std::move(line);
+        }
+
+        r["uid"] = uid;
+        r["history_file"] = history_file.string();
+        predicate(r);
+      };
+
+  auto parseChunk = [&hState, &parseLine](std::string& buffer, size_t size) {
+    // We may be appending this chunk to the end of the previous.
+    if (buffer.size() == size) {
+      hState.content += std::move(buffer);
+    } else {
+      hState.content += buffer.substr(0, size);
     }
+
+    // Search for newlines and parse each.
+    size_t last_newline = 0;
+    auto newline = hState.content.find('\n');
+    while (newline != std::string::npos) {
+      auto line = hState.content.substr(last_newline, newline - last_newline);
+      parseLine(line);
+
+      last_newline = newline + 1;
+      newline = hState.content.find('\n', last_newline);
+    }
+
+    if (last_newline != hState.content.size() - 1) {
+      // We need to buffer the end of the string.
+      hState.content = hState.content.substr(last_newline);
+    }
+  };
+
+  if (!readFile(history_file, 0, 4096, false, false, parseChunk, false)) {
+    return;
+  }
+
+  // Parse the final line.
+  if (!hState.content.empty()) {
+    parseLine(hState.content);
   }
 }
 
-void genShellHistoryForUser(const std::string& uid,
-                            const std::string& gid,
-                            const std::string& directory,
-                            QueryData& results) {
+void genShellHistoryForUser(
+    const std::string& uid,
+    const std::string& gid,
+    const std::string& directory,
+    std::function<void(DynamicTableRowHolder& row)> predicate) {
   for (const auto& hfile : kShellHistoryFiles) {
     boost::filesystem::path history_file = directory;
     history_file /= hfile;
-    genShellHistoryFromFile(uid, history_file, results);
+    genShellHistoryFromFile(uid, history_file, predicate);
   }
 }
 
-void genShellHistoryFromBashSessions(const std::string& uid,
-                                     const std::string& directory,
-                                     QueryData& results) {
+void genShellHistoryFromBashSessions(
+    const std::string& uid,
+    const std::string& directory,
+    std::function<void(DynamicTableRowHolder& row)> predicate) {
   boost::filesystem::path bash_sessions = directory;
   bash_sessions /= ".bash_sessions";
 
@@ -92,13 +135,13 @@ void genShellHistoryFromBashSessions(const std::string& uid,
 
     for (const auto& hfile : session_hist_files) {
       boost::filesystem::path history_file = hfile;
-      genShellHistoryFromFile(uid, history_file, results);
+      genShellHistoryFromFile(uid, history_file, predicate);
     }
   }
 }
 
-QueryData genShellHistory(QueryContext& context) {
-  QueryData results;
+void genShellHistory(RowYield& yield, QueryContext& context) {
+  auto predicate = [&yield](DynamicTableRowHolder& r) { yield(std::move(r)); };
 
   // Iterate over each user
   QueryData users = usersFromContext(context);
@@ -107,12 +150,10 @@ QueryData genShellHistory(QueryContext& context) {
     auto gid = row.find("gid");
     auto dir = row.find("directory");
     if (uid != row.end() && gid != row.end() && dir != row.end()) {
-      genShellHistoryForUser(uid->second, gid->second, dir->second, results);
-      genShellHistoryFromBashSessions(uid->second, dir->second, results);
+      genShellHistoryForUser(uid->second, gid->second, dir->second, predicate);
+      genShellHistoryFromBashSessions(uid->second, dir->second, predicate);
     }
   }
-
-  return results;
 }
-}
-}
+} // namespace tables
+} // namespace osquery
