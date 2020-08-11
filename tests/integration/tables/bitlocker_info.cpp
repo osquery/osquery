527516859e1e1cfcab9b63bb1062b902cd6698a7
seph <seph@directionless.org>
2020-08-11 13:47:26 -0400

    Update copyright notices
    Update copyright notices from Facebook to "The osquery authors"
    
    Bulk change implemented with https://github.com/directionless/copyright-change-headers This will need a manual followup for some lagging ones
    
    Fixes: osquery/foundation#32

--- tests/integration/tables/bitlocker_info.cpp
+++ tests/integration/tables/bitlocker_info.cpp
@@ -1,9 +1,10 @@
 /**
- *  Copyright (c) 2014-present, Facebook, Inc.
- *  All rights reserved.
+ * Copyright (c) 2014-present, The osquery authors
  *
- *  This source code is licensed in accordance with the terms specified in
- *  the LICENSE file found in the root directory of this source tree.
+ * This source code is licensed as defined by the LICENSE file found in the
+ * root directory of this source tree.
+ *
+ * SPDX-License-Identifier: (Apache-2.0 OR GPL-2.0-only)
  */
 
 // Sanity check integration test for bitlocker_info
