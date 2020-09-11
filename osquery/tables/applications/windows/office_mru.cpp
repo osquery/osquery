1b66443452f5e5b1f7cd6c1cd8a14f3f07d6a625
Teddy Reed <teddy@casualhacking.io>
2020-09-11 11:01:23 -0400

    nitpicks and small perf changes

--- osquery/tables/applications/windows/office_mru.cpp
+++ osquery/tables/applications/windows/office_mru.cpp
@@ -27,92 +27,88 @@ constexpr auto kOffice365Path =
 
 // Parse all the office MRU entries.
 void parseOfficeData(QueryData& results,
-                     const QueryData& office_entries,
+                     const Row& entry,
                      const std::string& sid,
-                     std::string office_version) {
-  for (const auto& aKey : office_entries) {
-    auto item = aKey.find("name");
-    // All file entries start with "Item", skip entries that are not named
-    // "Item"
-    if (item->second.find("Item") == std::string::npos) {
-      continue;
-    }
-
-    // MRU entries should also be over 20 characters long
-    if (aKey.find("data")->second.length() < 20) {
-      LOG(WARNING) << "MRU entry malformed: " << aKey.find("data")->second;
-      continue;
-    }
-    auto file_path = aKey.find("data");
-
-    Row r;
-    // File path starts with *
-    r["path"] = file_path->second.substr(file_path->second.find("*") + 1);
-
-    // Extract the office application version from the registry path
-    std::string version = office_version;
-    r["version"] =
-        version.substr(office_version.find("Office\\"), 11).substr(7);
-
-    // Extract the office application name from the registry path
-    std::string application = office_version;
-    std::string office_app =
-        application.substr(office_version.find(r["version"]));
-    r["application"] = office_app.substr(office_app.find("\\") + 1,
-                                         office_app.find(" MRU") - 10);
-
-    // Last opened time stored in Big endian Windows FILETIME Hex format, also
-    // starts with T
-    std::string time_data =
-        aKey.at("data").substr(aKey.at("data").find("T") + 1, 16);
-
-    // Try to convert last open time string to ull
-    unsigned long long last_open =
-        tryTo<unsigned long long>(time_data, 16).takeOr(0ull);
-
-    FILETIME file_time;
-    ULARGE_INTEGER large_time;
-    large_time.QuadPart = last_open;
-    file_time.dwHighDateTime = large_time.HighPart;
-    file_time.dwLowDateTime = large_time.LowPart;
-    auto open_time = filetimeToUnixtime(file_time);
-
-    r["last_opened_time"] = INTEGER(open_time);
-    r["sid"] = sid;
-    results.push_back(r);
+                     const std::string& office_version) {
+  auto item = entry.find("name");
+  // All file entries start with "Item", skip entries that are not named
+  // "Item"
+  if (item->second.find("Item") == std::string::npos) {
+    return;
   }
+
+  // MRU entries should also be over 20 characters long
+  auto data = entry.find("data");
+  auto& file_path = data->second;
+  if (file_path.length() < 20) {
+    LOG(WARNING) << "MRU entry malformed: " << file_path;
+    return;
+  }
+
+  Row r;
+  // File path starts with *
+  r["path"] = file_path.substr(file_path.find("*") + 1);
+
+  // Extract the office application version from the registry path
+  auto version = office_version;
+  r["version"] = version.substr(office_version.find("Office\\"), 11).substr(7);
+
+  // Extract the office application name from the registry path
+  auto application = office_version;
+  auto office_app = application.substr(office_version.find(r["version"]));
+  r["application"] = office_app.substr(office_app.find("\\") + 1,
+                                       office_app.find(" MRU") - 10);
+
+  // Last opened time stored in Big endian Windows FILETIME Hex format, also
+  // starts with T
+  auto time_data = file_path.substr(file_path.find("T") + 1, 16);
+
+  // Try to convert last open time string to ull
+  unsigned long long last_open =
+      tryTo<unsigned long long>(time_data, 16).takeOr(0ull);
+
+  FILETIME file_time;
+  ULARGE_INTEGER large_time;
+  large_time.QuadPart = last_open;
+  file_time.dwHighDateTime = large_time.HighPart;
+  file_time.dwLowDateTime = large_time.LowPart;
+  auto open_time = filetimeToUnixtime(file_time);
+
+  r["last_opened_time"] = INTEGER(open_time);
+  r["sid"] = sid;
+  results.push_back(r);
 }
 
 // Get non-office 365 documents in File MRU key
 void officeData(QueryData& results,
-                const std::set<std::string>& registry_results,
+                const std::string& office_data,
                 const std::string& sid) {
-  for (const auto& rKey : registry_results) {
-    QueryData office_entries;
-    queryKey(rKey, office_entries);
-    parseOfficeData(results, office_entries, sid, rKey);
+  QueryData office_entries;
+  queryKey(office_data, office_entries);
+  for (const auto& entry : office_entries) {
+    parseOfficeData(results, entry, sid, office_data);
   }
 }
 
 // Get Office 365 documents in User MRU and File MRU keys
 void office365Data(QueryData& results,
-                   const std::set<std::string>& office_365_results,
+                   const std::string& office_365_data,
                    const std::string& sid) {
-  for (const auto& rKey : office_365_results) {
-    std::string key = rKey;
-    std::set<std::string> office_365_results_files;
-    // Iterate through all User MRU entries
-    expandRegistryGlobs(key.append("\\%"), office_365_results_files);
-    for (const auto& fKey : office_365_results_files) {
-      // User accounts should have "_" in them, skip all keys that do not have
-      // "_"
-      if (fKey.find("_") == std::string::npos) {
-        continue;
-      }
-      QueryData office_entries;
-      std::string final_key = fKey;
-      queryKey(final_key.append("\\File MRU"), office_entries);
-      parseOfficeData(results, office_entries, sid, rKey);
+  std::string key = office_365_data;
+  std::set<std::string> office_365_results_files;
+  // Iterate through all User MRU entries
+  expandRegistryGlobs(key.append("\\%"), office_365_results_files);
+  for (const auto& fKey : office_365_results_files) {
+    // User accounts should have "_" in them, skip all keys that do not have
+    // "_"
+    if (fKey.find("_") == std::string::npos) {
+      continue;
+    }
+    QueryData office_entries;
+    std::string final_key = fKey;
+    queryKey(final_key.append("\\File MRU"), office_entries);
+    for (const auto& entry : office_entries) {
+      parseOfficeData(results, entry, sid, office_365_data);
     }
   }
 }
@@ -132,15 +128,19 @@ QueryData genOfficeMru(QueryContext& context) {
 
     std::string office_path = keyPath->second + kOfficePath;
     std::string office_365_path = keyPath->second + kOffice365Path;
+    std::string sid = uKey.at("name");
 
     std::set<std::string> office_results;
     expandRegistryGlobs(office_path, office_results);
-    std::string sid = uKey.at("name");
-    officeData(results, office_results, sid);
+    for (const auto& office_data : office_results) {
+      officeData(results, office_data, sid);
+    }
 
     std::set<std::string> office_365_results;
     expandRegistryGlobs(office_365_path, office_365_results);
-    office365Data(results, office_365_results, sid);
+    for (const auto& office_365_data : office_365_results) {
+      office365Data(results, office_365_data, sid);
+    }
   }
   return results;
 }
