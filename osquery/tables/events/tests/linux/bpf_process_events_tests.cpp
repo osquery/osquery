80b1dc798460c107dad3eb9bb059040e976d5fec
Alessandro Gario <alessandro.gario@gmail.com>
2020-10-01 15:07:56 +0200

    BPF: Add tests for bpf_socket_events

---
+++ osquery/tables/events/tests/linux/bpf_process_events_tests.cpp
@@ -0,0 +1,18 @@
+/**
+ * Copyright (c) 2014-present, The osquery authors
+ *
+ * This source code is licensed as defined by the LICENSE file found in the
+ * root directory of this source tree.
+ *
+ * SPDX-License-Identifier: (Apache-2.0 OR GPL-2.0-only)
+ */
+
+#include <gtest/gtest.h>
+
+#include <osquery/tables/events/linux/bpf_process_events.h>
+
+namespace osquery {
+class BPFProcessEventsTests : public testing::Test {};
+
+TEST_F(BPFProcessEventsTests, xx) {}
+} // namespace osquery
