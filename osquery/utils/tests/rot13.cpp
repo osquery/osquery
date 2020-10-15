a7b32df3f77f0315eaa63e09e4a1462c12fcc241
Alessandro Gario <alessandro.gario@gmail.com>
2020-10-15 12:47:25 +0200

    BPF: Revert header changes in rot13.cpp

--- osquery/utils/tests/rot13.cpp
+++ osquery/utils/tests/rot13.cpp
@@ -1,10 +1,9 @@
 /**
- * Copyright (c) 2014-present, The osquery authors
+ *  Copyright (c) 2014-present, Facebook, Inc.
+ *  All rights reserved.
  *
- * This source code is licensed as defined by the LICENSE file found in the
- * root directory of this source tree.
- *
- * SPDX-License-Identifier: (Apache-2.0 OR GPL-2.0-only)
+ *  This source code is licensed in accordance with the terms specified in
+ *  the LICENSE file found in the root directory of this source tree.
  */
 
 #include <gtest/gtest.h>
