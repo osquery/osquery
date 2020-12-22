704d75abedc60ee63363b26d4f720d7e4ef1f995
Stefano Bonicatti <stefano.bonicatti@gmail.com>
2020-12-22 15:10:32 +0100

    CMake: Add an option to disable building AWS tables and library (#6831)
    Added the OSQUERY_BUILD_AWS option due to
    oss-fuzz CI failing to build the AWS EC2 library,
    due to its high requirement of memory.
    The fuzzers do not gain much from having these tables enabled,
    moreover this can be used during development
    to avoid building a possibly unneeded table.
    
    Done some minor cleanups to remove logic duplication
    and enable ec2 integration tests on all the platforms
    that supports them.

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -85,7 +85,8 @@ endif()
 option(OSQUERY_ENABLE_CLANG_TIDY "Enables clang-tidy support")
 set(OSQUERY_CLANG_TIDY_CHECKS "-checks=cert-*,cppcoreguidelines-*,performance-*,portability-*,readability-*,modernize-*,bugprone-*" CACHE STRING "List of checks performed by clang-tidy")
 
-option(OSQUERY_BUILD_BPF "Whether to enable and build BPF support" true)
+option(OSQUERY_BUILD_BPF "Whether to enable and build BPF support" ON)
+option(OSQUERY_BUILD_AWS "Whether to build the aws tables and library, to decrease memory usage and increase speed during build." ON)
 
 # This is a temporary option to ignore the version check if there's no intention to generate RPM packages
 option(OSQUERY_IGNORE_CMAKE_MAX_VERSION_CHECK "Ignore the maximum cmake version check introduced due to CPack generating incorrect RPM packages")
