c958108a548c62c76cf782a45dbb221140146f01
Stefano Bonicatti <stefano.bonicatti@gmail.com>
2020-12-22 10:44:18 +0100

    Apply review suggestions

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -77,15 +77,14 @@ option(OSQUERY_ENABLE_ADDRESS_SANITIZER "Whether to enable Address Sanitizer")
 option(OSQUERY_ENABLE_CLANG_TIDY "Enables clang-tidy support")
 set(OSQUERY_CLANG_TIDY_CHECKS "-checks=cert-*,cppcoreguidelines-*,performance-*,portability-*,readability-*,modernize-*,bugprone-*" CACHE STRING "List of checks performed by clang-tidy")
 
-option(OSQUERY_BUILD_BPF "Whether to enable and build BPF support" true)
+option(OSQUERY_BUILD_BPF "Whether to enable and build BPF support" ON)
+option(OSQUERY_BUILD_AWS "Whether to build the aws tables and library, to decrease memory usage and increase speed during build." ON)
 
 # This is a temporary option to ignore the version check if there's no intention to generate RPM packages
 option(OSQUERY_IGNORE_CMAKE_MAX_VERSION_CHECK "Ignore the maximum cmake version check introduced due to CPack generating incorrect RPM packages")
 
 # Unfortunately, due glog always enabling BUILD_TESTING, we have to force it off, so that tests won't be built
 overwrite_cache_variable("BUILD_TESTING" "BOOL" "OFF")
-
-option(OSQUERY_BUILD_AWS "Whether to build the aws tables and library, to decrease memory usage and increase speed during build." ON)
 
 set(third_party_source_list "source;formula")
 
