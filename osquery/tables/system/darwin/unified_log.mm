846b16950992fbc1e6645fabcc81495d12692c07
Garret Reece <GarretReece@users.noreply.github.com>
2021-01-14 18:41:12 -0600

    format update

--- osquery/tables/system/darwin/unified_log.mm
+++ osquery/tables/system/darwin/unified_log.mm
@@ -27,10 +27,9 @@ const std::map<ConstraintOperator, NSPredicateOperatorType> supportedOps = {
     {GREATER_THAN_OR_EQUALS, NSGreaterThanOrEqualToPredicateOperatorType},
     {LESS_THAN, NSLessThanPredicateOperatorType},
     {LESS_THAN_OR_EQUALS, NSLessThanOrEqualToPredicateOperatorType},
-    {LIKE, NSLikePredicateOperatorType}
-};
+    {LIKE, NSLikePredicateOperatorType}};
 
-const std::map<std::string, std::string>  columnToOSLogEntryProp {
+const std::map<std::string, std::string> columnToOSLogEntryProp = {
     {"timestamp", "date"},
     {"message", "composedMessage"},
     {"storage", "storeCategory"},
@@ -40,144 +39,151 @@ const std::map<std::string, std::string>  columnToOSLogEntryProp {
     {"sender", "sender"},
     {"tid", "threadIdentifier"},
     {"subsystem", "subsystem"},
-    {"category", "category"}
-};
+    {"category", "category"}};
 
-const std::map<std::string, bool>  columnIsNumeric {
-    {"timestamp", false},
-    {"message", false},
-    {"storage", true},
-    {"activity", true},
-    {"process", false},
-    {"pid", true},
-    {"sender", false},
-    {"tid", true},
-    {"subsystem", false},
-    {"category", false}
-};
+const std::map<std::string, bool> columnIsNumeric = {{"timestamp", false},
+                                                     {"message", false},
+                                                     {"storage", true},
+                                                     {"activity", true},
+                                                     {"process", false},
+                                                     {"pid", true},
+                                                     {"sender", false},
+                                                     {"tid", true},
+                                                     {"subsystem", false},
+                                                     {"category", false}};
 
-std::string convertLikeExpr(const std::string &value) {
-    // for the LIKE operator in NSPredicates, '*' matches 0 or more characters
-    //   and '?' matches a single character
-    std::string res = ba::replace_all_copy(value, "%", "*");
-    ba::replace_all(res, "_", "?");
-    return res;
+std::string convertLikeExpr(const std::string& value) {
+  // for the LIKE operator in NSPredicates, '*' matches 0 or more characters
+  //   and '?' matches a single character
+  std::string res = ba::replace_all_copy(value, "%", "*");
+  ba::replace_all(res, "_", "?");
+  return res;
 }
 
-void addQueryOp(NSMutableArray *preds,
-                const std::string &key,
-                const std::string &value,
+void addQueryOp(NSMutableArray* preds,
+                const std::string& key,
+                const std::string& value,
                 ConstraintOperator op) {
-    if (supportedOps.count(op) > 0) {
-        std::string modified_val = value;
-        std::string modified_key = columnToOSLogEntryProp.at(key);
-        if (op == LIKE) {
-            modified_val = convertLikeExpr(value);
-        }
-        NSExpression *keyExp = [NSExpression expressionForKeyPath:[NSString stringWithUTF8String:modified_key.c_str()]];
-        NSString *valStr = [NSString stringWithUTF8String:modified_val.c_str()];
-
-        NSExpression *valExp = nil;
-        if (key == "timestamp") {
-            double provided_timestamp = [valStr doubleValue];
-            valExp = [NSExpression expressionForConstantValue:[NSDate dateWithTimeIntervalSince1970: provided_timestamp]];
-        } else if (columnIsNumeric.at(key)) {
-            valExp = [NSExpression expressionWithFormat:@"%lld", [valStr longLongValue]];
-        } else {
-            valExp = [NSExpression expressionForConstantValue:valStr];
-        }
-
-        NSPredicate *pred = [NSComparisonPredicate
-            predicateWithLeftExpression:keyExp
-            rightExpression:valExp
-            modifier:NSDirectPredicateModifier
-            type:supportedOps.at(op)
-            options:0];
-        [preds addObject:pred];
+  if (supportedOps.count(op) > 0) {
+    std::string modified_val = value;
+    std::string modified_key = columnToOSLogEntryProp.at(key);
+    if (op == LIKE) {
+      modified_val = convertLikeExpr(value);
     }
+    NSExpression* keyExp = [NSExpression
+        expressionForKeyPath:[NSString
+                                 stringWithUTF8String:modified_key.c_str()]];
+    NSString* valStr = [NSString stringWithUTF8String:modified_val.c_str()];
+
+    NSExpression* valExp = nil;
+    if (key == "timestamp") {
+      double provided_timestamp = [valStr doubleValue];
+      valExp = [NSExpression
+          expressionForConstantValue:
+              [NSDate dateWithTimeIntervalSince1970:provided_timestamp]];
+    } else if (columnIsNumeric.at(key)) {
+      valExp =
+          [NSExpression expressionWithFormat:@"%lld", [valStr longLongValue]];
+    } else {
+      valExp = [NSExpression expressionForConstantValue:valStr];
+    }
+
+    NSPredicate* pred = [NSComparisonPredicate
+        predicateWithLeftExpression:keyExp
+                    rightExpression:valExp
+                           modifier:NSDirectPredicateModifier
+                               type:supportedOps.at(op)
+                            options:0];
+    [preds addObject:pred];
+  }
 }
 
-
-void genUnifiedLog(QueryContext& queryContext, QueryData &results) {
-
-    if (@available(macOS 10.15, *)) {
-    NSError *error = nil;
-    OSLogStore *logstore = [OSLogStore localStoreAndReturnError:&error];
+void genUnifiedLog(QueryContext& queryContext, QueryData& results) {
+  if (@available(macOS 10.15, *)) {
+    NSError* error = nil;
+    OSLogStore* logstore = [OSLogStore localStoreAndReturnError:&error];
     if (error != nil) {
-        VLOG(1) << "error getting handle to log store: " << [[error localizedDescription] UTF8String];
-        return;
+      VLOG(1) << "error getting handle to log store: "
+              << [[error localizedDescription] UTF8String];
+      return;
     }
 
-    OSLogPosition *position = nil;
+    OSLogPosition* position = nil;
 
-    NSMutableArray *subpredicates = [[NSMutableArray alloc] init];
+    NSMutableArray* subpredicates = [[NSMutableArray alloc] init];
     if (queryContext.hasConstraint("timestamp", GREATER_THAN)) {
-        auto start_time = queryContext.constraints["timestamp"].getAll(GREATER_THAN);
+      auto start_time =
+          queryContext.constraints["timestamp"].getAll(GREATER_THAN);
 
-        double provided_timestamp = [[NSString stringWithUTF8String:start_time.begin()->c_str()] doubleValue];
-        NSDate *provided_date = [NSDate dateWithTimeIntervalSince1970:provided_timestamp];
+      double provided_timestamp = [[NSString
+          stringWithUTF8String:start_time.begin()->c_str()] doubleValue];
+      NSDate* provided_date =
+          [NSDate dateWithTimeIntervalSince1970:provided_timestamp];
 
-        position = [logstore positionWithDate:provided_date];
+      position = [logstore positionWithDate:provided_date];
     }
 
-    for (const auto &it : queryContext.constraints) {
-        const std::string &key = it.first;
-        for (const auto &constraint : it.second.getAll()) {
-            addQueryOp(subpredicates, key, constraint.expr,
-                       static_cast<ConstraintOperator>(constraint.op));
-        }
+    for (const auto& it : queryContext.constraints) {
+      const std::string& key = it.first;
+      for (const auto& constraint : it.second.getAll()) {
+        addQueryOp(subpredicates,
+                   key,
+                   constraint.expr,
+                   static_cast<ConstraintOperator>(constraint.op));
+      }
     }
 
-    // QueryContext.constraints doesn't list how the constraints interact with each other,
-    // so we assume they're all ANDed together.
-    NSPredicate *predicate = [NSCompoundPredicate andPredicateWithSubpredicates:subpredicates];
+    NSPredicate* predicate =
+        [NSCompoundPredicate andPredicateWithSubpredicates:subpredicates];
 
     // enumerate the entries in ascending order by timestamp
     OSLogEnumeratorOptions option = 0;
 
-    OSLogEnumerator *enumerator = [logstore entriesEnumeratorWithOptions:option
-                                                                position:position
-                                                               predicate:predicate
-                                                                   error:&error];
+    OSLogEnumerator* enumerator =
+        [logstore entriesEnumeratorWithOptions:option
+                                      position:position
+                                     predicate:predicate
+                                         error:&error];
     if (error != nil) {
-        VLOG(1) << "error enumerating entries in system log: " <<  [[error localizedDescription] UTF8String];
-        return;
+      VLOG(1) << "error enumerating entries in system log: "
+              << [[error localizedDescription] UTF8String];
+      return;
     }
-    for (OSLogEntryLog *entry in enumerator) {
-        Row r;
-        r["timestamp"] = BIGINT([[entry date] timeIntervalSince1970]);
-        r["message"] = TEXT([[entry composedMessage] UTF8String]);
-        r["storage"] = INTEGER([entry storeCategory]);
+    for (OSLogEntryLog* entry in enumerator) {
+      Row r;
+      r["timestamp"] = BIGINT([[entry date] timeIntervalSince1970]);
+      r["message"] = TEXT([[entry composedMessage] UTF8String]);
+      r["storage"] = INTEGER([entry storeCategory]);
 
-        if ([entry respondsToSelector:@selector(activityIdentifier)]) {
-          r["activity"] = INTEGER([entry activityIdentifier]);
-          r["process"] = TEXT([[entry process] UTF8String]);
-          r["pid"] = INTEGER([entry processIdentifier]);
-          r["sender"] = TEXT([[entry sender] UTF8String]);
-          r["tid"] = INTEGER([entry threadIdentifier]);
-        }
+      if ([entry respondsToSelector:@selector(activityIdentifier)]) {
+        r["activity"] = INTEGER([entry activityIdentifier]);
+        r["process"] = TEXT([[entry process] UTF8String]);
+        r["pid"] = INTEGER([entry processIdentifier]);
+        r["sender"] = TEXT([[entry sender] UTF8String]);
+        r["tid"] = INTEGER([entry threadIdentifier]);
+      }
 
-        if ([entry respondsToSelector:@selector(subsystem)]) {
-          NSString *subsystem = [entry subsystem];
-          if (subsystem != nil) {
-            r["subsystem"] = TEXT([subsystem UTF8String]);
-          }
-          NSString *category = [entry category];
-          if (category != nil) {
-            r["category"] = TEXT([category UTF8String]);
-          }
+      if ([entry respondsToSelector:@selector(subsystem)]) {
+        NSString* subsystem = [entry subsystem];
+        if (subsystem != nil) {
+          r["subsystem"] = TEXT([subsystem UTF8String]);
         }
-        results.push_back(r);
+        NSString* category = [entry category];
+        if (category != nil) {
+          r["category"] = TEXT([category UTF8String]);
+        }
+      }
+      results.push_back(r);
     }
-    }
+  }
 }
 
 QueryData genUnifiedLog(QueryContext& context) {
   QueryData results;
   if (@available(macOS 10.15, *)) {
-  @autoreleasepool {
-    genUnifiedLog(context, results);
-  }
+    @autoreleasepool {
+      genUnifiedLog(context, results);
+    }
   } else {
     VLOG(1) << "OSLog framework is not available";
   }
