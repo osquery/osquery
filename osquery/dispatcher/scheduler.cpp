0e1de30ec8fb98b42744b3162cbeca5392cb1a34
Breakwell <48999626+Breakwell@users.noreply.github.com>
2021-02-05 02:35:44 +0000

    Update the scheduler to log the query name at info level (#6934)

--- osquery/dispatcher/scheduler.cpp
+++ osquery/dispatcher/scheduler.cpp
@@ -47,6 +47,11 @@ FLAG(uint64,
 
 FLAG(uint64, schedule_epoch, 0, "Epoch for scheduled queries");
 
+FLAG(bool,
+     schedule_lognames,
+     false,
+     "Log the running scheduled query name at INFO level");
+
 HIDDEN_FLAG(bool,
             schedule_reload_sql,
             false,
@@ -55,6 +60,7 @@ HIDDEN_FLAG(bool,
 /// Used to bypass (optimize-out) the set-differential of query results.
 DECLARE_bool(events_optimize);
 DECLARE_bool(enable_numeric_monitoring);
+DECLARE_bool(verbose);
 
 SQLInternal monitor(const std::string& name, const ScheduledQuery& query) {
   if (FLAGS_enable_numeric_monitoring) {
@@ -100,7 +106,11 @@ SQLInternal monitor(const std::string& name, const ScheduledQuery& query) {
 
 Status launchQuery(const std::string& name, const ScheduledQuery& query) {
   // Execute the scheduled query and create a named query object.
-  VLOG(1) << "Executing scheduled query " << name << ": " << query.query;
+  if (FLAGS_verbose) {
+    VLOG(1) << "Executing scheduled query " << name << ": " << query.query;
+  } else if (FLAGS_schedule_lognames) {
+    LOG(INFO) << "Executing scheduled query " << name;
+  }
   runDecorators(DECORATE_ALWAYS);
 
   auto sql = monitor(name, query);
