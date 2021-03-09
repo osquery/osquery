dfe2d9aa89f085997e4310a98a42ba238ab75f84
Mike Myers <30631532+mike-myers-tob@users.noreply.github.com>
2021-03-08 17:28:16 -0800

    Return early if NSLog isn't supported

--- osquery/tables/system/darwin/unified_log.mm
+++ osquery/tables/system/darwin/unified_log.mm
@@ -42,15 +42,15 @@ const std::map<std::string, std::string> kColumnToOSLogEntryProp = {
     {"category", "category"}};
 
 const std::map<std::string, bool> kColumnIsNumeric = {{"timestamp", false},
-                                                     {"message", false},
-                                                     {"storage", true},
-                                                     {"activity", true},
-                                                     {"process", false},
-                                                     {"pid", true},
-                                                     {"sender", false},
-                                                     {"tid", true},
-                                                     {"subsystem", false},
-                                                     {"category", false}};
+                                                      {"message", false},
+                                                      {"storage", true},
+                                                      {"activity", true},
+                                                      {"process", false},
+                                                      {"pid", true},
+                                                      {"sender", false},
+                                                      {"tid", true},
+                                                      {"subsystem", false},
+                                                      {"category", false}};
 
 std::string convertLikeExpr(const std::string& value) {
   // for the LIKE operator in NSPredicates, '*' matches 0 or more characters
@@ -100,112 +100,113 @@ void addQueryOp(NSMutableArray* preds,
 
 QueryData genUnifiedLog(QueryContext& queryContext) {
   QueryData results;
-  if (@available(macOS 10.15, *)) {
-    @autoreleasepool {
-      NSError* error = nil;
-      OSLogStore* logstore = [OSLogStore localStoreAndReturnError:&error];
-      if (error != nil) {
-        TLOG << "error getting handle to log store: "
-             << [[error localizedDescription] UTF8String];
-        return {};
+  if (!@available(macOS 10.15, *)) {
+    VLOG(1) << "OSLog framework is not available";
+    return {};
+  }
+
+  @autoreleasepool {
+    NSError* error = nil;
+    OSLogStore* logstore = [OSLogStore localStoreAndReturnError:&error];
+    if (error != nil) {
+      TLOG << "error getting handle to log store: "
+           << [[error localizedDescription] UTF8String];
+      return {};
+    }
+
+    OSLogPosition* position = nil;
+
+    // the timestamp column can be used to aggressively filter
+    // results returned from the log store
+    if (queryContext.hasConstraint("timestamp", GREATER_THAN) ||
+        queryContext.hasConstraint("timestamp", GREATER_THAN_OR_EQUALS)) {
+      std::string start_time;
+      if (queryContext.hasConstraint("timestamp", GREATER_THAN)) {
+        start_time =
+            *queryContext.constraints["timestamp"].getAll(GREATER_THAN).begin();
+      } else {
+        start_time = *queryContext.constraints["timestamp"]
+                          .getAll(GREATER_THAN_OR_EQUALS)
+                          .begin();
       }
 
-      OSLogPosition* position = nil;
+      double provided_timestamp =
+          [[NSString stringWithUTF8String:start_time.c_str()] doubleValue];
+      NSDate* provided_date =
+          [NSDate dateWithTimeIntervalSince1970:provided_timestamp];
 
-      // the timestamp column can be used to aggressively filter
-      // results returned from the log store
-      if (queryContext.hasConstraint("timestamp", GREATER_THAN) ||
-          queryContext.hasConstraint("timestamp", GREATER_THAN_OR_EQUALS)) {
-        std::string start_time;
-        if (queryContext.hasConstraint("timestamp", GREATER_THAN)) {
-          start_time = *queryContext.constraints["timestamp"]
-                            .getAll(GREATER_THAN)
-                            .begin();
-        } else {
-          start_time = *queryContext.constraints["timestamp"]
-                            .getAll(GREATER_THAN_OR_EQUALS)
-                            .begin();
-        }
+      position = [logstore positionWithDate:provided_date];
+    }
 
-        double provided_timestamp =
-            [[NSString stringWithUTF8String:start_time.c_str()] doubleValue];
-        NSDate* provided_date =
-            [NSDate dateWithTimeIntervalSince1970:provided_timestamp];
-
-        position = [logstore positionWithDate:provided_date];
-      }
-
-      // grab all the supported columns used in simple constraints and make a
-      // compound predicate out of them.
-      NSMutableArray* subpredicates = [[NSMutableArray alloc] init];
-      for (const auto& it : queryContext.constraints) {
-        const std::string& key = it.first;
-        for (const auto& constraint : it.second.getAll()) {
-          addQueryOp(subpredicates,
-                     key,
-                     constraint.expr,
-                     static_cast<ConstraintOperator>(constraint.op));
-        }
-      }
-
-      NSPredicate* predicate =
-          [NSCompoundPredicate andPredicateWithSubpredicates:subpredicates];
-
-      // enumerate the entries in ascending order by timestamp
-      OSLogEnumeratorOptions option = 0;
-
-      OSLogEnumerator* enumerator =
-          [logstore entriesEnumeratorWithOptions:option
-                                        position:position
-                                       predicate:predicate
-                                           error:&error];
-      if (error != nil) {
-        TLOG << "error enumerating entries in system log: "
-             << [[error localizedDescription] UTF8String];
-        return {};
-      }
-      for (OSLogEntryLog* entry in enumerator) {
-        Row r;
-        r["timestamp"] = BIGINT([[entry date] timeIntervalSince1970]);
-        r["message"] = TEXT([[entry composedMessage] UTF8String]);
-        r["storage"] = INTEGER([entry storeCategory]);
-
-        if ([entry respondsToSelector:@selector(activityIdentifier)]) {
-          r["activity"] = INTEGER([entry activityIdentifier]);
-          r["process"] = TEXT([[entry process] UTF8String]);
-          r["pid"] = INTEGER([entry processIdentifier]);
-          r["sender"] = TEXT([[entry sender] UTF8String]);
-          r["tid"] = INTEGER([entry threadIdentifier]);
-        }
-
-        if ([entry respondsToSelector:@selector(subsystem)]) {
-          NSString* subsystem = [entry subsystem];
-          if (subsystem != nil) {
-            r["subsystem"] = TEXT([subsystem UTF8String]);
-          }
-          NSString* category = [entry category];
-          if (category != nil) {
-            r["category"] = TEXT([category UTF8String]);
-          }
-        }
-        if ([entry respondsToSelector:@selector(level)]) {
-          const char* logLevelNames[] = {
-              [OSLogEntryLogLevelUndefined] = "undefined",
-              [OSLogEntryLogLevelDebug] = "debug",
-              [OSLogEntryLogLevelInfo] = "info",
-              [OSLogEntryLogLevelNotice] = "default",
-              [OSLogEntryLogLevelError] = "error",
-              [OSLogEntryLogLevelFault] = "fault",
-          };
-
-          r["level"] = TEXT(logLevelNames[[entry level]]);
-        }
-        results.push_back(r);
+    // grab all the supported columns used in simple constraints and make a
+    // compound predicate out of them.
+    NSMutableArray* subpredicates = [[NSMutableArray alloc] init];
+    for (const auto& it : queryContext.constraints) {
+      const std::string& key = it.first;
+      for (const auto& constraint : it.second.getAll()) {
+        addQueryOp(subpredicates,
+                   key,
+                   constraint.expr,
+                   static_cast<ConstraintOperator>(constraint.op));
       }
     }
-  } else {
-    VLOG(1) << "OSLog framework is not available";
+
+    NSPredicate* predicate =
+        [NSCompoundPredicate andPredicateWithSubpredicates:subpredicates];
+
+    // enumerate the entries in ascending order by timestamp
+    OSLogEnumeratorOptions option = 0;
+
+    OSLogEnumerator* enumerator =
+        [logstore entriesEnumeratorWithOptions:option
+                                      position:position
+                                     predicate:predicate
+                                         error:&error];
+    if (error != nil) {
+      TLOG << "error enumerating entries in system log: "
+           << [[error localizedDescription] UTF8String];
+      return {};
+    }
+    for (OSLogEntryLog* entry in enumerator) {
+      Row r;
+      r["timestamp"] = BIGINT([[entry date] timeIntervalSince1970]);
+      r["message"] = TEXT([[entry composedMessage] UTF8String]);
+      r["storage"] = INTEGER([entry storeCategory]);
+
+      if ([entry respondsToSelector:@selector(activityIdentifier)]) {
+        r["activity"] = INTEGER([entry activityIdentifier]);
+        r["process"] = TEXT([[entry process] UTF8String]);
+        r["pid"] = INTEGER([entry processIdentifier]);
+        r["sender"] = TEXT([[entry sender] UTF8String]);
+        r["tid"] = INTEGER([entry threadIdentifier]);
+      }
+
+      if ([entry respondsToSelector:@selector(subsystem)]) {
+        NSString* subsystem = [entry subsystem];
+        if (subsystem != nil) {
+          r["subsystem"] = TEXT([subsystem UTF8String]);
+        }
+        NSString* category = [entry category];
+        if (category != nil) {
+          r["category"] = TEXT([category UTF8String]);
+        }
+      }
+      if ([entry respondsToSelector:@selector(level)]) {
+        const char* logLevelNames[] = {
+            [OSLogEntryLogLevelUndefined] = "undefined",
+            [OSLogEntryLogLevelDebug] = "debug",
+            [OSLogEntryLogLevelInfo] = "info",
+            [OSLogEntryLogLevelNotice] = "default",
+            [OSLogEntryLogLevelError] = "error",
+            [OSLogEntryLogLevelFault] = "fault",
+        };
+
+        r["level"] = TEXT(logLevelNames[[entry level]]);
+      }
+      results.push_back(r);
+    }
   }
+
   return results;
 }
 
