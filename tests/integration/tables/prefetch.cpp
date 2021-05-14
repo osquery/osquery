ca7ac2c5d79f75468b7e3560cb7fcf81ad0ec9e1
puffycid <puffycyan@gmail.com>
2021-05-13 21:17:18 -0700

    tests

--- tests/integration/tables/prefetch.cpp
+++ tests/integration/tables/prefetch.cpp
@@ -22,13 +22,13 @@ TEST_F(PrefetchTest, test_sanity) {
   QueryData const rows = execute_query(
       "select * from prefetch where path like "
       "'D:"
-      "\\a\\osquery\\osquery\\w\\src\\tools\\tests\\configs\\windows\\prefetch"
+      "\a\osquery\osquery\w\src\tools\tests\configs\windows\prefetch"
       "\\%.pf");
   QueryData const specific_rows = execute_query(
       "select * from prefetch where path like "
       "'D:"
-      "\\a\\osquery\\osquery\\w\\src\\tools\\tests\\configs\\windows\\prefetch"
-      "\\%.pf AND last_execution_time = 1620953788 AND count = 3 AND "
+      "\a\osquery\osquery\w\src\tools\tests\configs\windows\prefetch"
+      "\%.pf AND last_execution_time = 1620953788 AND count = 3 AND "
       "number_of_accessed_files=53");
   ValidationMap row_map = {
       {"path", NonEmptyString},
@@ -45,11 +45,17 @@ TEST_F(PrefetchTest, test_sanity) {
       {"volume_creation", NormalType},
       {"number_of_accessed_files", NormalType},
   };
+  QueryData const default_rows = execute_query("select * from prefetch");
   ASSERT_GT(rows.size(), 0ul);
   validate_rows(rows, row_map);
 
   ASSERT_EQ(specific_rows.size(), 1ul);
   validate_rows(specific_rows, row_map);
+
+  if (!default_rows.empty()) {
+    ASSERT_GT(rows.size(), 0ul);
+    validate_rows(rows, row_map);
+  }
 }
 } // namespace table_tests
 } // namespace osquery
