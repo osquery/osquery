3cbfcde42c8e1a779926f98e96cee83f3128a9ba
puffycid <puffycyan@gmail.com>
2021-06-13 16:47:06 -0400

    fixing lief, adding windows

--- osquery/tables/system/darwin/machoinfo.cpp
+++ osquery/tables/system/darwin/machoinfo.cpp
@@ -14,15 +14,230 @@
 #include <LIEF/LIEF.hpp>
 #include <sstream>
 
-#include <iostream>
 
 namespace osquery {
 namespace tables {
 
+QueryData genMachoFunctions(QueryContext& context){
+  QueryData results;
+  auto paths = context.constraints["path"].getAll(EQUALS);
+
+  // Expand contstraints
+  context.expandConstraints(
+      "path",
+      LIKE,
+      paths,
+      ([&](const std::string& pattern, std::set<std::string>& out) {
+        std::vector<std::string> patterns;
+        auto status =
+            resolveFilePattern(pattern, patterns, GLOB_ALL | GLOB_NO_CANON);
+        if (status.ok()) {
+          for (const auto& resolved : patterns) {
+            out.insert(resolved);
+          }
+        }
+        return status;
+      }));
+  boost::system::error_code ec;
+   
+
+    for (const auto& path_string : paths) {
+      boost::filesystem::path path = path_string;
+      if (!boost::filesystem::is_regular_file(path, ec)) {
+        continue;
+      }
+      // Skip non-macho files
+      if (!LIEF::MachO::is_macho(path_string)) {
+        continue;
+      }
+      auto config = LIEF::MachO::ParserConfig().deep();
+      std::unique_ptr<LIEF::MachO::FatBinary> mac_binary =
+          LIEF::MachO::Parser::parse(path_string, config.deep());
+      try {
+        for (const auto& data : mac_binary->begin()) {
+          for (const auto& function : data.imported_functions()) {
+            Row r; 
+
+            r["path"] = path_string;
+            r["filename"] = path.filename().string();
+            r["arch"] = LIEF::MachO::to_string(data.header().cpu_type());
+            r["function_type"] = "import";
+            // Macho function names have extra "_", strip first character if its
+            // "_"
+            if (function.name().at(0) == '_') {
+              std::string func = function.name();
+              r["function_name"] = func.erase(0, 1);
+
+            } else {
+              r["function_name"] = function.name();
+            }
+
+	    std::ostringstream stream;
+	    stream << std::hex << function.address();
+            r["function_address"] = stream.str();
+            results.push_back(r);
+          }
+          for (const auto& function : data.exported_functions()) {
+            Row r;
+            r["path"] = path_string;
+            r["filename"] = path.filename().string();
+            r["arch"] = LIEF::MachO::to_string(data.header().cpu_type());
+            r["function_type"] = "export";
+            r["function_name"] = function.name();
+	    std::ostringstream stream;
+	    stream << std::hex << function.address();
+            r["function_address"] = stream.str();
+            results.push_back(r);
+          }
+        }
+
+      } catch (std::exception& error) {
+        LOG(WARNING) << "Failed to parse Mach-O file: " << error.what();
+      }
+    }
+    return results;
+  }
+
+QueryData genMachoLibraries(QueryContext& context){
+  QueryData results;
+  auto paths = context.constraints["path"].getAll(EQUALS);
+
+  // Expand contstraints
+  context.expandConstraints(
+      "path",
+      LIKE,
+      paths,
+      ([&](const std::string& pattern, std::set<std::string>& out) {
+        std::vector<std::string> patterns;
+        auto status =
+            resolveFilePattern(pattern, patterns, GLOB_ALL | GLOB_NO_CANON);
+        if (status.ok()) {
+          for (const auto& resolved : patterns) {
+            out.insert(resolved);
+          }
+        }
+        return status;
+      }));
+  auto config = LIEF::MachO::ParserConfig().deep();
+    boost::system::error_code ec;
+
+    for (const auto& path_string : paths) {
+      boost::filesystem::path path = path_string;
+      if (!boost::filesystem::is_regular_file(path, ec)) {
+        continue;
+      }
+      // Skip non-macho files
+      if (!LIEF::MachO::is_macho(path_string)) {
+        continue;
+      }
+      try {
+        // Parse macho file and get some information
+        std::unique_ptr<LIEF::MachO::FatBinary> mac_binary =
+            LIEF::MachO::Parser::parse(path_string, config.deep());
+
+        for (const auto& data : mac_binary->begin()) {
+          for (const auto& library : data.libraries().begin()) {
+            Row r;
+            r["path"] = path_string;
+            r["filename"] = path.filename().string();
+            r["arch"] = LIEF::MachO::to_string(data.header().cpu_type());
+            r["library_name"] = library.name();
+            std::ostringstream ss;
+            for (const auto& version : library.current_version()) {
+              ss << std::to_string(version) << ".";
+            }
+            r["version"] = ss.str().substr(0, ss.str().size() - 1);
+            ss.str("");
+            for (const auto& version : library.compatibility_version()) {
+              ss << std::to_string(version) << ".";
+            }
+            r["compatibility_version"] =
+                ss.str().substr(0, ss.str().size() - 1);
+            ss.str("");
+
+            results.push_back(r);
+          }
+        }
+      } catch (std::exception& error) {
+        LOG(WARNING) << "Failed to parse Mach-O file: " << error.what();
+      }
+    }
+    return results;
+  }
+
+QueryData genMachoSections(QueryContext& context){
+  QueryData results;
+  auto paths = context.constraints["path"].getAll(EQUALS);
+
+  // Expand contstraints
+  context.expandConstraints(
+      "path",
+      LIKE,
+      paths,
+      ([&](const std::string& pattern, std::set<std::string>& out) {
+        std::vector<std::string> patterns;
+        auto status =
+            resolveFilePattern(pattern, patterns, GLOB_ALL | GLOB_NO_CANON);
+        if (status.ok()) {
+          for (const auto& resolved : patterns) {
+            out.insert(resolved);
+          }
+        }
+        return status;
+      }));
+  auto config = LIEF::MachO::ParserConfig().deep();
+    boost::system::error_code ec;
+
+    for (const auto& path_string : paths) {
+      boost::filesystem::path path = path_string;
+      if (!boost::filesystem::is_regular_file(path, ec)) {
+        continue;
+      }
+      // Skip non-macho files
+      if (!LIEF::MachO::is_macho(path_string)) {
+        continue;
+      }
+      try {
+        std::unique_ptr<LIEF::MachO::FatBinary> mac_binary =
+            LIEF::MachO::Parser::parse(path_string, config.deep());
+
+        for (const auto& data : mac_binary->begin()) {
+          for (const auto& section : data.sections().begin()) {
+            Row r;
+
+            r["path"] = path_string;
+            r["filename"] = path.filename().string();
+            r["arch"] = LIEF::MachO::to_string(data.header().cpu_type());
+            r["section_name"] = section.name();
+	    std::ostringstream stream;
+	    stream << std::hex << section.address();
+            r["section_address"] = stream.str();
+	    stream.str("");
+	    stream << std::hex << section.flags();
+            r["section_flags"] = stream.str();
+            r["section_size"] = INTEGER(section.Section::size());
+
+            // LIEF returns 0 as -0.0000, strip negative sign from 0 value
+            if (std::to_string(section.entropy()).find("-") !=
+                std::string::npos) {
+              r["entropy"] = std::to_string(section.entropy()).erase(0, 1);
+
+            } else {
+              r["entropy"] = std::to_string(section.entropy());
+            }
+            results.push_back(r);
+          }
+        }
+      } catch (std::exception& error) {
+        LOG(WARNING) << "Failed to parse Mach-O file: " << error.what();
+      }
+    }
+    return results;
+  }
+
 QueryData genMachoInfo(QueryContext& context) {
   QueryData results;
-  std::cout << "Hi!!" << std::endl;
-  /*
+  
   auto paths = context.constraints["path"].getAll(EQUALS);
 
   // Expand contstraints
@@ -110,7 +325,7 @@ QueryData genMachoInfo(QueryContext& context) {
       LOG(WARNING) << "Failed to parse Mach-O file: " << error.what();
     }
   }
-  */
+  
   return results;
 }
 } // namespace tables
