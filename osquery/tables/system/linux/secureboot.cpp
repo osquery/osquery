729870128966d4dc83180907b9e7e0b98208aa0a
seph <seph@directionless.org>
2021-07-18 15:18:24 -0400

    linux

--- osquery/tables/system/linux/secureboot.cpp
+++ osquery/tables/system/linux/secureboot.cpp
@@ -9,23 +9,50 @@
 
 #include <osquery/core/core.h>
 #include <osquery/core/tables.h>
+#include <osquery/filesystem/filesystem.h>
 #include <osquery/logger/logger.h>
 #include <osquery/tables/system/secureboot.hpp>
 
 namespace osquery {
 namespace tables {
 
-// Linux directory for efi vars
-const std::string efivarsPath = "/sys/firmware/efi/vars/";
+// Linux has 2 places efivars can be accessed:
+//   /sys/firmware/efi/efivars -- Single file, world readable
+//   /sys/firmware/efi/vars    -- Split into attributes and data, only root
+//   readable
+//
+// There's not much documentation about the provenance of these two
+// interfaces. While the `vars` directory is more usable (having a
+// split data from attributes), the benefit of not requiring root
+// outweighs that.
+const std::string efivarsDir = "/sys/firmware/efi/efivars/";
 
 int readBoolEfiVar(std::string guid, std::string name) {
-  const std::string efivar = efivarsPath + name + '-' + guid;
+  const std::string efivarPath = efivarsDir + name + '-' + guid;
 
-  BYTE data;
+  // The first 4 bytes of efivars are attribute data, we don't need
+  // that data here, so we can just ignore it. The 5th byte is a
+  // boolean representation.
+  std::string efiData;
+  if (!readFile(efivarPath, efiData, 5).ok()) {
+    // FIXME: this is likely to mean it's unsupported by the
+    // kernel/boot/whatever. Probably don't log and return null?
+    TLOG << "Cannot read efivar file : " << efivarPath;
+    return -1;
+  }
 
-  if (!readFile(path, data).ok()) {
-    TLOG << "Cannot read efivar file : " << path
-      return -1;
+  if (efiData.length() != 5) {
+    TLOG << "Under read on efivar file : " << efivarPath;
+    return -1;
+  }
+
+  auto val = (int)(unsigned char)(efiData.back());
+
+  switch (val) {
+  case 0:
+    return 0;
+  case 1:
+    return 1;
   }
 
   return -1;
@@ -33,10 +60,15 @@ int readBoolEfiVar(std::string guid, std::string name) {
 
 QueryData genSecureBoot(QueryContext& context) {
   QueryData results;
-
   Row r;
-  r["secure_boot"] = readBoolEfiVar(kBootGUID, kSecureBootName);
-  r["setup_mode"] = readBoolEfiVar(kBootGUID, kSetupModeName);
+
+  // There's a kernel rate limit on non-root reads to the EFI
+  // filesystem of 100 reads per second. We could consider adding a
+  // sleep, as a means to a rate limit (this is what the efivar tool
+  // does), but this seems unlikely to be an issue in normal osquery
+  // use. So we do nothing, aside from note it here.
+  r["secure_boot"] = INTEGER(readBoolEfiVar(kEFIBootGUID, kEFISecureBootName));
+  r["setup_mode"] = INTEGER(readBoolEfiVar(kEFIBootGUID, kEFISetupModeName));
 
   results.push_back(r);
   return results;
