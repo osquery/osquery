8ae7286b5195577a1514b3666f4e4c69548b85c8
Aleksandr Maus <aleksandr.maus@elastic.co>
2021-12-08 15:54:47 -0500

    Fix Integration test when there is no windows updates history records

--- tests/integration/tables/windows_update_history.cpp
+++ tests/integration/tables/windows_update_history.cpp
@@ -26,24 +26,27 @@ TEST_F(windows_update_history, test_sanity) {
   auto const data =
       execute_query("select * from windows_update_history LIMIT 1");
 
-  ASSERT_EQ(data.size(), 1ul);
+  // The build box might not have any history of updates
+  if (data.size() > 0) {
+    ASSERT_EQ(data.size(), 1ul);
 
-  ValidationMap row_map = {
-      {"client_app_id", NormalType},
-      {"date", IntType},
-      {"description", NormalType},
-      {"hresult", IntType},
-      {"operation", NormalType},
-      {"result_code", NormalType},
-      {"server_selection", NormalType},
-      {"service_id", NormalType},
-      {"support_url", NormalType},
-      {"title", NormalType},
-      {"update_id", NormalType},
-      {"update_revision", IntType},
-  };
+    ValidationMap row_map = {
+        {"client_app_id", NormalType},
+        {"date", IntType},
+        {"description", NormalType},
+        {"hresult", IntType},
+        {"operation", NormalType},
+        {"result_code", NormalType},
+        {"server_selection", NormalType},
+        {"service_id", NormalType},
+        {"support_url", NormalType},
+        {"title", NormalType},
+        {"update_id", NormalType},
+        {"update_revision", IntType},
+    };
 
-  validate_rows(data, row_map);
+    validate_rows(data, row_map);
+  }
 }
 
 } // namespace table_tests
