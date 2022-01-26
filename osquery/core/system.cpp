852d87b0eb6718ec527fa8484390cb4ae82b76ae
seph <seph@directionless.org>
2022-01-26 08:58:48 -0500

    Add `utc` flag back for compatibility(#7460)
    Add a no-op hidden `utc` flag to allow for backward compatibility and transition time with older configuration

--- osquery/core/system.cpp
+++ osquery/core/system.cpp
@@ -97,6 +97,14 @@ FLAG(string,
      "",
      "Field used to specify the host_identifier when set to \"specified\"");
 
+// Deprecated, unused. Exists for backwards compatibility with existing config
+// files.
+HIDDEN_FLAG(
+    bool,
+    utc,
+    true,
+    "Deprecated utc flag. From https://github.com/osquery/osquery/pull/7276");
+
 namespace {
 
 const std::vector<std::string> kPlaceholderHardwareUUIDList{
