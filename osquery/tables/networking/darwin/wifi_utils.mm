f39e8e7548cfde8ef1aae4d99aea9111ee9516ff
Sharvil Shah <sharvil@sharvilshah.com>
2022-03-14 19:18:16 +0530

    update wifi networks to work with big sur and higher

--- osquery/tables/networking/darwin/wifi_utils.mm
+++ osquery/tables/networking/darwin/wifi_utils.mm
@@ -10,10 +10,44 @@
 #include <CoreFoundation/CoreFoundation.h>
 
 #include <osquery/core/system.h>
+#include <osquery/core/tables.h>
 #include <osquery/tables/networking/darwin/wifi_utils.h>
+#include <osquery/utils/conversions/darwin/cfdata.h>
+#include <osquery/utils/conversions/darwin/cfnumber.h>
+#include <osquery/utils/conversions/darwin/cfstring.h>
 
 namespace osquery {
 namespace tables {
+
+std::string getPropertiesFromDictionary(const CFDictionaryRef& dict,
+                                        const std::string& key) {
+  std::string value;
+
+  auto cfkey = CFStringCreateWithCString(
+      kCFAllocatorDefault, key.c_str(), kCFStringEncodingUTF8);
+  auto property = CFDictionaryGetValue(dict, cfkey);
+  CFRelease(cfkey);
+
+  if (property == nullptr) {
+    return value;
+  }
+
+  if (CFGetTypeID(property) == CFNumberGetTypeID()) {
+    value = stringFromCFNumber((CFDataRef)property);
+  } else if (CFGetTypeID(property) == CFStringGetTypeID()) {
+    value = stringFromCFString((CFStringRef)property);
+  } else if (CFGetTypeID(property) == CFDataGetTypeID()) {
+    value = stringFromCFData((CFDataRef)property);
+  } else if (CFGetTypeID(property) == CFBooleanGetTypeID()) {
+    value = (CFBooleanGetValue((CFBooleanRef)property)) ? "1" : "0";
+  } else if (CFGetTypeID(property) == CFDateGetTypeID()) {
+    auto unix_time = CFDateGetAbsoluteTime((CFDateRef)property) +
+                     kCFAbsoluteTimeIntervalSince1970;
+    value = INTEGER(std::llround(unix_time));
+  }
+
+  return value;
+}
 
 std::string extractSsid(const CFDataRef& data) {
   if (data == nil) {
