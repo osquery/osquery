31940f2c5f52f3c692645655332de833746aaa61
Mike Myers <30631532+mike-myers-tob@users.noreply.github.com>
2022-05-02 17:11:45 -0700

    Explicitly set context for the logged_in_users table

--- osquery/tables/system/posix/logged_in_users.cpp
+++ osquery/tables/system/posix/logged_in_users.cpp
@@ -38,6 +38,8 @@ QueryData genLoggedInUsers(QueryContext& context) {
   QueryData results;
   struct utmpx* entry = nullptr;
 
+  utmpxname(_PATH_UTMP);
+
   while ((entry = getutxent()) != nullptr) {
     if (entry->ut_pid == 1) {
       continue;
