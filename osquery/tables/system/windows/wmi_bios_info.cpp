794e55732bc3da54353fd9c811b5b51ce593de39
Alessandro Gario <alessandro.gario@gmail.com>
2022-06-14 20:41:20 +0200

    wmi_bios_info: Include Win32_BIOS attributes for all systems

--- osquery/tables/system/windows/wmi_bios_info.cpp
+++ osquery/tables/system/windows/wmi_bios_info.cpp
@@ -7,16 +7,20 @@
  * SPDX-License-Identifier: (Apache-2.0 OR GPL-2.0-only)
  */
 
+#include <algorithm>
+#include <iomanip>
+#include <map>
 #include <regex>
 #include <string>
+#include <vector>
 
 #include <osquery/core/tables.h>
+#include <osquery/core/windows/wmi.h>
 #include <osquery/logger/logger.h>
-
 #include <osquery/utils/conversions/split.h>
 #include <osquery/utils/conversions/tryto.h>
-
-#include <osquery/core/windows/wmi.h>
+#include <osquery/utils/conversions/windows/strings.h>
+#include <osquery/utils/conversions/windows/windows_time.h>
 
 namespace osquery {
 namespace tables {
@@ -26,27 +30,30 @@ const std::vector<std::string> kHP = {
     "hp", "hewlett-packard", "hewlett packard"};
 const std::vector<std::string> kLenovo = {"lenovo"};
 const std::vector<std::string> kDell = {"dell inc."};
-const std::map<std::string, std::pair<std::string, std::wstring>> kQueryMap = {
-    {"hp",
-     {"select Name,Value from HP_BiosSetting", L"root\\hp\\instrumentedBIOS"}},
-    {"lenovo", {"select CurrentSetting from Lenovo_BiosSetting", L"root\\wmi"}},
-    // Dell machines have two different wmi classes for bios information.
-    // Biosattributes class is present on all machines released after 2018 and
-    // DCIM_BIOSEnumeration is on the machines released prior to 2018 or have
-    // Dell Command Monitor driver installed on them.
-    {"dell",
-     {"select AttributeName,CurrentValue from EnumerationAttribute",
-      L"root\\dcim\\sysman\\biosattributes"}},
-    {"dell-legacy",
-     {"select AttributeName,CurrentValue,PossibleValues, "
-      "PossibleValuesDescription from DCIM_BIOSEnumeration",
-      L"root\\dcim\\sysman"}}};
+const std::map<std::string, std::pair<std::string, std::wstring>>
+    kVendorSpecificQueryMap = {
+        {"hp",
+         {"select Name,Value from HP_BiosSetting",
+          L"root\\hp\\instrumentedBIOS"}},
+        {"lenovo",
+         {"select CurrentSetting from Lenovo_BiosSetting", L"root\\wmi"}},
+        // Dell machines have two different wmi classes for bios information.
+        // Biosattributes class is present on all machines released after 2018
+        // and DCIM_BIOSEnumeration is on the machines released prior to 2018 or
+        // have Dell Command Monitor driver installed on them.
+        {"dell",
+         {"select AttributeName,CurrentValue from EnumerationAttribute",
+          L"root\\dcim\\sysman\\biosattributes"}},
+        {"dell-legacy",
+         {"select AttributeName,CurrentValue,PossibleValues, "
+          "PossibleValuesDescription from DCIM_BIOSEnumeration",
+          L"root\\dcim\\sysman"}}};
 
 std::string getManufacturer(std::string manufacturer) {
-  transform(manufacturer.begin(),
-            manufacturer.end(),
-            manufacturer.begin(),
-            ::tolower);
+  std::transform(manufacturer.begin(),
+                 manufacturer.end(),
+                 manufacturer.begin(),
+                 ::tolower);
 
   if (std::find(kHP.begin(), kHP.end(), manufacturer) != kHP.end()) {
     manufacturer = "hp";
@@ -57,7 +64,7 @@ std::string getManufacturer(std::string manufacturer) {
              kDell.end()) {
     // If it's a Dell machine, we check if the legacy class exists or not and
     // accordingly return the corresponding manufacturer name.
-    auto it = kQueryMap.find("dell-legacy");
+    auto it = kVendorSpecificQueryMap.find("dell-legacy");
     const auto wmiBiosReq = WmiRequest::CreateWmiRequest(
         std::get<0>(it->second), std::get<1>(it->second));
     if (wmiBiosReq && !wmiBiosReq->results().empty()) {
@@ -155,52 +162,307 @@ Row getDellBiosInfo(const WmiResultItem& item) {
   return r;
 }
 
-QueryData genBiosInfo(QueryContext& context) {
-  const auto wmiComputerSystemReq = WmiRequest::CreateWmiRequest(
-      "select Manufacturer from Win32_ComputerSystem");
-  if (!wmiComputerSystemReq || wmiComputerSystemReq->results().empty()) {
+QueryData generateCommonBiosRows() {
+  static const std::string kWin32BiosQuery{
+      "SELECT *, BiosCharacteristics, BIOSVersion, BuildNumber, Caption, "
+      "CodeSet, CurrentLanguage, Description, EmbeddedControllerMajorVersion, "
+      "EmbeddedControllerMinorVersion, IdentificationCode, "
+      "InstallableLanguages, InstallDate, LanguageEdition, ListOfLanguages, "
+      "Manufacturer, Name, OtherTargetOS, PrimaryBIOS, ReleaseDate, "
+      "SerialNumber, SMBIOSBIOSVersion, SMBIOSMajorVersion, "
+      "SMBIOSMinorVersion, SMBIOSPresent, SoftwareElementID, "
+      "SoftwareElementState, Status, SystemBiosMajorVersion, "
+      "SystemBiosMinorVersion, TargetOperatingSystem, Version FROM Win32_BIOS"};
+
+  static const std::vector<std::string> kStringKeyNameList{"BuildNumber",
+                                                           "Caption",
+                                                           "CodeSet",
+                                                           "CurrentLanguage",
+                                                           "Description",
+                                                           "IdentificationCode",
+                                                           "LanguageEdition",
+                                                           "Manufacturer",
+                                                           "Name",
+                                                           "OtherTargetOS",
+                                                           "SerialNumber",
+                                                           "SMBIOSBIOSVersion",
+                                                           "SoftwareElementID",
+                                                           "Status",
+                                                           "Version"};
+
+  static const std::vector<std::string> kStringVectorKeyNameList{
+      "BIOSVersion", "ListOfLanguages"};
+
+  static const std::vector<std::string> kUInt16KeyNameList{
+      "InstallableLanguages",
+      "SMBIOSMajorVersion",
+      "SMBIOSMinorVersion",
+      "SoftwareElementState",
+      "TargetOperatingSystem"};
+
+  static const std::vector<std::string> kUInt16VectorKeyNameList{
+      "BiosCharacteristics"};
+
+  static const std::vector<std::string> kUInt8KeyNameList{
+      "SystemBiosMajorVersion",
+      "SystemBiosMinorVersion",
+      "EmbeddedControllerMajorVersion",
+      "EmbeddedControllerMinorVersion"};
+
+  static const std::vector<std::string> kBooleanKeyNameList{"PrimaryBIOS",
+                                                            "SMBIOSPresent"};
+
+  static const std::vector<std::string> kDateKeyNameList{"InstallDate",
+                                                         "ReleaseDate"};
+
+  auto exp_wmi_request = WmiRequest::CreateWmiRequest(kWin32BiosQuery);
+  if (exp_wmi_request.isError()) {
+    const auto& error = exp_wmi_request.getError();
+
+    LOG(ERROR)
+        << "wmi_bios_info: The following WMI query could not be constructed: "
+        << kWin32BiosQuery << ". " << error.getMessage();
+
     return {};
   }
-  const auto& wmiComputerSystemResults = wmiComputerSystemReq->results();
+
+  auto wmi_request = exp_wmi_request.take();
+  if (!wmi_request.getStatus().ok()) {
+    LOG(ERROR) << "wmi_bios_info: The following WMI query has failed: "
+               << kWin32BiosQuery;
+    return {};
+  }
+
+  const auto& wmi_item_list = wmi_request.results();
+  if (wmi_item_list.empty()) {
+    LOG(ERROR)
+        << "wmi_bios_info: The following WMI query did not return any item: "
+        << kWin32BiosQuery;
+    return {};
+  }
+
+  if (wmi_item_list.size() != 1) {
+    LOG(ERROR) << "wmi_bios_info: The following WMI query returned an "
+                  "unexpected number of items: "
+               << kWin32BiosQuery;
+  }
+
+  const auto& wmi_item = wmi_item_list.back();
+
+  QueryData row_list;
+  auto L_generateRow = [&row_list](std::string key, std::string value) {
+    Row row{};
+    row["name"] = std::move(key);
+    row["value"] = std::move(value);
+
+    row_list.push_back(std::move(row));
+  };
+
+  for (const auto& key_name : kStringKeyNameList) {
+    std::string value;
+    auto status = wmi_item.GetString(key_name, value);
+    if (!status.ok()) {
+      VLOG(1) << "wmi_bios_info: Failed to copy the following Win32_BIOS WMI "
+                 "column value: "
+              << key_name;
+
+      value.clear();
+    }
+
+    L_generateRow(key_name, std::move(value));
+  }
+
+  for (const auto& key_name : kStringVectorKeyNameList) {
+    std::vector<std::string> value_list;
+    auto status = wmi_item.GetVectorOfStrings(key_name, value_list);
+    if (!status.ok()) {
+      VLOG(1) << "wmi_bios_info: Failed to copy the following Win32_BIOS WMI "
+                 "column value: "
+              << key_name;
+
+      value_list.clear();
+    }
+
+    std::string buffer = "{";
+    for (const auto& value : value_list) {
+      if (buffer.size() > 1) {
+        buffer += ", ";
+      }
+
+      buffer += value;
+    }
+
+    buffer += "}";
+
+    L_generateRow(key_name, std::move(buffer));
+  }
+
+  for (const auto& key_name : kUInt16KeyNameList) {
+    long value{};
+    auto status = wmi_item.GetLong(key_name, value);
+    if (!status.ok()) {
+      VLOG(1) << "wmi_bios_info: Failed to copy the following Win32_BIOS WMI "
+                 "column value: "
+              << key_name;
+      value = 0;
+    }
+
+    L_generateRow(key_name, std::to_string(value));
+  }
+
+  for (const auto& key_name : kUInt16VectorKeyNameList) {
+    std::vector<long> value_list;
+    auto status = wmi_item.GetVectorOfLongs(key_name, value_list);
+    if (!status.ok()) {
+      VLOG(1) << "wmi_bios_info: Failed to copy the following Win32_BIOS WMI "
+                 "column value: "
+              << key_name;
+      value_list.clear();
+    }
+
+    std::string buffer = "{";
+    for (const auto& value : value_list) {
+      if (buffer.size() > 1) {
+        buffer += ", ";
+      }
+
+      buffer += std::to_string(value);
+    }
+
+    buffer += "}";
+
+    L_generateRow(key_name, std::move(buffer));
+  }
+
+  for (const auto& key_name : kUInt8KeyNameList) {
+    unsigned char value{};
+    auto status = wmi_item.GetUChar(key_name, value);
+    if (!status.ok()) {
+      VLOG(1) << "wmi_bios_info: Failed to copy the following Win32_BIOS WMI "
+                 "column value: "
+              << key_name;
+      value = 0;
+    }
+
+    L_generateRow(key_name, std::to_string(value));
+  }
+
+  for (const auto& key_name : kBooleanKeyNameList) {
+    bool value{};
+    auto status = wmi_item.GetBool(key_name, value);
+    if (!status.ok()) {
+      VLOG(1) << "wmi_bios_info: Failed to copy the following Win32_BIOS WMI "
+                 "column value: "
+              << key_name;
+      value = false;
+    }
+
+    L_generateRow(key_name, value ? "true" : "false");
+  }
+
+  for (const auto& key_name : kDateKeyNameList) {
+    std::uint64_t timestamp{};
+
+    {
+      FILETIME value{};
+      auto status = wmi_item.GetDateTime(key_name, false, value);
+      if (status.ok()) {
+        timestamp = static_cast<std::uint64_t>(filetimeToUnixtime(value));
+
+      } else {
+        VLOG(1)
+            << "wmi_bios_info: Failed to acquire the following column value: "
+            << key_name << ". " << status.getMessage();
+      }
+    }
+
+    L_generateRow(key_name, std::to_string(timestamp));
+  }
+
+  return row_list;
+}
+
+QueryData genBiosInfo(QueryContext& context) {
+  static const std::string kWin32ComputerSystemQuery{
+      "select Manufacturer from Win32_ComputerSystem"};
+
+  auto row_list = generateCommonBiosRows();
 
   std::string manufacturer;
-  wmiComputerSystemResults[0].GetString("Manufacturer", manufacturer);
-  manufacturer = getManufacturer(manufacturer);
 
-  auto it = kQueryMap.find(manufacturer);
-  if (it == kQueryMap.end()) {
-    LOG(INFO) << "Vendor \"" << manufacturer << "\" is currently not supported";
-    return {};
+  {
+    auto exp_wmi_request =
+        WmiRequest::CreateWmiRequest(kWin32ComputerSystemQuery);
+    if (exp_wmi_request.isError()) {
+      const auto& error = exp_wmi_request.getError();
+      LOG(ERROR) << "wmi_bios_info: The WMI query has failed: "
+                 << error.getMessage();
+
+      return row_list;
+    }
+
+    auto wmi_request = exp_wmi_request.take();
+
+    const auto& wmi_item_list = wmi_request.results();
+    if (wmi_item_list.size() != 1) {
+      LOG(ERROR) << "wmi_bios_info: Too many items returned when trying to "
+                    "query for the the "
+                    "manufacturer";
+      return row_list;
+    }
+
+    const auto& first_wmi_item = wmi_item_list.back();
+    if (!first_wmi_item.GetString("Manufacturer", manufacturer)) {
+      LOG(ERROR) << "wmi_bios_info: Failed to read the bios manufacturer";
+      return row_list;
+    }
+
+    manufacturer = getManufacturer(manufacturer);
   }
 
-  QueryData results;
-  const auto wmiBiosReq = WmiRequest::CreateWmiRequest(std::get<0>(it->second),
-                                                       std::get<1>(it->second));
-  if (!wmiBiosReq) {
-    LOG(WARNING) << wmiBiosReq.getError().getMessage();
-    return results;
+  auto vendor_specific_query_it = kVendorSpecificQueryMap.find(manufacturer);
+  if (vendor_specific_query_it == kVendorSpecificQueryMap.end()) {
+    return row_list;
   }
-  const auto& wmiResults = wmiBiosReq->results();
-  for (size_t i = 0; i < wmiResults.size(); ++i) {
-    Row r;
+
+  const auto& wmi_query = std::get<0>(vendor_specific_query_it->second);
+  const auto& wmi_namespace = std::get<1>(vendor_specific_query_it->second);
+
+  auto exp_wmi_request = WmiRequest::CreateWmiRequest(wmi_query, wmi_namespace);
+  if (exp_wmi_request.isError()) {
+    const auto& error = exp_wmi_request.getError();
+    LOG(ERROR) << "wmi_bios_info: The following WMI query has failed: "
+               << wmi_query << " (namespace: " << wstringToString(wmi_namespace)
+               << "). " << error.getMessage();
+
+    return row_list;
+  }
+
+  auto wmi_request = exp_wmi_request.take();
+
+  const auto& wmi_item_list = wmi_request.results();
+
+  for (const auto& wmi_item : wmi_item_list) {
+    Row r{};
     if (manufacturer == "hp") {
-      r = getHPBiosInfo(wmiResults[i]);
+      r = getHPBiosInfo(wmi_item);
 
     } else if (manufacturer == "lenovo") {
-      r = getLenovoBiosInfo(wmiResults[i]);
+      r = getLenovoBiosInfo(wmi_item);
 
     } else if (manufacturer == "dell") {
-      r = getDellBiosInfo(wmiResults[i]);
+      r = getDellBiosInfo(wmi_item);
 
     } else if (manufacturer == "dell-legacy") {
-      r = getDellLegacyBiosInfo(wmiResults[i]);
+      r = getDellLegacyBiosInfo(wmi_item);
     }
+
     if (!r.empty()) {
-      results.push_back(r);
+      row_list.push_back(r);
     }
   }
 
-  return results;
+  return row_list;
 }
 } // namespace tables
 } // namespace osquery
