530b6f7a07804f1144635c8d21c1e87ffc0d7d99
Alessandro Gario <5714290+alessandrogario@users.noreply.github.com>
2022-07-03 17:56:59 +0200

    Windows: Set default locale for path conversion (#7593)

--- tests/integration/tables/file.cpp
+++ tests/integration/tables/file.cpp
@@ -12,43 +12,81 @@
 
 #include <fstream>
 
+#include <osquery/filesystem/filesystem.h>
 #include <osquery/tests/integration/tables/helper.h>
 #include <osquery/utils/info/platform_type.h>
-
-#include <boost/filesystem.hpp>
 
 namespace osquery {
 namespace table_tests {
 
+namespace {
+
+const std::vector<std::string> kFileNameList{
+    // In order to test MBCS support, here's a japanese word
+    // that *should* mean "dictionary"
+    "辞書.txt"
+
+    "file-table-test.txt",
+};
+
+}
+
 class FileTests : public testing::Test {
  public:
-  boost::filesystem::path filepath;
+  boost::filesystem::path directory;
 
   virtual void SetUp() {
     setUpEnvironment();
-    auto directory =
+    initializeFilesystemAPILocale();
+
+    directory =
         boost::filesystem::temp_directory_path() /
         boost::filesystem::unique_path("test-integration-file-table.%%%%-%%%%");
+
     ASSERT_TRUE(boost::filesystem::create_directory(directory));
-    filepath = directory / boost::filesystem::path("file-table-test.txt");
-    {
-      auto fout = std::ofstream(filepath.native(), std::ios::out);
-      fout.open(filepath.string(), std::ios::out);
-      fout << "test";
+
+    for (const auto& file_name : kFileNameList) {
+      auto filepath = directory / boost::filesystem::path(file_name);
+
+      {
+        auto fout = std::ofstream(filepath.native(), std::ios::out);
+        fout.open(filepath.string(), std::ios::out);
+        fout << "test";
+      }
     }
   }
 
   virtual void TearDown() {
-    boost::filesystem::remove_all(filepath.parent_path());
+    boost::filesystem::remove_all(directory);
   }
 };
 
+boost::optional<std::size_t> getRowIndexForFileName(
+    const QueryData& data, const std::string& file_name) {
+  auto it = std::find_if(data.begin(),
+                         data.end(),
+
+                         [&](const Row& row) -> bool {
+                           if (row.count("filename") == 0) {
+                             return false;
+                           }
+
+                           return row.at("filename") == file_name;
+                         });
+
+  if (it == data.end()) {
+    return boost::none;
+  }
+
+  return std::distance(data.begin(), it);
+}
+
 TEST_F(FileTests, test_sanity) {
   std::string path_constraint =
-      (filepath.parent_path() / boost::filesystem::path("%.txt")).string();
+      (directory / boost::filesystem::path("%.txt")).string();
   QueryData data = execute_query("select * from file where path like \"" +
                                  path_constraint + "\"");
-  EXPECT_EQ(data.size(), 1ul);
+  EXPECT_EQ(data.size(), kFileNameList.size());
 
   ValidationMap row_map = {{"path", FileOnDisk},
                            {"directory", DirectoryOnDisk},
@@ -80,9 +118,27 @@ TEST_F(FileTests, test_sanity) {
   row_map["bsd_flags"] = NormalType;
 #endif
 
-  ASSERT_EQ(data[0]["path"], filepath.string());
-  ASSERT_EQ(data[0]["directory"], filepath.parent_path().string());
-  ASSERT_EQ(data[0]["filename"], filepath.filename().string());
+  for (const auto& test_file_name : kFileNameList) {
+    auto opt_index = getRowIndexForFileName(data, test_file_name);
+    ASSERT_TRUE(opt_index.has_value());
+
+    auto index = opt_index.value();
+    const auto& row = data.at(index);
+
+    auto expected_path = directory.string();
+
+#if WIN32
+    expected_path += "\\";
+#else
+    expected_path += "/";
+#endif
+
+    expected_path += test_file_name;
+
+    ASSERT_EQ(row.at("path"), expected_path);
+    ASSERT_EQ(row.at("directory"), directory.string());
+    ASSERT_EQ(row.at("filename"), test_file_name);
+  }
 
   validate_rows(data, row_map);
 
