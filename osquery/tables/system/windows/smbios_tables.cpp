d60eb7300e7f7fc19c13f2d02d8fda84ca57f361
Joe Sweeney <joe.sweeney@trailofbits.com>
2022-08-11 08:45:32 -0400

    Add `firmware_type` column to `platform_info` table on Windows. (#7710)
    Detect the firmware type between "unknown", "bios" or "uefi" using the GetFirmwareType API
    on Windows versions > 7, or using the SecureBoot State key in the registry otherwise.

--- osquery/tables/system/windows/smbios_tables.cpp
+++ osquery/tables/system/windows/smbios_tables.cpp
@@ -112,6 +112,51 @@ std::string to_iso8601_date(const FILETIME& ft) {
   return iso_date.str();
 }
 
+std::string getFirmwareType() {
+  enum class FirmwareType : std::uint32_t {
+    Unknown,
+    Bios,
+    Uefi,
+  };
+  using GetFirmwareTypePtr = BOOL (*)(FirmwareType*);
+
+  auto kernel32_module = GetModuleHandleA("kernel32");
+  auto function_ptr = reinterpret_cast<GetFirmwareTypePtr>(
+      GetProcAddress(kernel32_module, "GetFirmwareType"));
+  FirmwareType firmware_type = FirmwareType::Unknown;
+
+  if (function_ptr == nullptr) {
+    // We are on Windows 7: Attempt to determine the firmware type based on
+    // the registry keys
+    HKEY state_reg_key;
+    auto success =
+        RegOpenKeyExA(HKEY_LOCAL_MACHINE,
+                      "SYSTEM\\CurrentControlSet\\Control\\SecureBoot\\State",
+                      0,
+                      KEY_READ,
+                      &state_reg_key);
+
+    if (success != 0 && state_reg_key != INVALID_HANDLE_VALUE) {
+      firmware_type = FirmwareType::Uefi;
+      RegCloseKey(state_reg_key);
+
+    } else {
+      firmware_type = FirmwareType::Bios;
+    }
+
+  } else if (!function_ptr(&firmware_type)) {
+    LOG(ERROR) << "platform_info: Failed to acquire the firmware type";
+  }
+
+  if (firmware_type == FirmwareType::Bios) {
+    return "bios";
+  } else if (firmware_type == FirmwareType::Uefi) {
+    return "uefi";
+  }
+
+  return "unknown";
+}
+
 QueryData genPlatformInfo(QueryContext& context) {
   QueryData results;
 
@@ -142,6 +187,8 @@ QueryData genPlatformInfo(QueryContext& context) {
 
   auto s = to_iso8601_date(release_date);
   r["date"] = s.empty() ? "-1" : s;
+
+  r["firmware_type"] = SQL_TEXT(getFirmwareType());
 
   results.push_back(r);
   return results;
