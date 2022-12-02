61e02f3026fbea77e94969cb142668fdf8448756
bgirardeau-figma <103459843+bgirardeau-figma@users.noreply.github.com>
2022-12-01 19:41:40 -0800

    Update `es_process_events` table to include `codesigning_flags` column (#7726)
    * Add column to es_process_events indicating if binary is ad-hoc signed
    
    * Switch to a general codesigning_flags column and include additional flags

--- osquery/tables/events/darwin/es_process_events.cpp
+++ osquery/tables/events/darwin/es_process_events.cpp
@@ -62,6 +62,7 @@ Status ESProcessEventSubscriber::Callback(
   r["signing_id"] = ec->signing_id;
   r["team_id"] = ec->team_id;
   r["cdhash"] = ec->cdhash;
+  r["codesigning_flags"] = ec->codesigning_flags;
 
   r["cmdline"] = ec->args;
   r["cmdline_count"] = BIGINT(ec->argc);
