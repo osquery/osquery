9b0d202dddd4f07f97f660c4e64a8f2110d5faed
Mike Myers <30631532+mike-myers-tob@users.noreply.github.com>
2022-12-19 12:29:06 -0800

    Skip secureboot test on macOS ARM

--- tests/integration/tables/secureboot.cpp
+++ tests/integration/tables/secureboot.cpp
@@ -32,11 +32,21 @@ TEST_F(Secureboot, test_sanity) {
     const auto& platform_info = platform_info_rows[0];
     ASSERT_EQ(platform_info.count("firmware_type"), 1);
 
-    secureboot_supported = platform_info.at("firmware_type") == "uefi";
+    if (isPlatform(PlatformType::TYPE_OSX)) {
+#ifdef __aarch64__
+      secureboot_supported = false;
+#endif
+    } else {
+      secureboot_supported = platform_info.at("firmware_type") == "uefi";
+    }
+  }
+
+  if (!secureboot_supported) {
+    return;
   }
 
   auto secureboot_data = execute_query("SELECT * FROM secureboot;");
-  
+
   // There should always be exactly 1 row, regardless:
   ASSERT_EQ(secureboot_data.size(), 1);
 
@@ -46,15 +56,13 @@ TEST_F(Secureboot, test_sanity) {
   };
 
   // Windows and Linux have setup_mode, macOS has secure_mode:
-  if (!secureboot_supported) {
-    return;
-  } else if (isPlatform(PlatformType::TYPE_WINDOWS) ||
+  if (isPlatform(PlatformType::TYPE_WINDOWS) ||
       isPlatform(PlatformType::TYPE_LINUX)) {
     row_map.emplace("setup_mode", IntOrEmpty);
   } else if (isPlatform(PlatformType::TYPE_OSX)) {
     row_map.emplace("secure_mode", IntOrEmpty);
   }
-  
+
   // Check that the above assumptions are true:
   validate_rows(secureboot_data, row_map);
 }
