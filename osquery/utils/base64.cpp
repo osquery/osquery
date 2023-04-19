f1cc21fae979ec07794dccdfa6d8f92c250feef8
dreamer.dead <dreamer.dead@gmail.com>
2023-04-19 11:19:18 +0300

    Add more comments

--- osquery/utils/base64.cpp
+++ osquery/utils/base64.cpp
@@ -29,6 +29,7 @@ typedef bai::transform_width<base64_str, 8, 6> base64_dec;
 typedef bai::transform_width<std::string_view::const_iterator, 6, 8> base64_enc;
 typedef bai::base64_from_binary<base64_enc> it_base64;
 
+// Helper function to do all work for decoding a string.
 std::string decode_impl(std::string_view encoded) {
   if (encoded.empty()) {
     return std::string();
@@ -45,6 +46,10 @@ std::string decode_impl(std::string_view encoded) {
 
 } // namespace
 
+// This overload of the `decode` function expects the input
+// as a mutable string to erase few symbols from it.
+// Can be called in forms of:
+// `decode(someStringResultFunc())` or `decode(std::move(stringVar))`
 std::string decode(std::string&& encoded) {
   boost::erase_all(encoded, "\r\n");
   boost::erase_all(encoded, "\n");
@@ -53,11 +58,17 @@ std::string decode(std::string&& encoded) {
   return decode_impl(encoded);
 }
 
+// Main entry point to decode a string as a read-only buffer.
+// Will copy the input string and clear few special symbols only if needed.
 std::string decode(std::string_view encoded) {
+  // Do a linear lookup to decide whether we can avoid copying the input string.
+  // See the `decode(std::string&&)` function overload above for a list of
+  // symbols we are looking.
   if (encoded.find_first_of("\r\n=") == std::string_view::npos) {
     return decode_impl(encoded);
   }
 
+  // If we have to clear the string, then build a copy of it explicitly.
   return decode(std::string(encoded));
 }
 
