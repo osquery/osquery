0d3441a1788a8f4e118413907ca679a224432910
yux-m <yuxmeng@seas.upenn.edu>
2023-05-31 10:14:01 -0400

    Add aggregate stats as the first row

--- osquery/tables/system/windows/cpu_time.cpp
+++ osquery/tables/system/windows/cpu_time.cpp
@@ -35,7 +35,7 @@ QueryData genCpuTime(QueryContext& context) {
   const std::vector<WmiResultItem>& uptimeData = wmiSystemReq_uptime->results();
   uptimeData[0].GetLongLong("SystemUpTime", uptime);
 
-  // Percentage of time each core spent in different parts
+  // Percentage of time each core spent in different parts, in 1/100 seconds
   const Expected<WmiRequest, WmiError> wmiSystemReq =
       WmiRequest::CreateWmiRequest(
           "select Name, PercentUserTime, PercentPrivilegedTime, "
@@ -46,25 +46,51 @@ QueryData genCpuTime(QueryContext& context) {
     return results;
   }
 
+  // The first row aggregates stats of all cores
+  Row agg;
+  agg["core"] = "cpu";
+  long long totalUser = 0;
+  long long totalSystem = 0;
+  long long totalIdle = 0;
+  long long totalIrq = 0;
+  long long totalNice = 0;
+
   const std::vector<WmiResultItem>& wmiResults = wmiSystemReq->results();
   for (const auto& data : wmiResults) {
     data.GetString("Name", r["core"]);
     long percent = 0;
+
     data.GetLongLong("PercentUserTime", percent);
-    // Hundredths of a second, percent / 100 * uptime * 100
+    totalUser += percent;
     r["user"] = BIGINT(percent * uptime);
+
     data.GetLongLong("PercentPrivilegedTime", percent);
+    totalSystem += percent;
     r["system"] = BIGINT(percent * uptime);
+
     data.GetLongLong("PercentIdleTime", percent);
+    long long idle = percent;
+    totalIdle += percent;
     r["idle"] = BIGINT(percent * uptime);
-    long idle = percent;
+
     data.GetLongLong("PercentInterruptTime", percent);
+    totalIrq += percent;
     r["irq"] = BIGINT(percent * uptime);
+
     // Time spent on low priority threads
     data.GetLongLong("PercentPriorityTime", percent);
+    totalNice += percent;
     r["nice"] = BIGINT((100 - percent - idle) * uptime);
+
     results.push_back(r);
   }
+
+  agg["user"] = BIGINT(totalUser * uptime);
+  agg["system"] = BIGINT(totalSystem * uptime);
+  agg["idle"] = BIGINT(totalIdle * uptime);
+  agg["irq"] = BIGINT(totalIrq * uptime);
+  agg["nice"] = BIGINT(totalNice * uptime);
+  results.insert(results.begin(), agg);
 
   return results;
 }
