f6f2cfe3af25a03baa2aa7d97f2a65c65c31ac04
Stefano Bonicatti <stefano.bonicatti@gmail.com>
2023-07-28 20:55:17 +0200

    aws: Add new AWS valid regions

--- osquery/utils/aws/aws_util.cpp
+++ osquery/utils/aws/aws_util.cpp
@@ -126,13 +126,14 @@ const std::string kImdsTokenTtlDefaultValue = "21600";
 
 /// Map of AWS region name to AWS::Region enum.
 static const std::set<std::string> kAwsRegions = {
-    "af-south-1",     "ap-east-1",     "ap-northeast-1", "ap-northeast-2",
-    "ap-northeast-3", "ap-south-1",    "ap-southeast-1", "ap-southeast-2",
-    "ca-central-1",   "cn-north-1",    "cn-northwest-1", "eu-central-1",
-    "eu-north-1",     "eu-south-1",    "eu-west-1",      "eu-west-2",
-    "eu-west-3",      "me-south-1",    "sa-east-1",      "us-east-1",
-    "us-east-2",      "us-gov-east-1", "us-gov-west-1",  "us-west-1",
-    "us-west-2"};
+    "af-south-1",     "ap-south-2",     "ap-east-1",     "ap-northeast-1",
+    "ap-northeast-2", "ap-northeast-3", "ap-south-1",    "ap-southeast-1",
+    "ap-southeast-2", "ap-southeast-3", "ca-central-1",  "cn-north-1",
+    "cn-northwest-1", "eu-central-1",   "eu-central-2",  "eu-north-1",
+    "eu-south-1",     "eu-south-2",     "eu-west-1",     "eu-west-2",
+    "eu-west-3",      "me-central-1",   "me-south-1",    "sa-east-1",
+    "us-east-1",      "us-east-2",      "us-gov-east-1", "us-gov-west-1",
+    "us-west-1",      "us-west-2"};
 
 // Default AWS region to use when no region set in flags or profile
 static RegionName kDefaultAWSRegion = Aws::Region::US_EAST_1;
