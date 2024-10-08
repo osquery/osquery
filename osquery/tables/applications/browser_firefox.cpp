bfef5001267f2c36814fdec1db7cf317f750a50a
Andrea Scarpino <andrea@scarpino.dev>
2024-10-08 05:47:26 +0200

    Add support for Firefox addons for snap installations (#8374)
    Signed-off-by: Andrea Scarpino <andrea@scarpino.dev>
    Co-authored-by: seph <seph@kolide.co>

--- osquery/tables/applications/browser_firefox.cpp
+++ osquery/tables/applications/browser_firefox.cpp
@@ -28,11 +28,14 @@ namespace {
 
 /// Each home directory will include custom extensions.
 #if defined(__APPLE__)
-#define kFirefoxPath "/Library/Application Support/Firefox/Profiles/"
+const std::vector<std::string> kFirefoxPaths = {
+    "/Library/Application Support/Firefox/Profiles/"};
 #elif defined(__linux__)
-#define kFirefoxPath "/.mozilla/firefox/"
+const std::vector<std::string> kFirefoxPaths = {
+    "/.mozilla/firefox/", "/snap/firefox/common/.mozilla/firefox/"};
 #elif defined(WIN32)
-#define kFirefoxPath "\\AppData\\Roaming\\Mozilla\\Firefox\\Profiles"
+const std::vector<std::string> kFirefoxPaths = {
+    "\\AppData\\Roaming\\Mozilla\\Firefox\\Profiles"};
 #endif
 
 #define kFirefoxExtensionsFile "/extensions.json"
@@ -183,11 +186,13 @@ QueryData genFirefoxAddons(QueryContext& context) {
   QueryData users = usersFromContext(context);
   for (const auto& row : users) {
     if (row.count("uid") > 0 && row.count("directory") > 0) {
-      // For each user, enumerate all of their Firefox profiles.
+      // For each user, enumerate all of their Firefox profiles in each path.
       std::vector<std::string> profiles;
-      auto directory = fs::path(row.at("directory")) / kFirefoxPath;
-      if (!listDirectoriesInDirectory(directory, profiles).ok()) {
-        continue;
+      for (const auto& path : kFirefoxPaths) {
+        auto directory = fs::path(row.at("directory")) / path;
+        if (!listDirectoriesInDirectory(directory, profiles).ok()) {
+          continue;
+        }
       }
 
       // Generate an addons list from their extensions JSON.
