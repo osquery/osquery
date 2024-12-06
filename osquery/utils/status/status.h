82ba3f7f0ec618a143b31e2b78e3ff75970446ce
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2024-12-06 20:30:02 +0300

    [utils/status] Optimize default status essage construction speed
    This prevents strlen() call on std::string ctor.

--- osquery/utils/status/status.h
+++ osquery/utils/status/status.h
@@ -34,13 +34,14 @@ namespace osquery {
 class Status {
  public:
   static constexpr int kSuccessCode = 0;
+  static const std::string okMessage;
   /**
    * @brief Default constructor
    *
    * Note that the default constructor initialized an osquery::Status instance
    * to a state such that a successful operation is indicated.
    */
-  explicit Status(int c = Status::kSuccessCode) : code_(c), message_("OK") {}
+  explicit Status(int c = Status::kSuccessCode) : code_(c), message_(okMessage) {}
 
   /**
    * @brief A constructor which can be used to concisely express the status of
