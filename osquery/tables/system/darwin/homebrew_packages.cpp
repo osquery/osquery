2ba840dfa2e89b9d18d3943936cdd9ec291af173
Guillaume Winter <8502556+DocEmmetBrown@users.noreply.github.com>
2025-01-12 16:44:38 +0100

    Add the name of the installed app if it can be found in the metadata file (#8476)

--- osquery/tables/system/darwin/homebrew_packages.cpp
+++ osquery/tables/system/darwin/homebrew_packages.cpp
@@ -11,6 +11,7 @@
 #include <boost/algorithm/string/join.hpp>
 #include <boost/filesystem/operations.hpp>
 #include <boost/filesystem/path.hpp>
+#include <boost/foreach.hpp>
 #include <boost/property_tree/json_parser.hpp>
 #include <boost/property_tree/ptree.hpp>
 
@@ -75,7 +76,7 @@ std::vector<std::string> getHomebrewVersionsFromInfoPlistPath(
 }
 
 // Looking for a "auto_updates true" line in a .rb file
-bool checkAutoUpdatesInFile(const std::string& file_path) {
+bool checkAutoUpdatesInRubyFile(const std::string& file_path) {
   std::ifstream file(file_path);
   if (!file.is_open()) {
     TLOG << "Error opening file " << file_path;
@@ -116,13 +117,71 @@ bool getBooleanValueFromJsonFile(const std::string& file_path,
   return tree.get<bool>(key, false);
 }
 
-bool getHomebrewAutoUpdate(const std::string& path) {
+// Looking for the app name in a .json file. It is nested under the "artifacts"
+// key, which is an array. One of those arrays has an "app" key, which is an
+// array of strings... and the first string is the app name
+std::string getAppNameFromJsonManifest(const std::string& file_path) {
+  std::string content;
+  auto status = osquery::readFile(file_path, content);
+  if (!status.ok()) {
+    TLOG << "Error reading file " << file_path << ": " << status.toString();
+    return "";
+  }
+
+  pt::ptree tree;
+  std::istringstream is(content);
+  try {
+    pt::read_json(is, tree);
+  } catch (const pt::json_parser_error& e) {
+    TLOG << "Error parsing JSON from file " << file_path << ": " << e.what();
+    return "";
+  }
+
+  // Parsing JSON with arrays is a bit tricky, as the ptree is a tree-like
+  // structure, and we need to iterate over the children of the root node
+  // to find the app name
+
+  auto artifacts = tree.get_child_optional("artifacts");
+  if (!artifacts) {
+    return "";
+  }
+
+  BOOST_FOREACH (auto artifact, artifacts.get()) {
+    if (artifact.second.get_optional<std::string>("app")) {
+      BOOST_FOREACH (auto app, artifact.second.get_child("app")) {
+        return app.second.get_value("");
+      }
+    }
+  }
+  return "";
+}
+
+// Looking for a pattern like "app 'App.app'" in the .rb metadata file
+std::string getAppNameFromRubyManifest(const std::string& file_path) {
+  std::ifstream file(file_path);
+  if (!file.is_open()) {
+    TLOG << "Error opening file " << file_path;
+    return "";
+  }
+
+  std::string line;
+  std::regex app_regex(R"(^\s*app\s*\"(.*.app)\"\s*$)");
+  while (std::getline(file, line)) {
+    std::smatch match;
+    if (std::regex_match(line, match, app_regex) && match.size() > 1) {
+      return match[1].str();
+    }
+  }
+  return "";
+}
+
+std::string getMetadataFileForCask(const std::string& path) {
   // a Cask full path looks typically like
   // /opt/homebrew/Caskroom/iterm2/3.5.9/iTerm.app -> /Applications/iTerm.app
   //
   // The path passed to this function would be /opt/homebrew/Caskroom/iterm2
   //
-  // Auto update data generally exists in the .metadata directory
+  // MetaData data generally exists in the .metadata directory
   // Under a json OR an .rb file with the cask name:
   // /opt/homebrew/Caskroom//iterm2/.metadata/3.5.9/20241116155943.669/Casks/iterm2.json
   // /opt/homebrew/Caskroom//vlc/.metadata/3.0.18/20230607170348.510/Casks/vlc.rb
@@ -131,7 +190,6 @@ bool getHomebrewAutoUpdate(const std::string& path) {
   // (cask.rb#L175)
   // https://github.com/Homebrew/brew/blob/41be66fc2a3facc5a575f98ffa2a17323ef30c22/Library/Homebrew/cask/installer.rb#L402-L403
   // (installer.rb#L402-L403)
-
   std::vector<std::string> files;
 
   std::string app_name = fs::path(path).filename().string();
@@ -144,25 +202,51 @@ bool getHomebrewAutoUpdate(const std::string& path) {
   if (!status.ok()) {
     TLOG << "Error listing files in " << metadata_path << ": "
          << status.toString();
-    return false;
+    return "";
   }
-  // If the config file contains "auto_updates true", we can return early
-  // as we know the cask auto updates
+
   for (const auto& file : files) {
     // iterate over all files in the .metadata directory
     // and only consider .json and .rb files with the app name
     std::string filename = fs::path(file).filename().string();
-    if (filename == app_name + ".json") {
-      return getBooleanValueFromJsonFile(file);
-    }
-    if (filename == app_name + ".rb") {
-      return checkAutoUpdatesInFile(file);
+    if (filename == app_name + ".json" || filename == app_name + ".rb") {
+      return file;
     }
   }
-  // If we didn't find the config file in the .metadata directory, we default to
-  // false (should we return an error instead?)
+  // If we didn't find the config file in the .metadata directory, we default
+  // to false (should we return an error instead?)
   TLOG << "No config file found in  " << metadata_path;
+  return "";
+}
+
+bool getHomebrewAutoUpdate(const std::string& path) {
+  std::string filename = getMetadataFileForCask(path);
+  if (filename == "") {
+    TLOG << "No metadata file found under  " << path;
+    return false;
+  }
+  if (boost::algorithm::ends_with(filename, ".json")) {
+    return getBooleanValueFromJsonFile(filename);
+  }
+  if (boost::algorithm::ends_with(filename, ".rb")) {
+    return checkAutoUpdatesInRubyFile(filename);
+  }
   return false;
+}
+
+std::string getInstalledAppNameFromMetadata(const std::string& path) {
+  std::string filename = getMetadataFileForCask(path);
+  if (filename == "") {
+    TLOG << "No metadata file found under  " << path;
+    return "";
+  }
+  if (boost::algorithm::ends_with(filename, ".json")) {
+    return getAppNameFromJsonManifest(filename);
+  }
+  if (boost::algorithm::ends_with(filename, ".rb")) {
+    return getAppNameFromRubyManifest(filename);
+  }
+  return "";
 }
 
 void computeVersionsForFormulas(QueryData& results,
@@ -224,6 +308,7 @@ void computeVersionsForCasks(QueryData& results,
       r["type"] = type;
       r["prefix"] = prefix;
       r["auto_updates"] = getHomebrewAutoUpdate(path) ? "1" : "0";
+      r["app_name"] = getInstalledAppNameFromMetadata(path);
 
       results.push_back(r);
     }
