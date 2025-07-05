a18c8a2ae4f540a073286f53b4b28708d5443914
Supreeth Kurpad <skurpad@akamai.com>
2025-07-06 00:28:57 +0530

    Update copyright header

--- osquery/utils/affixes.h
+++ osquery/utils/affixes.h
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2025-present, Akamai Technologies Inc.
+ * Copyright (c) 2014-present, The osquery authors
  *
  * This source code is licensed as defined by the LICENSE file found in the
  * root directory of this source tree.
