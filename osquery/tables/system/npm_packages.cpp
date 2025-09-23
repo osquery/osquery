19be47f3df0ca52eeebbfc37548fb286fee98a9f
lichao127 <lichao127@github.com>
2025-09-23 13:25:55 -0700

    code style fix

--- osquery/tables/system/npm_packages.cpp
+++ osquery/tables/system/npm_packages.cpp
@@ -111,7 +111,8 @@ void genNodeSiteDirectories(const std::string& site,
   boost::filesystem::path pattern1("node_modules/%/package.json");
   resolveFilePattern(site / pattern1, manifest_paths);
 
-  // Search for nested packages: node_modules/@scope/package/package.json or similar nested structures
+  // Search for nested packages: node_modules/@scope/package/package.json
+  // or similar nested structures
   boost::filesystem::path pattern2("node_modules/%/%/package.json");
   resolveFilePattern(site / pattern2, manifest_paths);
 
