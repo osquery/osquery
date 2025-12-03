3c97fb53d6e5ea5efe440af45e1e55d2da5b5c5e
Zach Wasserman <zach@fleetdm.com>
2025-12-03 09:05:41 -0800

    Fix CI: Remove Login Items plist support from startup_items table (#8723)
    Fixes #8718
    
    Remove support for parsing Login Items from com.apple.loginitems.plist,
    keeping only traditional StartupItems from /System/Library/StartupItems/
    and /Library/StartupItems/ directories.
    
    This was causing CI failures after recent macOS updates (15.7.2),
    seemingly due to changes in plist parsing APIs. The functionality seems
    to be no longer supported, see
    
    https://forums.macrumors.com/threads/lost-login-items.2073306/?post=25193038#post-25193038
    https://eclecticlight.co/2022/06/21/how-helpers-are-changing-in-ventura/

--- osquery/tables/system/darwin/startup_items.cpp
+++ osquery/tables/system/darwin/startup_items.cpp
@@ -13,25 +13,15 @@
 #include <osquery/core/tables.h>
 #include <osquery/filesystem/filesystem.h>
 #include <osquery/logger/logger.h>
-#include <osquery/utils/darwin/plist.h>
 
 namespace fs = boost::filesystem;
-namespace pt = boost::property_tree;
 
 namespace osquery {
 namespace tables {
 
 const std::vector<std::string> kLibraryStartupItemPaths = {
-    "/System/Library/StartupItems/", "/Library/StartupItems/",
-};
-
-// Path (after /Users/foo) where the login items plist will be found
-const std::string kLoginItemsPlistPath =
-    "Library/Preferences/com.apple.loginitems.plist";
-
-// Key to the array within the Login Items plist containing the items
-const std::vector<std::string> kLoginItemsKeyPaths = {
-    "SessionItems.CustomListItems", "privilegedlist.CustomListItems",
+    "/System/Library/StartupItems/",
+    "/Library/StartupItems/",
 };
 
 void genLibraryStartupItems(const std::string& sysdir, QueryData& results) {
@@ -55,54 +45,8 @@ void genLibraryStartupItems(const std::string& sysdir, QueryData& results) {
   }
 }
 
-void genLoginItems(const fs::path& sipath, QueryData& results) {
-  pt::ptree tree;
-  if (!pathExists(sipath.string()).ok() || !isReadable(sipath.string()).ok()) {
-    // User does not have a startup items list, or bad permissions.
-    return;
-  }
-
-  if (!osquery::parsePlist(sipath.string(), tree).ok()) {
-    // Could not parse the user's startup items plist.
-    return;
-  }
-
-  // Enumerate Login Items if we successfully opened the plist.
-  for (const auto& plist_path : kLoginItemsKeyPaths) {
-    try {
-      for (const auto& entry : tree.get_child(plist_path)) {
-        Row r;
-        r["name"] = entry.second.get<std::string>("Name", "");
-        r["type"] = "Login Item";
-        r["source"] = sipath.string();
-
-        auto alias_data = entry.second.get<std::string>("Alias", "");
-        std::string bin_path;
-        auto s = pathFromPlistAliasData(alias_data, bin_path);
-        if (!s.ok()) {
-          VLOG(1) << "No valid path found for " << r["name"] << ": "
-                  << s.getMessage();
-        }
-        r["path"] = std::move(bin_path);
-        results.push_back(r);
-      }
-    } catch (const pt::ptree_error& e) {
-      continue;
-    }
-  }
-}
-
 QueryData genStartupItems(QueryContext& context) {
   QueryData results;
-
-  // Get the login items available for all users
-  genLoginItems("/", results);
-
-  // Get the login items available in System Preferences for each user.
-  for (const auto& dir : getHomeDirectories()) {
-    auto sipath = dir / kLoginItemsPlistPath;
-    genLoginItems(sipath.string(), results);
-  }
 
   // Find system wide startup items in Library directories.
   for (const auto& dir : kLibraryStartupItemPaths) {
@@ -111,5 +55,5 @@ QueryData genStartupItems(QueryContext& context) {
 
   return results;
 }
-}
-}
+} // namespace tables
+} // namespace osquery
