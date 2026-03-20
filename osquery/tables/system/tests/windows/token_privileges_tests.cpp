cd5bdc84820b5cbb3eb7db0c697b7fcd921e2b4e
Brian McKinney <brian.mckinney@elastic.co>
2026-03-20 13:14:18 -0700

    fix token_privileges_tests formatting

--- osquery/tables/system/tests/windows/token_privileges_tests.cpp
+++ osquery/tables/system/tests/windows/token_privileges_tests.cpp
@@ -16,7 +16,8 @@ namespace tables {
 class TokenPrivilegesTest : public testing::Test {};
 
 TEST_F(TokenPrivilegesTest, se_debug_privilege_guard) {
-  // Store the original state of the privilege so we can verify it remains unchanged after the test
+  // Store the original state of the privilege so we can verify it remains
+  // unchanged after the test
   SeDebugPrivState originalState = getDebugTokenPrivilegeState();
 
   // Create a guard and check that the privilege is enabled
@@ -24,19 +25,24 @@ TEST_F(TokenPrivilegesTest, se_debug_privilege_guard) {
   EXPECT_EQ(getDebugTokenPrivilegeState(), SeDebugPrivState::Enabled);
   EXPECT_EQ(guard1->refCount(), 1);
 
-  // Create 10 more guards and check that the privilege remains enabled and the ref count is correct
+  // Create 10 more guards and check that the privilege remains enabled and
+  // the ref count is correct
   std::vector<SeDebugPrivilegeGuard> guardVector(10);
   EXPECT_EQ(getDebugTokenPrivilegeState(), SeDebugPrivState::Enabled);
   EXPECT_EQ(guard1->refCount(), 11); // guard1 + 10 in the vector
 
-  // Destroy the guards one by one and check that the privilege remains enabled until the last guard is destroyed
+  // Destroy the guards one by one and check that the privilege remains enabled
+  // until the last guard is destroyed
   while (!guardVector.empty()) {
     guardVector.pop_back();
     EXPECT_EQ(getDebugTokenPrivilegeState(), SeDebugPrivState::Enabled);
-    EXPECT_EQ(guard1->refCount(), static_cast<int>(guardVector.size()) + 1); // guard1 + remaining guards in the vector
+    EXPECT_EQ(guard1->refCount(),
+              static_cast<int>(guardVector.size()) +
+                  1); // guard1 + remaining guards in the vector
   }
 
-  // After all guards in the vector are destroyed, only guard1 should be active and the privilege should still be enabled
+  // After all guards in the vector are destroyed, only guard1 should be active
+  // and the privilege should still be enabled
   EXPECT_TRUE(guardVector.empty());
   EXPECT_EQ(getDebugTokenPrivilegeState(), SeDebugPrivState::Enabled);
   EXPECT_EQ(guard1->refCount(), 1); // Only guard1 should be active
@@ -46,7 +52,6 @@ TEST_F(TokenPrivilegesTest, se_debug_privilege_guard) {
   // After all guards are destroyed, the privilege should be reset to its
   // original state
   EXPECT_EQ(getDebugTokenPrivilegeState(), originalState);
-
 }
 
 } // namespace tables
