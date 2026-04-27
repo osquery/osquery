e5114f38d387eaac066a63c2882af2f891bb248c
Zach Wasserman <zach@fleetdm.com>
2026-04-27 12:23:05 -0700

    Remove obsolete GPU metrics tests and add new integration test for gpu_metrics

---
+++ tests/integration/tables/gpu_metrics.cpp
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2014-present, The osquery authors
+ *
+ * This source code is licensed as defined by the LICENSE file found in the
+ * root directory of this source tree.
+ *
+ * SPDX-License-Identifier: (Apache-2.0 OR GPL-2.0-only)
+ */
+
+// Sanity check integration test for gpu_metrics
+// Spec file: specs/gpu_metrics.table
+
+#include <osquery/tests/integration/tables/helper.h>
+
+namespace osquery {
+namespace table_tests {
+
+class gpuMetrics : public testing::Test {
+ protected:
+  void SetUp() override {
+    setUpEnvironment();
+  }
+};
+
+TEST_F(gpuMetrics, test_sanity) {
+  auto const data = execute_query("select * from gpu_metrics");
+
+  // GPUs may not be present in all test environments; skip validation if empty.
+  if (data.empty()) {
+    return;
+  }
+
+  ValidationMap row_map = {
+      {"gpu_index", NonNegativeInt},
+      {"pci_bus", NormalType},
+      {"vendor_name", NormalType},
+      {"device_name", NonEmptyString},
+      {"driver_version", NormalType},
+      {"vram_total_bytes", IntOrEmpty},
+  };
+
+  validate_rows(data, row_map);
+}
+
+} // namespace table_tests
+} // namespace osquery
