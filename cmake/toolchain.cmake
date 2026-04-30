4c891c132a3a173592bc3957162e0898b733673f
Zach Wasserman <zach@fleetdm.com>
2026-04-30 15:51:04 -0400

    Set default build options for macOS builds
    - Find macOS SDK automatically
    - Use clang and clang++

--- cmake/toolchain.cmake
+++ cmake/toolchain.cmake
@@ -17,3 +17,37 @@ else()
   overwrite_cache_variable("CMAKE_LINK_SEARCH_START_STATIC" "BOOL" "${OSQUERY_STATIC_BUILD}")
   overwrite_cache_variable("CMAKE_LINK_SEARCH_END_STATIC" "BOOL" "${OSQUERY_STATIC_BUILD}")
 endif()
+
+
+if(APPLE)
+  if(NOT DEFINED CMAKE_OSX_SYSROOT OR "${CMAKE_OSX_SYSROOT}" STREQUAL "")
+    execute_process(
+      COMMAND xcrun --sdk macosx --show-sdk-path
+      OUTPUT_VARIABLE osquery_macosx_sysroot
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+      RESULT_VARIABLE osquery_xcrun_result
+    )
+
+    if(NOT osquery_xcrun_result EQUAL 0 OR "${osquery_macosx_sysroot}" STREQUAL "")
+      message(FATAL_ERROR "Unable to detect the macOS SDK path. Please set CMAKE_OSX_SYSROOT.")
+    endif()
+
+    set(CMAKE_OSX_SYSROOT "${osquery_macosx_sysroot}" CACHE PATH "Path to the macOS SDK" FORCE)
+    unset(osquery_macosx_sysroot)
+    unset(osquery_xcrun_result)
+  endif()
+
+  message(STATUS "Calculated macOS sysroot: ${CMAKE_OSX_SYSROOT}")
+
+  if(NOT DEFINED CMAKE_OSX_DEPLOYMENT_TARGET)
+    set(CMAKE_OSX_DEPLOYMENT_TARGET "10.15" CACHE STRING "Minimum macOS deployment target")
+  endif()
+
+  if(NOT DEFINED CMAKE_C_COMPILER)
+    set(CMAKE_C_COMPILER "clang" CACHE STRING "C compiler")
+  endif()
+
+  if(NOT DEFINED CMAKE_CXX_COMPILER)
+    set(CMAKE_CXX_COMPILER "clang++" CACHE STRING "C++ compiler")
+  endif()
+endif()
