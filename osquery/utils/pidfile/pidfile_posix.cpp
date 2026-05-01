caf63f7d33791094192f861c64bd7e4ab520a564
Zach Wasserman <zach@fleetdm.com>
2026-05-01 12:20:25 -0500

    fixes

--- osquery/utils/pidfile/pidfile_posix.cpp
+++ osquery/utils/pidfile/pidfile_posix.cpp
@@ -140,7 +140,7 @@ boost::optional<Pidfile::Error> Pidfile::writeFile(
 
 Expected<std::string, Pidfile::Error> Pidfile::readFile(
     FileHandle file_handle) noexcept {
-  struct stat file_stats {};
+  struct stat file_stats{};
   if (fstat(file_handle, &file_stats) != 0) {
     return createError(Pidfile::Error::IOError);
   }
@@ -153,9 +153,10 @@ Expected<std::string, Pidfile::Error> Pidfile::readFile(
   auto remaining_bytes = buffer.size();
 
   for (int retry = 0; retry < 5 && remaining_bytes > 0; ++retry) {
-    auto buffer_ptr = buffer.data() + buffer.size() - remaining_bytes;
+    char* buffer_ptr = buffer.data() + buffer.size() - remaining_bytes;
 
-    auto bytes_read = ::read(file_handle, buffer_ptr, remaining_bytes);
+    auto bytes_read = ::read(
+        file_handle, reinterpret_cast<void*>(buffer_ptr), remaining_bytes);
     if (bytes_read == -1) {
       if (errno == EINTR) {
         continue;
