3103d46d979be7300e2a84786bf8d451f26897b1
Zach Wasserman <zach@fleetdm.com>
2026-05-12 10:21:33 -0700

    fix permissions set on windows

--- osquery/filesystem/mock_file_structure.cpp
+++ osquery/filesystem/mock_file_structure.cpp
@@ -20,8 +20,7 @@ namespace fs = boost::filesystem;
 
 fs::path createMockFileStructure() {
   const auto root_dir =
-      fs::temp_directory_path() /
-      fs::unique_path("osquery.tests.%%%%.%%%%");
+      fs::temp_directory_path() / fs::unique_path("osquery.tests.%%%%.%%%%");
   fs::create_directories(root_dir / kTopLevelMockFolderName / "/");
   fs::create_directories(root_dir / kTopLevelMockFolderName / "secondlevel1");
   fs::create_directories(root_dir / kTopLevelMockFolderName / "secondlevel2");
@@ -45,8 +44,7 @@ fs::path createMockFileStructure() {
   writeTextFile(root_dir / "root2.txt", "l1");
 #else
   boost::system::error_code ec;
-  fs::create_symlink(
-      root_dir / "root.txt", root_dir / "root2.txt", ec);
+  fs::create_symlink(root_dir / "root.txt", root_dir / "root2.txt", ec);
 #endif
   return root_dir;
 }
@@ -56,8 +54,7 @@ void deleteMockFileStructure(const fs::path& path) {
   // On Windows, door.txt has restricted permissions (0550),
   // so we need to make it writable before deletion
   boost::system::error_code ec;
-  fs::permissions(
-      path / "door.txt", fs::perms::all, fs::perm_options::replace, ec);
+  fs::permissions(path / "door.txt", fs::perms::all, ec);
 #endif
   fs::remove_all(path);
 }
